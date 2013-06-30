c79524d8da9ebe3fbd86cb0e067f31f7a85c0439
Trent Lukaczyk <aerialhedgehog@gmail.com>
2013-06-30 11:53:48 -0700

    make file fixes

---
+++ configure_example.sh
@@ -0,0 +1,6 @@
+# /usr/bin.bash
+
+./configure -prefix=$SU2_RUN/.. --with-MPI=mpicxx --with-Metis-lib=$SU2_HOME/metis-4.0.3/ --with-Metis-include=$SU2_HOME/metis-4.0.3/Lib/
+
+# ./configure -prefix=$SU2_RUN/.. --with-MPI=mpicxx --with-Metis-lib=$SU2_HOME/metis-4.0.3/ --with-Metis-include=$SU2_HOME/metis-4.0.3/Lib/ --disable-CFD --disable-GPC --disable-MDC --disable-SMC --disable-PBC --disable-MAC
+
