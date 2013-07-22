3c5fb93c6987c2e75a6f24a1282d0c40dbcb0a2c
Francisco Palacios <fpalacios@stanford.edu>
2013-07-22 09:47:56 -0700

    Small change in configure_example.sh

--- configure_example.sh
+++ configure_example.sh
@@ -1,8 +1,7 @@
 # /usr/bin.bash
 
-#./configure -prefix=$SU2_RUN/.. --with-Metis-lib=$SU2_HOME/metis-4.0.3/ --with-Metis-include=$SU2_HOME/metis-4.0.3/Lib/ --with-CGNS-lib=/usr/local/lib --with-CGNS-include=/usr/local/include
-
 ./configure -prefix=$SU2_RUN/.. --with-MPI=mpicxx --with-Metis-lib=$SU2_HOME/metis-4.0.3/ --with-Metis-include=$SU2_HOME/metis-4.0.3/Lib/
 
-# ./configure -prefix=$SU2_RUN/.. --with-MPI=mpicxx --with-Metis-lib=$SU2_HOME/metis-4.0.3/ --with-Metis-include=$SU2_HOME/metis-4.0.3/Lib/ --disable-CFD --disable-GPC --disable-MDC --disable-SMC --disable-PBC --disable-MAC
+#./configure -prefix=$SU2_RUN/.. --with-Metis-lib=$SU2_HOME/metis-4.0.3/ --with-Metis-include=$SU2_HOME/metis-4.0.3/Lib/ --with-CGNS-lib=/usr/local/lib --with-CGNS-include=/usr/local/include
 
+# ./configure -prefix=$SU2_RUN/.. --with-MPI=mpicxx --with-Metis-lib=$SU2_HOME/metis-4.0.3/ --with-Metis-include=$SU2_HOME/metis-4.0.3/Lib/ --disable-CFD --disable-GPC --disable-MDC --disable-SMC --disable-PBC --disable-MAC
