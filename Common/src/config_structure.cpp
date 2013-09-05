4282a469b34c4cddf2feec155b14af9d12adcfcc
Thomas D. Economon <economon@stanford.edu>
2013-09-04 20:47:11 -0700

    Option to control writing of sectional forces.

--- Common/src/config_structure.cpp
+++ Common/src/config_structure.cpp
@@ -691,6 +691,8 @@ void CConfig::SetConfig_Options(unsigned short val_nZone) {
 	AddSpecialOption("WRT_RESIDUALS", Wrt_Residuals, SetBoolOption, false);
   /* DESCRIPTION: Output the rind layers in the solution files */
 	AddSpecialOption("WRT_HALO", Wrt_Halo, SetBoolOption, false);
+  /* DESCRIPTION: Output sectional forces for specified markers. */
+	AddSpecialOption("WRT_SECTIONAL_FORCES", Wrt_Sectional_Forces, SetBoolOption, false);
   
 	/*--- Options related to the equivalent area ---*/
 	/* CONFIG_CATEGORY: Equivalent Area */
