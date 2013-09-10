b29552dc01ccdc1b9a315fe8a68afd2dae3337e4
Francisco Palacios <fpalacios@stanford.edu>
2013-09-10 08:55:12 -0700

    Updated using master

--- Common/include/grid_adaptation_structure.hpp
+++ Common/include/grid_adaptation_structure.hpp
@@ -1,8 +1,28 @@
 /*!
  * \file grid_adaptation_structure.hpp
- * \brief Header file for the adaptation subroutines.
+ * \brief Headers of the main subroutines for doing the numerical grid
+ *        movement (including volumetric movement, surface movement and Free From
+ *        technique definition). The subroutines and functions are in
+ *        the <i>grid_movement_structure.cpp</i> file.
  * \author Aerospace Design Laboratory (Stanford University) <http://su2.stanford.edu>.
- * \version 2.0.6
+ * \version 2.0.7
+ *
+ * Stanford University Unstructured (SU2),
+ * copyright (C) 2012-2013 Aerospace Design Laboratory (ADL), is
+ * distributed under the GNU Lesser General Public License (GNU LGPL).
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
@@ -17,6 +37,12 @@
 
 using namespace std;
 
+/*!
+ * \class CGridAdaptation
+ * \brief Parent class for defining the grid adaptation.
+ * \author F. Palacios.
+ * \version 2.0.7
+ */
 class CGridAdaptation {
 protected:
 	unsigned long nPoint_new,	/*!< \brief Number of new points. */
