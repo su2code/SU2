d3aec05b86827f24154cb73cdda68dee50a2376e
Francisco Palacios <fpalacios@stanford.edu>
2013-10-02 14:57:17 -0700

    Remove derived data

--- SU2_IDE/Xcode/DerivedData/SU2_MDC/Build/Intermediates/SU2_MDC.build/Debug/SU2_MDC.build/Objects-normal/x86_64/config_structure.d
+++
@@ -1,4 +0,0 @@
-dependencies: /Users/fpalacios/SU2/trunk/Common/src/config_structure.cpp \
-  /Users/fpalacios/SU2/trunk/Common/src/../include/config_structure.hpp \
-  /Users/fpalacios/SU2/trunk/Common/src/../include/./option_structure.hpp \
-  /Users/fpalacios/SU2/trunk/Common/src/../include/config_structure.inl
