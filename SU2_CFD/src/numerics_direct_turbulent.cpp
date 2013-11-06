91405dc23eb6ab60c744c9e36619144791586d2e
btracey <bdtinternet@gmail.com>
2013-11-06 12:26:43 -0800

    Cleaned up new code

--- SU2_CFD/src/numerics_direct_turbulent.cpp
+++ SU2_CFD/src/numerics_direct_turbulent.cpp
@@ -287,13 +287,6 @@ void CSourcePieceWise_TurbSA::ComputeResidual(double *val_residual, double **val
   if (nDim == 3) Vorticity += ( (PrimVar_Grad_i[3][1]-PrimVar_Grad_i[2][2])*(PrimVar_Grad_i[3][1]-PrimVar_Grad_i[2][2]) + (PrimVar_Grad_i[1][2]-PrimVar_Grad_i[3][0])*(PrimVar_Grad_i[1][2]-PrimVar_Grad_i[3][0]) );
   Omega = sqrt(Vorticity);
   
-  if (dist_i < 1.0e-10){
-    cout << "Distance is lower than e-10: dist = " << dist_i <<endl;
-  }
-   
- // double tmpDist = dist_i;
-//   dist_i = max(dist_i, 1.0e-10);
-  
   /*--- Rotational correction term ---*/
   if (rotating_frame) {
     div = PrimVar_Grad_i[1][0] + PrimVar_Grad_i[2][1];
@@ -369,11 +362,6 @@ void CSourcePieceWise_TurbSA::ComputeResidual(double *val_residual, double **val
     dg = dr*(1.+cw2*(6.*pow(r,5.)-1.));
     dfw = dg*glim*(1.-g_6/(g_6+cw3_6));
     val_Jacobian_i[0][0] -= cw1*(dfw*TurbVar_i[0] +	2.*fw)*TurbVar_i[0]/dist_i_2*Volume;
-    /*
-    if (tmpDist == 0){
-      cout << "Source is: " << val_residual[0] <<endl;
-    }
-     */
   }
 }
 
