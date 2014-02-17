1b2cca5dd10ce6860931d7eda995f8f27db19050
btracey <bdtinternet@gmail.com>
2014-02-17 13:42:03 -0800

    neural net now compiles

--- SU2_CFD/include/nnet.hpp
+++ SU2_CFD/include/nnet.hpp
@@ -9,89 +9,110 @@
 
 using namespace std;
 
-class Scaler{
+class CScaler{
 public:
-	Scaler();
-	~Scaler();
+	CScaler();
+  virtual ~CScaler();
 	virtual void Scale(double *) = 0;
 	virtual void Unscale(double *) = 0;
 	//virtual void LoadJSON(int) = 0;
 };
 
-class NormalScaler: public Scaler{
+class CNormalScaler: public CScaler{
 private:
 	double * mu;
 	double * sigma;
 	int nInputs;
 
 public:
-	NormalScaler();
-	NormalScaler(int,double*,double*);
-	~NormalScaler();
-	NormalScaler(double *, double *);
+	CNormalScaler();
+	CNormalScaler(int,double*,double*);
+	~CNormalScaler();
 	void Scale(double *);
 	void Unscale(double *);
 };
 
-class Activator{
+class CActivator{
 public:
-	Activator();
-	~Activator();
-	virtual double Activate(double sum){cout<< "IN BASE ACTIVATOR THIS IS BAD" <<endl; return 0;};
+	CActivator();
+	~CActivator();
+	virtual double Activate(double combination){cout<< "IN BASE ACTIVATOR THIS IS BAD" <<endl; return 0;};
 };
 
-class TanhActivator : public Activator{
+class CTanhActivator : public CActivator{
 public:
-	TanhActivator();
-	~TanhActivator();
-	double Activate(double sum);
+	CTanhActivator();
+	~CTanhActivator();
+	double Activate(double combination);
 };
 
-class LinearActivator : public Activator{
+class CLinearActivator : public CActivator{
 public:
-	LinearActivator();
-	~LinearActivator();
-	double Activate(double sum);
+	CLinearActivator();
+	~CLinearActivator();
+	double Activate(double combination);
 };
 
-class Neuron{
+class CNeuron{
+public:
+  CNeuron();
+  ~CNeuron();
+  virtual double Activate(double combination){cout << "In base neuron. Bad";return 0;};
+  virtual double Combine(double * parameters, int nParameters, double *inputs, int nInputs){cout << "In base neuron. Bad";return 0;};
+};
+
+class CSumNeuron : public CNeuron{
 private:
-	
-	int parameterStart;
-	int nParameters;
+		CActivator *activator;
 public:
-	Activator *activator;
-	Neuron();
-	Neuron(Activator*, int, int); // activator, parameterStart, nParameters
-	~Neuron();
+	CSumNeuron();
+	CSumNeuron(CActivator*); // activator, parameterStart, nParameters
+	~CSumNeuron();
 	//Activator* GetActivator(void);
-	double Combine(double * input, double * parameters);
-	int ParameterStart();
-	int ParameterEnd();
+	double Combine(double * parameters, int nParameters, double * inputs, int nInputs);
+  double Activate(double combination);
 };
 
-class CNeurNet {
-private:
-	int nNodes;
-	int nInputs;
-	int nOutputs;
-	int nParameters;
-	int nLayers;
-	int *nNeuronsPerLayer;
-	double *parameters;
-	int **parameterIdx;
-	int **nParametersPerNeuron;
-	Neuron ***neurons; // Array of arrays to pointers to neuron
-	Scaler* LoadScaler(ifstream&,int);
-	int LoadInteger(ifstream&, string);
+
+class CPredictor{
 public:
-	Scaler *inputScaler;
-	Scaler *outputScaler;
+  CPredictor();
+  ~CPredictor();
+  virtual void Predict(double *, double *){cout << "In base Predict, this is bad";};
+};
+
+class CScalePredictor{
+public:
+  CScalePredictor();
+  CScalePredictor(string filename);
+  ~CScalePredictor();
+  CPredictor *Pred;
+  CScaler *InputScaler;
+  CScaler *OutputScaler;
+  
+  // Need to add predict method
+};
+
+class CNeurNet : public CPredictor{
+private:
+  int maxNeurons; // Number of neurons in the largest layer
+  int nLayers;
+  CNeuron ***neurons; // Array of arrays to pointers to neuron
+  double*** parameters; // Array of parameters for each neuron
+  int* nNeuronsInLayer; //one list for each layer
+  int** nParameters; // Number of parameters for the neuron
+  int inputDim;
+  
+  void processLayer(double *, int,CNeuron **, double **, int, int * ,double *);
+  
+  //----
+	int outputDim;
+	int totalNumParameters;
+public:
 	CNeurNet();
-	CNeurNet(string, string);
+    //CNeurNet(string filename); // Should have custom constructor with nInputs, etc.
 	~CNeurNet();
-	int NumInputs();
-	int NumOutputs();
+//	int InputDim();
+//	int OutputDim();
 	void Predict(double *, double *);
-	bool CheckPredictions(ifstream&);
 };
