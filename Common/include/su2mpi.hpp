1d5ca1f9e74d6124f3d9c2ceeb006fb65bb8fb6a
btracey <bdtinternet@gmail.com>
2014-04-10 23:43:12 -0700

    Added su2mpi files

---
+++ Common/include/su2mpi.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+#ifndef NO_MPI
+#include <mpi.h>
+#endif
+
+namespace SU2MPI{
+  void FinalizeAndExit1();
+  void PrintAndFinalize(std::string);
+  int Rank();
+}
