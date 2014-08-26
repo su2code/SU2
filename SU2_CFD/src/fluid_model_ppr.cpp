969df2ff9621d4305539ec943b9d2946cd92d2c7
salvovitale <s.vitale@tudelft.nl>
2014-08-26 15:05:35 +0200

    TDlibrary fixed

--- SU2_CFD/src/fluid_model_ppr.cpp
+++ SU2_CFD/src/fluid_model_ppr.cpp
@@ -41,8 +41,6 @@ CPengRobinson::CPengRobinson(double gamma, double R, double Pstar, double Tstar,
         else
         k = 0.379642 + 1.48503 * w - 0.164423 * w*w + 0.016666 * w*w*w;
 
-//	a= 0.0;
-//	b =0.0;
 
 }
 
@@ -74,6 +72,8 @@ void CPengRobinson::SetTDState_rhoe (double rho, double e ) {
 
     Pressure = rho*Temperature*Gas_Constant / (1 - rho*b) - a*alpha2(Temperature) / ( 1/rho/rho + 2*b/rho - b*b );
 
+    Entropy = A*log(Temperature) + Gas_Constant*log(1/Density -b) - a*sqrt(alpha2(Temperature)) *k*fv/(b*sqrt2*sqrt(Temperature*TstarCrit));
+
     A = (1/rho/rho + 2*b/rho - b*b);
     B = - 0.5*k/sqrt(Temperature*TstarCrit); //(D alpha / DT)
 
@@ -92,10 +92,12 @@ void CPengRobinson::SetTDState_rhoe (double rho, double e ) {
     SoundSpeed2 = dPdrho_e + Pressure/(Density*Density)*dPde_rho;
 
     dTde_rho = 1/Cv;
-    //DTDd_e = Gamma_Minus_One/Gas_Constant*a;
 
-    //DvDT_p = - DpDT_v / DpDv_T;
 
+//DTDd_e = Gamma_Minus_One/Gas_Constant*a;
+//
+//DvDT_p = - DpDT_v / DpDv_T;
+//
 //    Cv = Gas_Constant/Gamma_Minus_One - a_c/Covolume/sqrt2 * ( 2* sqrt(alpha2) * B +k*B*sqrt(T) + 0.5*k*\sqrt(T)*Crit_Temperature ) * fv; /// R
 //
 //    Cp = Cv -T*Crit_Temperature * pow(DpDT_v, 2) / DpDv_T;
@@ -105,43 +107,27 @@ void CPengRobinson::SetTDState_rhoe (double rho, double e ) {
 }
 
 void CPengRobinson::SetTDState_PT (double P, double T ) {
-//
-//    /// T E P MUST BE Tr e Pr
-//
-//	double a1;
-//	double a2;
-//	double a3;
-//	double A, B;
-//	double alpha;
-//	double* root  = new double [3];
-//	double rho;
-//	double ad;
-//	double e;
-//
-//	alpha = pow(  ( 1 + k*(1 - sqrt(T)) ) , 2)
-//    Mol_Attraction = a_c * alpha;
-//
-//	A = Mol_Attraction*P/pow(T * Gas_Constant, 2);
-//	B = Covolume*P/(T* Gas_Constant);
-//
-//	a1 = - ( 1 - B );
-//	a2 =   ( A - 3*B -2*B );
-//	a3 = - ( A*B -B*B - B*B*B );
-//
-//	poly_solve_cubic(a1, a2, a3, root);
-//
-//    // check the root of the polynomial
-//
-//	rho = P / ( root[2]*T*Gas_Constant ) * Crit_Density;
-//
-//    ad = a_c/Covolume/sqrt(2) * k*k * atanh( rho * Covolume * sqrt(2)/(1 + rho*Covolume) ) ;
-//    ad *= (1 + 2*k + k*k) + (3*k - 1) * k *sqrt(T);
-//    e = T*Crit_Temperature*Gas_Constant / Gamma_Minus_One - ad;
-//
-//	SetTDState_de(rho, e);
-//
-//	delete [] root;
-//
+	double toll= 1e-4;
+	double A, B, Z, DZ, F, F1;
+	double rho, fv, e;
+	double sqrt2=sqrt(2);
+	A= a*alpha2(T)*P/(T*Gas_Constant)/(T*Gas_Constant);
+	B= b*P/(T*Gas_Constant);
+
+    Z= max(B, 1.01);
+	DZ= 1.0;
+	do{
+		F = Z*Z*Z + Z*Z*(B - 1.0) + Z*(A - 2*B - 3*B*B)  + (B*B*B + B*B - A*B);
+		F1 = 3*Z*Z + 2*Z*(B - 1.0) + (A - 2*B - 3*B*B);
+		DZ = F/F1;
+		Z-= DZ;
+	}while(DZ>toll);
+
+	rho= P/(Z*Gas_Constant*T);
+	fv = atanh( rho * b * sqrt2/(1 + rho*b));
+
+    e = T*Gas_Constant/Gamma_Minus_One + a*k*(k+1)*fv/(b*sqrt2*sqrt(TstarCrit))*sqrt(T) - a*(k+1)*(k+1)*fv/(b*sqrt2);
+	SetTDState_rhoe(rho, e);
 }
 
 void CPengRobinson::SetTDState_Prho (double P, double rho ) {
@@ -154,18 +140,39 @@ void CPengRobinson::SetTDState_Prho (double P, double rho ) {
 
 void CPengRobinson::SetTDState_hs (double h, double s ){
 
-/// WARNING TODO
+	double fv, A, B, C, sqrt2=sqrt(2);
+	double f, f1, v;
+	double dv = 1.0;
+	double toll =1e-5;
+
+	Temperature = h*Gamma_Minus_One/Gas_Constant/Gamma;
+	v = exp(-1/Gamma_Minus_One*log(Temperature) + s/Gas_Constant);
+	Pressure = Temperature*Gas_Constant / (v - b) - a*alpha2(Temperature) / ( v*v + 2*b*v - b*b);
+	Density =1/v;
+
+	do{
+		fv = atanh( Density * b * sqrt2/(1 + Density*b));
+		A = Gas_Constant / Gamma_Minus_One;
+		B = a*k*(k+1)*fv/(b*sqrt2*sqrt(TstarCrit));
+		C = - a*(k+1)*(k+1)*fv/(b*sqrt2) - h + Pressure/Density;
+		Temperature = ( -B + sqrt(B*B - 4*A*C) ) / (2*A); /// Only positive root considered
+		Temperature *= Temperature;
+
+		f = A*log(Temperature) + Gas_Constant*log(1/Density -b) - a*sqrt(alpha2(Temperature)) *k*fv/(b*sqrt2*sqrt(Temperature*TstarCrit)) - s;
+		f1= Gas_Constant/(v-b)+ a*sqrt(alpha2(Temperature)) *k/(sqrt(Temperature*TstarCrit)*(v*v - b*b - 2*v*b));
+		dv= f/f1;
+		v-= dv;
+		Density = 1/v;
+		Pressure = Density*Temperature*Gas_Constant / (1 - Density*b) - a*alpha2(Temperature) / ( 1/Density/Density + 2*b/Density - b*b );
+
+	}while(abs(dv) > toll);
+
+	double e = Temperature*Gas_Constant/Gamma_Minus_One + a*k*(k+1)*fv/(b*sqrt2*sqrt(TstarCrit))*sqrt(Temperature) - a*(k+1)*(k+1)*fv/(b*sqrt2);
+	SetTDState_rhoe(Density, e);
+
 
 }
 
-//void CPengRobinson::SetTDState_Ps (double P, double s ) {
-//	/// Working
-//
-//    double rho = 0.0;
-//
-//	SetTDState_Pd(P, rho);
-//
-//}
 
 void CPengRobinson::SetEnergy_Prho (double P, double rho ) {
 
@@ -180,7 +187,7 @@ void CPengRobinson::SetEnergy_Prho (double P, double rho ) {
 
     C = - P - a*(1+k)*(1+k)/vb2;
 
-    T = ( -B + sqrt(B*B - 4*A*C) ) / (2*A); /// Only positive root considered
+    T = ( -B + sqrt(B*B - 4*A*C) ) / (2*A);
     T *= T;
 
 
@@ -189,99 +196,6 @@ void CPengRobinson::SetEnergy_Prho (double P, double rho ) {
 
 }
 
-
-
-
-
-//// function for solving a cubic equation
-//
-//#define SWAP(a,b) do { double tmp = b ; b = a ; a = tmp ; } while(0)
-//
-//double poly_solve_cubic (double a, double b, double c,
-//                      double *root) {
-//
-//  // the equation is in the form: x^3 + a x^2 + b x + c = 0
-//
-//  double q = (a * a - 3 * b);
-//  double r = (2 * a * a * a - 9 * a * b + 27 * c);
-//
-//  double Q = q / 9;
-//  double R = r / 54;
-//
-//  double Q3 = Q * Q * Q;
-//  double R2 = R * R;
-//
-//  double CR2 = 729 * r * r;
-//  double CQ3 = 2916 * q * q * q;
-//
-//  if (R == 0 && Q == 0)
-//    {
-//      root[0] = - a / 3 ;
-//      root[1] = - a / 3 ;
-//      root[2] = - a / 3 ;
-//      return 3 ;
-//    }
-//  else if (CR2 == CQ3)
-//    {
-//      /* this test is actually R2 == Q3, written in a form suitable
-//         for exact computation with integers */
-//
-//      /* Due to finite precision some double roots may be missed, and
-//         considered to be a pair of complex roots z = x +/- epsilon i
-//         close to the real axis. */
-//
-//      double sqrtQ = sqrt (Q);
-//
-//      if (R > 0)
-//        {
-//    	  root[0] = -2 * sqrtQ  - a / 3;
-//    	  root[1] = sqrtQ - a / 3;
-//    	  root[2] = sqrtQ - a / 3;
-//        }
-//      else
-//        {
-//    	  root[0] = - sqrtQ  - a / 3;
-//    	  root[1] = - sqrtQ - a / 3;
-//    	  root[2] = 2 * sqrtQ - a / 3;
-//        }
-//      return 3 ;
-//    }
-//  else if (R2 < Q3)
-//    {
-//      double sgnR = (R >= 0 ? 1 : -1);
-//      double ratio = sgnR * sqrt (R2 / Q3);
-//      double theta = acos (ratio);
-//      double norm = -2 * sqrt (Q);
-//      root[0] = norm * cos (theta / 3) - a / 3;
-//      root[1] = norm * cos ((theta + 2.0 * M_PI) / 3) - a / 3;
-//      root[2] = norm * cos ((theta - 2.0 * M_PI) / 3) - a / 3;
-//
-//      /* Sort root[0], root[1], root[2] into increasing order */
-//
-//      if (root[0] > root[1])
-//        SWAP(root[0], root[1]) ;
-//
-//      if (root[1] > root[2])
-//        {
-//          SWAP(root[1], root[2]) ;
-//
-//          if (root[0] > root[1])
-//            SWAP(root[0], root[1]) ;
-//        }
-//
-//      return 3;
-//    }
-//  else
-//    {
-//      double sgnR = (R >= 0 ? 1 : -1);
-//      double A = -sgnR * pow (fabs (R) + sqrt (R2 - Q3), 1.0/3.0);
-//      double B = Q / A ;
-//      root[0] = A + B - a / 3;
-//      return 1;
-//    }
-//}
-//
-//
 
 
 
--- SU2_CFD/src/fluid_model_pvdw.cpp
+++ SU2_CFD/src/fluid_model_pvdw.cpp
@@ -31,8 +31,7 @@ CVanDerWaalsGas::CVanDerWaalsGas() : CIdealGas() {
 CVanDerWaalsGas::CVanDerWaalsGas(double gamma, double R, double Pstar, double Tstar): CIdealGas(gamma, R) {
     a = 27.0/64.0*Gas_Constant*Gas_Constant*Tstar*Tstar/Pstar;
 	b = 1.0/8.0*Gas_Constant*Tstar/Pstar;
-//	a = 0.0;
-//    b = 0.0;
+
 
 }
 
@@ -46,8 +45,8 @@ void CVanDerWaalsGas::SetTDState_rhoe (double rho, double e ) {
 
 	Pressure = Gamma_Minus_One*Density/(1.0-Density*b)*(StaticEnergy + Density*a) - a*Density*Density;
     Temperature = (Pressure+Density*Density*a)*((1-Density*b)/(Density*Gas_Constant));
-//	Temperature = Gamma_Minus_One/Gas_Constant*(StaticEnergy + Density*a);
     Entropy = Gas_Constant *( log(Temperature)/Gamma_Minus_One + log(1/Density - b));
+
 
     dPde_rho = Density*Gamma_Minus_One/(1.0 - Density*b);
     dPdrho_e = Gamma_Minus_One/(1.0 - Density*b)*((StaticEnergy + 2*Density*a) + Density*b*(StaticEnergy + Density*a)/(1.0 - Density*b)) - 2*Density*a;
@@ -58,70 +57,24 @@ void CVanDerWaalsGas::SetTDState_rhoe (double rho, double e ) {
 
 }
 
+
 void CVanDerWaalsGas::SetTDState_PT (double P, double T ) {
-	double v1;
-    double v2;
-    double vtmp;
-    double f1, f2, ftmp;
-    double FACTOR = 1.6;
-    int NTRY = 50, j;
+	double toll= 1e-4;
+	double A, B, Z, DZ, F, F1;
+	A= a*P/(T*Gas_Constant)/(T*Gas_Constant);
+	B= b*P/(T*Gas_Constant);
 
-	double toll = 1e-4;
-
-	v1 = T*Gas_Constant/P;
-	v2 = v1 *(1 + toll);
-
-
-    f1 = P*pow(v1,3) - (P*b + Gas_Constant*T) * pow(v1, 2) +a * v1 -a*b;
-
-    f2 = P*pow(v2,3) - (P*b + Gas_Constant*T) * pow(v2, 2) +a * v2 -a*b;
-
-    for (j=0;j<=NTRY;j++)
-    {
-        if (f1*f2 < 0.0) break;
-
-        if (fabs(f1) < fabs(f2))
-        {
-            v1 += FACTOR*(v1-v2);
-            f1 = P*pow(v1,3) - (P*b + Gas_Constant*T) * pow(v1, 2) +a * v1 -a*b;
-        }
-        else
-        {
-            v2 += FACTOR*(v2-v1);
-            f2 = P*pow(v2,3) - (P*b + Gas_Constant*T) * pow(v2, 2) +a * v2 -a*b;
-        }
-    }
-
-
-    if (f1*f2 >= 0.0)
-    {
-        cout << "Warning! Bracketing in Van Der Waals Fluid model failed\n";
-    }
-    else
-    {
-        /// Bisection method
-        while (fabs((v1-v2)/v1) > toll )
-        {
-            vtmp = (v1+v2) / 2;
-            ftmp = P*pow(vtmp,3) - (P*b + Gas_Constant*T) * pow(vtmp, 2) +a * vtmp -a*b;
-
-            if (ftmp*f1 > 0)
-            {
-                v1 = vtmp;
-                f1 = ftmp;
-            }
-            else
-            {
-                v2 = vtmp;
-                f2 = ftmp;
-            }
-        }
-    }
-
-    Density = 1/(0.5*(v1 + v2));
+    Z= max(B, 1.01);
+	DZ= 1.0;
+	do{
+		F = Z*Z*Z - Z*Z*(B+1.0) + Z*A - A*B;
+		F1 = 3*Z*Z - 2*Z*(B+1.0) + A;
+		DZ = F/F1;
+		Z-= DZ;
+	}while(DZ>toll);
+	Density = P/(Z*Gas_Constant*T);
 
     double e = T*Gas_Constant/Gamma_Minus_One - a*Density;
-
 	SetTDState_rhoe(Density, e);
 
 }
@@ -136,91 +89,26 @@ void CVanDerWaalsGas::SetTDState_Prho (double P, double rho ) {
 
 void CVanDerWaalsGas::SetTDState_hs (double h, double s ){
 
-    double v1;
-    double v2;
-    double vtmp;
-    double f1, f2, ftmp;
-    double FACTOR = 1.6;
-    int NTRY = 50, j;
+    double v, T, dv, f, f1;
+    double toll = 1e-4;
 
-    double T = h*Gamma_Minus_One/Gamma/Gas_Constant;
-	double P = exp(Gamma/Gamma_Minus_One*log(T) - s/Gas_Constant);
+    T = h*Gamma_Minus_One/Gas_Constant/Gamma;
+    v = exp(-1/Gamma_Minus_One*log(T) + s/Gas_Constant);
+	do{
+		f=  log(v-b) - s/Gas_Constant + log(T)/Gamma_Minus_One;
+		f1= 1/(v-b);
+		dv= f/f1;
+		v-= dv;
+		T= (h+ 2*a/v)/Gas_Constant/(1/Gamma_Minus_One+ v/(v-b));
+	}while(abs(dv) > toll);
 
-	double toll = 1e-4;
-
-	v1 = T*Gas_Constant/P;
-	v2 = v1 *(1 + toll);
-
-
-    f1 = pow( h + 2*a/v1, Gas_Constant/Gamma_Minus_One) * pow(v1-b, Gas_Constant) - exp(s)*pow(Gas_Constant/Gamma_Minus_One + Gas_Constant*v1/(v1-b), Gas_Constant/Gamma_Minus_One);
-
-    f2 = pow( h + 2*a/v2, Gas_Constant/Gamma_Minus_One) * pow(v2-b, Gas_Constant) - exp(s)*pow(Gas_Constant/Gamma_Minus_One + Gas_Constant*v2/(v2-b), Gas_Constant/Gamma_Minus_One);
-
-    for (j=1;j<=NTRY;j++)
-    {
-        if (f1*f2 < 0.0) break;
-
-        if (fabs(f1) < fabs(f2))
-        {
-            v1 += FACTOR*(v1-v2);
-            f1 = pow( h + 2*a/v1, Gas_Constant/Gamma_Minus_One) * pow(v1-b, Gas_Constant) - exp(s)*pow(Gas_Constant/Gamma_Minus_One + Gas_Constant*v1/(v1-b), Gas_Constant/Gamma_Minus_One);
-        }
-        else
-        {
-            v2 += FACTOR*(v2-v1);
-            f2 = pow( h + 2*a/v2, Gas_Constant/Gamma_Minus_One) * pow(v2-b, Gas_Constant) - exp(s)*pow(Gas_Constant/Gamma_Minus_One + Gas_Constant*v2/(v2-b), Gas_Constant/Gamma_Minus_One);
-        }
-    }
-
-
-    if (f1*f2 >= 0.0)
-    {
-        cout << "Warning! Bracketing in Van Der Waals Fluid model failed\n";
-    }
-    else
-    {
-        /// Bisection method
-        while (fabs((v1-v2)/v1) > toll )
-        {
-            vtmp = (v1+v2) / 2;
-            ftmp = pow( h + 2*a/vtmp, Gas_Constant/Gamma_Minus_One) * pow(vtmp-b, Gas_Constant) - exp(s)*pow(Gas_Constant/Gamma_Minus_One + Gas_Constant*vtmp/(vtmp-b), Gas_Constant/Gamma_Minus_One);
-
-            if (ftmp*f1 > 0)
-            {
-                v1 = vtmp;
-                f1 = ftmp;
-            }
-            else
-            {
-                v2 = vtmp;
-                f2 = ftmp;
-            }
-        }
-    }
-
-    Density = 1/(0.5*(v1 + v2));
-
-    //cout << "density"
-
-    Temperature = (h + a*Density) / (Gas_Constant/Gamma_Minus_One + Gas_Constant/(1-Density*b));
-
+	Density = 1/v;
+	Temperature = T;
     Pressure = Gas_Constant*Temperature*Density / (1 - Density*b) - a*Density*Density;
 
     SetTDState_Prho(Pressure, Density);
 
 }
-
-
-//void CVanDerWaalsGas::SetTDState_Ps (double P, double s ) {
-//	// an implicit equation must be solved for rho
-//	// The equation is in the form:
-//	// (P+a*rho^2)*(od-b)^Gamma = s, where od = 1/rho
-//
-//    double rho = 0.0;
-//
-//	SetTDState_Pd(P, rho);
-//
-//}
 
 void CVanDerWaalsGas::SetEnergy_Prho (double P, double rho ) {
 	double T = (P+rho*rho*a)*(1-rho*b)/(rho*Gas_Constant);
@@ -228,101 +116,7 @@ void CVanDerWaalsGas::SetEnergy_Prho (double P, double rho ) {
 
 }
 
-//void CVanDerWaalsGas::SetEntropy_Prho (double P, double rho ) {
-//	double od = 1/rho;
-//	double odmb = od -b;
-//	Entropy = ( P +a/pow(od,2) ) *pow(odmb,Gamma);
-//
-//}
 
-
-// function for solving a cubic equation
-
-//#define SWAP(a,b) do { double tmp = b ; b = a ; a = tmp ; } while(0)
-//
-//double poly_solve_cubic (double a, double b, double c,
-//                      double *root) {
-//
-//  // the equation is in the form: x^3 + a x^2 + b x + c = 0
-//
-//  double q = (a * a - 3 * b);
-//  double r = (2 * a * a * a - 9 * a * b + 27 * c);
-//
-//  double Q = q / 9;
-//  double R = r / 54;
-//
-//  double Q3 = Q * Q * Q;
-//  double R2 = R * R;
-//
-//  double CR2 = 729 * r * r;
-//  double CQ3 = 2916 * q * q * q;
-//
-//  if (R == 0 && Q == 0)
-//    {
-//      root[0] = - a / 3 ;
-//      root[1] = - a / 3 ;
-//      root[2] = - a / 3 ;
-//      return 3 ;
-//    }
-//  else if (CR2 == CQ3)
-//    {
-//      /* this test is actually R2 == Q3, written in a form suitable
-//         for exact computation with integers */
-//
-//      /* Due to finite precision some double roots may be missed, and
-//         considered to be a pair of complex roots z = x +/- epsilon i
-//         close to the real axis. */
-//
-//      double sqrtQ = sqrt (Q);
-//
-//      if (R > 0)
-//        {
-//    	  root[0] = -2 * sqrtQ  - a / 3;
-//    	  root[1] = sqrtQ - a / 3;
-//    	  root[2] = sqrtQ - a / 3;
-//        }
-//      else
-//        {
-//    	  root[0] = - sqrtQ  - a / 3;
-//    	  root[1] = - sqrtQ - a / 3;
-//    	  root[2] = 2 * sqrtQ - a / 3;
-//        }
-//      return 3 ;
-//    }
-//  else if (R2 < Q3)
-//    {
-//      double sgnR = (R >= 0 ? 1 : -1);
-//      double ratio = sgnR * sqrt (R2 / Q3);
-//      double theta = acos (ratio);
-//      double norm = -2 * sqrt (Q);
-//      root[0] = norm * cos (theta / 3) - a / 3;
-//      root[1] = norm * cos ((theta + 2.0 * M_PI) / 3) - a / 3;
-//      root[2] = norm * cos ((theta - 2.0 * M_PI) / 3) - a / 3;
-//
-//      /* Sort root[0], root[1], root[2] into increasing order */
-//
-//      if (root[0] > root[1])
-//        SWAP(root[0], root[1]) ;
-//
-//      if (root[1] > root[2])
-//        {
-//          SWAP(root[1], root[2]) ;
-//
-//          if (root[0] > root[1])
-//            SWAP(root[0], root[1]) ;
-//        }
-//
-//      return 3;
-//    }
-//  else
-//    {
-//      double sgnR = (R >= 0 ? 1 : -1);
-//      double A = -sgnR * pow (fabs (R) + sqrt (R2 - Q3), 1.0/3.0);
-//      double B = Q / A ;
-//      root[0] = A + B - a / 3;
-//      return 1;
-//    }
-//}
 
 
 
