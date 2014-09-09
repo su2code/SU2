33c4e1c4875f739dcd86bec773328a1b496f452f
mpini <mpini@tud277439.(none)>
2014-09-09 12:03:13 +0200

    still working on 2D Jacobian

--- SU2_CFD/src/transport_model.cpp
+++ SU2_CFD/src/transport_model.cpp
@@ -83,8 +83,8 @@ void CSutherland::SetViscosity(double T, double rho) {
 void CSutherland::SetDerViscosity(double T, double rho)  {
 
 	dmudrho_T = 0.0;
-	dmudT_rho = Mu_ref*( (3.0/2.0)*pow((T/T_ref),(1.0/2.0))*((T_ref + S)/(T + S))
-			    -pow((T/T_ref),(3.0/2.0))*(T_ref + S)/(T + S)/(T + S) );
+	dmudT_rho = Mu_ref*( (3.0/2.0)*pow( (T/T_ref),(1.0/2.0) )*( (T_ref + S)/(T + S) )
+			    -pow( (T/T_ref),(3.0/2.0) )*(T_ref + S)/(T + S)/(T + S) );
 
 }
 
