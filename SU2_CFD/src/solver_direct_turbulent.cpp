a9e634c57b0d4a4e8d328a796a2864580a0df3aa
btracey <bdtinternet@gmail.com>
2014-09-30 10:04:19 -0700

    Added an option to print the SA source value at a node

--- SU2_CFD/src/solver_direct_turbulent.cpp
+++ SU2_CFD/src/solver_direct_turbulent.cpp
@@ -1311,6 +1311,10 @@ void CTurbSASolver::Source_Residual(CGeometry *geometry, CSolver **solver_contai
     /*--- Compute the source term ---*/
     numerics->ComputeResidual(Residual, Jacobian_i, NULL, config);
     
+    if (config->GetSA_Source_Print() == iPoint){
+      cout << "Residual at point " << iPoint << " = " << Residual[0];
+    }
+    
     unsigned long idx = 0;
     unsigned long base = 0;
     if (config->GetExtraOutput()) {
