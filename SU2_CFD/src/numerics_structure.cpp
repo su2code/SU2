701702e09ce56c96aacf5bb52bad1c7f0c50c4d0
salvovitale <s.vitale@tudelft.nl>
2014-10-23 12:16:28 +0200

    better indentation invPMatrix

--- SU2_CFD/src/numerics_structure.cpp
+++ SU2_CFD/src/numerics_structure.cpp
@@ -1061,21 +1061,21 @@ void CNumerics::GetPMatrix_inv(double **val_invp_tensor, double *val_density, do
 		sqvel = val_velocity[0]*val_velocity[0]+val_velocity[1]*val_velocity[1]+val_velocity[2]*val_velocity[2];
 		dp_drho = *val_chi + 0.5*sqvel*(*val_kappa);
 
-		val_invp_tensor[0][0]=val_normal[0]-val_normal[2]*val_velocity[1] / *val_density+val_normal[1]*val_velocity[2] / *val_density-val_normal[0]*dp_drho/c2;
+		val_invp_tensor[0][0]=val_normal[0]-val_normal[2]*val_velocity[1] / *val_density + val_normal[1]*val_velocity[2] / *val_density - val_normal[0]*dp_drho/c2;
 		val_invp_tensor[0][1]=val_normal[0]*val_velocity[0]*k_o_c2;
-		val_invp_tensor[0][2]=val_normal[2] / *val_density+val_normal[0]*val_velocity[1]*k_o_c2;
-		val_invp_tensor[0][3]=-val_normal[1] / *val_density+val_normal[0]*val_velocity[2]*k_o_c2;
+		val_invp_tensor[0][2]=val_normal[2] / *val_density + val_normal[0]*val_velocity[1]*k_o_c2;
+		val_invp_tensor[0][3]=-val_normal[1] / *val_density + val_normal[0]*val_velocity[2]*k_o_c2;
 		val_invp_tensor[0][4]=-val_normal[0]*k_o_c2;
 
-		val_invp_tensor[1][0]=val_normal[1]+val_normal[2]*val_velocity[0] / *val_density-val_normal[0]*val_velocity[2] / *val_density-val_normal[1]*dp_drho/c2;
-		val_invp_tensor[1][1]=-val_normal[2] / *val_density+val_normal[1]*val_velocity[0]*k_o_c2;
+		val_invp_tensor[1][0]=val_normal[1]+val_normal[2]*val_velocity[0] / *val_density - val_normal[0]*val_velocity[2] / *val_density - val_normal[1]*dp_drho/c2;
+		val_invp_tensor[1][1]=-val_normal[2] / *val_density + val_normal[1]*val_velocity[0]*k_o_c2;
 		val_invp_tensor[1][2]=val_normal[1]*val_velocity[1]*k_o_c2;
-		val_invp_tensor[1][3]=val_normal[0] / *val_density+val_normal[1]*val_velocity[2]*k_o_c2;
+		val_invp_tensor[1][3]=val_normal[0] / *val_density + val_normal[1]*val_velocity[2]*k_o_c2;
 		val_invp_tensor[1][4]=-val_normal[1]*k_o_c2;
 
-		val_invp_tensor[2][0]=val_normal[2]-val_normal[1]*val_velocity[0] / *val_density+val_normal[0]*val_velocity[1] / *val_density-val_normal[2]*dp_drho/c2;
-		val_invp_tensor[2][1]=val_normal[1] / *val_density+val_normal[2]*val_velocity[0]*k_o_c2;
-		val_invp_tensor[2][2]=-val_normal[0] / *val_density+val_normal[2]*val_velocity[1]*k_o_c2;
+		val_invp_tensor[2][0]=val_normal[2]-val_normal[1]*val_velocity[0] / *val_density + val_normal[0]*val_velocity[1] / *val_density - val_normal[2]*dp_drho/c2;
+		val_invp_tensor[2][1]=val_normal[1] / *val_density + val_normal[2]*val_velocity[0]*k_o_c2;
+		val_invp_tensor[2][2]=-val_normal[0] / *val_density + val_normal[2]*val_velocity[1]*k_o_c2;
 		val_invp_tensor[2][3]=val_normal[2]*val_velocity[2]*k_o_c2;
 		val_invp_tensor[2][4]=-val_normal[2]*k_o_c2;
 
