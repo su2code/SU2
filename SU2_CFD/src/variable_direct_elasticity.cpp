2bb45952a196f7a7128391c7462a0ee0ce968fc3
Ruben Sanchez <r.sanchez-fernandez14@imperial.ac.uk>
2015-05-13 10:43:58 +0100

    Added FSI features.

--- SU2_CFD/src/variable_direct_elasticity.cpp
+++ SU2_CFD/src/variable_direct_elasticity.cpp
@@ -1,7 +1,7 @@
 /*!
  * \file variable_direct_elasticity.cpp
  * \brief Definition of the solution fields.
- * \author F. Palacios
+ * \author F. Palacios, R. Sanchez
  * \version 3.2.9 "eagle"
  *
  * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
@@ -35,7 +35,9 @@ CFEAVariable::CFEAVariable(void) : CVariable() { }
 
 CFEAVariable::CFEAVariable(double *val_fea, unsigned short val_nDim, unsigned short val_nvar, CConfig *config)
 : CVariable(val_nDim, val_nvar, config) {
-	unsigned short iVar, iDim;
+	unsigned short iVar, iDim, jDim;
+
+	dynamicFEA = (config->GetDynamic_Analysis() == DYNAMIC);
   
 	/*--- Allocate residual structures ---*/
 	Residual_Sum = new double [nVar]; Residual_Old = new double [nVar];
@@ -45,12 +47,43 @@ CFEAVariable::CFEAVariable(double *val_fea, unsigned short val_nDim, unsigned sh
 		Solution[iVar] = val_fea[iVar];
 		Solution_Old[iVar] = val_fea[iVar];
 	}
+
+	if (dynamicFEA){
+
+		/*--- Allocate solution structures ---*/
+		Solution_Pred =  new double [nVar];
+		Solution_Pred_Old =  new double [nVar];
+		Solution_time_n = new double [nVar];
+		Solution_Vel = new double [nVar];
+		Solution_Vel_time_n = new double [nVar];
+		Solution_Accel = new double [nVar];
+		Solution_Accel_time_n = new double [nVar];
+
+		/*--- Initialization of variables for dynamic problem ---*/
+		for (iVar = 0; iVar < nVar; iVar++) {
+			Solution_Pred[iVar] =  val_fea[iVar];
+			Solution_Pred_Old[iVar] =  val_fea[iVar];
+			Solution_time_n[iVar] = val_fea[iVar];
+			Solution_Vel[iVar] = val_fea[iVar];
+			Solution_Vel_time_n[iVar] = val_fea[iVar];
+			Solution_Accel[iVar] = val_fea[iVar];
+			Solution_Accel_time_n[iVar] = val_fea[iVar];
+		}
+	}
+
+
   
   /*--- Allocate stress tensor ---*/
 	Stress = new double* [nDim];
 	for (iDim = 0; iDim < nDim; iDim++)
 		Stress[iDim] = new double [nDim];
-  
+
+  /*--- Initialize stress tensor---*/
+	for (iDim = 0; iDim < nDim; iDim++){
+		for (jDim = 0; jDim < nDim; jDim++){
+			Stress[iDim][jDim]=0.0;
+		}
+	}
 }
 
 CFEAVariable::~CFEAVariable(void) {
@@ -59,6 +92,156 @@ CFEAVariable::~CFEAVariable(void) {
 	for (iDim = 0; iDim < nDim; iDim++)
 		delete [] Stress[iDim];
 	delete [] Stress;
+  delete [] Residual_Sum;
+  delete [] Residual_Old;
+  delete [] Solution;
+
+  if (dynamicFEA){
+	  delete[] Solution_Pred;
+	  delete[] Solution_Pred_Old;
+	  delete[] Solution_time_n;
+	  delete[] Solution_Vel;
+	  delete[] Solution_Vel_time_n;
+	  delete[] Solution_Accel;
+	  delete[] Solution_Accel_time_n ;
+  }
+
+  
+
+}
+
+void CFEAVariable::SetSolution_time_n(void) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_time_n[iVar] = Solution[iVar];
+
+}
+
+void CFEAVariable::SetSolution_time_n(double *val_solution_time_n) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_time_n[iVar] = val_solution_time_n[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Vel(double *val_solution_vel) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Vel[iVar] = val_solution_vel[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Vel_time_n(double *val_solution_vel_time_n) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Vel_time_n[iVar] = val_solution_vel_time_n[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Vel_time_n(void) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Vel_time_n[iVar] = Solution_Vel[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Accel(double *val_solution_accel) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Accel[iVar] = val_solution_accel[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Accel_time_n(double *val_solution_accel_time_n) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Accel_time_n[iVar] = val_solution_accel_time_n[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Accel_time_n(void) {
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Accel_time_n[iVar] = Solution_Accel[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Pred(void){
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Pred[iVar] = Solution[iVar];
+
+}
+
+void CFEAVariable::SetSolution_Pred_Old(void){
+
+	for (unsigned short iVar = 0; iVar < nVar; iVar++)
+		Solution_Pred_Old[iVar] = Solution_Pred[iVar];
+
+}
+
+
+CFEABoundVariable::CFEABoundVariable(void) : CVariable() { }
+
+CFEABoundVariable::CFEABoundVariable(unsigned short val_nDim, unsigned short val_nvar, unsigned short val_nElBound, CConfig *config)
+: CVariable(val_nDim, val_nvar, config) {
+	unsigned short iVar, iDim, jDim;
+
+	/*--- Allocate residual structures ---*/
+	Residual_Sum = new double [nVar]; Residual_Old = new double [nVar];
+
+	/*--- Allocate stress tensor ---*/
+	if (nDim == 2){
+		Traction = new double* [2*nDim];
+		for (iDim = 0; iDim < 2*nDim ; iDim++)
+			Traction[iDim] = new double [val_nElBound];
+	}
+	else if (nDim == 3){
+		/*--- Allocate stress tensor ---*/
+		Traction = new double* [4*nDim];
+		for (iDim = 0; iDim < 4*nDim ; iDim++)
+			Traction[iDim] = new double [val_nElBound];
+	}
+
+	/*--- Initialize stress tensor ---*/
+	if (nDim == 2){
+		/*--- Initialize stress tensor---*/
+		for (iDim = 0; iDim < 2*nDim; iDim++){
+			for (jDim = 0; jDim < val_nElBound; jDim++){
+				Traction[iDim][jDim]=0.0;
+			}
+		}
+	}
+	else if (nDim == 3){
+		/*--- Initialize stress tensor---*/
+		for (iDim = 0; iDim < 4*nDim; iDim++){
+			for (jDim = 0; jDim < val_nElBound; jDim++){
+				Traction[iDim][jDim]=0.0;
+			}
+		}
+	}
+
+}
+
+CFEABoundVariable::~CFEABoundVariable(void) {
+  unsigned short iDim;
+
+	/*--- Initialize stress tensor ---*/
+	if (nDim == 2){
+		/*--- Initialize stress tensor---*/
+		for (iDim = 0; iDim < 2*nDim; iDim++){
+			delete [] Traction[iDim];
+		}
+	}
+	else if (nDim == 3){
+		/*--- Initialize stress tensor---*/
+		for (iDim = 0; iDim < 4*nDim; iDim++){
+			delete [] Traction[iDim];
+		}
+	}
+
+	delete [] Traction;
+
   delete [] Residual_Sum;
   delete [] Residual_Old;
   
