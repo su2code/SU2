921816bd1bb4bcb420a5bf4df3fc7a4f60220e20
hlkline <hlkline@stanford.edu>
2015-06-15 11:29:15 -0700

    small fixes to interpolation

--- Common/include/dual_grid_structure.hpp
+++ Common/include/dual_grid_structure.hpp
@@ -875,6 +875,7 @@ private:
 	double Aux_Var;			/*!< \brief Auxiliar variable defined only on the surface. */
 	double CartCoord[3];		/*!< \brief Vertex cartesians coordinates. */
 	double VarCoord[3];		/*!< \brief Used for storing the coordinate variation due to a surface modification. */
+	double VarRot[3];   /*!< \brief Used for storing the rotation variation due to a surface modification. */
 	long PeriodicPoint[2];			/*!< \brief Store the periodic point of a boundary (iProcessor, iPoint) */
 	short Rotation_Type;			/*!< \brief Type of rotation associated with the vertex (MPI and periodic) */
 	unsigned long Normal_Neighbor; /*!< \brief Index of the closest neighbor. */
@@ -1139,6 +1140,13 @@ public:
    * Uses nDonor_Points
    */
 	void	Allocate_DonorInfo(void);
+
+	/*!
+   * \brief Get the rotation variation
+   * \return  - pointer to the vector defining the rotation
+   */
+  double* getVarRot(void);
+
 };
 
 #include "dual_grid_structure.inl"
