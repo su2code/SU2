c7bb1c2f1f60bc6774c1b8d03934087a14d657a3
salvovitale <s.vitale@tudelft.nl>
2015-09-01 09:08:43 +0200

    add Ps call FluidModel

--- SU2_CFD/src/fluid_model_pig.cpp
+++ SU2_CFD/src/fluid_model_pig.cpp
@@ -93,6 +93,15 @@ void CIdealGas::SetTDState_hs (su2double h, su2double s ) {
 
 }
 
+void CIdealGas::SetTDState_Ps (su2double P, su2double s ){
+
+	su2double T   = exp(Gamma_Minus_One/Gamma* (s/Gas_Constant +log(P) -log(Gas_Constant)) );
+	su2double rho = P/(T*Gas_Constant);
+
+	SetTDState_Prho(P, rho);
+
+}
+
 void CIdealGas::SetTDState_rhoT (su2double rho, su2double T ) {
 
 	su2double e = T*Gas_Constant/Gamma_Minus_One;
