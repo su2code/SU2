a93162fd14e9a99e9ed08571748ef5ccff681033
padronas <padronas@stanford.edu>
2015-09-01 12:26:42 -0700

    In gust small change to dynamic memory and tiny update of values in aeroelastic regression test

--- TestCases/parallel_regression.py
+++ TestCases/parallel_regression.py
@@ -391,7 +391,7 @@ def main():
     aeroelastic.cfg_dir   = "aeroelastic"
     aeroelastic.cfg_file  = "aeroelastic_NACA64A010.cfg"
     aeroelastic.test_iter = 2
-    aeroelastic.test_vals = [0.077319, 0.036428, -1.685289e-03, -1.129111e-04] #last 4 columns
+    aeroelastic.test_vals = [0.077319, 0.036428, -1.685277e-03, -1.129186e-04] #last 4 columns
     aeroelastic.su2_exec  = "parallel_computation.py -f"
     aeroelastic.timeout   = 1600
     aeroelastic.tol       = 0.000001
