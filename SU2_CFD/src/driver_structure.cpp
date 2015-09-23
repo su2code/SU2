8bbf32b025bb503e9973ca2cc2df100d61b57846
Thomas D. Economon <economon@stanford.edu>
2015-09-23 14:38:52 -0700

    First implementation of a new driver class. Compiles, but not active yet.

---
+++ SU2_CFD/src/driver_structure.cpp
@@ -0,0 +1,75 @@
+/*!
+ * \file driver_structure.cpp
+ * \brief The main subroutines for driving single or multi-zone problems.
+ * \author T. Economon, H. Kline, R. Sanchez
+ * \version 4.0.1 "Cardinal"
+ *
+ * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+ *                      Dr. Thomas D. Economon (economon@stanford.edu).
+ *
+ * SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+ *                 Prof. Piero Colonna's group at Delft University of Technology.
+ *                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+ *                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+ *                 Prof. Rafael Palacios' group at Imperial College London.
+ *
+ * Copyright (C) 2012-2015 SU2, the open-source CFD code.
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../include/driver_structure.hpp"
+
+CDriver::CDriver(CConfig *config) {
+  /*--- For now, set the default to one zone. ---*/
+  nZone = 1;
+}
+
+CDriver::~CDriver(void) { }
+
+//! TO DO: Inline file for this perhaps? It's purely virtual.
+void CDriver::Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+                  CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+                  CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox) { }
+
+CSingleZoneDriver::CSingleZoneDriver(CConfig *config) : CDriver(config) { }
+
+CSingleZoneDriver::~CSingleZoneDriver(void) { }
+
+void CSingleZoneDriver::Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+                            CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+                            CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox) {
+  
+}
+
+CMultiZoneDriver::CMultiZoneDriver(CConfig *config) : CDriver(config) {}
+
+CMultiZoneDriver::~CMultiZoneDriver(void) { }
+
+void CMultiZoneDriver::Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+                           CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+                           CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox) {
+  
+}
+
+CFSIDriver::CFSIDriver(CConfig *config) : CDriver(config) {}
+
+CFSIDriver::~CFSIDriver(void) { }
+
+void CFSIDriver::Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+                     CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+                     CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox) {
+  
+}
+
