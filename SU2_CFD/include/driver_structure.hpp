0b5c2451f95c9af21b8deefb39e88a33b89937be
Thomas D. Economon <economon@stanford.edu>
2015-09-23 14:38:52 -0700

    First implementation of a new driver class. Compiles, but not active yet.

---
+++ SU2_CFD/include/driver_structure.hpp
@@ -0,0 +1,211 @@
+/*!
+ * \file driver_structure.hpp
+ * \brief Headers of the main subroutines for driving single or multi-zone problems.
+ *        The subroutines and functions are in the <i>driver_structure.cpp</i> file.
+ * \author T. Economon, H. Kline, R. Sanchez
+ * \version 4.0.1 "Cardinal"
+ *
+ * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+ *                      Dr. Thomas D. Economon (economon@stanford.edu).
+ *
+ * SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+ *                 Prof. Piero Colonna's group at Delft University of Technology.
+ *                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+ *                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+ *                 Prof. Rafael Palacios' group at Imperial College London.
+ *
+ * Copyright (C) 2012-2015 SU2, the open-source CFD code.
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "../../Common/include/mpi_structure.hpp"
+#include "solver_structure.hpp"
+#include "integration_structure.hpp"
+#include "output_structure.hpp"
+#include "numerics_structure.hpp"
+#include "../../Common/include/geometry_structure.hpp"
+#include "../../Common/include/grid_movement_structure.hpp"
+#include "../../Common/include/config_structure.hpp"
+
+using namespace std;
+
+/*! 
+ * \class CDriver
+ * \brief Parent class for driving an iteration of a single or multi-zone problem.
+ * \author T. Economon
+ * \version 4.0.1 "Cardinal"
+ */
+class CDriver {
+protected:
+  unsigned short nZone;	/*!< \brief Total number of zones in the problem. */
+
+
+public:
+	
+	/*! 
+	 * \brief Constructor of the class. 
+	 */
+	CDriver(CConfig *config);
+	
+	/*! 
+	 * \brief Destructor of the class. 
+	 */
+	virtual ~CDriver(void);
+
+	/*!
+	 * \brief A virtual member.
+   * \param[in] output - Pointer to the COutput class.
+   * \param[in] integration_container - Container vector with all the integration methods.
+   * \param[in] geometry_container - Geometrical definition of the problem.
+   * \param[in] solver_container - Container vector with all the solutions.
+   * \param[in] numerics_container - Description of the numerical method (the way in which the equations are solved).
+   * \param[in] config_container - Definition of the particular problem.
+   * \param[in] surface_movement - Surface movement classes of the problem.
+   * \param[in] grid_movement - Volume grid movement classes of the problem.
+   * \param[in] FFDBox - FFD FFDBoxes of the problem.
+	 */
+  
+  //! TO DO: Add transfer container at end of list
+  virtual void Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+                   CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+                   CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox);
+
+};
+
+
+/*! 
+ * \class CSingleZoneDriver
+ * \brief Class for driving an iteration of the physics within a single zone.
+ * \author T. Economon
+ * \version 4.0.1 "Cardinal"
+ */
+class CSingleZoneDriver : public CDriver {
+public:
+	
+	/*! 
+	 * \brief Constructor of the class.
+	 * \param[in] config - Definition of the particular problem.
+	 */
+	CSingleZoneDriver(CConfig *config);
+	
+	/*! 
+	 * \brief Destructor of the class. 
+	 */
+	~CSingleZoneDriver(void);
+	
+	/*! 
+	 * \brief Run a single iteration of the physics within a single zone.
+   * \param[in] output - Pointer to the COutput class.
+   * \param[in] integration_container - Container vector with all the integration methods.
+   * \param[in] geometry_container - Geometrical definition of the problem.
+   * \param[in] solver_container - Container vector with all the solutions.
+   * \param[in] numerics_container - Description of the numerical method (the way in which the equations are solved).
+   * \param[in] config_container - Definition of the particular problem.
+   * \param[in] surface_movement - Surface movement classes of the problem.
+   * \param[in] grid_movement - Volume grid movement classes of the problem.
+   * \param[in] FFDBox - FFD FFDBoxes of the problem.
+	 */
+  
+  //! TO DO: Add transfer container at end of list
+  void Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+           CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+           CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox);
+ 
+};
+
+
+/*!
+ * \class CMultiZoneDriver
+ * \brief Class for driving an iteration of the physics within a single zone.
+ * \author T. Economon
+ * \version 4.0.1 "Cardinal"
+ */
+class CMultiZoneDriver : public CDriver {
+public:
+  
+  /*!
+   * \brief Constructor of the class.
+   * \param[in] config - Definition of the particular problem.
+   */
+  CMultiZoneDriver(CConfig *config);
+  
+  /*!
+   * \brief Destructor of the class.
+   */
+  ~CMultiZoneDriver(void);
+  
+  /*!
+   * \brief Run a single iteration of the physics within multiple zones.
+   * \param[in] output - Pointer to the COutput class.
+   * \param[in] integration_container - Container vector with all the integration methods.
+   * \param[in] geometry_container - Geometrical definition of the problem.
+   * \param[in] solver_container - Container vector with all the solutions.
+   * \param[in] numerics_container - Description of the numerical method (the way in which the equations are solved).
+   * \param[in] config_container - Definition of the particular problem.
+   * \param[in] surface_movement - Surface movement classes of the problem.
+   * \param[in] grid_movement - Volume grid movement classes of the problem.
+   * \param[in] FFDBox - FFD FFDBoxes of the problem.
+   */
+  
+  //! TO DO: Add transfer container at end of list
+  void Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+           CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+           CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox);
+  
+};
+
+
+/*!
+ * \class CFSIDriver
+ * \brief Class for driving a BGS iteration for a fluid-structure interaction problem in multiple zones.
+ * \author R. Sanchez.
+ * \version 4.0.1 "Cardinal"
+ */
+class CFSIDriver : public CDriver {
+public:
+
+	/*!
+	 * \brief Constructor of the class.
+	 * \param[in] config - Definition of the particular problem.
+	 */
+	CFSIDriver(CConfig *config);
+
+	/*!
+	 * \brief Destructor of the class.
+	 */
+	~CFSIDriver(void);
+
+	/*!
+	 * \brief Run a Block Gauss-Seidel iteration of the FSI problem.
+   * \param[in] output - Pointer to the COutput class.
+   * \param[in] integration_container - Container vector with all the integration methods.
+   * \param[in] geometry_container - Geometrical definition of the problem.
+   * \param[in] solver_container - Container vector with all the solutions.
+   * \param[in] numerics_container - Description of the numerical method (the way in which the equations are solved).
+   * \param[in] config_container - Definition of the particular problem.
+   * \param[in] surface_movement - Surface movement classes of the problem.
+   * \param[in] grid_movement - Volume grid movement classes of the problem.
+   * \param[in] FFDBox - FFD FFDBoxes of the problem.
+	 */
+  
+  //! TO DO: Add transfer container at end of list
+  void Run(COutput *output, CIntegration ***integration_container, CGeometry ***geometry_container,
+           CSolver ****solver_container, CNumerics *****numerics_container, CConfig **config_container,
+           CSurfaceMovement **surface_movement, CVolumetricMovement **grid_movement, CFreeFormDefBox*** FFDBox);
+
+};
+
