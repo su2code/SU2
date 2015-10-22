a88e4433c0a6aa7a648e898d0f9685a646452119
salvovitale <s.vitale@tudelft.nl>
2015-10-22 15:35:18 +0200

    change performace labels from config and fix small bug

--- SU2_CFD/src/iteration_structure.cpp
+++ SU2_CFD/src/iteration_structure.cpp
@@ -2181,6 +2181,8 @@ void SetTurboPerformance(CGeometry ***geometry_container, CSolver ****solver_con
 							solver_container[iZone][MESH_0][FLOW_SOL]->TurboPerformance(solver_container[jZone][MESH_0][FLOW_SOL], config_container[iZone], inMarker, outMarker, Kind_TurboPerf, inMarkerTP);
 							solver_container[ZONE_0][MESH_0][FLOW_SOL]->StoreTurboPerformance(solver_container[iZone][MESH_0][FLOW_SOL], inMarkerTP);
 //							cout << solver_container[ZONE_0][MESH_0][FLOW_SOL]->GetTotalPressureLoss(inMarkerTP) << endl;
+//							cout << solver_container[ZONE_0][MESH_0][FLOW_SOL]->GetTotalStaticEfficiency(inMarkerTP) << endl;
+
 						}
 			}
 }
