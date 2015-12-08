d586b80a7b882ca71be2b3bd3d76407310ccf91a
Salvatore Vitale <s.vitale@tudelft.nl>
2015-12-08 22:24:43 +0100

    add alloacate flag when calling SetTurboVertex

--- SU2_CFD/src/definition_structure.cpp
+++ SU2_CFD/src/definition_structure.cpp
@@ -308,8 +308,8 @@ void Geometrical_Preprocessing(CGeometry ***geometry, CConfig **config, unsigned
     if ((config[iZone]->GetnMGLevels() != 0) && (rank == MASTER_NODE))
       cout << "Setting the multigrid structure." << endl;
     
-    geometry[iZone][MESH_0]->SetTurboVertex(config[iZone], INFLOW);
-    geometry[iZone][MESH_0]->SetTurboVertex(config[iZone], OUTFLOW);
+    geometry[iZone][MESH_0]->SetTurboVertex(config[iZone], INFLOW, true);
+    geometry[iZone][MESH_0]->SetTurboVertex(config[iZone], OUTFLOW, false);
 
   }
   
