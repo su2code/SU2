7fad26fbdcde31c396a1b87e20b9af6f4b6f71cc
vdweide <e.t.a.vanderweide@utwente.nl>
2016-02-19 14:46:58 +0100

    Added the files that will contain the functions for the FEM standard elements

---
+++ Common/include/fem_standard_element.hpp
@@ -0,0 +1,72 @@
+/*!
+ * \file fem_standard_element.hpp
+ * \brief Headers of the main functions for the FEM standard elements.
+ *        The functions are in the <i>fem_standard_element.cpp</i> file.
+ * \author E. van der Weide
+ * \version 4.1.0 "Cardinal"
+ *
+ * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+ *                      Dr. Thomas D. Economon (economon@stanford.edu).
+ *
+ * SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+ *                 Prof. Piero Colonna's group at Delft University of Technology.
+ *                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+ *                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+ *                 Prof. Rafael Palacios' group at Imperial College London.
+ *
+ * Copyright (C) 2012-2015 SU2, the open-source CFD code.
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+/*!
+ * \class FEMStandardElementClass
+ * \brief Class to define an FEM standard element.
+ * \author E. van der Weide
+ * \version 4.1.0 "Cardinal"
+ */
+class FEMStandardElementClass {
+private:
+  unsigned short VTK_Type;     /*!< \brief Element type using the VTK convention. */
+  unsigned short nPoly;        /*!< \brief Polynomial degree of the element. */
+  unsigned short nDOFs;        /*!< \brief Number of DOFs of the element. */
+  unsigned short nIntegration; /*!< \brief Number of points used in the numerical integration. */
+
+public:
+  /*--- Constructors and destructors. ---*/
+
+  FEMStandardElementClass();
+  ~FEMStandardElementClass();
+
+  /*--- Copy constructor and assignment operator. ---*/
+
+  FEMStandardElementClass(const FEMStandardElementClass &other);
+
+  FEMStandardElementClass& operator=(const FEMStandardElementClass &other);
+
+private:
+  /*--- Copy function, which copies the data of the given object into the current object. ---*/
+
+  void Copy(const FEMStandardElementClass &other);
+};
+
+#include "fem_standard_element.inl"
