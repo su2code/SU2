018c8574316d276f547ef2f80f2e4f5ea83f1df4
Guillaume Bâty <guillaume.baty@gmx.fr>
2016-03-10 14:46:36 +0100

    Support both Python 2.6+ and Python 3.4+
    Main changes from python 2 to python 3 are:
    'raise Exception "message"'  ->  'raise Exception("message")'
    print "message"  -> print("message")
    long, int  ->  int
    raw_input  -> input
    cPickle  ->  pickle
    
    input  -> eval(input()) # not used in SU2, just for information
    
    This commit also removes unused import, especially import of os, shutil and copy
    
    Useful links:
      - https://docs.python.org/3/howto/pyporting.html
      - http://python3porting.com
      - https://docs.python.org/3/whatsnew/3.0.html#library-changes, 4th item.

--- SU2_PY/SU2/io/__init__.py
+++ SU2_PY/SU2/io/__init__.py
@@ -1,14 +1,10 @@
 # SU2/io/__init__.py
 
-from tools    import *
-from redirect import output as redirect_output
-from redirect import folder as redirect_folder
-from data     import load_data, save_data
-from filelock import filelock
+from .tools    import *
+from .redirect import output as redirect_output
+from .redirect import folder as redirect_folder
+from .data     import load_data, save_data
+from .filelock import filelock
 
-from config   import Config
-from state    import State_Factory as State
-
-
-
-
+from .config   import Config
+from .state    import State_Factory as State
