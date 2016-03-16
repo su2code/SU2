ce8af2a82da6a9662cb6ba7ff920bc895e2fabc4
Thomas D. Economon <economon@stanford.edu>
2016-03-15 17:16:16 -0700

    Added shape_optimization.py regression test.

--- TestCases/TestCase.py
+++ TestCases/TestCase.py
@@ -252,6 +252,134 @@ class TestCase:
         os.chdir(workdir)
         return passed
 
+    def run_opt(self):
+
+        print '==================== Start Test: %s ===================='%self.tag
+        passed       = True
+        exceed_tol   = False
+        timed_out    = False
+        iter_missing = True
+        start_solver = True
+
+        # Adjust the number of iterations in the config file   
+        self.adjust_opt_iter()
+
+        # Assemble the shell command to run SU2
+        logfilename = '%s.log' % os.path.splitext(self.cfg_file)[0]
+        command = "%s %s > %s" % (self.su2_exec, self.cfg_file,logfilename)
+
+        # Run SU2
+        workdir = os.getcwd()
+        os.chdir(self.cfg_dir)
+        print os.getcwd()
+        start   = datetime.datetime.now()
+        process = subprocess.Popen(command, shell=True)  # This line launches SU2
+
+        # check for timeout
+        while process.poll() is None:
+            time.sleep(0.1)
+            now = datetime.datetime.now()
+            running_time = (now - start).seconds
+            if running_time > self.timeout:
+                try:
+                    process.kill()
+                    os.system('killall %s' % self.su2_exec)   # In case of parallel execution
+                except AttributeError: # popen.kill apparently fails on some versions of subprocess... the killall command should take care of things!
+                    pass
+                timed_out = True
+                passed    = False
+
+        # Examine the output
+        f = open(logfilename,'r')
+        output = f.readlines()
+        delta_vals = []
+        sim_vals = []
+        if not timed_out:
+            start_solver = False
+            for line in output:
+                if not start_solver: # Don't bother parsing anything before optimizer starts
+                    if line.find('OBJFUN') > -1:
+                        start_solver=True
+                else:   # Found the OBJFUN line; parse the input
+                    raw_data = line.split()
+                    try:
+                        iter_number = int(raw_data[0])
+                        data        = raw_data[len(raw_data)-4:]    # Take the last 4 columns for comparison
+                    except ValueError:
+                        continue
+                    except IndexError:
+                        continue
+
+                    if iter_number == self.test_iter:  # Found the iteration number we're checking for
+                        iter_missing = False
+                        if not len(self.test_vals)==len(data):   # something went wrong... probably bad input
+                            print "Error in test_vals!"
+                            passed = False
+                            break
+                        for j in range(len(data)):
+                            sim_vals.append( float(data[j]) )
+                            delta_vals.append( abs(float(data[j])-self.test_vals[j]) )
+                            if delta_vals[j] > self.tol:
+                                exceed_tol = True
+                                passed     = False
+                        break
+                    else:
+                        iter_missing = True
+
+            if not start_solver:
+                passed = False
+
+            if iter_missing:
+                passed = False
+
+        # Write the test results 
+        #for j in output:
+        #  print j
+
+        if passed:
+            print "%s: PASSED"%self.tag
+        else:
+            print "%s: FAILED"%self.tag
+            print 'Output for the failed case'
+            subprocess.call(['cat', logfilename])      
+
+        print 'execution command: %s'%command
+
+        if timed_out:
+            print 'ERROR: Execution timed out. timeout=%d'%self.timeout
+
+        if exceed_tol:
+            print 'ERROR: Difference between computed input and test_vals exceeded tolerance. TOL=%f'%self.tol
+
+        if not start_solver:
+            print 'ERROR: The code was not able to get to the "OBJFUN" section.'
+
+        if iter_missing:
+            print 'ERROR: The optimizer iteration number %d could not be found.'%self.test_iter
+
+        print 'test_iter=%d \n'%self.test_iter,
+
+        print 'test_vals (stored): ',
+        for j in self.test_vals:
+            print '%f,'%j,
+        print '\n',
+
+        print 'sim_vals (computed): ',
+        for j in sim_vals:
+            print '%f,'%j,
+        print '\n',
+
+        print 'delta_vals: ',
+        for j in delta_vals:
+            print '%f,'%j,
+        print '\n',
+        
+        print 'test duration: %.2f min'%(running_time/60.0) 
+        print '==================== End Test: %s ====================\n'%self.tag
+        
+        os.chdir(workdir)
+        return passed
+          
     def adjust_iter(self):
 
         # Read the cfg file
@@ -271,6 +399,30 @@ class TestCase:
                 file_out.write(line)
             else:
                 file_out.write("EXT_ITER=%d\n"%(self.test_iter+1))
+        file_out.close()
+        os.chdir(workdir)
+
+        return
+
+    def adjust_opt_iter(self):
+
+        # Read the cfg file
+        workdir = os.getcwd()
+        os.chdir(self.cfg_dir)
+        file_in = open(self.cfg_file, 'r')
+        lines   = file_in.readlines()
+        file_in.close()
+
+        # Rewrite the file with a .autotest extension
+        self.cfg_file = "%s.autotest"%self.cfg_file
+        file_out = open(self.cfg_file,'w')
+        file_out.write('%% This file automatically generated by the regression script\n')
+        file_out.write('%% Number of optimizer iterations changed to %d\n'%(self.test_iter))
+        for line in lines:
+            if not line.startswith("OPT_ITERATIONS"):
+                file_out.write(line)
+            else:
+                file_out.write("OPT_ITERATIONS= %d\n"%(self.test_iter))
         file_out.close()
         os.chdir(workdir)
 
