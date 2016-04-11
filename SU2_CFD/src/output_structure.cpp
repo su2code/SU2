e7f09791f6f4003a49b04d95a6e3ca02630b0ed8
hlkline <hlkline@stanford.edu>
2016-04-11 12:39:35 -0700

    commenting out output_structure delete that appears to cause an error

--- SU2_CFD/src/output_structure.cpp
+++ SU2_CFD/src/output_structure.cpp
@@ -83,8 +83,8 @@ COutput::~COutput(void) {
   /* delete pointers initialized at construction*/
   /* Coords and Conn_*(Connectivity) have their own dealloc functions */
   /* Data is taken care of in DeallocateSolution function */
-  /* delete[] Volume causes error in fsi test cases TODO: fix this*/
-  if(Volume!=NULL)      delete [] Volume;
+  /* delete[] Volume causes error in some test cases TODO: fix this*/
+ // if(Volume!=NULL)      delete [] Volume;
 
 }
 
