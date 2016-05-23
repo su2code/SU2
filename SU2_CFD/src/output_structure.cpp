e6fe652da2fc95ffb314f77eaee1551d0a03cbe2
Tim Albring <tim.albring@scicomp.uni-kl.de>
2016-05-23 16:48:06 +0200

    Fixed writing of residuals for the discrete adjoint solver.

--- SU2_CFD/src/output_structure.cpp
+++ SU2_CFD/src/output_structure.cpp
@@ -2255,7 +2255,10 @@ void COutput::MergeSolution(CConfig *config, CGeometry *geometry, CSolver **solv
           }
           
           if (config->GetWrt_Residuals()) {
-            Buffer_Send_Res[jPoint] = solver[CurrentIndex]->LinSysRes.GetBlock(iPoint, jVar);
+            if (!config->GetDiscrete_Adjoint())
+              Buffer_Send_Res[jPoint] = solver[CurrentIndex]->LinSysRes.GetBlock(iPoint, jVar);
+            else
+              Buffer_Send_Res[jPoint] = solver[CurrentIndex]->node[iPoint]->GetSolution(iVar) - solver[CurrentIndex]->node[iPoint]->GetSolution_Old(iVar);
           }
           
         }
