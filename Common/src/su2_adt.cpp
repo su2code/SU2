8363b82f707e50fb4d7b6994e6a24d36766867ea
vdweide <e.t.a.vanderweide@utwente.nl>
2016-06-14 13:48:33 +0200

    Set up the infrastructure for the wall distance computation via an ADT

---
+++ Common/src/su2_adt.cpp
@@ -0,0 +1,51 @@
+/*!
+ * \file su2_adt.cpp
+ * \brief Main subroutines for for carrying out geometrical searches using an
+ *        alternating digital tree (ADT).
+ * \author E. van der Weide
+ * \version 4.1.3 "Cardinal"
+ *
+ * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+ *                      Dr. Thomas D. Economon (economon@stanford.edu).
+ *
+ * SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+ *                 Prof. Piero Colonna's group at Delft University of Technology.
+ *                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+ *                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+ *                 Prof. Rafael Palacios' group at Imperial College London.
+ *
+ * Copyright (C) 2012-2016 SU2, the open-source CFD code.
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../include/su2_adt.hpp"
+
+su2_adtPointsOnlyClass::su2_adtPointsOnlyClass(unsigned short      nDim,
+                                               unsigned long       nPoints,
+                                               const su2double     *coor,
+                                               const unsigned long *pointID) {
+
+  cout << "su2_adtPointsOnlyClass::su2_adtPointsOnlyClass: Not implemented yet" << endl;
+  exit(1);
+}
+
+void su2_adtPointsOnlyClass::DetermineNearestNode(const su2double *coor,
+                                                  su2double       &dist,
+                                                  unsigned long   &pointID,
+                                                  int             &rankID) {
+
+  cout << "su2_adtPointsOnlyClass::DetermineNearestNode: Not implemented yet" << endl;
+  exit(1);
+}
