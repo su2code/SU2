b1687d95103125fd29abb716519fffa24081eadd
hlkline <hlkline@stanford.edu>
2016-07-11 15:55:17 -0400

    adjusted test case values: this is due to change in where objective scaling happens

--- TestCases/serial_regression.py
+++ TestCases/serial_regression.py
@@ -240,7 +240,7 @@ def main():
     contadj_wedge.cfg_dir   = "cont_adj_euler/wedge"
     contadj_wedge.cfg_file  = "inv_wedge_ROE.cfg"
     contadj_wedge.test_iter = 10
-    contadj_wedge.test_vals = [-7.364977, -13.301134, 0.000266, 0.000000] #last 4 columns
+    contadj_wedge.test_vals = [-7.364977, -13.301134, 0.000219, -0.000003] #last 4 columns
     contadj_wedge.su2_exec  = "SU2_CFD"
     contadj_wedge.timeout   = 1600
     contadj_wedge.tol       = 0.00001
