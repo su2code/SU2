38f55df9a86361c2e0497c7a5108317773886152
hlkline <hlkline@stanford.edu>
2016-07-27 18:33:31 -0700

    regression test values updated: matches with current Testcases/develop branch restart files, may update later on for more reasonable residiual values

--- TestCases/parallel_regression.py
+++ TestCases/parallel_regression.py
@@ -242,7 +242,8 @@ def main():
     contadj_wedge.cfg_dir   = "cont_adj_euler/wedge"
     contadj_wedge.cfg_file  = "inv_wedge_ROE.cfg"
     contadj_wedge.test_iter = 10  
-    contadj_wedge.test_vals = [-11.216707,     -16.773088,    9.6249e+04,   -6.7951e+04] #last 4 columns
+    contadj_wedge.test_vals = [2.780403,      -3.223266,   -1.1930e+06,   -2.9093e-06] #last 4 columns
+    #contadj_wedge.test_vals = [-11.216707,     -16.773088,    9.6249e+04,   -6.7951e+04] #last 4 columns
     contadj_wedge.su2_exec  = "parallel_computation.py -f"
     contadj_wedge.timeout   = 1600
     contadj_wedge.tol       = 0.00001
