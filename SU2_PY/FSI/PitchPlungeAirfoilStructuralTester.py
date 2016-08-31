66671c073684bfd97e0821fb1c9e18d721891dc1
tobadavid <tdavid91@hotmail.com>
2016-08-31 12:03:13 +0200

    Update files headers

--- SU2_PY/FSI/PitchPlungeAirfoilStructuralTester.py
+++ SU2_PY/FSI/PitchPlungeAirfoilStructuralTester.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python
-# -*-coding:utf-8 -* 
 
-#  \file SolidSolverTester.py
+## \file SolidSolverTester.py
 #  \brief Structural solver tester (one or two degree of freedom) used for testing the Py wrapper for external FSI coupling.
-#  \author D. THOMAS, University of Liege, Belgium. Department of Mechanical and Aerospace Engineering.
-#  \version BETA 
+#  \author David Thomas
+#  \version 4.3.0 "Cardinal"
+#
+# SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+#                      Dr. Thomas D. Economon (economon@stanford.edu).
+#
+# SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+#                 Prof. Piero Colonna's group at Delft University of Technology.
+#                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+#                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+#                 Prof. Rafael Palacios' group at Imperial College London.
+#                 Prof. Edwin van der Weide's group at the University of Twente.
+#                 Prof. Vincent Terrapon's group at the University of Liege.
+#
+# Copyright (C) 2012-2016 SU2, the open-source CFD code.
+#
+# SU2 is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# SU2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with SU2. If not, see <http://www.gnu.org/licenses/>. 
 
 # ----------------------------------------------------------------------
 #  Imports
 # ----------------------------------------------------------------------
 
 import os, sys, shutil, copy
-from mpi4py import MPI
 import numpy as np
 import scipy as sp
 import scipy.linalg as linalg
