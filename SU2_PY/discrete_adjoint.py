74857268c676505214b96e7577bdb0abef191cd1
hlkline <kline.hl@gmail.com>
2016-09-05 20:07:02 -0700

    discrete adjoint.py w/ external function

--- SU2_PY/discrete_adjoint.py
+++ SU2_PY/discrete_adjoint.py
@@ -103,6 +103,18 @@ def discrete_adjoint( filename           ,
         SU2.io.restart2solution(config,state)
     
     # Adjoint Solution
+    objectives      = config['OBJECTIVE_FUNCTION']
+    objectives = objectives.split(',')
+    n_obj = len(objectives)
+    marker_monitoring = config['MARKER_MONITORING']
+
+    # If using chain rule update coefficients using gradients as defined in downstream_function (local file)
+    if 'OUTFLOW_GENERALIZED' in config.OBJECTIVE_FUNCTION:
+        import downstream_function # Must be defined in run folder
+        chaingrad = downstream_function.downstream_gradient(config,state,step)
+        # Set coefficients for gradients
+        config.OBJ_CHAIN_RULE_COEFF = str(chaingrad[0:5])
+    # Run all-at-once 
     if compute:
         info = SU2.run.adjoint(config)
         state.update(info)
