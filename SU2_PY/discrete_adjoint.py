2e86d3fdf8b1d58354d23c99c6753ac1cce51440
hlkline <kline.hl@gmail.com>
2016-10-04 20:12:14 -0700

    warning added to discrete adjoint: exits if user attempts generalized outflow functional (not implemented)

--- SU2_PY/discrete_adjoint.py
+++ SU2_PY/discrete_adjoint.py
@@ -112,10 +112,8 @@ def discrete_adjoint( filename           ,
 
     # If using chain rule update coefficients using gradients as defined in downstream_function (local file)
     if 'OUTFLOW_GENERALIZED' in config.OBJECTIVE_FUNCTION:
-        import downstream_function # Must be defined in run folder
-        chaingrad = downstream_function.downstream_gradient(config,state,step)
-        # Set coefficients for gradients
-        config.OBJ_CHAIN_RULE_COEFF = str(chaingrad[0:5])
+        raise Exception('Generalized outflow functionals not implemented for discrete adjoint.')
+
     # Run all-at-once 
     if compute:
         info = SU2.run.adjoint(config)
