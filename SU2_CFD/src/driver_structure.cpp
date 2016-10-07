736ccdc34c394c85b35bcc728b482480260a61b6
LaSerpe <giulio.gori@polimi.it>
2016-10-07 15:10:41 +0200

    buxfix and small change

--- SU2_CFD/src/driver_structure.cpp
+++ SU2_CFD/src/driver_structure.cpp
@@ -185,7 +185,7 @@ CDriver::CDriver(char* confFile,
 
   fsi = config_container[ZONE_0]->GetFSI_Simulation();
 
-  if(nZone == SINGLE_ZONE && config_container[iZone]->GetKind_Solver() == FEM_ELASTICITY) {
+  if(nZone == SINGLE_ZONE && config_container[0]->GetKind_Solver() == FEM_ELASTICITY) {
     if (rank == MASTER_NODE) cout << "A single zone driver has been instantiated." << endl;
   }
   else if (config_container[ZONE_0]->GetUnsteady_Simulation() == HARMONIC_BALANCE) {
