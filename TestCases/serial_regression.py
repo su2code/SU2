0da118f73e7456a881ab6bdcc06f9641356b23f4
Francisco Palacios <fpalacios@gmail.com>
2016-10-16 20:43:06 -0700

    updated reg test

--- TestCases/serial_regression.py
+++ TestCases/serial_regression.py
@@ -201,7 +201,7 @@ def main():
     propeller.cfg_dir   = "rans/propeller"
     propeller.cfg_file  = "propeller.cfg"
     propeller.test_iter = 10
-    propeller.test_vals = [0.723141, -4.557869, -0.143910, 0.522880] #last 4 columns
+    propeller.test_vals = [-3.379448, -8.620821, 0.000042, 0.070027] #last 4 columns
     propeller.su2_exec  = "SU2_CFD"
     propeller.timeout   = 3200
     propeller.tol       = 0.00001
