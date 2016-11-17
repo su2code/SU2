e5a4032fac66d4c60c969ea127f91e90b6075aef
Francisco Palacios <fpalacios@gmail.com>
2016-11-17 07:17:34 -0800

    Previous version

--- SU2_PY/set_ffd_design_var.py
+++ SU2_PY/set_ffd_design_var.py
@@ -3,14 +3,21 @@
 ## \file set_ffd_design_var.py.py
 #  \brief Python script for automatically generating a list of FFD variables.
 #  \author T. Economon, F. Palacios
-#  \version 4.2.0 "Cardinal"
+#  \version 4.3.0 "Cardinal"
 #
 #
-# ACDC (Advanced Concepts Design Code) v3.3.0
-# Advanced Concepts | Product Development | Commercial Airplanes
-# The Boeing Company
+# SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+#                      Dr. Thomas D. Economon (economon@stanford.edu).
 #
-# ACDC is built on top of the open-source code SU2 v4.1.2 "Cardinal".
+# SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+#                 Prof. Piero Colonna's group at Delft University of Technology.
+#                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+#                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+#                 Prof. Rafael Palacios' group at Imperial College London.
+#                 Prof. Edwin van der Weide's group at the University of Twente.
+#                 Prof. Vincent Terrapon's group at the University of Liege.
+#
+# Copyright (C) 2012-2016 SU2, the open-source CFD code.
 #
 # SU2 is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -40,8 +47,6 @@ parser.add_option("-b", "--ffdid", dest="ffd_id", default=0,
                   help="ID of the FFD box", metavar="FFD_ID")
 parser.add_option("-m", "--marker", dest="marker",
                   help="marker name of the design surface", metavar="MARKER")
-parser.add_option("-a", "--axis", dest="axis",
-                  help="axis to define twist 'x_Orig, y_Orig, z_Orig, x_End, y_End, z_End'", metavar="AXIS")
 parser.add_option("-s", "--scale", dest="scale", default=1.0,
                   help="scale factor for the bump functions", metavar="SCALE")
 parser.add_option("-d", "--dimension", dest="dimension", default=3.0,
@@ -53,6 +58,7 @@ parser.add_option("-y", "--yMovement", dest="yMove", default=0.0,
 parser.add_option("-z", "--zMovement", dest="zMove", default=0.0,
                   help="movement in z direction", metavar="ZMOVEMENT")
 
+
 (options, args)=parser.parse_args()
 
 # Process options
@@ -61,7 +67,6 @@ options.jOrder  = int(options.jDegree) + 1
 options.kOrder  = int(options.kDegree) + 1
 options.ffd_id  = str(options.ffd_id)
 options.marker = str(options.marker)
-options.axis = str(options.axis)
 options.scale  = float(options.scale)
 options.dim  = int(options.dimension)
 options.xMove = float(options.xMove)
@@ -71,42 +76,8 @@ options.zMove = float(options.zMove)
 if options.dim == 3:
   
   print " "
-  print "% FFD_CONTROL_POINT (X)"
+  print "FFD_CONTROL_POINT"
 
-  iVariable = 0
-  dvList = "DEFINITION_DV= "
-  for kIndex in range(options.kOrder):
-    for jIndex in range(options.jOrder):
-      for iIndex in range(options.iOrder):
-        iVariable = iVariable + 1
-        dvList = dvList + "( 7, " + str(options.scale) + " | " + options.marker + " | "
-        dvList = dvList + options.ffd_id + ", " + str(iIndex) + ", " + str(jIndex) + ", " + str(kIndex) + ", 1.0, 0.0, 0.0 )"
-        if iVariable < (options.iOrder*(options.jOrder)*options.kOrder):
-          dvList = dvList + "; "
-
-
-  print dvList
-
-  print " "
-  print "% FFD_CONTROL_POINT (Y)"
-  
-  iVariable = 0
-  dvList = "DEFINITION_DV= "
-  for kIndex in range(options.kOrder):
-    for jIndex in range(options.jOrder):
-      for iIndex in range(options.iOrder):
-        iVariable = iVariable + 1
-        dvList = dvList + "( 7, " + str(options.scale) + " | " + options.marker + " | "
-        dvList = dvList + options.ffd_id + ", " + str(iIndex) + ", " + str(jIndex) + ", " + str(kIndex) + ", 0.0, 1.0, 0.0 )"
-        if iVariable < (options.iOrder*(options.jOrder)*options.kOrder):
-          dvList = dvList + "; "
-
-
-  print dvList
-
-  print " "
-  print "% FFD_CONTROL_POINT (Z)"
-  
   iVariable = 0
   dvList = "DEFINITION_DV= "
   for kIndex in range(options.kOrder):
@@ -122,58 +93,7 @@ if options.dim == 3:
   print dvList
 
   print " "
-  print "% FFD_NACELLE (RHO)"
-
-  iVariable = 0
-  dvList = "DEFINITION_DV= "
-  for kIndex in range(options.kOrder):
-    for jIndex in range(1+options.jOrder/2):
-      for iIndex in range(options.iOrder):
-        iVariable = iVariable + 1
-        dvList = dvList + "( 22, " + str(options.scale) + " | " + options.marker + " | "
-        dvList = dvList + options.ffd_id + ", " + str(iIndex) + ", " + str(jIndex) + ", " + str(kIndex) + ", 1.0, 0.0 )"
-        if iVariable < (options.iOrder*(1+options.jOrder/2)*options.kOrder):
-          dvList = dvList + "; "
-
-
-  print dvList
-
-  print " "
-  print "% FFD_NACELLE (PHI)"
-  
-  iVariable = 0
-  dvList = "DEFINITION_DV= "
-  for kIndex in range(options.kOrder):
-    for jIndex in range(1+options.jOrder/2):
-      for iIndex in range(options.iOrder):
-        iVariable = iVariable + 1
-        dvList = dvList + "( 22, " + str(options.scale) + " | " + options.marker + " | "
-        dvList = dvList + options.ffd_id + ", " + str(iIndex) + ", " + str(jIndex) + ", " + str(kIndex) + ", 0.0, 1.0 )"
-        if iVariable < (options.iOrder*(1+options.jOrder/2)*options.kOrder):
-          dvList = dvList + "; "
-
-
-  print dvList
-
-  print " "
-  print "% FFD_CONTROL_POINT (Z) (MULTIPLE INTERSECTIONS)"
-
-  iVariable = 0
-  dvList = "DEFINITION_DV= "
-  for kIndex in range(options.kOrder-4):
-    for jIndex in range(options.jOrder-4):
-      for iIndex in range(options.iOrder-4):
-        iVariable = iVariable + 1
-        dvList = dvList + "( 7, " + str(options.scale) + " | " + options.marker + " | "
-        dvList = dvList + options.ffd_id + ", " + str(iIndex+2) + ", " + str(jIndex+2) + ", " + str(kIndex+2) + ", 0.0, 0.0, 1.0 )"
-        if iVariable < (options.iOrder*(options.jOrder)*options.kOrder):
-          dvList = dvList + "; "
-
-
-  print dvList
-
-  print " "
-  print "% FFD_CAMBER, FFD_THICKNESS, FFS_TWIST"
+  print "FFD_CAMBER & FFD_THICKNESS"
 
   iVariable = 0
   dvList = "DEFINITION_DV= "
@@ -189,14 +109,8 @@ if options.dim == 3:
       iVariable = iVariable + 1
       dvList = dvList + "( 12, " + str(options.scale) + " | " + options.marker + " | "
       dvList = dvList + options.ffd_id + ", " + str(iIndex) + ", " + str(jIndex) + " )"
-      dvList = dvList + "; "
-  iVariable = 0
-  for jIndex in range(options.jOrder):
-    iVariable = iVariable + 1
-    dvList = dvList + "( 19, " + str(options.scale) + " | " + options.marker + " | "
-    dvList = dvList + options.ffd_id + ", " + str(jIndex) + ", " + options.axis + " )"
-    if iVariable < (options.jOrder):
-      dvList = dvList + "; "
+      if iVariable < (options.iOrder*(options.jOrder)):
+        dvList = dvList + "; "
 
   print dvList
 
@@ -215,7 +129,7 @@ if options.dim == 2:
   print dvList
 
   print " "
-  print "% FFD_CAMBER & FFD_THICKNESS"
+  print "FFD_CAMBER & FFD_THICKNESS"
 
   iVariable = 0
   dvList = "DEFINITION_DV= "
