94db9c547de5923d9443c9984a582f609aeaf1ec
hlkline <kline.hl@gmail.com>
2016-11-28 11:12:13 -0800

    added custom MPI command set by user-defined environment variable

--- SU2_PY/SU2/run/interface.py
+++ SU2_PY/SU2/run/interface.py
@@ -56,8 +56,13 @@ slurm_job = os.environ.has_key('SLURM_JOBID')
 #check for tacc
 tacc_job = os.environ.has_key('TACC_PUBLIC_MACHINE')
 
+# Check for custom mpi command
+user_defined = os.environ.has_key('SU2_MPI_COMMAND')
+
 # set mpi command
-if slurm_job:
+if user_defined:
+    mpi_Command = os.environ['SU2_MPI_COMMAND']
+elif slurm_job:
     mpi_Command = 'srun -n %i %s'
     if tacc_job:
         mpi_Command = 'ibrun -o 0 -n %i %s'
