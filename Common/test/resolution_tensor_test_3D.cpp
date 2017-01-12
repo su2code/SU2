58b651c8190e76a56dad8bf3c2f6e6ea18267079
clarkpede <clarkp@utexas.edu>
2017-01-12 10:33:37 -0600

    Added a fully-functioning 3D version

--- Common/test/resolution_tensor_test_3D.cpp
+++ Common/test/resolution_tensor_test_3D.cpp
@@ -42,46 +42,71 @@
 #include "config_structure.hpp"
 #include "../../Common/include/geometry_structure.hpp"
 
-inline su2double dot_prod(su2double v[2], su2double w[2]) {
-  return v[0]*w[0] + v[1]*w[1];
+template<std::size_t nDim>
+inline su2double dot_prod(su2double v[nDim], su2double w[nDim]) {
+  su2double dot_product = 0.0;
+  for (unsigned int iDim = 0; iDim < nDim; ++iDim) {
+    dot_product += v[iDim]*w[iDim];
+  }
+  return dot_product;
 }
 
+template<std::size_t nDim>
 inline su2double dot_prod(vector<su2double> v, vector<su2double> w) {
-  return v[0]*w[0] + v[1]*w[1];
+  su2double dot_product = 0.0;
+  for (unsigned int iDim = 0; iDim < nDim; ++iDim) {
+    dot_product += v[iDim]*w[iDim];
+  }
+  return dot_product;
 }
 
-inline su2double magnitude(su2double v[2]) {
-  return std::sqrt(dot_prod(v,v));
+template<std::size_t nDim>
+inline su2double magnitude(su2double v[nDim]) {
+  return std::sqrt(dot_prod<nDim>(v,v));
 }
 
-inline void print_matrix(su2double v[2][2]) {
-  std::cout << "[[" << v[0][0] << "," << v[0][1] << "],[";
-  std::cout << v[1][0] << "," << v[1][1] << "]]" << std::endl;
+template<std::size_t nDim>
+inline void print_matrix(su2double v[nDim][nDim]) {
+  std::cout << "[[";
+  for (unsigned int iDim = 0; iDim < nDim; ++iDim) {
+    for (unsigned int jDim = 0; jDim < nDim; ++jDim) {
+      std::cout << v[iDim][jDim];
+      if (jDim != nDim-1) std::cout << ",";
+    }
+    if (iDim != nDim-1)  {
+      std::cout << "],[";
+    } else {
+      std::cout << "]]" << std::endl;
+    }
+  }
 }
 
-void GramSchmidt(su2double w[2][2], su2double v[2][2]) {
+template<std::size_t nDim>
+void GramSchmidt(su2double w[nDim][nDim], su2double v[nDim][nDim]) {
   unsigned short iDim, jDim;
 
   // Set the first basis vector to the first input vector
-  for (iDim = 0; iDim < 2; ++iDim) {
+  for (iDim = 0; iDim < nDim; ++iDim) {
     v[0][iDim] = w[0][iDim];
   }
 
   // Compute the next orthogonal vector
-  for (iDim = 0; iDim < 2; ++iDim) {
-    v[1][iDim] = w[1][iDim] - dot_prod(w[1],v[0])/dot_prod(v[0],v[0])*v[0][iDim];
+  for (iDim = 0; iDim < nDim; ++iDim) {
+    v[1][iDim] = w[1][iDim] - dot_prod<nDim>(w[1],v[0])
+        /dot_prod<nDim>(v[0],v[0])*v[0][iDim];
   }
 
 }
 
-class Test2DElem : public CPrimalGrid {
+class Test3DElem : public CPrimalGrid {
  protected:
   su2double **Mij;
-  static const unsigned short nFaces = 4;
+  su2double **M2;
+  static const unsigned short nFaces = 6;
  public:
-  Test2DElem() : CPrimalGrid() {
+  Test3DElem() : CPrimalGrid() {
     unsigned short iDim, iFace;
-    nDim = 2;
+    nDim = 3;
     /*--- Allocate CG coordinates ---*/
     Coord_CG = new su2double[nDim];
     for (iDim = 0; iDim < nDim; iDim++)
@@ -90,16 +115,27 @@ class Test2DElem : public CPrimalGrid {
     for (iFace = 0; iFace < nFaces; iFace++) {
       Coord_FaceElems_CG[iFace] = new su2double [nDim];
     }
-    Coord_FaceElems_CG[0][0] =  8.0;
+    // The order here is scrambled, as a check.
+    Coord_FaceElems_CG[1][0] =  2.0;
+    Coord_FaceElems_CG[1][1] =  0.0;
+    Coord_FaceElems_CG[1][2] =  0.0;
+    Coord_FaceElems_CG[0][0] = -2.0;
     Coord_FaceElems_CG[0][1] =  0.0;
-    Coord_FaceElems_CG[2][0] = -8.0;
-    Coord_FaceElems_CG[2][1] =  0.0;
-    Coord_FaceElems_CG[1][0] =  0.0;
-    Coord_FaceElems_CG[1][1] = -2.0;
+    Coord_FaceElems_CG[0][2] =  0.0;
+    Coord_FaceElems_CG[5][0] =  0.0;
+    Coord_FaceElems_CG[5][1] =  1.0;
+    Coord_FaceElems_CG[5][2] =  0.0;
     Coord_FaceElems_CG[3][0] =  0.0;
-    Coord_FaceElems_CG[3][1] =  2.0;
+    Coord_FaceElems_CG[3][1] = -1.0;
+    Coord_FaceElems_CG[3][2] =  0.0;
+    Coord_FaceElems_CG[4][0] =  0.0;
+    Coord_FaceElems_CG[4][1] =  0.0;
+    Coord_FaceElems_CG[4][2] =  0.5;
+    Coord_FaceElems_CG[2][0] =  0.0;
+    Coord_FaceElems_CG[2][1] =  0.0;
+    Coord_FaceElems_CG[2][2] = -0.5;
   };
-  ~Test2DElem(void) {};
+  ~Test3DElem(void) {};
 
   //---------------------------------------------------------------------------
   void SetResolutionTensor(void) {
@@ -111,12 +147,16 @@ class Test2DElem : public CPrimalGrid {
     // sort them so that the face indices in paired_faces[0] and paired_faces[1]
     // match, then paired_faces[2] and paired_faces[3] match, etc.
 
+
     // Allocate Mij
     Mij = new su2double* [nDim];
+    M2  = new su2double* [nDim];
     for (iDim = 0; iDim < nDim; iDim++) {
       Mij[iDim] = new su2double [nDim];
+      M2 [iDim] = new su2double [nDim];
       for (jDim = 0; jDim < nDim; ++jDim) {
         Mij[iDim][jDim] = 0.0;
+        M2 [iDim][jDim] = 0.0;
       }
     }
 
@@ -132,42 +172,70 @@ class Test2DElem : public CPrimalGrid {
       }
     }
 
+    //--------------------------------------------------------------------------
     // First vector
     paired_faces[0] = 0; // Choose vector 1 as our first vector to pair up
     // Find vector mostly parallel to first
     su2double min_dp = 1.0;
     su2double current_dp;
     for (iFace = 1; iFace < nFaces; ++iFace) {
-      current_dp = dot_prod(center2face[0],center2face[iFace])
-          /(magnitude(center2face[0])*magnitude(center2face[1]));
+      // NOTE: You cannot use nDim in the template functions for the array size;
+      // nDim is not const
+      current_dp = dot_prod<3>(center2face[0],center2face[iFace])
+          /(magnitude<3>(center2face[0])*magnitude<3>(center2face[iFace]));
       if (current_dp < min_dp) {
         min_dp = current_dp;
         paired_faces[1] = iFace;
       }
     }
 
-    for (iDim = 0; iDim < nDim; ++iDim) {
-      eigvecs[0][iDim] = Coord_FaceElems_CG[0][iDim] -
-          Coord_FaceElems_CG[paired_faces[1]][iDim];
-    }
-
+    //--------------------------------------------------------------------------
     // Second vector
-    paired_faces[2] = 0;
-    paired_faces[3] = 0;
     for (iFace = 1; iFace < nFaces; ++iFace) {
       if (iFace != paired_faces[1]) {
-        if (paired_faces[2] == 0) {
-          paired_faces[2] = iFace;
+        paired_faces[2] = iFace;
+        break;
+      }
+    }
+
+    min_dp = 1.0;
+    for (iFace = 1; iFace < nFaces; ++iFace) {
+      // NOTE: You cannot use nDim in the template functions for the array size;
+      // nDim is not const
+      if (iFace == paired_faces[1]) continue;
+      current_dp = dot_prod<3>(center2face[paired_faces[2]],center2face[iFace])
+          /(magnitude<3>(center2face[paired_faces[2]])
+              *magnitude<3>(center2face[1]));
+      if (current_dp < min_dp) {
+        min_dp = current_dp;
+        paired_faces[3] = iFace;
+      }
+    }
+
+    //--------------------------------------------------------------------------
+    // Third vector
+    paired_faces[4] = 0;
+    paired_faces[5] = 0;
+    for (iFace = 1; iFace < nFaces; ++iFace) {
+      if (iFace != paired_faces[1] &&
+          iFace != paired_faces[2] &&
+          iFace != paired_faces[3]) {
+        if (paired_faces[4] == 0) {
+          paired_faces[4] = iFace;
         } else {
-          paired_faces[3] = iFace;
+          paired_faces[5] = iFace;
         }
       }
     }
 
+    // Use paired_faces list to build vectors
     for (iDim = 0; iDim < nDim; ++iDim) {
-      eigvecs[1][iDim] = Coord_FaceElems_CG[paired_faces[2]][iDim] -
-          Coord_FaceElems_CG[paired_faces[3]][iDim];
+      for (jDim = 0; jDim < nDim; ++jDim) {
+        eigvecs[jDim][iDim] = Coord_FaceElems_CG[paired_faces[jDim*2]][iDim] -
+            Coord_FaceElems_CG[paired_faces[jDim*2+1]][iDim];
+      }
     }
+
 
     // Normalized vectors
     su2double eigvalues[nDim][nDim];
@@ -175,7 +243,7 @@ class Test2DElem : public CPrimalGrid {
       for (jDim = 0; jDim < nDim; ++jDim) {
         eigvalues[iDim][jDim] = 0.0;
       }
-      eigvalues[iDim][iDim] = magnitude(eigvecs[iDim]);
+      eigvalues[iDim][iDim] = magnitude<3>(eigvecs[iDim]);
       for (jDim = 0; jDim < nDim; ++jDim) {
         eigvecs[iDim][jDim] /= eigvalues[iDim][iDim];
       }
@@ -188,15 +256,18 @@ class Test2DElem : public CPrimalGrid {
         temp_eigvecs[iDim][jDim] = eigvecs[iDim][jDim];
       }
     }
-    GramSchmidt(temp_eigvecs, eigvecs);
+    GramSchmidt<3>(temp_eigvecs, eigvecs);
 
     // Perform matrix multiplication
     for (iDim = 0; iDim < nDim; ++iDim) {
       for (jDim = 0; jDim < nDim; ++jDim) {
         for (kDim = 0; kDim < nDim; ++kDim) {
           for (lDim = 0; lDim < nDim; ++lDim) {
-            Mij[iDim][jDim] += eigvecs[kDim][iDim]*
-                std::sqrt(eigvalues[kDim][lDim])*eigvecs[lDim][jDim];
+            Mij[iDim][jDim] += eigvecs[kDim][iDim]
+                *eigvalues[kDim][lDim]*eigvecs[lDim][jDim];
+            M2[iDim][jDim]  += eigvecs[kDim][iDim]
+                *(eigvalues[kDim][lDim]*eigvalues[kDim][lDim])
+                *eigvecs[lDim][jDim];
           }
         }
       }
@@ -210,6 +281,16 @@ class Test2DElem : public CPrimalGrid {
     for (unsigned short iDim = 0; iDim < nDim; ++iDim) {
       for (unsigned short jDim = 0; jDim < nDim; ++jDim) {
         output[iDim][jDim] = Mij[iDim][jDim];
+      }
+    }
+    return output;
+  }
+
+  vector<vector<su2double> > GetResolutionTensorSquared(void) {
+    vector<vector<su2double> > output(nDim, vector<su2double>(nDim));
+    for (unsigned short iDim = 0; iDim < nDim; ++iDim) {
+      for (unsigned short jDim = 0; jDim < nDim; ++jDim) {
+        output[iDim][jDim] = M2 [iDim][jDim];
       }
     }
     return output;
@@ -236,45 +317,6 @@ class Test2DElem : public CPrimalGrid {
 
 };
 
-void ReduceVectors(su2double (&vecs_in)[6][3], su2double (&vecs_out)[3][3]) {
-  // Use the first vector
-  for (int i = 0; i<3; ++i) vecs_out[0][i] = vecs_in[0][i];
-
-  // Find the vectors that are the closest to being orthogonal
-  // FIXME: The second vector must be close to orthogonal with the third
-  su2double norm_0 = 0.0;
-  norm_0 = dot_prod(vecs_in[0], vecs_in[0]);
-  su2double min_dp = norm_0;
-  su2double second_dp = norm_0;
-  int min_dp_index, second_dp_index;
-  su2double dot_product;
-  for (int i = 1; i<6; ++i) {
-    dot_product = dot_prod(vecs_in[0], vecs_in[i]);
-    if (std::abs(dot_product) < min_dp) {
-      min_dp = dot_product;
-      min_dp_index = i;
-    } else if (std::abs(dot_product) < second_dp) {
-      second_dp = dot_product;
-      second_dp_index = i;
-    }
-  }
-
-  // Construct the second vector
-  su2double factor0 = min_dp/norm_0;
-  std::cout << factor0 << std::endl;
-  for (int i=0; i<3; ++i) vecs_out[1][i] = vecs_in[min_dp_index][i] - factor0*vecs_in[0][i];
-  su2double norm_1 = dot_prod(vecs_out[1], vecs_out[1]);
-
-  // Construct the third vector
-  factor0 = second_dp/norm_0;
-  su2double factor1 = dot_prod(vecs_in[second_dp_index], vecs_out[1])/norm_1;
-  for (int i = 0; i < 3; ++i) {
-    vecs_out[2][i] = vecs_in[second_dp_index][i];
-    vecs_out[2][i] -= factor0*vecs_out[0][i];
-    vecs_out[2][i] -= factor1*vecs_out[1][i];
-  }
-
-}
 
 int main() {
 
@@ -297,32 +339,16 @@ int main() {
   const su2double eps = std::numeric_limits<su2double>::epsilon();
   su2double tol = 10*eps;
 
-  Test2DElem* elem = new Test2DElem();
-  static int nDim = 2;
+  Test3DElem* elem = new Test3DElem();
+  static const int nDim = 3;
 
   elem->SetResolutionTensor();
   vector<vector<su2double> > Mij = elem->GetResolutionTensor();
 
   // ---------------------------------------------------------------------------
-  // Check that the resolution tensor is nonzero
-  su2double sum = 0.0;
-  for (int i=0; i<nDim; ++i) {
-    for (int j=0; j<nDim; ++j) {
-      sum += std::abs(Mij[i][j]);
-    }
-  }
-  if (sum < eps) {
-    std::cout << "ERROR: The resolution tensor for a quadrilateral was not correct."
-        << std::endl;
-    std::cout << "The sum was within machine precision of zero." << std::endl;
-    std::cout << "Sum: " << sum << std::endl;
-    return_flag = 1;
-  }
-
-  // ---------------------------------------------------------------------------
   // Check that all columns of resolution tensor are orthogonal
   su2double dp = 0;
-  dp += dot_prod(Mij[0],Mij[1]);
+  dp += dot_prod<2>(Mij[0],Mij[1]);
 
   if (std::abs(dp) > tol) {
     std::cout << "ERROR: The resolution tensor for a quadrilateral was not correct."
@@ -337,15 +363,22 @@ int main() {
   bool entries_correct = true;
   if (Mij[0][0] != 4.0 ) entries_correct = false;
   if (Mij[0][1] != 0.0 ) entries_correct = false;
+  if (Mij[0][2] != 0.0 ) entries_correct = false;
   if (Mij[1][0] != 0.0 ) entries_correct = false;
   if (Mij[1][1] != 2.0 ) entries_correct = false;
+  if (Mij[1][2] != 0.0 ) entries_correct = false;
+  if (Mij[2][0] != 0.0 ) entries_correct = false;
+  if (Mij[2][1] != 0.0 ) entries_correct = false;
+  if (Mij[2][2] != 1.0 ) entries_correct = false;
 
   if (not(entries_correct)) {
     std::cout << "ERROR: The resolution tensor for a quadrilateral was not correct."
         << std::endl;
     std::cout << "The elements of the array were incorrect." << std::endl;
-    std::cout << "Array elements: [[" << Mij[0][0] << "," << Mij[0][1] << "],[";
-    std::cout << Mij[1][0] << "," << Mij[1][1] << "]]" << std::endl;
+    std::cout << "Array elements: [[";
+    std::cout << Mij[0][0] << "," << Mij[0][1] << "," << Mij[0][2] << "],[";
+    std::cout << Mij[1][0] << "," << Mij[1][1] << "," << Mij[0][2] << "],[";
+    std::cout << Mij[2][0] << "," << Mij[2][1] << "," << Mij[2][2] << "]]" << std::endl;
     return_flag = 1;
   }
 
