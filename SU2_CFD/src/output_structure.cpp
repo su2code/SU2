2765d9ba6a1bd11f048a91483d8172b899a9796c
Francisco Palacios <francisco.d.palacios@boeing.com>
2017-01-18 13:30:07 -0800

    Working version

--- SU2_CFD/src/output_structure.cpp
+++ SU2_CFD/src/output_structure.cpp
@@ -7801,7 +7801,7 @@ void COutput::SetForceSections(CSolver *solver_container, CGeometry *geometry, C
       Plane_Normal[2] = 0.0;    Plane_P0[2] = 0.0;
       
       Plane_Normal[config->GetAxis_Orientation()] = 1.0;
-      Plane_P0[config->GetAxis_Orientation()] = MinPlane + iSection*(MaxPlane - MinPlane)/su2double(nSection-1);
+			Plane_P0[config->GetAxis_Orientation()] = config->GetAirfoilSections(iSection);
       
       /*--- Compute the airfoil sections (note that we feed in the Cp) ---*/
       
