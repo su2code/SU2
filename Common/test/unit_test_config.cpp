abb46fa962df0d8e18ba92818cb3748cca6af71a
clarkpede <clarkp@utexas.edu>
2017-02-23 09:18:46 -0600

    Updated version numbers in files to 5.0.0

--- Common/test/unit_test_config.cpp
+++ Common/test/unit_test_config.cpp
@@ -2,7 +2,7 @@
  * \file unit_test_config.cpp
  * \brief Test CConfig ctor that gives valid config object without input file.
  * \author T. A. Oliver
- * \version 4.3.0 "Cardinal"
+ * \version 5.0.0 "Raven"
  *
  * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
  *                      Dr. Thomas D. Economon (economon@stanford.edu).
--- SU2_CFD/test/noop.cpp
+++ SU2_CFD/test/noop.cpp
@@ -2,7 +2,7 @@
  * \file noop.cpp
  * \brief An example test to help develop infrastructure.  Tests nothing.
  * \author T. A. Oliver
- * \version 4.3.0 "Cardinal"
+ * \version 5.0.0 "Raven"
  *
  * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
  *                      Dr. Thomas D. Economon (economon@stanford.edu).
