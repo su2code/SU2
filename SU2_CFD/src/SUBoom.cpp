adc984706c1514bfb7e5d877743054e0a213d43e
bmunguia <bmunguia@stanford.edu>
2017-04-08 19:27:45 -0700

    Minor changes

--- SU2_CFD/src/SUBoom.cpp
+++ SU2_CFD/src/SUBoom.cpp
@@ -1464,8 +1464,6 @@ void SUBoom::WriteSensitivities(){
 
   nVar = nDim+3;
 
-  if (rank == MASTER_NODE)
-    cout << "Packing sensitivities in each processor." << endl;
   /* pack sensitivity values in each processor and send to root */
   su2double *Buffer_Send_dJdU = new su2double [Max_nSig*nVar];
   unsigned long *Buffer_Send_GlobalIndex = new unsigned long [Max_nSig];
@@ -1495,22 +1493,17 @@ void SUBoom::WriteSensitivities(){
      Buffer_Send_GlobalIndex[iSig] = PointID[iSig];
   }
 
-  if (rank == MASTER_NODE)
-    cout << "Sending sensitivities to root." << endl;
-
 #ifdef HAVE_MPI
   SU2_MPI::Gather(Buffer_Send_dJdU, Max_nSig*nVar, MPI_DOUBLE, Buffer_Recv_dJdU,  Max_nSig*nVar , MPI_DOUBLE, MASTER_NODE, MPI_COMM_WORLD);
   SU2_MPI::Gather(Buffer_Send_GlobalIndex,Max_nSig, MPI_UNSIGNED_LONG, Buffer_Recv_GlobalIndex, Max_nSig , MPI_UNSIGNED_LONG, MASTER_NODE, MPI_COMM_WORLD);
 #endif
 
   if (rank == MASTER_NODE){
-  cout << "Writing sensitivities." << endl;
   Boom_AdjointFile.precision(15);
   Boom_AdjointFile.open("Adj_Boom.dat", ios::out);
 
   /*--- Loop through all of the collected data and write each node's values ---*/
   for (iProcessor = 0; iProcessor < nProcessor; iProcessor++) {
-    cout << "iProcessor = " << iProcessor << endl;
     for (iSig = 0; iSig < Buffer_Recv_nSig[iProcessor]; iSig++) {
         Global_Index = Buffer_Recv_GlobalIndex[iProcessor*Max_nSig+iSig];
         Boom_AdjointFile  << scientific << Global_Index << "\t";
