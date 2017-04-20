bb07b61ca96371c7dfd5e7b3ef54c21eabcdc4c9
Salvatore Vitale <s.vitale@tudelft.nl>
2017-04-20 14:36:58 +0200

    init nSpanwisemaxALL for output

--- Common/src/config_structure.cpp
+++ Common/src/config_structure.cpp
@@ -849,6 +849,8 @@ void CConfig::SetConfig_Options(unsigned short val_iZone, unsigned short val_nZo
   addBoolOption("TURBULENT_MIXINGPLANE", turbMixingPlane, false);
   /*!\brief MARKER_MIXINGPLANE \n DESCRIPTION: Identify the boundaries in which the mixing plane is applied. \ingroup Config*/
   addTurboPerfOption("MARKER_TURBOMACHINERY", nMarker_Turbomachinery, Marker_TurboBoundIn, Marker_TurboBoundOut);
+  /*--- Initialize nspanwise max number for output structure. ---*/
+  nSpanMaxAllZones = 1;
   /* DESCRIPTION: Integer number of spanwise sections to compute 3D BC and Performance for turbomachinery */
   addUnsignedShortOption("NUM_SPANWISE_SECTIONS", nSpanWiseSections_User, 1);
   /* DESCRIPTION: Integer number of spanwise sections to compute 3D BC and Performance for turbomachinery */
