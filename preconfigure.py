8052990dab16895ca3337561a36ef20591d5fb83
Tim Albring <tim.albring@scicomp.uni-kl.de>
2017-07-30 16:16:17 +0200

    Updated preconfigure.py and configure

--- preconfigure.py
+++ preconfigure.py
@@ -284,13 +284,13 @@ def init_codi(argument_dict, modes, mpi_support = False, update = False):
     # The sha tag must be maintained manually to point to the correct commit
     sha_version_codi = '2ec7cccf3ccd4b052f9d4ef95d6dc69244484f13'
     github_repo_codi = 'https://github.com/scicompkl/CoDiPack'
-    sha_version_ampi = '31b2267c3a55a391a37d830369f2e0dba09008d1'
-    github_repo_ampi = 'https://github.com/michel2323/AdjointMPI'
+    sha_version_medi = '3344286560d92aa114b934cea2cba8f302d02d58'
+    github_repo_medi = 'https://github.com/SciCompKL/MeDiPack'
 
-    ampi_name = 'AdjointMPI'
+    medi_name = 'MeDiPack'
     codi_name = 'CoDiPack'
 
-    alt_name_ampi = 'adjointmpi'
+    alt_name_medi = 'medi'
     alt_name_codi = 'codi'
 
     # Some log and error files
@@ -312,76 +312,52 @@ def init_codi(argument_dict, modes, mpi_support = False, update = False):
             print 'Removing' + ' externals/' + alt_name_ampi
             shutil.rmtree('externals/'+alt_name_ampi)
 
-
-    # If project was cloned using git, we can use the 
-    # submodule feature of git to initialize the packages
-    if all([os.path.exists('.gitmodules')]):
-        codi_status = submodule_status('externals/'+alt_name_codi, update)
-        if mpi_support:
-            ampi_status = submodule_status('externals/'+alt_name_ampi, update)
-            modules_failed =  not all([codi_status, ampi_status])
-        else:
-            modules_failed = not codi_status
-    else:
-        print '.gitmodules not found, using fall-back method...'
-
     os.chdir('externals')
-    
-    # If modules still dont exists, use wget to download zip
-    if all([any([not os.path.exists('codi/' + sha_version_codi), update]), modules_failed]):
-        if all([os.path.exists('codi'), not os.path.exists('codi/' + sha_version_codi)]):
-            print 'Found an old or unspecified version of CoDiPack in externals/codi.\nUse -u to reset module.'
-            sys.exit()
 
-        download_module(codi_name, alt_name_codi, github_repo_codi, sha_version_codi, log, err)
-    elif modules_failed:
-        print 'Found correct version of CoDiPack in externals/codi.'
-         
-    if all([any([not os.path.exists('adjointmpi/' + sha_version_ampi), update]), mpi_support, modules_failed]):
-        if all([os.path.exists('adjointmpi'), not os.path.exists('adjointmpi/' + sha_version_ampi)]):
-            print 'Found an old or unspecified version of AdjointMPI in externals/adjointmpi.\nUse -u to reset module.'
-            sys.exit()
+    submodule_check(codi_name, alt_name_codi, github_repo_codi, sha_version_codi, log, err, update)
 
-        download_module(ampi_name, alt_name_ampi, github_repo_ampi, sha_version_ampi, log, err)
-
-    elif modules_failed:
-        print 'Found correct version of AdjointMPI in externals/adjointmpi.'
-
-
-    # Build AdjointMPI if MPI Support is requested. If a C compiler was specified as an argument use it here.
     if mpi_support:
-        os.chdir('adjointmpi')
-        configure_ampi = './configure --prefix=' + os.getcwd().rstrip()
-        if any([not os.path.exists('libAMPI.a'), update]):
-            if not argument_dict.get('--with-cc', ' ') == ' ':
-                configure_ampi = configure_ampi + ' --with-mpicc=' + argument_dict['--with-cc']
-            print '\nConfiguring and building AdjointMPI in externals/' + alt_name_ampi
-            print '====================================================================='
-            run_command(configure_ampi + ' && make clean && make', os.getcwd().rstrip() + '/preconf_ampi.log', os.getcwd().rstrip() + '/preconf_ampi.err', os.environ)
-        os.chdir(os.pardir)
+        submodule_check(medi_name, alt_name_medi, github_repo_medi, sha_version_medi, log, err, update)
 
     os.chdir(os.pardir)
 
     return pkg_environ, True
+
+def submodule_check(name, alt_name, github_rep, sha_tag, log, err, update = False):
+
+    try:
+        status = submodule_status(alt_name, update)
+        if status:
+            print 'Found correct version of ' + name + ' in externals/' + alt_name + '.'
+
+    except RuntimeError:
+        if all([os.path.exists(alt_name), not os.path.exists(alt_name + '/' + sha_tag)]):
+          print 'Found an old or unspecified version of ' + name + ' in externals/' + alt_name + '.\nUse -u to reset module.'
+          sys.exit()
+        if not os.path.exists(alt_name):
+          print '\ngit command failed (either git is not installed or this is not a git repository).'
+          print '\nUsing fall-back method to initialize submodule ' + name
+          download_module(name, alt_name, github_rep, sha_tag, log, err)
+        else:
+          print 'Found correct version of ' + name + ' in externals/' + alt_name + '.'
+
 
 def submodule_status(path, update):
 
     try:
         status = check_output('git submodule status ' + path)
     except RuntimeError:
-        return False
-
+        raise RuntimeError
 
     status_indicator = status[0][0]
 
     if status_indicator == '+':
         sys.stderr.write('WARNING: the currently checked out submodule commit in ' + path + ' does not match the SHA-1 found in the index.\n')
         sys.stderr.write('Use \'git submodule update --init '+ path + '\' to reset the module if necessary.\n')
+        return False
     elif any([status_indicator == '-', update]):
         print 'Initialize submodule ' + path + ' using git ... '
         subprocess.check_call('git submodule update --init ' + path, shell = True)
-    elif status_indicator == ' ':
-        print 'Found submodule ' + path + '.'
 
     return True
 
