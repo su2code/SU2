641b7866b8a472d27b86d0e311cb918df1ba0a89
Salvatore Vitale <s.vitale@tudelft.nl>
2017-08-16 15:03:58 +0200

    modify computation of entropy Gen [ci skip]

--- SU2_CFD/src/output_physics.cpp
+++ SU2_CFD/src/output_physics.cpp
@@ -42,7 +42,7 @@ void COutput::ComputeTurboPerformance(CSolver *solver_container, CGeometry *geom
   unsigned short nMarkerTP = config->GetnMarker_Turbomachinery();
   FluidModel = solver_container->GetFluidModel();
   su2double area, absVel2, soundSpeed, mach, tangVel, tangVel2, *relVel, relVel2;
-  su2double relPressureIn, relPressureOut, enthalpyOutIs, relVelOutIs2;
+  su2double relPressureIn, relPressureOut, enthalpyOutIs, relVelOutIs2, VelSpout_T;
   relVel = new su2double[nDim];
   su2double muLam, kine, omega, nu;
   bool turbulent = ((config->GetKind_Solver() == RANS) || (config->GetKind_Solver() == DISC_ADJ_RANS));
@@ -63,6 +63,11 @@ void COutput::ComputeTurboPerformance(CSolver *solver_container, CGeometry *geom
       FluidModel->SetTDState_PT(config->GetTotalPressureIn_BC(), config->GetTotalTemperatureIn_BC());
       TotalEnthalpyIn_BC[iMarkerTP][iSpan] = FluidModel->GetStaticEnergy()+ FluidModel->GetPressure()/FluidModel->GetDensity();
       EntropyIn_BC[iMarkerTP][iSpan]       = FluidModel->GetEntropy();
+      if (iSpan == config->GetnSpan_iZones(iMarkerTP)){
+        FluidModel->SetTDState_Ps(PressureOut_BC[iMarkerTP][iSpan],EntropyIn_BC[iMarkerTP][iSpan]);
+        enthalpyOutIs = FluidModel->GetStaticEnergy() + PressureOut_BC[iMarkerTP][iSpan]/FluidModel->GetDensity();
+        VelSpout_T = sqrt(2*(TotalEnthalpyIn_BC[iMarkerTP][iSpan] - enthalpyOutIs))/config->GetTotalPressureIn_BC();
+      }
     }
   }
 
@@ -239,7 +244,7 @@ void COutput::ComputeTurboPerformance(CSolver *solver_container, CGeometry *geom
       }
 
       /*--- TURBO-PERFORMANCE---*/
-      EntropyGen[iMarkerTP][iSpan]         = (EntropyOut[iMarkerTP][iSpan] - EntropyIn[iMarkerTP][iSpan])/abs(EntropyIn_BC[iMarkerTP][iSpan] + 1);
+      EntropyGen[iMarkerTP][iSpan]         = (EntropyOut[iMarkerTP][iSpan] - EntropyIn[iMarkerTP][iSpan])/VelSpout_T;
       EulerianWork[iMarkerTP][iSpan]       = TotalEnthalpyIn[iMarkerTP][iSpan] - TotalEnthalpyOut[iMarkerTP][iSpan];
       TotalPressureLoss[iMarkerTP][iSpan]  = (relPressureIn - relPressureOut)/(relPressureIn - PressureOut[iMarkerTP][iSpan]);
       KineticEnergyLoss[iMarkerTP][iSpan]  = 2*(EnthalpyOut[iMarkerTP][iSpan] - enthalpyOutIs)/relVelOutIs2;
