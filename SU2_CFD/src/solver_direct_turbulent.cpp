43d69c4cc86aa0f8e942314fc7cfab2511f9225c
clarkpede <clarkp@utexas.edu>
2017-09-08 15:41:37 -0500

    Fixed the restart files so they run without segfaults

--- SU2_CFD/src/solver_direct_turbulent.cpp
+++ SU2_CFD/src/solver_direct_turbulent.cpp
@@ -3805,35 +3805,37 @@ CTurbKESolver::CTurbKESolver(CGeometry *geometry, CConfig *config,
 
     /*--- In case this is a parallel simulation, we need to perform the
      Global2Local index transformation first. ---*/
-    long *Global2Local;
-    Global2Local = new long[geometry->GetGlobal_nPointDomain()];
-    /*--- First, set all indices to a negative value by default ---*/
-    for (iPoint = 0; iPoint < geometry->GetGlobal_nPointDomain(); iPoint++) {
-      Global2Local[iPoint] = -1;
-    }
+
+    map<unsigned long,unsigned long> Global2Local;
+    map<unsigned long,unsigned long>::const_iterator MI;
+
     /*--- Now fill array with the transform values only for local points ---*/
     for (iPoint = 0; iPoint < nPointDomain; iPoint++) {
       Global2Local[geometry->node[iPoint]->GetGlobalIndex()] = iPoint;
     }
 
     /*--- Read all lines in the restart file ---*/
-    long iPoint_Local; unsigned long iPoint_Global = 0; string text_line;
+    long iPoint_Local; unsigned long iPoint_Global = 0; string text_line; unsigned long iPoint_Global_Local = 0;
+    unsigned short rbuf_NotMatching = 0, sbuf_NotMatching = 0;
 
     /*--- The first line is the header ---*/
     getline (restart_file, text_line);
 
-    while (getline (restart_file, text_line)) {
+    for (iPoint_Global = 0; iPoint_Global < geometry->GetGlobal_nPointDomain(); iPoint_Global++ ) {
+
+      getline (restart_file, text_line);
+
       istringstream point_line(text_line);
 
       /*--- Retrieve local index. If this node from the restart file lives
-       on a different processor, the value of iPoint_Local will be -1.
-       Otherwise, the local index for this node on the current processor
-       will be returned and used to instantiate the vars. ---*/
-      iPoint_Local = Global2Local[iPoint_Global];
-      if (iPoint_Local >= 0) {
+       on the current processor, we will load and instantiate the vars. ---*/
+
+      MI = Global2Local.find(iPoint_Global);
+      if (MI != Global2Local.end()) {
+
+        iPoint_Local = Global2Local[iPoint_Global];
 
         if (compressible) {
-
           if (nDim == 2) {
             point_line >> index
                        >> dull_val >> dull_val >> dull_val
@@ -3841,7 +3843,6 @@ CTurbKESolver::CTurbKESolver(CGeometry *geometry, CConfig *config,
                        >> Solution[0] >> Solution[1]
                        >> Solution[2] >> Solution[3];
           }
-
           if (nDim == 3) {
             point_line >> index
                        >> dull_val >> dull_val >> dull_val >> dull_val
@@ -3849,17 +3850,15 @@ CTurbKESolver::CTurbKESolver(CGeometry *geometry, CConfig *config,
                        >> Solution[0] >> Solution[1]
                        >> Solution[2] >> Solution[3];
           }
-
         }
-
         if (incompressible) {
           cout << "WARNING: Have not tested v2-f with incompressible!!" << endl;
           cout << "         Proceed at your own risk!!                " << endl;
 
           if (nDim == 2) {
             point_line >> index
-                       >> dull_val >> dull_val >> dull_val >> dull_val
                        >> dull_val >> dull_val >> dull_val
+                       >> dull_val >> dull_val
                        >> Solution[0] >> Solution[1]
                        >> Solution[2] >> Solution[3];
           }
@@ -3867,8 +3866,7 @@ CTurbKESolver::CTurbKESolver(CGeometry *geometry, CConfig *config,
           if (nDim == 3) {
             point_line >> index
                        >> dull_val >> dull_val >> dull_val >> dull_val
-                       >> dull_val >> dull_val >> dull_val >> dull_val
-                       >> dull_val
+                       >> dull_val >> dull_val >> dull_val
                        >> Solution[0] >> Solution[1]
                        >> Solution[2] >> Solution[3];
           }
@@ -3876,11 +3874,35 @@ CTurbKESolver::CTurbKESolver(CGeometry *geometry, CConfig *config,
 
         /*--- Instantiate the solution at this node, note that the muT_Inf should recomputed ---*/
         node[iPoint_Local] = new CTurbKEVariable(Solution[0], Solution[1],
-                                                 Solution[2], Solution[3],
-                                                 muT_Inf, Tm_Inf, Lm_Inf,
-                                                 nDim, nVar, constants, config);
+                                                         Solution[2], Solution[3],
+                                                         muT_Inf, Tm_Inf, Lm_Inf,
+                                                         nDim, nVar, constants, config);
+        iPoint_Global_Local++;
       }
-      iPoint_Global++;
+
+    }
+
+    /*--- Detect a wrong solution file ---*/
+
+    if (iPoint_Global_Local < nPointDomain) { sbuf_NotMatching = 1; }
+
+#ifndef HAVE_MPI
+    rbuf_NotMatching = sbuf_NotMatching;
+#else
+    SU2_MPI::Allreduce(&sbuf_NotMatching, &rbuf_NotMatching, 1, MPI_UNSIGNED_SHORT, MPI_SUM, MPI_COMM_WORLD);
+#endif
+    if (rbuf_NotMatching != 0) {
+      if (rank == MASTER_NODE) {
+        cout << endl << "The solution file " << filename.data() << " doesn't match with the mesh file!" << endl;
+        cout << "It could be empty lines at the end of the file." << endl << endl;
+      }
+#ifndef HAVE_MPI
+      exit(EXIT_FAILURE);
+#else
+      MPI_Barrier(MPI_COMM_WORLD);
+      MPI_Abort(MPI_COMM_WORLD,1);
+      MPI_Finalize();
+#endif
     }
 
     /*--- Instantiate the variable class with an arbitrary solution
@@ -3895,9 +3917,6 @@ CTurbKESolver::CTurbKESolver(CGeometry *geometry, CConfig *config,
 
     /*--- Close the restart file ---*/
     restart_file.close();
-
-    /*--- Free memory needed for the transformation ---*/
-    delete [] Global2Local;
   }
 
   /*--- MPI solution ---*/
