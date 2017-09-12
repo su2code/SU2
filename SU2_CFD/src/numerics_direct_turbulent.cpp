af9ac63ca070ada90f8dad56580c991d4c215c8f
Jayant Mukhopadhaya <jmukho@stanford.edu>
2017-09-12 04:02:39 -0700

    Production term fix

--- SU2_CFD/src/numerics_direct_turbulent.cpp
+++ SU2_CFD/src/numerics_direct_turbulent.cpp
@@ -1219,7 +1219,8 @@ void CSourcePieceWise_TurbSST::ComputeResidual(su2double *val_residual, su2doubl
     diverg = 0.0;
     for (iDim = 0; iDim < nDim; iDim++)
       diverg += PrimVar_Grad_i[iDim+1][iDim];
-    
+
+    pk = Eddy_Viscosity_i*StrainMag_i*StrainMag_i - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
     if (config->GetUsing_UQ()){
       SetReynoldsStressMatrix(TurbVar_i[0]);
       SetPerturbedRSM(TurbVar_i[0], config->GetEig_Val_Comp(),config->GetBeta_Delta());
@@ -1227,14 +1228,15 @@ void CSourcePieceWise_TurbSST::ComputeResidual(su2double *val_residual, su2doubl
       pk = Eddy_Viscosity_i*PerturbedStrainMag*PerturbedStrainMag
               - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
     }
-    else {
-        pk = Eddy_Viscosity_i*StrainMag_i*StrainMag_i - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
-    }
+
     pk = min(pk,20.0*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]);
     pk = max(pk,0.0);
     
     zeta = max(TurbVar_i[1], StrainMag_i*F2_i/a1);
     pw = StrainMag_i*StrainMag_i - 2.0/3.0*zeta*diverg;
+    if (config->GetUsing_UQ()){
+        pw = PerturbedStrainMag * PerturbedStrainMag - 2.0/3.0*zeta*diverg;
+    }
     pw = max(pw,0.0);
     
     val_residual[0] += pk*Volume;
