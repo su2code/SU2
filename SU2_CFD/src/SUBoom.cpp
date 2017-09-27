4ca812cda9507769474ee33c50bc9eb2752114a6
bmunguia <bmunguia10@gmail.com>
2017-09-27 12:59:27 -0700

    Modified logic in SearchLinear

--- SU2_CFD/src/SUBoom.cpp
+++ SU2_CFD/src/SUBoom.cpp
@@ -557,21 +557,30 @@ void SUBoom::SearchLinear(CConfig *config, CGeometry *geometry,
           jElem = geometry->node[iPointmin[iNearest]]->GetElem(iElem);
           inside = InsideElem(geometry, r0, 0.0, jElem, p0, p1);
           if(inside){
-            nPanel[0] = 1;
-            pointID_original = new unsigned long*[1];
-            pointID_original[0] = new unsigned long[nPanel[0]];
-            Coord_original = new su2double**[1];
-            Coord_original[0] = new su2double*[nPanel[0]];
-            Coord_original[0][0] = new su2double[nDim];
+            if(nPanel[0] == 0){
+              nPanel[0] = 1;
+              pointID_original = new unsigned long*[1];
+              pointID_original[0] = new unsigned long[nPanel[0]];
+              Coord_original = new su2double**[1];
+              Coord_original[0] = new su2double*[nPanel[0]];
+              Coord_original[0][0] = new su2double[nDim];
 
-            pointID_original[0][0] = jElem;
-            Coord_original[0][0][0] = (p0[0] + p1[0])/2.0;
-            Coord_original[0][0][1] = -r0;
+              pointID_original[0][0] = jElem;
+              Coord_original[0][0][0] = (p0[0] + p1[0])/2.0;
+              Coord_original[0][0][1] = -r0;
 
-            break;
+//              break;
+            }
+            else{ // If we already found a point, compare
+              if((p0[0]+p1[0])/2 < Coord_original[0][0][0]){
+                pointID_original[0][0] = jElem;
+                Coord_original[0][0][0] = (p0[0] + p1[0])/2.0;
+              }
+
+            }
           }
         }
-        if(inside) break;
+//        if(inside) break;
       }
       if(!inside) startline[0] = false;
     }
