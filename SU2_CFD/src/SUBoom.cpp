c6180f76e362c8ef7ef726e5500981df1bf43fc9
bmunguia <bmunguia10@gmail.com>
2017-10-05 17:43:09 -0700

    Removing expansion check

--- SU2_CFD/src/SUBoom.cpp
+++ SU2_CFD/src/SUBoom.cpp
@@ -2075,8 +2075,8 @@ void SUBoom::CreateSignature(unsigned short iPhi){
     }
     else if(mm[i] < -tol_m){  // "expansion shock" present
       /*---Remove segment i---*/
-      ll[i] = tol_l;
-      mm[i] = (pp[1][i] - pp[0][i])/ll[i];
+      //ll[i] = tol_l;
+      //mm[i] = (pp[1][i] - pp[0][i])/ll[i];
     }
     i += 1;
   }
