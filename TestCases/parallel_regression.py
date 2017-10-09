1b04952368dad81109a63da04dd6127a50f7d5c8
JSmith36 <jonathan.smith.1936@gmail.com>
2017-10-08 20:56:09 -0700

    Reg. test adjustment

--- TestCases/parallel_regression.py
+++ TestCases/parallel_regression.py
@@ -246,7 +246,7 @@ def main():
     propeller.cfg_dir   = "rans/propeller"
     propeller.cfg_file  = "propeller.cfg"
     propeller.test_iter = 10
-    propeller.test_vals = [-3.380312, -8.129519, 0.000043, 0.056159] #last 4 columns
+    propeller.test_vals = [-3.378804, -8.130406, 0.000051, 0.055764] #last 4 columns
     propeller.su2_exec  = "parallel_computation.py -f"
     propeller.timeout   = 3200
     propeller.tol       = 0.00001
