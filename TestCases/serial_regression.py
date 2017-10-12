ee360012195c6e0baf1f5854b4982d05962dde66
Jason Howison <32197541+jhowison@users.noreply.github.com>
2017-10-12 13:45:59 -0400

    Update serial_regression.py

--- TestCases/serial_regression.py
+++ TestCases/serial_regression.py
@@ -502,7 +502,7 @@ def main():
     harmonic_balance.cfg_file  = "davis.cfg"
     harmonic_balance.test_iter = 25
     harmonic_balance.test_vals = [-1.784954,-5.471762, 0.007973, 0.077757] #last 4 columns
-    harmonic_balance.su2_exec  = "parallel_computation.py -f"
+    harmonic_balance.su2_exec  = "SU2_CFD"
     harmonic_balance.timeout   = 1600
     harmonic_balance.tol       = 0.00001
     test_list.append(turbulent_hb)
