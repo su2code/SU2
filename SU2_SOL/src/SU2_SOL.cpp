64100462bf45ca1f496944f0bcb873d1349ab0c3
bmunguia <bmunguia10@gmail.com>
2017-10-15 13:04:53 -0700

    Fixed zone bug in preprocessing computation

--- SU2_SOL/src/SU2_SOL.cpp
+++ SU2_SOL/src/SU2_SOL.cpp
@@ -202,7 +202,7 @@ geometry_container[ZONE_0]->SetCoord_CG();
 /*--- Create the dual control volume structures ---*/
 
 if (rank == MASTER_NODE) cout << "Setting the control volume structure." << endl;
-geometry_container[ZONE_0]->SetControlVolume(config_container[iZone], ALLOCATE);
+geometry_container[ZONE_0]->SetControlVolume(config_container[ZONE_0], ALLOCATE);
 ////if (rank == MASTER_NODE) cout << "Setting the bound control volume structure." << endl;
 geometry_container[ZONE_0]->SetBoundControlVolume(config_container[ZONE_0], ALLOCATE);
 
