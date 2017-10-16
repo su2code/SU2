6a35005b7b024cb672eef77d2bc7299544369956
bmunguia <bmunguia10@gmail.com>
2017-10-15 19:54:31 -0700

    Reintroduce preprocessing computations

--- SU2_SOL/src/SU2_SOL.cpp
+++ SU2_SOL/src/SU2_SOL.cpp
@@ -194,20 +194,20 @@ geometry_container[ZONE_0]->SetEdges(); geometry_container[ZONE_0]->SetVertex(co
 
 /*--- Compute center of gravity ---*/
 
-///if (rank == MASTER_NODE) cout << "Computing centers of gravity." << endl;
-///geometry_container[ZONE_0]->SetCoord_CG();
+if (rank == MASTER_NODE) cout << "Computing centers of gravity." << endl;
+geometry_container[ZONE_0]->SetCoord_CG();
 
 /*--- Create the dual control volume structures ---*/
 
-///if (rank == MASTER_NODE) cout << "Setting the control volume structure." << endl;
-///geometry_container[ZONE_0]->SetControlVolume(config_container[ZONE_0], ALLOCATE);
-///if (rank == MASTER_NODE) cout << "Setting the bound control volume structure." << endl;
-///geometry_container[ZONE_0]->SetBoundControlVolume(config_container[ZONE_0], ALLOCATE);
+if (rank == MASTER_NODE) cout << "Setting the control volume structure." << endl;
+geometry_container[ZONE_0]->SetControlVolume(config_container[ZONE_0], ALLOCATE);
+if (rank == MASTER_NODE) cout << "Setting the bound control volume structure." << endl;
+geometry_container[ZONE_0]->SetBoundControlVolume(config_container[ZONE_0], ALLOCATE);
 
 /*--- Store the global to local mapping after preprocessing. ---*/
 
-///if (rank == MASTER_NODE) cout << "Storing a mapping from global to local point index." << endl;
-///geometry_container[ZONE_0]->SetGlobal_to_Local_Point();
+if (rank == MASTER_NODE) cout << "Storing a mapping from global to local point index." << endl;
+geometry_container[ZONE_0]->SetGlobal_to_Local_Point();
 
 ////////      FWH_container = new FWHSolver* [nZone];
 ////////for (iZone = 0; iZone < nZone; iZone++) {
