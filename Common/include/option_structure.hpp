2f593d92a25c78442bec3de00da281fb057384d2
bmunguia <bmunguia10@gmail.com>
2017-10-26 15:18:54 -0700

    Defined ERR_GNORM_SOLL in option_structure

--- Common/include/option_structure.hpp
+++ Common/include/option_structure.hpp
@@ -364,6 +364,7 @@ enum RUNTIME_TYPE {
 
 const int FLOW_SOL = 0;		/*!< \brief Position of the mean flow solution in the solver container array. */
 const int ADJFLOW_SOL = 1;	/*!< \brief Position of the continuous adjoint flow solution in the solver container array. */
+const int ERR_GNORM_SOL = 4; /*!< \brief Position of the error estimation solution in the solver container array. */
 
 const int TURB_SOL = 2;		/*!< \brief Position of the turbulence model solution in the solver container array. */
 const int ADJTURB_SOL = 3;	/*!< \brief Position of the continuous adjoint turbulence solution in the solver container array. */
