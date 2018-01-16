482d826f8d4a18fc75e792a7b7c83706fa70bf90
oleburghardt <ole.burghardt@gmx.net>
2018-01-16 15:18:26 +0100

    MPI statement in solver's restart routine that was turned off for some unknown reason.

--- SU2_CFD/src/solver_structure.cpp
+++ SU2_CFD/src/solver_structure.cpp
@@ -3520,7 +3520,7 @@ void CBaselineSolver::LoadRestart(CGeometry **geometry, CSolver ***solver, CConf
 
   /*--- MPI solution ---*/
   
-  //Set_MPI_Solution(geometry[iZone], config);
+  Set_MPI_Solution(geometry[iZone], config);
   
   /*--- Update the geometry for flows on dynamic meshes ---*/
   
