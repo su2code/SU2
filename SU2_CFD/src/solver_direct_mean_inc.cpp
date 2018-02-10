e38dbc3739c2a536739c3e5a8778b25b2a275ed7
hlkline <hlkline@users.noreply.github.com>
2018-02-10 18:29:26 -0500

    removing extraneous code snippet

--- SU2_CFD/src/solver_direct_mean_inc.cpp
+++ SU2_CFD/src/solver_direct_mean_inc.cpp
@@ -4340,19 +4340,8 @@ void CIncEulerSolver::Evaluate_ObjFunc(CConfig *config) {
   
   /*--- Loop over all monitored markers, add to the 'combo' objective ---*/
 
-  for (iMarker_Monitoring = 0; iMarker_Monitoring < config->GetnMarker_Monitoring(); iMarker_Monitoring++) {
-
-    /*--- If there is only one objective function, it will use it for all the markers ---*/
-    
-    if (config->GetnMarker_Monitoring() != config->GetnObj())  {
-      Weight_ObjFunc = config->GetWeight_ObjFunc(0);
-      Kind_ObjFunc = config->GetKind_ObjFunc(0);
-    }
-    else {
-      Weight_ObjFunc = config->GetWeight_ObjFunc(iMarker_Monitoring);
-      Kind_ObjFunc = config->GetKind_ObjFunc(iMarker_Monitoring);
-    }
-    
+  for (iMarker_Monitoring = 0; iMarker_Monitoring < config->GetnMarker_Monitoring(); iMarker_Monitoring++) {   
+   
     switch(Kind_ObjFunc) {
       case DRAG_COEFFICIENT:
         Total_ComboObj+=Weight_ObjFunc*(Surface_CD[iMarker_Monitoring]);
