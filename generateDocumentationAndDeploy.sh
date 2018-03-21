d0f3d49d8c63401ba05bee26adf7fecd7802d8d0
Tim Albring <tim.albring@scicomp.uni-kl.de>
2018-03-21 11:16:07 +0100

    Added master branch for doc explicitly

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -44,7 +44,7 @@ mkdir code_docs
 cd code_docs
 
 # Get the current gh-pages branch
-git clone https://git@$GH_REPO_REF
+git clone -b master https://git@$GH_REPO_REF
 cd $GH_REPO_NAME
 git checkout -B ${TRAVIS_BRANCH}
 
