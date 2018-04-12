ac5975437563e8f357a53d0191f2b043ce02dfe2
Tim Albring <tim.albring@scicomp.uni-kl.de>
2018-04-12 15:32:20 +0200

    Added travis test scripts

---
+++ travis_scripts/test/before_install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Temporarily fixes Travis CI issue with paths for Python packages
+export PATH=/usr/bin:$PATH
+
+# Install the necessary packages using apt-get with sudo
+sudo apt-get update -qq
+sudo apt-get install -qq build-essential libopenmpi-dev
+
+# Install Python dependencies
+# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda install -q python=$TRAVIS_PYTHON_VERSION numpy scipy mpi4py swig
+
+# to avoid interference with MPI
+test -n $CC  && unset CC
+test -n $CXX && unset CXX
+
---
+++ travis_scripts/test/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Configure, make, and install SU2
+echo $TRAVIS_BUILD_DIR
+echo $CONFIGURE_COMMAND
+$CONFIGURE_COMMAND
+make -j 4
+make install
+
+# Add environmental variables according to the configure step
+export SU2_RUN=$TRAVIS_BUILD_DIR/bin
+export SU2_HOME=$TRAVIS_BUILD_DIR
+export PATH=$PATH:$SU2_RUN
+export PYTHONPATH=$PYTHONPATH:$SU2_RUN
+
