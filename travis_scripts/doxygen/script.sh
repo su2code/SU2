49fa14033a59bdba6c7302483618773fc91e3fc7
Tim Albring <tim.albring@scicomp.uni-kl.de>
2018-06-08 10:02:40 +0200

    Fixed small bug in doxygen script

--- travis_scripts/doxygen/script.sh
+++ travis_scripts/doxygen/script.sh
@@ -3,5 +3,5 @@
 export GH_REPO_NAME=Doxygen
 export DOXYFILE=$TRAVIS_BUILD_DIR/doc/Doxyfile
 export GH_REPO_REF=github.com/su2code/Doxygen.git
-sh ../../doc/generateDocumentationAndDeploy.sh
+sh doc/generateDocumentationAndDeploy.sh
 
