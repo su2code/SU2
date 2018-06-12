27db684ff852077b3743d5990090d6149696b5b5
timothy.b.costa <tbcosta@ortce-tbcosta.jf.intel.com>
2018-06-11 21:28:23 -0700

    Remove configure script.

--- intel-configure-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Requires I_MPI_ROOT and MKLROOT are set by Intel tools environment script.
-
-# Set to path for SU2 installation
-CURRENT_DIR=$(pwd)
-INSTALL_DIR=${CURRENT_DIR}/build
-
-./configure -prefix=${INSTALL_DIR} --enable-mpi --with-cc=${I_MPI_ROOT}/intel64/bin/mpiicc --with-cxx=${I_MPI_ROOT}/intel64/bin/mpiicpc CXXFLAGS="-O3 -I${MKLROOT}/include -DHAVE_MKL -DMKL_DIRECT_CALL_SEQ -xCORE-AVX512" LIBS="-Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_sequential.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -lpthread -lm -ldl"
