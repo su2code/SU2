34e283bc3cf9307f767b9741657a692d6b14b501
bmunguia <bmunguia10@gmail.com>
2018-07-05 15:32:54 -0700

    Added separate boom objectives for loudness and energy

--- SU2_CFD/src/solver_adjoint_discrete.cpp
+++ SU2_CFD/src/solver_adjoint_discrete.cpp
@@ -381,18 +381,6 @@ void CDiscAdjSolver::ExtractAdjoint_Solution(CGeometry *geometry, CConfig *confi
 
     direct_solver->node[iPoint]->GetAdjointSolution(Solution);
 
-    /*--- Add any sources ---*/
-    if (KindDirect_Solver == RUNTIME_FLOW_SYS   ){
-    // Boom
-      if(config->GetKind_ObjFunc()==BOOM){
-        if (LocalPointIndex[iPoint] >= 0){
-          for (iVar = 0; iVar < nVar; iVar++){
-              Solution[iVar] += dJdU_CAA[LocalPointIndex[iPoint]][iVar];
-           }
-        }
-      }
-    }
-
     /*--- Store the adjoint solution ---*/
 
     node[iPoint]->SetSolution(Solution);
@@ -511,13 +499,13 @@ void CDiscAdjSolver::SetAdjoint_Output(CGeometry *geometry, CConfig *config) {
     }
     }
     // Boom
-    /*else if(config->GetKind_ObjFunc()==BOOM){
+    else if(config->GetKind_ObjFunc()==BOOM_LOUD || config->GetKind_ObjFunc()==BOOM_ENERGY){
     if (LocalPointIndex[iPoint] >= 0){
         for (iVar = 0; iVar < nVar; iVar++){
             Solution[iVar] += dJdU_CAA[LocalPointIndex[iPoint]][iVar];
          }
     }
-    }*/
+    }
     }
 
 
