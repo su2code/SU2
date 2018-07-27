2a48bb8e8a3b8beb643fe6ff16912390ad8a43c7
Jayant Mukhopadhaya <jmukho@stanford.edu>
2018-07-26 17:13:53 -0700

    added comments for clarification

--- SU2_CFD/src/numerics_direct_turbulent.cpp
+++ SU2_CFD/src/numerics_direct_turbulent.cpp
@@ -1184,7 +1184,7 @@ CSourcePieceWise_TurbSST::CSourcePieceWise_TurbSST(unsigned short val_nDim, unsi
 CSourcePieceWise_TurbSST::~CSourcePieceWise_TurbSST(void) { }
 
 void CSourcePieceWise_TurbSST::ComputeResidual(su2double *val_residual, su2double **val_Jacobian_i, su2double **val_Jacobian_j, CConfig *config) {
-  
+
   AD::StartPreacc();
   AD::SetPreaccIn(StrainMag_i);
   AD::SetPreaccIn(TurbVar_i, nVar);
@@ -1222,321 +1222,320 @@ void CSourcePieceWise_TurbSST::ComputeResidual(su2double *val_residual, su2doubl
   beta_blended = F1_i*beta_1 + (1.0 - F1_i)*beta_2;
   
   if (dist_i > 1e-10) {
-    
-    /*--- Production ---*/
-    
-    diverg = 0.0;
-    for (iDim = 0; iDim < nDim; iDim++)
-      diverg += PrimVar_Grad_i[iDim+1][iDim];
-    
-    if (config->GetUsing_UQ()){
-      SetReynoldsStressMatrix(TurbVar_i[0]);
-      SetPerturbedRSM(TurbVar_i[0], config);
-      SetPerturbedStrainMag(TurbVar_i[0]);
-      pk = Eddy_Viscosity_i*PerturbedStrainMag*PerturbedStrainMag
-              - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
-    }
-    else {
-        pk = Eddy_Viscosity_i*StrainMag_i*StrainMag_i - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
-    }
+
+   /*--- Production ---*/
+
+   diverg = 0.0;
+   for (iDim = 0; iDim < nDim; iDim++)
+     diverg += PrimVar_Grad_i[iDim+1][iDim];
+   
+   /* if using UQ methodolgy, calculate production using perturbed Reynolds stress matrix */
+
+   if (config->GetUsing_UQ()){
+     SetReynoldsStressMatrix(TurbVar_i[0]);
+     SetPerturbedRSM(TurbVar_i[0], config);
+     SetPerturbedStrainMag(TurbVar_i[0]);
+     pk = Eddy_Viscosity_i*PerturbedStrainMag*PerturbedStrainMag
+     - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
+   }
+   else {
+     pk = Eddy_Viscosity_i*StrainMag_i*StrainMag_i - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
+   }
 
 
-    pk = min(pk,20.0*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]);
-    pk = max(pk,0.0);
-    
-    zeta = max(TurbVar_i[1], StrainMag_i*F2_i/a1);
+   pk = min(pk,20.0*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]);
+   pk = max(pk,0.0);
 
-    if (config->GetUsing_UQ()){
-        pw = PerturbedStrainMag * PerturbedStrainMag - 2.0/3.0*zeta*diverg;
-    }
-    else {
-	pw = StrainMag_i*StrainMag_i - 2.0/3.0*zeta*diverg;
-    }
-    pw = max(pw,0.0);
-    
-    val_residual[0] += pk*Volume;
-    val_residual[1] += alfa_blended*Density_i*pw*Volume;
-    
-    /*--- Dissipation ---*/
-    
-    val_residual[0] -= beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]*Volume;
-    val_residual[1] -= beta_blended*Density_i*TurbVar_i[1]*TurbVar_i[1]*Volume;
-    
-    /*--- Cross diffusion ---*/
-    
-    val_residual[1] += (1.0 - F1_i)*CDkw_i*Volume;
-    
-    /*--- Implicit part ---*/
-    
-    val_Jacobian_i[0][0] = -beta_star*TurbVar_i[1]*Volume;
-    val_Jacobian_i[0][1] = -beta_star*TurbVar_i[0]*Volume;
-    val_Jacobian_i[1][0] = 0.0;
-    val_Jacobian_i[1][1] = -2.0*beta_blended*TurbVar_i[1]*Volume;
+   zeta = max(TurbVar_i[1], StrainMag_i*F2_i/a1);
+
+   /* if using UQ methodolgy, calculate production using perturbed Reynolds stress matrix */
+
+   if (config->GetUsing_UQ()){
+    pw = PerturbedStrainMag * PerturbedStrainMag - 2.0/3.0*zeta*diverg;
+   }
+   else {
+     pw = StrainMag_i*StrainMag_i - 2.0/3.0*zeta*diverg;
+   }
+   pw = max(pw,0.0);
+
+   val_residual[0] += pk*Volume;
+   val_residual[1] += alfa_blended*Density_i*pw*Volume;
+
+   /*--- Dissipation ---*/
+
+   val_residual[0] -= beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]*Volume;
+   val_residual[1] -= beta_blended*Density_i*TurbVar_i[1]*TurbVar_i[1]*Volume;
+
+   /*--- Cross diffusion ---*/
+
+   val_residual[1] += (1.0 - F1_i)*CDkw_i*Volume;
+
+   /*--- Implicit part ---*/
+
+   val_Jacobian_i[0][0] = -beta_star*TurbVar_i[1]*Volume;
+   val_Jacobian_i[0][1] = -beta_star*TurbVar_i[0]*Volume;
+   val_Jacobian_i[1][0] = 0.0;
+   val_Jacobian_i[1][1] = -2.0*beta_blended*TurbVar_i[1]*Volume;
   }
-  
-  AD::SetPreaccOut(val_residual, nVar);
-  AD::EndPreacc();
+
+ AD::SetPreaccOut(val_residual, nVar);
+ AD::EndPreacc();
 
 }
 
 void CSourcePieceWise_TurbSST::GetMeanRateOfStrainMatrix(su2double **S_ij)
 {
-    if (nDim == 3){
-        S_ij[0][0] = PrimVar_Grad_i[1][0];
-        S_ij[1][1] = PrimVar_Grad_i[2][1];
-        S_ij[2][2] = PrimVar_Grad_i[3][2];
-        S_ij[0][1] = 0.5 * (PrimVar_Grad_i[1][1] + PrimVar_Grad_i[2][0]);
-        S_ij[0][2] = 0.5 * (PrimVar_Grad_i[1][2] + PrimVar_Grad_i[3][0]);
-        S_ij[1][2] = 0.5 * (PrimVar_Grad_i[2][2] + PrimVar_Grad_i[3][1]);
-        S_ij[1][0] = S_ij[0][1];
-        S_ij[2][1] = S_ij[1][2];
-        S_ij[2][0] = S_ij[0][2];
-    }
-    else {
-        S_ij[0][0] = PrimVar_Grad_i[1][0];
-        S_ij[1][1] = PrimVar_Grad_i[2][1];
-        S_ij[2][2] = 0.0;
-        S_ij[0][1] = 0.5 * (PrimVar_Grad_i[1][1] + PrimVar_Grad_i[2][0]);
-        S_ij[0][2] = 0.0;
-        S_ij[1][2] = 0.0;
-        S_ij[1][0] = S_ij[0][1];
-        S_ij[2][1] = S_ij[1][2];
-        S_ij[2][0] = S_ij[0][2];
+    /* --- Calculate the rate of strain tensor, using mean velocity gradients --- */
 
-    }
+  if (nDim == 3){
+    S_ij[0][0] = PrimVar_Grad_i[1][0];
+    S_ij[1][1] = PrimVar_Grad_i[2][1];
+    S_ij[2][2] = PrimVar_Grad_i[3][2];
+    S_ij[0][1] = 0.5 * (PrimVar_Grad_i[1][1] + PrimVar_Grad_i[2][0]);
+    S_ij[0][2] = 0.5 * (PrimVar_Grad_i[1][2] + PrimVar_Grad_i[3][0]);
+    S_ij[1][2] = 0.5 * (PrimVar_Grad_i[2][2] + PrimVar_Grad_i[3][1]);
+    S_ij[1][0] = S_ij[0][1];
+    S_ij[2][1] = S_ij[1][2];
+    S_ij[2][0] = S_ij[0][2];
+  }
+  else {
+    S_ij[0][0] = PrimVar_Grad_i[1][0];
+    S_ij[1][1] = PrimVar_Grad_i[2][1];
+    S_ij[2][2] = 0.0;
+    S_ij[0][1] = 0.5 * (PrimVar_Grad_i[1][1] + PrimVar_Grad_i[2][0]);
+    S_ij[0][2] = 0.0;
+    S_ij[1][2] = 0.0;
+    S_ij[1][0] = S_ij[0][1];
+    S_ij[2][1] = S_ij[1][2];
+    S_ij[2][0] = S_ij[0][2];
+
+  }
 }
 
 void CSourcePieceWise_TurbSST::SetReynoldsStressMatrix(su2double turb_ke){
-    unsigned short iDim, jDim;
-    su2double **S_ij = new su2double* [3];
-    su2double divVel = 0;
-    su2double TWO3 = 2.0/3.0;
+  unsigned short iDim, jDim;
+  su2double **S_ij = new su2double* [3];
+  su2double divVel = 0;
+  su2double TWO3 = 2.0/3.0;
 
 
 
-    for (iDim = 0; iDim < 3; iDim++){
-        S_ij[iDim] = new su2double [3];
+  for (iDim = 0; iDim < 3; iDim++){
+    S_ij[iDim] = new su2double [3];
+  }
+
+  GetMeanRateOfStrainMatrix(S_ij);
+
+    /* --- Using rate of strain matrix, calculate Reynolds stress tensor --- */
+
+  for (iDim = 0; iDim < 3; iDim++){
+    divVel += S_ij[iDim][iDim];
+  }
+
+  for (iDim = 0; iDim < 3; iDim++){
+    for (jDim = 0; jDim < 3; jDim++){
+      MeanReynoldsStress[iDim][jDim] = TWO3 * turb_ke * delta[iDim][jDim]
+      - Eddy_Viscosity_i / Density_i * (2 * S_ij[iDim][jDim] - TWO3 * divVel * delta[iDim][jDim]);
     }
+  }
 
-
-    GetMeanRateOfStrainMatrix(S_ij);
-
-    for (iDim = 0; iDim < 3; iDim++){
-        divVel += S_ij[iDim][iDim];
-    }
-
-    for (iDim = 0; iDim < 3; iDim++){
-        for (jDim = 0; jDim < 3; jDim++){
-            MeanReynoldsStress[iDim][jDim] = TWO3 * turb_ke * delta[iDim][jDim]
-                    - Eddy_Viscosity_i / Density_i * (2 * S_ij[iDim][jDim] - TWO3 * divVel * delta[iDim][jDim]);
-        }
-    }
-
-    for (iDim = 0; iDim < 3; iDim++)
-        delete [] S_ij[iDim];
-    delete [] S_ij;
+  for (iDim = 0; iDim < 3; iDim++)
+    delete [] S_ij[iDim];
+  delete [] S_ij;
 }
 
 void CSourcePieceWise_TurbSST::SetPerturbedRSM (su2double turb_ke, CConfig *config){
 
-    unsigned short iDim,jDim;
-    su2double **A_ij;
-    su2double **Eig_Vec;
-    su2double **New_Eig_Vec;
-    su2double **newA_ij;
-    su2double **Corners;
-    su2double *Eig_Val;
-    su2double *Barycentric_Coord;
-    su2double *New_Coord;
+  unsigned short iDim,jDim;
+  su2double **A_ij;
+  su2double **Eig_Vec;
+  su2double **New_Eig_Vec;
+  su2double **newA_ij;
+  su2double **Corners;
+  su2double *Eig_Val;
+  su2double *Barycentric_Coord;
+  su2double *New_Coord;
 
-    unsigned short Eig_Val_Comp = config->GetEig_Val_Comp();
-    su2double beta_delta = config->GetBeta_Delta();
-    su2double urlx = config->GetURLX();
-    bool permute = config->GetPermute();
+  /* --- Get perturbation parameters from config options --- */
 
-    A_ij = new su2double* [3];
-    newA_ij = new su2double* [3];
-    Eig_Vec = new su2double* [3];
-    New_Eig_Vec = new su2double* [3];
-    Corners = new su2double* [3];
-    Eig_Val = new su2double [3];
-    Barycentric_Coord = new su2double [2];
-    New_Coord = new su2double [2];
+  unsigned short Eig_Val_Comp = config->GetEig_Val_Comp();
+  su2double beta_delta = config->GetBeta_Delta();
+  su2double urlx = config->GetURLX();
+  bool permute = config->GetPermute();
 
-    for (iDim= 0; iDim< 3; iDim++){
-        A_ij[iDim] = new su2double [3];
-        newA_ij[iDim] = new su2double [3];
-        Eig_Vec[iDim] = new su2double [3];
-        New_Eig_Vec[iDim] = new su2double [3];
-        Corners[iDim] = new su2double [2];
-        Eig_Val[iDim] = 0;
+  /* --- Initialize data structures required to perform perturbation --- */
+
+  A_ij = new su2double* [3];
+  newA_ij = new su2double* [3];
+  Eig_Vec = new su2double* [3];
+  New_Eig_Vec = new su2double* [3];
+  Corners = new su2double* [3];
+  Eig_Val = new su2double [3];
+  Barycentric_Coord = new su2double [2];
+  New_Coord = new su2double [2];
+
+  for (iDim= 0; iDim< 3; iDim++){
+    A_ij[iDim] = new su2double [3];
+    newA_ij[iDim] = new su2double [3];
+    Eig_Vec[iDim] = new su2double [3];
+    New_Eig_Vec[iDim] = new su2double [3];
+    Corners[iDim] = new su2double [2];
+    Eig_Val[iDim] = 0;
+  }
+
+  /* --- Calculate anisotropic part of Reynolds Stress tensor --- */
+
+  for (iDim = 0; iDim< 3; iDim++){
+    for (jDim = 0; jDim < 3; jDim++){
+      A_ij[iDim][jDim] = .5 * MeanReynoldsStress[iDim][jDim] / turb_ke - delta[iDim][jDim] / 3.0;
+      Eig_Vec[iDim][jDim] = A_ij[iDim][jDim];
     }
+  }
 
-    for (iDim = 0; iDim< 3; iDim++){
-        for (jDim = 0; jDim < 3; jDim++){
-            A_ij[iDim][jDim] = .5 * MeanReynoldsStress[iDim][jDim] / turb_ke - delta[iDim][jDim] / 3.0;
-            Eig_Vec[iDim][jDim] = A_ij[iDim][jDim];
-        }
+  /* --- Get ordered eigenvectors and eigenvalues of A_ij --- */
+
+  EigenDecomposition(A_ij, Eig_Vec, Eig_Val);
+
+  /* compute convex combination coefficients */
+  su2double c1c = Eig_Val[2] - Eig_Val[1];
+  su2double c2c = 2.0 * (Eig_Val[1] - Eig_Val[0]);
+  su2double c3c = 3.0 * Eig_Val[0] + 1.0;
+
+  /* define barycentric traingle corner points */
+  Corners[0][0] = 1.0;
+  Corners[0][1] = 0.0;
+  Corners[1][0] = 0.0;
+  Corners[1][1] = 0.0;
+  Corners[2][0] = 0.5;
+  Corners[2][1] = 0.866025;
+
+  /* define barycentric coordinates */
+  Barycentric_Coord[0] = Corners[0][0] * c1c + Corners[1][0] * c2c + Corners[2][0] * c3c;
+  Barycentric_Coord[1] = Corners[0][1] * c1c + Corners[1][1] * c2c + Corners[2][1] * c3c;
+
+  if (Eig_Val_Comp == 1) {
+    /* 1C turbulence */
+    New_Coord[0] = Corners[0][0];
+    New_Coord[1] = Corners[0][1];
+  }
+  else if (Eig_Val_Comp == 2) {
+    /* 2C turbulence */
+    New_Coord[0] = Corners[1][0];
+    New_Coord[1] = Corners[1][1];
+  }
+  else if (Eig_Val_Comp == 3) {
+    /* 3C turbulence */
+    New_Coord[0] = Corners[2][0];
+    New_Coord[1] = Corners[2][1];
+  }
+  else {
+    /* 2C turbulence */
+    New_Coord[0] = Corners[1][0];
+    New_Coord[1] = Corners[1][1];
+  }
+  /* calculate perturbed barycentric coordinates */
+
+  Barycentric_Coord[0] = Barycentric_Coord[0] + (beta_delta) * (New_Coord[0] - Barycentric_Coord[0]);
+  Barycentric_Coord[1] = Barycentric_Coord[1] + (beta_delta) * (New_Coord[1] - Barycentric_Coord[1]);
+
+  /* rebuild c1c,c2c,c3c based on new barycentric coordinates */
+  c3c = Barycentric_Coord[1] / Corners[2][1];
+  c1c = Barycentric_Coord[0] - Corners[2][0] * c3c;
+  c2c = 1 - c1c - c3c;
+
+  /* build new anisotropy eigenvalues */
+  Eig_Val[0] = (c3c - 1) / 3.0;
+  Eig_Val[1] = 0.5 *c2c + Eig_Val[0];
+  Eig_Val[2] = c1c + Eig_Val[1];
+
+  /* permute eigenvectors if required */
+  if (permute) {
+    for (iDim=0; iDim<3; iDim++) {
+      for (jDim=0; jDim<3; jDim++) {
+        New_Eig_Vec[iDim][jDim] = Eig_Vec[2-iDim][jDim];
+      }
     }
+  }
 
-    EigenDecomposition(A_ij, Eig_Vec, Eig_Val);
-
-    /* compute convex combination coefficients */
-    su2double c1c = Eig_Val[2] - Eig_Val[1];
-    su2double c2c = 2.0 * (Eig_Val[1] - Eig_Val[0]);
-    su2double c3c = 3.0 * Eig_Val[0] + 1.0;
-
-    /* define barycentric traingle corner points */
-    Corners[0][0] = 1.0;
-    Corners[0][1] = 0.0;
-    Corners[1][0] = 0.0;
-    Corners[1][1] = 0.0;
-    Corners[2][0] = 0.5;
-    Corners[2][1] = 0.866025;
-
-    /* define barycentric coordinates */
-    Barycentric_Coord[0] = Corners[0][0] * c1c + Corners[1][0] * c2c + Corners[2][0] * c3c;
-    Barycentric_Coord[1] = Corners[0][1] * c1c + Corners[1][1] * c2c + Corners[2][1] * c3c;
-
-    if (Eig_Val_Comp == 1) {
-        /* 1C turbulence */
-        New_Coord[0] = Corners[0][0];
-        New_Coord[1] = Corners[0][1];
+  else {
+    for (iDim=0; iDim<3; iDim++) {
+      for (jDim=0; jDim<3; jDim++) {
+        New_Eig_Vec[iDim][jDim] = Eig_Vec[iDim][jDim];
+      }
     }
-    else if (Eig_Val_Comp == 2) {
-        /* 2C turbulence */
-        New_Coord[0] = Corners[1][0];
-        New_Coord[1] = Corners[1][1];
+  }
+
+  EigenRecomposition(newA_ij, New_Eig_Vec, Eig_Val);
+
+  /* compute perturbed Reynolds stress matrix; use under-relaxation factor (urlx)*/
+  for (iDim = 0; iDim< 3; iDim++){
+    for (jDim = 0; jDim < 3; jDim++){
+      MeanPerturbedRSM[iDim][jDim] = 2.0 * turb_ke * (newA_ij[iDim][jDim] + 1.0/3.0 * delta[iDim][jDim]);
+      MeanPerturbedRSM[iDim][jDim] = MeanReynoldsStress[iDim][jDim] +
+      urlx*(MeanPerturbedRSM[iDim][jDim] - MeanReynoldsStress[iDim][jDim]);
     }
-    else if (Eig_Val_Comp == 3) {
-        /* 3C turbulence */
-        New_Coord[0] = Corners[2][0];
-        New_Coord[1] = Corners[2][1];
-    }
-    else {
-        /* 2C turbulence */
-        New_Coord[0] = Corners[1][0];
-        New_Coord[1] = Corners[1][1];
-    }
+  }
 
-    Barycentric_Coord[0] = Barycentric_Coord[0] + (beta_delta) * (New_Coord[0] - Barycentric_Coord[0]);
-    Barycentric_Coord[1] = Barycentric_Coord[1] + (beta_delta) * (New_Coord[1] - Barycentric_Coord[1]);
 
-    /* rebuild c1c,c2c,c3c based on new barycentric coordinates */
-    c3c = Barycentric_Coord[1] / Corners[2][1];
-    c1c = Barycentric_Coord[0] - Corners[2][0] * c3c;
-    c2c = 1 - c1c - c3c;
+  /* delete variables */
 
-    /* build new anisotropy eigenvalues */
-    Eig_Val[0] = (c3c - 1) / 3.0;
-    Eig_Val[1] = 0.5 *c2c + Eig_Val[0];
-    Eig_Val[2] = c1c + Eig_Val[1];
+  for (iDim= 0; iDim< 3; iDim++){
+    delete [] A_ij[iDim];
+    delete [] newA_ij[iDim];
+    delete [] Eig_Vec[iDim];
+    delete [] New_Eig_Vec[iDim];
+    delete [] Corners[iDim];
+  }
 
-    if (permute) {
-        for (iDim=0; iDim<3; iDim++) {
-            for (jDim=0; jDim<3; jDim++) {
-                New_Eig_Vec[iDim][jDim] = Eig_Vec[2-iDim][jDim];
-            }
-        }
-    }
-
-    else {
-        for (iDim=0; iDim<3; iDim++) {
-            for (jDim=0; jDim<3; jDim++) {
-                New_Eig_Vec[iDim][jDim] = Eig_Vec[iDim][jDim];
-            }
-        }
-    }
-
-    EigenRecomposition(newA_ij, New_Eig_Vec, Eig_Val);
-
-    for (iDim = 0; iDim< 3; iDim++){
-        for (jDim = 0; jDim < 3; jDim++){
-            MeanPerturbedRSM[iDim][jDim] = 2.0 * turb_ke * (newA_ij[iDim][jDim] + 1.0/3.0 * delta[iDim][jDim]);
-            MeanPerturbedRSM[iDim][jDim] = MeanReynoldsStress[iDim][jDim] +
-                    urlx*(MeanPerturbedRSM[iDim][jDim] - MeanReynoldsStress[iDim][jDim]);
-        }
-    }
-
-//  Printing matrices for debugging
-//    cout<<"Reynolds stress: { ";
-//    for (iDim = 0; iDim< 3; iDim++){
-//        cout<<"{ ";
-//        for (jDim = 0; jDim < 3; jDim++){
-//            cout<<MeanReynoldsStress[iDim][jDim]<<", ";
-//        }
-//        cout<<"}, ";
-//    }
-//    cout<<"} "<<endl;
-
-//    cout<<"Perturbed RSM: { ";
-//    for (iDim = 0; iDim< 3; iDim++){
-//        cout<<"{ ";
-//        for (jDim = 0; jDim < 3; jDim++){
-//            cout<<MeanPerturbedRSM[iDim][jDim]<<", ";
-//        }
-//        cout<<"}, ";
-//    }
-//    cout<<"} "<<endl;
-
-    // delete variables
-
-    for (iDim= 0; iDim< 3; iDim++){
-        delete [] A_ij[iDim];
-        delete [] newA_ij[iDim];
-        delete [] Eig_Vec[iDim];
-        delete [] New_Eig_Vec[iDim];
-        delete [] Corners[iDim];
-    }
-
-    delete [] A_ij;
-    delete [] newA_ij;
-    delete [] Eig_Vec;
-    delete [] New_Eig_Vec;
-    delete [] Corners;
-    delete [] Eig_Val;
-    delete [] Barycentric_Coord;
-    delete [] New_Coord;
+  delete [] A_ij;
+  delete [] newA_ij;
+  delete [] Eig_Vec;
+  delete [] New_Eig_Vec;
+  delete [] Corners;
+  delete [] Eig_Val;
+  delete [] Barycentric_Coord;
+  delete [] New_Coord;
 
 }
 
 void CSourcePieceWise_TurbSST::SetPerturbedStrainMag(su2double turb_ke){
-    unsigned short iDim, jDim;
-    PerturbedStrainMag = 0;
-    su2double **StrainRate = new su2double* [nDim];
-    for (iDim= 0; iDim< nDim; iDim++){
-        StrainRate[iDim] = new su2double [nDim];
+  unsigned short iDim, jDim;
+  PerturbedStrainMag = 0;
+  su2double **StrainRate = new su2double* [nDim];
+  for (iDim= 0; iDim< nDim; iDim++){
+    StrainRate[iDim] = new su2double [nDim];
+  }
+
+  /* compute perturbed strain rate tensor */
+
+  for (iDim = 0; iDim < nDim; iDim++){
+    for (jDim =0; jDim < nDim; jDim++){
+      StrainRate[iDim][jDim] = MeanPerturbedRSM[iDim][jDim]
+      - TWO3 * turb_ke * delta[iDim][jDim];
+      StrainRate[iDim][jDim] = - StrainRate[iDim][jDim] * Density_i / (2 * Eddy_Viscosity_i);
     }
+  }
 
-    for (iDim = 0; iDim < nDim; iDim++){
-        for (jDim =0; jDim < nDim; jDim++){
-            StrainRate[iDim][jDim] = MeanPerturbedRSM[iDim][jDim]
-                    - TWO3 * turb_ke * delta[iDim][jDim];
-            StrainRate[iDim][jDim] = - StrainRate[iDim][jDim] * Density_i / (2 * Eddy_Viscosity_i);
-        }
-    }
+  /*--- Add diagonal part ---*/
 
-    /*--- Add diagonal part ---*/
+  for (iDim = 0; iDim < nDim; iDim++) {
+    PerturbedStrainMag += pow(StrainRate[iDim][iDim], 2.0);
+  }
 
-    for (iDim = 0; iDim < nDim; iDim++) {
-      PerturbedStrainMag += pow(StrainRate[iDim][iDim], 2.0);
-    }
+  /*--- Add off diagonals ---*/
 
-    /*--- Add off diagonals ---*/
+  PerturbedStrainMag += 2.0*pow(StrainRate[1][0], 2.0);
 
-    PerturbedStrainMag += 2.0*pow(StrainRate[1][0], 2.0);
+  if (nDim == 3) {
+    PerturbedStrainMag += 2.0*pow(StrainRate[0][2], 2.0);
+    PerturbedStrainMag += 2.0*pow(StrainRate[1][2], 2.0);
+  }
 
-    if (nDim == 3) {
-      PerturbedStrainMag += 2.0*pow(StrainRate[0][2], 2.0);
-      PerturbedStrainMag += 2.0*pow(StrainRate[1][2], 2.0);
-    }
+  PerturbedStrainMag = sqrt(2.0*PerturbedStrainMag);
 
-    PerturbedStrainMag = sqrt(2.0*PerturbedStrainMag);
+  for (iDim= 0; iDim< nDim; iDim++){
+    delete [] StrainRate[iDim];
+  }
 
-    for (iDim= 0; iDim< nDim; iDim++){
-        delete [] StrainRate[iDim];
-    }
-
-    delete [] StrainRate;
+  delete [] StrainRate;
 }
-
