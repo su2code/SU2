01e3eee6ad81d2404ec26084101387bb212382a3
Jayant Mukhopadhaya <jmukho@stanford.edu>
2018-09-12 16:47:27 -0700

    removed some whitespaces

--- SU2_CFD/src/numerics_direct_mean.cpp
+++ SU2_CFD/src/numerics_direct_mean.cpp
@@ -4696,7 +4696,7 @@ void CAvgGradCorrected_Flow::ComputeResidual(su2double *val_residual, su2double 
   }
 
   else {
-    GetViscousProjFlux(Mean_PrimVar, Mean_GradPrimVar, Mean_turb_ke, Normal, Mean_Laminar_Viscosity, Mean_Eddy_Viscosity, Mean_TauWall,QCR);
+    GetViscousProjFlux(Mean_PrimVar, Mean_GradPrimVar, Mean_turb_ke, Normal, Mean_Laminar_Viscosity, Mean_Eddy_Viscosity, Mean_TauWall, QCR);
   }
 
   /*--- Save residual value ---*/
