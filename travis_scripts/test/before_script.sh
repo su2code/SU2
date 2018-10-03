7a98b09790d671e59c0697082f609505cc88c40f
Tim Albring <tim.albring@scicomp.uni-kl.de>
2018-10-03 22:31:38 +0200

    Update before_script.sh

--- travis_scripts/test/before_script.sh
+++ travis_scripts/test/before_script.sh
@@ -5,5 +5,5 @@ git clone -b develop https://github.com/su2code/TestCases.git ./TestData
 cp -R ./TestData/* ./TestCases/
 
 # Get the tutorial cases
-git clone -b master https://github.com/su2code/su2code.github.io ./Tutorials
+git clone -b develop https://github.com/su2code/su2code.github.io ./Tutorials
 
