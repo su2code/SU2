ad1dfbbaceddb6b7677ec7a8944cec00a4b87934
vdweide <e.t.a.vanderweide@utwente.nl>
2018-10-04 09:34:29 +0200

    Included the email conversation with John Burkardt, in which he approves the usage of his code

--- Common/include/gauss_jacobi_quadrature.hpp
+++ Common/include/gauss_jacobi_quadrature.hpp
@@ -33,6 +33,56 @@
  * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+From: "Burkardt, John" <jburkardt@fsu.edu>
+Subject: Re: Gauss-Jacobi Quadrature Code
+Date: July 6, 2016 at 4:14:17 AM PDT
+To: "Thomas D. Economon" <economon@stanford.edu>
+Cc: Edwin van der Weide <E.T.A.vanderWeide@ctw.utwente.nl>
+
+Dear Thomas Economon,
+
+Thank you for your note about jacobi_rule.cpp; I am glad you find it of use.
+
+You are welcome to use this code in any way you choose.
+
+The only reason I put a Gnu LGPL on it was because, from a number of inquiries,
+it became clear to me that people were hesitant to use a piece of software if
+there was no license information on it at all.  At the time, LGPL seemed the least
+restrictive.  I didn't pay attention to the vexing fact that now there are several
+versions of LGPL, because I really don't care.
+
+If this statement is not enough for you, let me know what would be better.
+I have no objection to you taking a copy of jacobi_rule.cpp and altering the
+license to agree with your preferred version of LGPL, for instance.
+
+John Burkardt
+________________________________________
+From: Thomas D. Economon <economon@stanford.edu>
+Sent: Tuesday, July 5, 2016 11:04:34 PM
+To: Burkardt, John
+Cc: Edwin van der Weide
+Subject: Gauss-Jacobi Quadrature Code
+
+Dear John,
+
+I am the lead developer for the open-source SU2 suite, which is primarily for
+computational fluid dynamics, but also for solving other PDE systems 
+(http://su2.stanford.edu, https://github.com/su2code/SU2). Over the past few months,
+we have been working on a new high-order discontinuous Galerkin fluid solver within
+the codebase, led by Prof. Edwin van der Weide at the University of Twente (cc’d).
+
+We found your attached code to be very helpful in resolving some issues with integration
+for our pyramid elements, and we would really like to reuse the implementation if possible.
+First, we would like to check if this is ok with you personally, and ask how we can 
+properly attribute your work in the code? Second, we are curious just what version of the
+GNU LGPL license you are using to make sure that we don’t have any licensing issues. 
+For SU2, we are using GNU LGPL v2.1.
+
+Thanks for the time and take care,
+Tom
+*/
+
 #pragma once
 
 #include <cmath>
