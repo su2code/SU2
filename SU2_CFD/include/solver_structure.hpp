63194d608c7258e1c68c96f38620884564906388
Thomas D. Economon <thomas.economon@us.bosch.com>
2018-11-14 01:18:22 -0800

    Working version of incompressible pressure inlet and mass flow outlet BCs.

--- SU2_CFD/include/solver_structure.hpp
+++ SU2_CFD/include/solver_structure.hpp
@@ -2055,6 +2055,11 @@ public:
   
   /*!
    * \brief A virtual member.
+   */
+  virtual void GetOutlet_Properties(CGeometry *geometry, CConfig *config, unsigned short iMesh, bool Output);
+  
+  /*!
+   * \brief A virtual member.
    * \param[in] geometry - Geometrical definition of the problem.
    * \param[in] solution - Container vector with all the solutions.
    */
@@ -8260,7 +8265,12 @@ public:
                              string val_marker,
                              CGeometry *geometry,
                              CConfig *config);
-
+  
+  /*!
+   * \brief A virtual member.
+   */
+  void GetOutlet_Properties(CGeometry *geometry, CConfig *config, unsigned short iMesh, bool Output);
+  
 };
 
 /*!
