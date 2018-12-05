c565db4467b933b6ca8e943799f0f98485baf783
Tim Albring <tim.albring@scicomp.uni-kl.de>
2018-12-05 11:47:57 +0100

    Adapted and added output to FEA solver

--- SU2_CFD/src/output_direct_elasticity.cpp
+++ SU2_CFD/src/output_direct_elasticity.cpp
@@ -47,6 +47,29 @@ CFEAOutput::CFEAOutput(CConfig *config, CGeometry *geometry, unsigned short val_
   if (nonlinear_analysis) nVar_FEM = 3;
   
   nDim = geometry->GetnDim();
+
+  if (nRequestedHistoryFields == 0){
+    RequestedHistoryFields.push_back("ITER");
+    RequestedHistoryFields.push_back("RMS_RES");
+    nRequestedHistoryFields = RequestedHistoryFields.size();
+  }
+  
+  if (nRequestedScreenFields == 0){
+    RequestedScreenFields.push_back("OUTER_ITER");
+    RequestedScreenFields.push_back("INNER_ITER");
+    RequestedScreenFields.push_back("RMS_UTOL");
+    RequestedScreenFields.push_back("RMS_RTOL");
+    RequestedScreenFields.push_back("VMS");
+    nRequestedScreenFields = RequestedScreenFields.size();
+  }
+  
+  if (nRequestedVolumeFields == 0){
+    RequestedVolumeFields.push_back("COORDINATES");
+    RequestedVolumeFields.push_back("CONSERVATIVE");
+    RequestedVolumeFields.push_back("PRIMITIVE");
+    nRequestedVolumeFields = RequestedVolumeFields.size();
+  }
+
   
   stringstream ss;
   ss << "Zone " << config->GetiZone() << " (Structure)";
@@ -78,8 +101,8 @@ void CFEAOutput::LoadHistoryData(CGeometry ****geometry,
   bool linear_analysis = (config[val_iZone]->GetGeometricConditions() == SMALL_DEFORMATIONS);  // Linear analysis.
   bool nonlinear_analysis = (config[val_iZone]->GetGeometricConditions() == LARGE_DEFORMATIONS);  // Nonlinear analysis.
 
-  SetHistoryOutputValue("INT_ITER", config[val_iZone]->GetIntIter());
-  SetHistoryOutputValue("EXT_ITER", config[val_iZone]->GetExtIter());
+  SetHistoryOutputValue("OUTER_ITER", config[val_iZone]->GetOuterIter());
+  SetHistoryOutputValue("INNER_ITER", config[val_iZone]->GetInnerIter());
   
   SetHistoryOutputValue("PHYS_TIME", timeused);
   
@@ -89,28 +112,65 @@ void CFEAOutput::LoadHistoryData(CGeometry ****geometry,
 
   
   if (linear_analysis){
-    SetHistoryOutputValue("UTOL", log10(fea_solver->GetRes_RMS(0)));
-    SetHistoryOutputValue("RTOL", log10(fea_solver->GetRes_RMS(1)));
+    SetHistoryOutputValue("RMS_UTOL", log10(fea_solver->GetRes_RMS(0)));
+    SetHistoryOutputValue("RMS_RTOL", log10(fea_solver->GetRes_RMS(1)));
     if (nVar_FEM == 3){
-      SetHistoryOutputValue("ETOL", log10(fea_solver->GetRes_RMS(2)));    
+      SetHistoryOutputValue("RMS_ETOL", log10(fea_solver->GetRes_RMS(2)));    
     }
-    SetHistoryOutputValue("DISP_X", log10(fea_solver->GetRes_RMS(0)));
-    SetHistoryOutputValue("DISP_Y", log10(fea_solver->GetRes_RMS(1)));
+    SetHistoryOutputValue("RMS_DISP_X", log10(fea_solver->GetRes_RMS(0)));
+    SetHistoryOutputValue("RMS_DISP_Y", log10(fea_solver->GetRes_RMS(1)));
     if (nVar_FEM == 3){
-      SetHistoryOutputValue("DISP_Z", log10(fea_solver->GetRes_RMS(2)));    
+      SetHistoryOutputValue("RMS_DISP_Z", log10(fea_solver->GetRes_RMS(2)));    
     }
   } else if (nonlinear_analysis){
-    SetHistoryOutputValue("UTOL", log10(fea_solver->GetRes_FEM(0)));
-    SetHistoryOutputValue("RTOL", log10(fea_solver->GetRes_FEM(1)));
+    SetHistoryOutputValue("RMS_UTOL", log10(fea_solver->GetRes_FEM(0)));
+    SetHistoryOutputValue("RMS_RTOL", log10(fea_solver->GetRes_FEM(1)));
     if (nVar_FEM == 3){
-      SetHistoryOutputValue("ETOL", log10(fea_solver->GetRes_FEM(2)));    
+      SetHistoryOutputValue("RMS_ETOL", log10(fea_solver->GetRes_FEM(2)));    
     }
-    SetHistoryOutputValue("DISP_X", log10(fea_solver->GetRes_FEM(0)));
-    SetHistoryOutputValue("DISP_Y", log10(fea_solver->GetRes_FEM(1)));
+    SetHistoryOutputValue("RMS_DISP_X", log10(fea_solver->GetRes_FEM(0)));
+    SetHistoryOutputValue("RMS_DISP_Y", log10(fea_solver->GetRes_FEM(1)));
     if (nVar_FEM == 3){
-      SetHistoryOutputValue("DISP_Z", log10(fea_solver->GetRes_FEM(2)));    
+      SetHistoryOutputValue("RMS_DISP_Z", log10(fea_solver->GetRes_FEM(2)));    
     }
   }
+  
+  if (linear_analysis){
+    SetHistoryOutputValue("MAX_UTOL", log10(fea_solver->GetRes_Max(0)));
+    SetHistoryOutputValue("MAX_RTOL", log10(fea_solver->GetRes_Max(1)));
+    if (nVar_FEM == 3){
+      SetHistoryOutputValue("MAX_ETOL", log10(fea_solver->GetRes_Max(2)));    
+    }
+    SetHistoryOutputValue("MAX_DISP_X", log10(fea_solver->GetRes_Max(0)));
+    SetHistoryOutputValue("MAX_DISP_Y", log10(fea_solver->GetRes_Max(1)));
+    if (nVar_FEM == 3){
+      SetHistoryOutputValue("MAX_DISP_Z", log10(fea_solver->GetRes_Max(2)));    
+    }
+  } else if (nonlinear_analysis){
+    SetHistoryOutputValue("RMS_UTOL", log10(fea_solver->GetRes_FEM(0)));
+    SetHistoryOutputValue("RMS_RTOL", log10(fea_solver->GetRes_FEM(1)));
+    if (nVar_FEM == 3){
+      SetHistoryOutputValue("RMS_ETOL", log10(fea_solver->GetRes_FEM(2)));    
+    }
+    SetHistoryOutputValue("RMS_DISP_X", log10(fea_solver->GetRes_FEM(0)));
+    SetHistoryOutputValue("RMS_DISP_Y", log10(fea_solver->GetRes_FEM(1)));
+    if (nVar_FEM == 3){
+      SetHistoryOutputValue("RMS_DISP_Z", log10(fea_solver->GetRes_FEM(2)));    
+    }
+  }
+  
+  SetHistoryOutputValue("BGS_UTOL", log10(fea_solver->GetRes_BGS(0)));
+  SetHistoryOutputValue("BGS_RTOL", log10(fea_solver->GetRes_BGS(1)));
+  if (nVar_FEM == 3){
+    SetHistoryOutputValue("BGS_ETOL", log10(fea_solver->GetRes_BGS(2)));    
+  }
+  SetHistoryOutputValue("BGS_DISP_X", log10(fea_solver->GetRes_BGS(0)));
+  SetHistoryOutputValue("BGS_DISP_Y", log10(fea_solver->GetRes_BGS(1)));
+  if (nVar_FEM == 3){
+    SetHistoryOutputValue("BGS_DISP_Z", log10(fea_solver->GetRes_BGS(2)));    
+  }
+
+  
   
   SetHistoryOutputValue("VMS", fea_solver->GetTotal_CFEA());
   SetHistoryOutputValue("LOAD_INCREMENT", fea_solver->GetLoad_Increment());
@@ -121,20 +181,35 @@ void CFEAOutput::LoadHistoryData(CGeometry ****geometry,
 void CFEAOutput::SetHistoryOutputFields(CConfig *config){
   
   // Iteration numbers
-  AddHistoryOutput("INT_ITER",   "Int_Iter",  FORMAT_INTEGER, "INT_ITER");
-  AddHistoryOutput("EXT_ITER",   "Ext_Iter",  FORMAT_INTEGER, "EXT_ITER");
-  
+  AddHistoryOutput("OUTER_ITER",   "Outer_Iter",  FORMAT_INTEGER, "ITER");  
+  AddHistoryOutput("INNER_ITER",   "Inner_Iter",  FORMAT_INTEGER, "ITER");
+
   // Misc.
   AddHistoryOutput("PHYS_TIME",   "Time(min)", FORMAT_SCIENTIFIC, "PHYS_TIME");
   AddHistoryOutput("LINSOL_ITER", "Linear_Solver_Iterations", FORMAT_INTEGER, "LINSOL_ITER");
   
   // Residuals
-  AddHistoryOutput("UTOL",   "Res[U]", FORMAT_FIXED,  "RESIDUALS");
-  AddHistoryOutput("RTOL",   "Res[R]", FORMAT_FIXED,  "RESIDUALS");
-  AddHistoryOutput("ETOL",   "Res[E]", FORMAT_FIXED,  "RESIDUALS");
-  AddHistoryOutput("DISP_X", "Res[DispX]", FORMAT_FIXED,  "RESIDUALS");
-  AddHistoryOutput("DISP_Y", "Res[DispY]", FORMAT_FIXED,  "RESIDUALS");
-  AddHistoryOutput("DISP_Z", "Res[DispZ]", FORMAT_FIXED,  "RESIDUALS");
+  AddHistoryOutput("RMS_UTOL",   "rms[U]", FORMAT_FIXED,  "RMS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("RMS_RTOL",   "rms[R]", FORMAT_FIXED,  "RMS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("RMS_ETOL",   "rms[E]", FORMAT_FIXED,  "RMS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("RMS_DISP_X", "rms[DispX]", FORMAT_FIXED,  "RMS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("RMS_DISP_Y", "rms[DispY]", FORMAT_FIXED,  "RMS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("RMS_DISP_Z", "rms[DispZ]", FORMAT_FIXED,  "RMS_RES", TYPE_RESIDUAL);
+  
+  AddHistoryOutput("MAX_UTOL",   "max[U]", FORMAT_FIXED,  "MAX_RES");
+  AddHistoryOutput("MAX_RTOL",   "max[R]", FORMAT_FIXED,  "MAX_RES");
+  AddHistoryOutput("MAX_ETOL",   "max[E]", FORMAT_FIXED,  "MAX_RES");
+  AddHistoryOutput("MAX_DISP_X", "max[DispX]", FORMAT_FIXED,  "MAX_RES");
+  AddHistoryOutput("MAX_DISP_Y", "max[DispY]", FORMAT_FIXED,  "MAX_RES");
+  AddHistoryOutput("MAX_DISP_Z", "max[DispZ]", FORMAT_FIXED,  "MAX_RES");
+  
+  AddHistoryOutput("BGS_UTOL",   "bgs[U]", FORMAT_FIXED,  "BGS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("BGS_RTOL",   "bgs[R]", FORMAT_FIXED,  "BGS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("BGS_ETOL",   "bgs[E]", FORMAT_FIXED,  "BGS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("BGS_DISP_X", "bgs[DispX]", FORMAT_FIXED,  "BGS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("BGS_DISP_Y", "bgs[DispY]", FORMAT_FIXED,  "BGS_RES", TYPE_RESIDUAL);
+  AddHistoryOutput("BGS_DISP_Z", "bgs[DispZ]", FORMAT_FIXED,  "BGS_RES", TYPE_RESIDUAL);
+  
   
   
   AddHistoryOutput("VMS",            "VonMises_Stress", FORMAT_FIXED, "VMS");
