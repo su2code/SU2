7eef1bacfcc264a8b6263cc690d87c08845763b3
Jairo <jairo.pcfilho@gmail.com>
2018-12-08 01:36:08 -0300

    Included Arina2K test case for SU2_CFD_AD

--- TestCases/parallel_regression_AD.py
+++ TestCases/parallel_regression_AD.py
@@ -74,6 +74,17 @@ def main():
     discadj_cylinder3D.tol       = 0.00001
     test_list.append(discadj_cylinder3D)
 
+    # Arina nozzle 2D
+    discadj_arina2k              = TestCase('discadj_arina2k’)
+    discadj_arina2k.cfg_dir      = "disc_adj_euler/arina2k”
+    discadj_arina2k.cfg_file     = “Arina2K.cfg"
+    discadj_arina2k.test_iter    = 3000
+    discadj_arina2k.test_vals    = [-3.576977, -4.165617, 4.7303e+04, 0.0000e+0] #last 4 columns
+    discadj_arina2k.su2_exec     = "SU2_CFD_AD"
+    discadj_arina2k.timeout      = 8400
+    discadj_arina2k.tol          = 0.00001
+    test_list.append(discadj_arina2k)
+    
     ####################################
     ### Disc. adj. compressible RANS ###
     ####################################
