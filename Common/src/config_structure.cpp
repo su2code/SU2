88204506e953249ac53162302d7de7fe25c41221
Eduardo Molina <molinaedu85@gmail.com>
2019-02-04 13:21:15 -0800

    Update error message for Low Dissipation Schemes.

--- Common/src/config_structure.cpp
+++ Common/src/config_structure.cpp
@@ -2874,8 +2874,8 @@ void CConfig::SetPostprocessing(unsigned short val_software, unsigned short val_
     SU2_MPI::Error("Buffet monitoring incompatible with Euler Solver", CURRENT_FUNCTION);
   }
   
-  if ((Kind_Turb_Model == NONE) && (Kind_RoeLowDiss != NO_ROELOWDISS) && (Unsteady_Simulation == DT_STEPPING_2ND)){
-    SU2_MPI::Error("Low dissipation convective schemes are only available for Unsteady Dual Time-Step RANS simulations.", CURRENT_FUNCTION);
+  if ((Kind_Solver == EULER) && (Kind_RoeLowDiss != NO_ROELOWDISS) && (Unsteady_Simulation == DT_STEPPING_2ND)){
+    SU2_MPI::Error("Low dissipation convective schemes are only available for Unsteady Dual Time-Step NS or RANS simulations.", CURRENT_FUNCTION);
   }
   
   /*--- Check for Fluid model consistency ---*/
