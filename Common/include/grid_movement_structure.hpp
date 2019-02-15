a2f3ba061dd66f2a95bde27355eaa3960119937e
pcarruscag <pcarruscag@gmail.com>
2019-02-15 17:57:06 +0000

    SU2_CFD and _AD compile and run with templated classes instantiated for su2double

--- Common/include/grid_movement_structure.hpp
+++ Common/include/grid_movement_structure.hpp
@@ -967,10 +967,10 @@ protected:
 
 	unsigned long nIterMesh;	/*!< \brief Number of iterations in the mesh update. +*/
 
-	CSysSolve  System;
-  CSysMatrix StiffMatrix; /*!< \brief Matrix to store the point-to-point stiffness. */
-  CSysVector LinSysSol;
-  CSysVector LinSysRes;
+  CSysSolve<su2double>  System;
+  CSysMatrix<su2double> StiffMatrix; /*!< \brief Matrix to store the point-to-point stiffness. */
+  CSysVector<su2double> LinSysSol;
+  CSysVector<su2double> LinSysRes;
 
 public:
 
@@ -1330,10 +1330,10 @@ protected:
   su2double MinVolume;
   su2double MaxVolume;
 
-  CSysSolve  System;
-  CSysMatrix StiffMatrix;      /*!< \brief Matrix to store the point-to-point stiffness. */
-  CSysVector LinSysSol;
-  CSysVector LinSysRes;
+  CSysSolve<su2double>  System;
+  CSysMatrix<su2double> StiffMatrix; /*!< \brief Matrix to store the point-to-point stiffness. */
+  CSysVector<su2double> LinSysSol;
+  CSysVector<su2double> LinSysRes;
 
   su2double E;                  /*!< \brief Young's modulus of elasticity. */
   su2double Nu;                 /*!< \brief Poisson's ratio. */
