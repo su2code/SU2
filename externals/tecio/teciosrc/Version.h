738f2336e93805a2d8bb039554d9f8611bdf022b
Dave Taflin <d.taflin@tecplot.com>
2019-02-20 16:22:49 -0800

    Eliminated compiler warnings.

--- externals/tecio/teciosrc/Version.h
+++ externals/tecio/teciosrc/Version.h
@@ -3,7 +3,7 @@
  #define TecplotMajorVersion 2018
  #define TecplotMinorVersion 3
  #define TecplotPatchVersion 0
- #define TecplotRepositoryRevision 94314
- #define TecplotProductVersion "2018.3.0.94314"
+ #define TecplotRepositoryRevision 94816
+ #define TecplotProductVersion "2018.3.0.94816"
  #define TecplotProductEdition ""
  #define TecplotProductRelease ""
