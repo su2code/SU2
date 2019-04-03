fe8f2153ac3243070fe70ede8a0d60f086638833
Tim Albring <tim.albring@scicomp.uni-kl.de>
2019-04-03 15:37:35 +0200

    Some small fixes

--- SU2_CFD/src/iteration_structure.cpp
+++ SU2_CFD/src/iteration_structure.cpp
@@ -1488,8 +1488,7 @@ void CFEAIteration::Iterate(COutput *output,
 
         /*--- Write the convergence history (first, compute Von Mises stress) ---*/
         solver_container[val_iZone][val_iInst][MESH_0][FEA_SOL]->Compute_NodalStress(geometry_container[val_iZone][val_iInst][MESH_0], solver_container[val_iZone][val_iInst][MESH_0], numerics_container[val_iZone][val_iInst][MESH_0][FEA_SOL], config_container[val_iZone]);
-        write_output = output->PrintOutput(IntIter-1, config_container[val_iZone]->GetWrt_Con_Freq_DualTime());
-        if (write_output) output->SetConvHistory_Body(geometry_container, solver_container, config_container, integration_container, false, 0.0, val_iZone, val_iInst);
+        output->SetConvHistory_Body(geometry_container, solver_container, config_container, integration_container, false, 0.0, val_iZone, val_iInst);
 
         config_container[val_iZone]->SetInnerIter(IntIter);
         config_container[val_iZone]->SetIntIter(IntIter);
