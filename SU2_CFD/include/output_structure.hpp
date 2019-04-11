fd21edd962784e19404efd75e6c8719980196f78
Tim Albring <tim.albring@scicomp.uni-kl.de>
2019-04-11 15:19:58 +0200

    Added a routine to append all necessary numbers to a filename

--- SU2_CFD/include/output_structure.hpp
+++ SU2_CFD/include/output_structure.hpp
@@ -612,6 +612,15 @@ public:
   void WriteSurface_CSV(CConfig *config, CGeometry *geometry);
   
   /*!
+   * \brief Add any numbers necessary to the filename (iteration number, zone ID ...)
+   * \param[in] config - Definition of the particular problem.
+   * \param[in] filename - the base filename.
+   * \param[in] ext - the extension to be added.
+   * \return The new filename
+   */
+  string GetFilename(CConfig *config, string filename, string ext);
+  
+  /*!
    * \brief Write the nodal coordinates and connectivity to a Tecplot binary mesh file.
    * \param[in] config - Definition of the particular problem.
    * \param[in] geometry - Geometrical definition of the problem.
