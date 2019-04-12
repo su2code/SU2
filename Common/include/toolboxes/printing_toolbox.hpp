a5359b0de4574a0022cab387ae189405bc5b8045
Tim Albring <tim.albring@scicomp.uni-kl.de>
2019-04-12 17:44:34 +0200

    added a routine to split strings and made ReadBoundSensitivity work

--- Common/include/toolboxes/printing_toolbox.hpp
+++ Common/include/toolboxes/printing_toolbox.hpp
@@ -157,4 +157,16 @@ private:
   print_header_bottom_line_;   /*< \brief Printing the header bottom line. */
 };
 
+inline std::vector<std::string> split(const std::string& s, char delimiter)
+{
+  std::vector<std::string> tokens;
+  std::string token;
+  std::istringstream tokenStream(s);
+  while (std::getline(tokenStream, token, delimiter))
+  {
+    tokens.push_back(token);
+  }
+  return tokens;
+}
+
 }
