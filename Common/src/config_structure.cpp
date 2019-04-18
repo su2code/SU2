182084ad43092a8f40d9509419181ec13e87e87b
WallyMaier <wallytmaier@gmail.com>
2019-04-18 09:47:24 -0700

    changing 0 => REYNOLD

--- Common/src/config_structure.cpp
+++ Common/src/config_structure.cpp
@@ -2948,7 +2948,7 @@ void CConfig::SetPostprocessing(unsigned short val_software, unsigned short val_
   }
   
   /*--- Check for Boundary condition option agreement ---*/
-  if (Kind_InitOption == 0){
+  if (Kind_InitOption == REYNOLDS){
     if (Kind_Regime == COMPRESSIBLE && (Kind_Solver == NAVIER_STOKES || Kind_Solver == RANS) && Reynolds <=0){
       SU2_MPI::Error("Reynolds number required for NAVIER_STOKES and RANS !!", CURRENT_FUNCTION);
     }
