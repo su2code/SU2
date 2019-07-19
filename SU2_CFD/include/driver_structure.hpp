c8f0e4b35cca185d1c9ddb77862c0bc835fb6302
Tim Albring <tim.albring@scicomp.uni-kl.de>
2019-07-19 14:45:23 +0200

    Added some comments

--- SU2_CFD/include/driver_structure.hpp
+++ SU2_CFD/include/driver_structure.hpp
@@ -1583,17 +1583,31 @@ public:
 
 };
 
-
+/*!
+ * \brief CDummyDriver class that constructs the driver without running a solver.
+ */
 class CDummyDriver : public CDriver {
   
 public:
   
+  /*!
+   * \brief Constructor of the class.
+   * \param[in] confFile - Configuration file name.
+   * \param[in] val_nZone - Total number of zones.
+   * \param[in] MPICommunicator - MPI communicator for SU2.
+   */
   CDummyDriver (char* confFile,
                unsigned short val_nZone,
                SU2_Comm MPICommunicator);
   
+  /*!
+   * \brief Destructor of the class.
+   */
   ~CDummyDriver(){}
   
+  /*!
+   * \brief Does nothing except printing the information that no solver is running.
+   */
   void StartSolver();
   
 };
