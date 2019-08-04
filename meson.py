8921f8f4ef8c4120de847ed30626a6d8ac455bbd
Tim Albring <tim.albring@scicomp.uni-kl.de>
2019-08-04 15:49:14 +0200

    Rework of meson.py

--- meson.py
+++ meson.py
@@ -26,10 +26,12 @@
 # License along with SU2. If not, see <http://www.gnu.org/licenses/>.
 
 
-import sys, os, subprocess, shutil
+import sys, os, subprocess, shutil, urllib.request, zipfile
 from pathlib import Path
 
 def setup_environment():
+
+  cur_dir = sys.path[0]
 
   # This information of the modules is used if projects was not cloned using git
   # The sha tag must be maintained manually to point to the correct commit
@@ -47,115 +49,116 @@ def setup_environment():
   meson_name = 'meson'
   ninja_name= 'ninja'
 
-  alt_name_medi =   sys.path[0] + '/externals/medi'
-  alt_name_codi =   sys.path[0] + '/externals/codi'
-  alt_name_meson =  sys.path[0] + '/externals/meson'
-  alt_name_ninja =  sys.path[0] + '/externals/ninja'
+  alt_name_medi =   cur_dir + '/externals/medi'
+  alt_name_codi =   cur_dir + '/externals/codi'
+  alt_name_meson =  cur_dir + '/externals/meson'
+  alt_name_ninja =  cur_dir + '/externals/ninja'
 
-  # Some log and error files
-  log = open( 'config.log', 'w' )
-  err = open( 'config.err', 'w' )
-  pkg_environ = os.environ
+  # If directory was cloned using git, use submodule feature 
+  # to check and initialize submodules if necessary
+  if is_git_directory(cur_dir):
+    submodule_status(alt_name_codi, sha_version_codi)
+    submodule_status(alt_name_medi, sha_version_medi)
+    submodule_status(alt_name_meson, sha_version_meson)
+    submodule_status(alt_name_ninja, sha_version_ninja)
+  # Otherwise download the zip file from git
+  else:
+    download_module(codi_name, alt_name_codi, github_repo_codi, sha_version_codi)
+    download_module(medi_name, alt_name_medi, github_repo_medi, sha_version_medi)
+    download_module(meson_name, alt_name_meson, github_repo_meson, sha_version_meson)
+    download_module(ninja_name, alt_name_ninja, github_repo_ninja, sha_version_ninja)
 
-  submodule_check(codi_name, alt_name_codi, github_repo_codi, sha_version_codi, log, err)
-  submodule_check(medi_name, alt_name_medi, github_repo_medi, sha_version_medi, log, err)
-  submodule_check(meson_name, alt_name_meson, github_repo_meson, sha_version_meson, log, err)
-  submodule_check(ninja_name, alt_name_ninja, github_repo_ninja, sha_version_ninja, log, err)
+def is_git_directory(path = '.'):
+  return subprocess.call(['git', '-C', path, 'status'], stderr=subprocess.STDOUT, stdout = open(os.devnull, 'w')) == 0 
+
+def submodule_status(path, sha_commit):
   
-  log.close()
-  err.close()
+  # Check the status of the submodule
+  status = subprocess.run(['git', 'submodule','status', path], stdout=subprocess.PIPE, check = True).stdout.decode('utf-8')
 
-
-def submodule_check(name, alt_name, github_rep, sha_tag, log, err, update = False):
-  try:
-      status = submodule_status(alt_name, update)
-      if status:
-          print('Found correct version of ' + name + '.')
-
-  except RuntimeError:
-      if all([os.path.exists(alt_name), not os.path.exists(alt_name + '/' + sha_tag)]):
-        print('Found an old or unspecified version of ' + name + ' in ' + alt_name)
-        sys.exit()
-      if not os.path.exists(alt_name):
-        print('\ngit command failed (either git is not installed or this is not a git repository).')
-        print('\nUsing fall-back method to initialize submodule ' + name)
-        download_module(name, alt_name, github_rep, sha_tag, log, err)
-      else:
-        print('Found correct version of ' + name + ' in ' + alt_name + '.')
-
-
-def submodule_status(path, update):
-
-  try:
-      status   = check_output('git submodule status ' + path).decode()
-  except RuntimeError as e:
-      print(e)
-      raise RuntimeError
-
+  # The first character of the output indicates the status of the submodule
+  # '+' : The submodule does not match the SHA-1 currently in the index of the repository
+  # '-' : The submodule is not initialized
+  # ' ' : Correct version of submodule is initialized
   status_indicator = status[0][0]
 
+  
   if status_indicator == '+':
-      sys.stderr.write('WARNING: the currently checked out submodule commit in ' + path + ' does not match the SHA-1 found in the index.\n')
-      sys.stderr.write('Use \'git submodule update --init '+ path + '\' to reset the module if necessary.\n')
-      return False
-  elif any([status_indicator == '-', update]):
-      print('Initialize submodule ' + path + ' using git ... ')
-      subprocess.check_call('git submodule update --init ' + path, shell = True)
+    # Write a warning that the sha tags do not match
+    sys.stderr.write('WARNING: the currently checked out submodule commit in '
+                      + path + ' does not match the SHA-1 found in the index.\n')
+    sys.stderr.write('Use \'git submodule update --init '+ path + '\' to reset the module if necessary.\n')
+  elif status_indicator == '-':
+    # Initialize the submodule if necessary 
+    print('Initialize submodule ' + path + ' using git ... ')
+    subprocess.run(['git', 'submodule', 'update', '--init', path], check = True)
 
-  return True
+    # Check that the SHA tag stored in this file matches the one stored in the git index
+    cur_sha_commit = status[1:].split(' ')[0]
+    if (cur_sha_commit != sha_commit):
+      print('SHA-1 tag stored in index does not match SHA tag stored in this script.')
+      sys.exit(1)
+  
 
-def download_module(name, alt_name, git_repo, commit_sha, logfile, errorfile):
+def download_module(name, alt_name, git_repo, commit_sha):
+  
+  # ZipFile does not preserve file permissions. 
+  # This is a workaround for that problem:
+  # https://stackoverflow.com/questions/39296101/python-zipfile-removes-execute-permissions-from-binaries
+  class MyZipFile(zipfile.ZipFile):
+    def _extract_member(self, member, targetpath, pwd):
+      if not isinstance(member, zipfile.ZipInfo):
+        member = self.getinfo(member)
 
-  print('\nInitializing ' + name + ' \'' + commit_sha + '\'')
-  print('=====================================================================')
-  # Download package
-  try:
-      print('Downloading module from ' + git_repo)
-      subprocess.check_call('wget -N ' + git_repo + '/archive/' + commit_sha + '.zip', stdout = logfile, stderr = errorfile, shell = True )
-  except subprocess.CalledProcessError:
-      print('Download of module ' + name + ' failed. See config.err for more information.')
-      print('To download it manually, perform the following steps:')
-      print('\t - Download the zip at \"' + git_repo + '/archive/' + commit_sha + '.zip\"')
-      print('\t - Extract the archive to externals/' + alt_name)
-      print('\t - Execute command \'touch externals/'+ alt_name + '/' + commit_sha + '\'')
-      print('\t - Run preconfigure.py again')
-      sys.exit()
+      targetpath = super()._extract_member(member, targetpath, pwd)
 
-  # Extract zip archive
-  try:
-      print('Extracting archive ...')
-      subprocess.check_call('unzip -u ' + commit_sha + '.zip', stdout = logfile, stderr = errorfile, shell=True)
-  except subprocess.CalledProcessError:
-      print('Extraction of module ' + name + ' failed. See config.err for more information.')
-      sys.exit()
+      attr = member.external_attr >> 16
+      if attr != 0:
+        os.chmod(targetpath, attr)
+      return targetpath
 
-  # Rename folder and create a file to identify the version
-  try:
-      print('Creating identifier ...')
-      subprocess.check_call('mv '+ name + '-' + commit_sha + ' ' + alt_name + ' && touch ' + alt_name + '/' + commit_sha, stdout = logfile, stderr = errorfile, shell = True)
-  except subprocess.CalledProcessError:
-      print('Renaming of module ' + name + ' failed. See config.err for more information.')
-      sys.exit()
+  if not os.path.exists(alt_name + '/' + commit_sha):
 
-  # Remove archive
-  subprocess.check_call('rm ' + commit_sha + '.zip', shell=True)
+    print('Downloading ' + name + ' \'' + commit_sha + '\'')
+  
+    filename = commit_sha + '.zip'
 
-def check_output(cmd):
-    std, err = subprocess.Popen([cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True).communicate()
-    if err:
-        raise RuntimeError(err)
-    return std
+    url = git_repo + '/archive/' + filename
+
+    if not os.path.exists(sys.path[0] + '/' + filename):
+      try:
+        urllib.request.urlretrieve (url, commit_sha + '.zip')
+      except RuntimeError as e:
+        print(e)
+        print('Download of module ' + name + ' failed.')
+        print('Get archive at ' + url)
+        print('and place it in the source code root folder')
+        print('Run meson.py again')
+        sys.exit()
+ 
+    # Unzip file 
+    zipf = MyZipFile(sys.path[0] + '/' + filename)
+    zipf.extractall(sys.path[0] + '/externals')
+
+    shutil.move(sys.path[0] + '/externals/' + name + '-' + commit_sha, alt_name)
+
+    # Delete zip file
+    os.remove(sys.path[0] + '/' + filename)
+
+    # Create identifier
+    f = open(alt_name + '/' + commit_sha, 'w')
+    f.close()
+   
 
 def build_ninja():
 
-  ninjapath = 'externals/ninja'
+  ninjapath = sys.path[0] + '/externals/ninja'
     
   try:
-    p = subprocess.Popen([sys.path[0] + '/ninja', '--version'], stdout=subprocess.PIPE, shell=False)
+    p = subprocess.run([sys.path[0] + '/ninja', '--version'], stdout=subprocess.PIPE)
   except:
     print("ninja executable not found. Building ...")
-    p = subprocess.Popen(['./configure.py', '--bootstrap'], stdout=subprocess.PIPE, shell=False, cwd=ninjapath)
-    out, err = p.communicate() 
+    p = subprocess.run(['./configure.py', '--bootstrap'], cwd=ninjapath)
     shutil.copy(ninjapath+'/ninja', '.')
 
 if __name__ == '__main__':
@@ -165,6 +168,7 @@ if __name__ == '__main__':
   # Set up the build environment, i.e. clone or download all submodules
   setup_environment()
 
+  # Build ninja if it cannot be found
   build_ninja()
 
   # Add paths for meson and ninja to environment
