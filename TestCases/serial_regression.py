887493367bea63da5be65f0d86bc57817e95218a
Tim Albring <tim.albring@scicomp.uni-kl.de>
2019-08-28 10:29:25 +0200

    Adapted FEA test cases

--- TestCases/serial_regression.py
+++ TestCases/serial_regression.py
@@ -1121,7 +1121,7 @@ def main():
     dynbeam2d.unsteady  = True
     dynbeam2d.new_output= True
     dynbeam2d.test_iter = 6
-    dynbeam2d.test_vals = [0.000000, 0.000000, 0.000000, 6.6127e+04] #last 4 columns
+    dynbeam2d.test_vals = [-3.240015, 2.895057, -0.353146, 6.6127e+04] #last 4 columns
     dynbeam2d.su2_exec  = "SU2_CFD"
     dynbeam2d.timeout   = 1600
     dynbeam2d.tol       = 0.00001
