09f48679f58ffded3b168a504f34855d8a05e5c0
bmunguia <bmunguia10@gmail.com>
2019-11-05 12:11:47 -0800

    Reverting

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -826,8 +826,7 @@ void COneShotFluidDriver::StoreGradDotDir(){
   GradDotDir = 0.0;
   for (iDV=0;iDV<nDV_Total;iDV++){
     /*--- AugmentedLagrangianGradient is the gradient at the old iterate. ---*/
-    // GradDotDir += DesignVarUpdate[iDV]*AugmentedLagrangianGradient[iDV];
-    GradDotDir += SearchDirection[iDV]*AugmentedLagrangianGradient[iDV];
+    GradDotDir += DesignVarUpdate[iDV]*AugmentedLagrangianGradient[iDV];
   }
 }
 
