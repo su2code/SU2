e3923f6f2ea3476c63e336c383715dfe25690253
bmunguia <bmunguia10@gmail.com>
2019-11-11 10:38:10 -0800

    UpdateMultiplier with stepsize

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -256,21 +256,21 @@ void COneShotFluidDriver::RunOneShot(){
 
         /*---Load the old design for line search---*/
         solver[ADJFLOW_SOL]->LoadMeshPointsOld(config, geometry);
-        // LoadMultiplier();
+        LoadMultiplier();
         // UpdateMultiplier(stepsize);
       }
       else{
         /*--- Store and update constraint multiplier ---*/
-        // StoreMultiplier();
+        StoreMultiplier();
         // StoreMultiplierGrad();
-        UpdateMultiplier(1.0);
+        // UpdateMultiplier(1.0);
       }
 
       /*--- Compute and store GradL dot p ---*/
       StoreGradDotDir();
 
       /*--- Update multiplier ---*/
-      // UpdateMultiplier(stepsize);
+      UpdateMultiplier(stepsize);
 
       /*--- Load the old solution for line search (either y_k or y_k-1) ---*/
       solver[ADJFLOW_SOL]->LoadSolution();
@@ -285,7 +285,7 @@ void COneShotFluidDriver::RunOneShot(){
       else {
         stepsize = 0.0;
         grid_movement[ZONE_0][INST_0]->UpdateDualGrid(geometry, config);
-        // LoadMultiplier();
+        LoadMultiplier();
         ComputeDesignVarUpdate(0.0);
       }
 
