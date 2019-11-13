5e8af29e763ce05aa15f87dd3967a890edf55496
Steffen Schotthöfer <steffen.schotthoefer@outlook.de>
2019-11-13 08:59:42 +0100

    minimal version for cluster

--- SU2_CFD/src/output/COutput.cpp
+++ SU2_CFD/src/output/COutput.cpp
@@ -1555,10 +1555,11 @@ void COutput::Postprocess_HistoryData(CConfig *config){
         if (config->GetTime_Domain()){
           //runningAverages[historyOutput_List[iField]].addValue(currentField.value,config->GetTimeIter(), config->GetStartWindowIteration()); //Collecting Values for Windowing
           //SetHistoryOutputValue("TAVG_" + fieldIdentifier,runningAverages[fieldIdentifier].WindowedUpdate(config->GetWindowIdx()));
-          SetHistoryOutputValue("TAVG_" + fieldIdentifier,windowedTimeAverages[fieldIdentifier].Update(currentField.value));
-
+            SetHistoryOutputValue("TAVG_" + fieldIdentifier,
+                                  windowedTimeAverages[fieldIdentifier].Update(currentField.value));
           if (config->GetDirectDiff() != NO_DERIVATIVE) {
-            SetHistoryOutputValue("D_TAVG_" + fieldIdentifier, SU2_TYPE::GetDerivative(windowedTimeAverages[fieldIdentifier].Get()));
+            SetHistoryOutputValue("D_TAVG_" + fieldIdentifier,
+                                  SU2_TYPE::GetDerivative(windowedTimeAverages[fieldIdentifier].Get()));
           }
         }
       }
