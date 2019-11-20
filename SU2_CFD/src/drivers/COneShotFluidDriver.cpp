3a9a09d346b37d7f853c59c5cf3f2e0c57f6d2dd
bmunguia <bmunguia10@gmail.com>
2019-11-19 21:26:57 -0800

    Update max(mu_store + gamma*(h-P_I(h+mu_store/gamma)), 0.)

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1218,7 +1218,8 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
       Multiplier[iConstr] = 0.;
     }
     else {
-      Multiplier[iConstr] += stepsize*gamma*ConstrFunc_Store[iConstr];
+      Multiplier[iConstr] = Multiplier_Store[iConstr] + stepsize*gamma*ConstrFunc_Store[iConstr];
+      Multiplier_Store[iConstr] = Multiplier[iConstr];
     }
 
     if(config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) {
