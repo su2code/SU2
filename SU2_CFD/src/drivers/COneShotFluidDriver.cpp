cd9af41e5df1c1b3ed7a3015e66c4c044b5a2c34
bmunguia <bmunguia10@gmail.com>
2019-11-25 09:24:40 -0800

    Overestimate inactive set for update, and use mu_store

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1201,7 +1201,7 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
     // }
 
     /*--- gamma*(h-P_I(h+mu/gamma)) ---*/
-    if(config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR && ConstrFunc_Store[iConstr] + Multiplier_Old[iConstr]/gamma  <= 0.) {
+    if(config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR && ConstrFunc_Store[iConstr] - Multiplier_Old[iConstr]/gamma  <= 0.) {
       Multiplier[iConstr] = 0.;
       // Multiplier_Store[iConstr] += stepsize*gamma*ConstrFunc_Store[iConstr];
     }
