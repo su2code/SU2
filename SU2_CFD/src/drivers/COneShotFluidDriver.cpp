6088d8e9cbb64a112adc1cb9eb000712b3822456
bmunguia <bmunguia10@gmail.com>
2019-11-25 09:14:30 -0800

    Overestimate active set for update, and use mu_store

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1201,15 +1201,15 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
     // }
 
     /*--- gamma*(h-P_I(h+mu/gamma)) ---*/
-    if(config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR && ConstrFunc_Store[iConstr] <= 0.) {
+    if(config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR && ConstrFunc_Store[iConstr] + Multiplier_Old[iConstr]/gamma  <= 0.) {
       Multiplier[iConstr] = 0.;
       // Multiplier_Store[iConstr] += stepsize*gamma*ConstrFunc_Store[iConstr];
     }
     else {
-      Multiplier[iConstr] = Multiplier_Old[iConstr] + stepsize*gamma*ConstrFunc_Store[iConstr];
+      Multiplier[iConstr] = Multiplier_Store[iConstr] + stepsize*gamma*ConstrFunc_Store[iConstr];
       // Multiplier_Store[iConstr] = Multiplier[iConstr];
     }
-    // Multiplier_Store[iConstr] += stepsize*gamma*ConstrFunc_Store[iConstr];
+    Multiplier_Store[iConstr] += stepsize*gamma*ConstrFunc_Store[iConstr];
 
     if(config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) {
       if(Multiplier[iConstr]*ConstrFunc_Store[iConstr] < 0.) {
