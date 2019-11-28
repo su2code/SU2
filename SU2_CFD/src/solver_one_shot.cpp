ff6ce78b2c681203e139015125be439d7c4d4d55
bmunguia <bmunguia10@gmail.com>
2019-11-27 19:17:15 -0800

    New max rho

--- SU2_CFD/src/solver_one_shot.cpp
+++ SU2_CFD/src/solver_one_shot.cpp
@@ -278,7 +278,7 @@ void COneShotSolver::CalculateRhoTheta(CConfig *config){
   // helper       = myHelper;
 #endif
 
-  rho   = min(max(sqrt(normDeltaNew)/sqrt(normDelta), 0.9*rho_old), 1.0-1E-8); // Saturate contractivity
+  rho   = min(max(sqrt(normDeltaNew)/sqrt(normDelta), 0.9*rho_old), 0.99); // Saturate contractivity
   // theta = max(sqrt(fabs(helper)/normDelta*theta_old), 0.9*theta_old);
 
   /* --- Store rho and theta values for this iteration --- */
