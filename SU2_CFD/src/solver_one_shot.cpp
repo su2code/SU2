4a08f5803f17c215405417a6627ee7aeb5949fa1
bmunguia <bmunguia10@gmail.com>
2019-11-29 20:19:48 -0800

    Store rho/theta every iteration

--- SU2_CFD/src/solver_one_shot.cpp
+++ SU2_CFD/src/solver_one_shot.cpp
@@ -281,6 +281,10 @@ void COneShotSolver::CalculateRhoTheta(CConfig *config){
   rho   = min(max(sqrt(normDeltaNew)/sqrt(normDelta), 0.9*rho_old), 1.0-1.0E-8); // Saturate contractivity
   theta = min(max(sqrt(fabs(helper)/normDelta*theta_old), 0.9*theta_old), theta_old/0.9);
 
+  /* --- Store rho and theta values for this iteration --- */
+  rho_old   = rho;
+  theta_old = theta;
+
 }
 
 void COneShotSolver::CalculateAlphaBeta(CConfig *config){
@@ -294,9 +298,6 @@ void COneShotSolver::CalculateAlphaBeta(CConfig *config){
   config->SetOneShotAlpha(alpha);
   config->SetOneShotBeta(beta);
 
-  /* --- Store rho and theta values for this iteration --- */
-  rho_old   = rho;
-  theta_old = theta;
 }
 
 void COneShotSolver::CalculateGamma(CConfig *config, su2double val_bcheck_norm){
