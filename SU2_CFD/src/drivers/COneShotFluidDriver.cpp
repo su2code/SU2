787e2832611981a275979f1090463d09f124ce28
bmunguia <bmunguia10@gmail.com>
2019-11-29 18:54:28 -0800

    Precondition even when inactive

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1231,9 +1231,9 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
         helper += BCheck_Inv[iConstr][jConstr]*ConstrFunc_Store[jConstr];
       }
       else {
-        // helper -= BCheck_Inv[iConstr][jConstr]*Multiplier_Old[jConstr]/gamma;
-        helper -= Multiplier_Old[iConstr]/stepsize;
-        break;
+        helper -= BCheck_Inv[iConstr][jConstr]*Multiplier_Old[jConstr]/gamma;
+        // helper -= Multiplier_Old[iConstr]/stepsize;
+        // break;
       }
     }
     Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
