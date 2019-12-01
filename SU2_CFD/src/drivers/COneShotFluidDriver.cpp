641a676d40e1624e1925f0834ed350acb3403427
bmunguia <bmunguia10@gmail.com>
2019-11-30 16:31:13 -0800

    gamma*h

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1227,19 +1227,19 @@ void COneShotFluidDriver::LoadOldMultiplier(){
 void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
   su2double helper, gamma = config->GetOneShotGamma();
   for(unsigned short iConstr = 0; iConstr < nConstr; iConstr++){
-    /*--- BCheck^(-1)*(h-P_I(h+mu/gamma)) ---*/
-    helper = 0.0;
-    for(unsigned short jConstr = 0; jConstr < nConstr; jConstr++){
-      if((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) || (ConstrFunc_Store[iConstr] - Multiplier_Old[iConstr]/gamma > 0.)) {
-        helper += BCheck_Inv[iConstr][jConstr]*ConstrFunc_Store[jConstr];
-      }
-      else {
-        helper -= BCheck_Inv[iConstr][jConstr]*Multiplier_Old[jConstr]/gamma;
-        // helper -= Multiplier_Old[iConstr]/stepsize;
-        // break;
-      }
-    }
-    Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
+    // /*--- BCheck^(-1)*(h-P_I(h+mu/gamma)) ---*/
+    // helper = 0.0;
+    // for(unsigned short jConstr = 0; jConstr < nConstr; jConstr++){
+    //   if((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) || (ConstrFunc_Store[iConstr] - Multiplier_Old[iConstr]/gamma > 0.)) {
+    //     helper += BCheck_Inv[iConstr][jConstr]*ConstrFunc_Store[jConstr];
+    //   }
+    //   else {
+    //     helper -= BCheck_Inv[iConstr][jConstr]*Multiplier_Old[jConstr]/gamma;
+    //     // helper -= Multiplier_Old[iConstr]/stepsize;
+    //     // break;
+    //   }
+    // }
+    // Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
 
     // /*--- BCheck^(-1)*(h-P_I(h+mu/gamma)) ---*/
     // if(config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR && ConstrFunc_Store[iConstr] - Multiplier_Old[iConstr]/gamma <= 0.) {
@@ -1253,13 +1253,13 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
     //   Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
     // }
 
-    // /*--- gamma*(h-P_I(h+mu/gamma)) ---*/
-    // if((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) || (ConstrFunc_Store[iConstr] + Multiplier_Old[iConstr]/gamma > 0.)) {
-    //   Multiplier[iConstr] = Multiplier_Old[iConstr] + stepsize*gamma*ConstrFunc_Store[iConstr];
-    // }
-    // else {
-    //   Multiplier[iConstr] = 0.;
-    // }
+    /*--- gamma*(h-P_I(h+mu/gamma)) ---*/
+    if((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) || (ConstrFunc_Store[iConstr] + Multiplier_Old[iConstr]/gamma > 0.)) {
+      Multiplier[iConstr] = Multiplier_Old[iConstr] + stepsize*gamma*ConstrFunc_Store[iConstr];
+    }
+    else {
+      Multiplier[iConstr] = 0.;
+    }
     // Multiplier_Store[iConstr] += stepsize*gamma*ConstrFunc_Store[iConstr];
 
     if(config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) {
