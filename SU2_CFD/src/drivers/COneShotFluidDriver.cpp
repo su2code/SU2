425d07a02f048181843eac58bb798f5a623da95d
bmunguia <bmunguia10@gmail.com>
2019-12-03 15:38:21 -0800

    mu_store

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1262,9 +1262,13 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
     /*--- Only update if constraint violation improves ---*/
     else if(((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) && (ConstrFunc_Store[iConstr]*(ConstrFunc[iConstr]-ConstrFunc_Store[iConstr]) < 0.)) ||
             (ConstrFunc[iConstr] - ConstrFunc_Store[iConstr] < 0.)) {
-      // Multiplier[iConstr] = Multiplier_Store[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
-      Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
-      Multiplier_Store[iConstr] = Multiplier[iConstr];
+      Multiplier[iConstr] = Multiplier_Store[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
+      // Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
+      // Multiplier_Store[iConstr] = Multiplier[iConstr];
+    }
+    if(((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) && (ConstrFunc_Store[iConstr]*(ConstrFunc[iConstr]-ConstrFunc_Store[iConstr]) < 0.)) ||
+       (ConstrFunc[iConstr] - ConstrFunc_Store[iConstr] < 0.)) {
+      Multiplier_Store[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
     }
     // Multiplier_Store[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
 
