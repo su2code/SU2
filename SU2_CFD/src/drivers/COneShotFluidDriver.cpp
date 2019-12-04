0fa1177fcf8812ad0640221f8c7a1b35de53f217
bmunguia <bmunguia10@gmail.com>
2019-12-04 00:25:34 -0800

    Always update multiplier

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1252,6 +1252,27 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
     // }
     // Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
 
+    // /*--- BCheck^(-1)*(h-P_I(h+mu/gamma)) ---*/
+    // helper = 0.0;
+    // for(unsigned short jConstr = 0; jConstr < nConstr; jConstr++){
+    //   helper += BCheck_Inv[iConstr][jConstr]*ConstrFunc_Store[jConstr];
+    // }
+    // Multiplier[iConstr] = Multiplier_Store[iConstr];
+    // if(config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR && ConstrFunc_Store[iConstr] + Multiplier_Old[iConstr]/gamma <= 0.) {
+    //   Multiplier[iConstr] = 0.;
+    // }
+    // /*--- Only update if constraint violation improves ---*/
+    // else if(((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) && (ConstrFunc_Store[iConstr]*(ConstrFunc[iConstr]-ConstrFunc_Store[iConstr]) < 0.)) ||
+    //         (ConstrFunc[iConstr] - ConstrFunc_Store[iConstr] < 0.)) {
+    //   Multiplier[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
+    //   // Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
+    //   // Multiplier_Store[iConstr] = Multiplier[iConstr];
+    // }
+    // if(((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) && (ConstrFunc_Store[iConstr]*(ConstrFunc[iConstr]-ConstrFunc_Store[iConstr]) < 0.)) ||
+    //    (ConstrFunc[iConstr] - ConstrFunc_Store[iConstr] < 0.)) {
+    //   Multiplier_Store[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
+    // }
+
     /*--- BCheck^(-1)*(h-P_I(h+mu/gamma)) ---*/
     helper = 0.0;
     for(unsigned short jConstr = 0; jConstr < nConstr; jConstr++){
@@ -1262,18 +1283,13 @@ void COneShotFluidDriver::UpdateMultiplier(su2double stepsize){
       Multiplier[iConstr] = 0.;
     }
     /*--- Only update if constraint violation improves ---*/
-    else if(((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) && (ConstrFunc_Store[iConstr]*(ConstrFunc[iConstr]-ConstrFunc_Store[iConstr]) < 0.)) ||
-            (ConstrFunc[iConstr] - ConstrFunc_Store[iConstr] < 0.)) {
+    else {
       Multiplier[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
       // Multiplier[iConstr] = Multiplier_Old[iConstr] + helper*stepsize*config->GetMultiplierScale(iConstr);
       // Multiplier_Store[iConstr] = Multiplier[iConstr];
     }
-    if(((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) && (ConstrFunc_Store[iConstr]*(ConstrFunc[iConstr]-ConstrFunc_Store[iConstr]) < 0.)) ||
-       (ConstrFunc[iConstr] - ConstrFunc_Store[iConstr] < 0.)) {
-      Multiplier_Store[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
-    }
-    // Multiplier_Store[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
-
+    Multiplier_Store[iConstr] += helper*stepsize*config->GetMultiplierScale(iConstr);
+ 
     // /*--- gamma*(h-P_I(h+mu/gamma)) ---*/
     // if((config->GetKind_ConstrFuncType(iConstr) == EQ_CONSTR) || (ConstrFunc_Store[iConstr] + Multiplier_Old[iConstr]/gamma > 0.)) {
     //   Multiplier[iConstr] = Multiplier_Old[iConstr] + stepsize*gamma*ConstrFunc_Store[iConstr];
