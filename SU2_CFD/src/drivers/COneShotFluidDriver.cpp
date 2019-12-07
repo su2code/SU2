c9b3dfec39e98f4ac72db2c6796a8a961a873745
bmunguia <bmunguia10@gmail.com>
2019-12-07 15:10:02 -0800

    Alpha/beta/gamma every iteration

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -383,8 +383,10 @@ void COneShotFluidDriver::RunOneShot(){
     SetAugLagGrad(TOTAL_AUGMENTED_OLD);
   }
   else if(InnerIter > config->GetOneShotStart() && 
-          InnerIter < config->GetOneShotStop()  && 
-          ((!CheckFirstWolfe(true)) || (ArmijoIter > nArmijoIter-1) || (bool_tol))){
+          InnerIter < config->GetOneShotStop()) {
+  // else if(InnerIter > config->GetOneShotStart() && 
+          // InnerIter < config->GetOneShotStop()  && 
+          // ((!CheckFirstWolfe(true)) || (ArmijoIter > nArmijoIter-1) || (bool_tol))){
     // LoadOldLambda();
     // UpdateLambda(1.0);
     solver[ADJFLOW_SOL]->CalculateAlphaBeta(config);
