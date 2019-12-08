3197dfa18000e49195a37401d3daff1dafc6a81e
bmunguia <bmunguia10@gmail.com>
2019-12-08 13:49:14 -0800

    No Lambda_Tilde

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -559,7 +559,7 @@ void COneShotFluidDriver::PrimalDualStep(){
   /*--- Initialize the adjoint of the objective function with 1.0. ---*/
 
   SetAdj_ObjFunction();
-  SetAdj_ConstrFunction(Lambda_Tilde);
+  SetAdj_ConstrFunction(Lambda_Old);
 
   /*--- Interpret the stored information by calling the corresponding routine of the AD tool. ---*/
 
