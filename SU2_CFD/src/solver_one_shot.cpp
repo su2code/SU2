8ebb8fc8739e5755617f241e4d77c85b6d941cdd
bmunguia <bmunguia10@gmail.com>
2019-12-07 21:24:50 -0800

    Non-decreasing gamma

--- SU2_CFD/src/solver_one_shot.cpp
+++ SU2_CFD/src/solver_one_shot.cpp
@@ -308,10 +308,10 @@ void COneShotSolver::CalculateGamma(CConfig *config, su2double val_bcheck_norm, 
   /* --- Estimate gamma value --- */
   for(iConstr = 0; iConstr < nConstr; iConstr++) {
     su2double gamma = 2./val_bcheck_norm;
-    if((config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR) && (val_constr_func[iConstr] <= 0.0)) {
-      gamma = max(min(max(gamma, config->GetOneShotGammaRate()*config->GetOneShotGamma(iConstr)), config->GetOneShotGammaMax()), 1.0E-6);
-    }
-    // gamma = max(min(max(gamma, config->GetOneShotGammaRate()*config->GetOneShotGamma(iConstr)), config->GetOneShotGammaMax()), 1.0E-6);
+    // if((config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR) && (val_constr_func[iConstr] <= 0.0)) {
+      // gamma = max(min(max(gamma, config->GetOneShotGammaRate()*config->GetOneShotGamma(iConstr)), config->GetOneShotGammaMax()), 1.0E-6);
+    // }
+    gamma = max(min(max(gamma, config->GetOneShotGammaRate()*config->GetOneShotGamma(iConstr)), config->GetOneShotGammaMax()), 1.0E-6);
     config->SetOneShotGamma(gamma, iConstr);
   }
   // su2double gamma = min(max(2./val_bcheck_norm, config->GetOneShotGammaRate()*config->GetOneShotGamma()), config->GetOneShotGammaMax());
