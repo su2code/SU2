e5a51dcd95179dcc215362963919fe95af1bf6c3
bmunguia <bmunguia10@gmail.com>
2019-12-07 20:29:34 -0800

    Lower bound on theta

--- SU2_CFD/src/solver_one_shot.cpp
+++ SU2_CFD/src/solver_one_shot.cpp
@@ -278,9 +278,9 @@ void COneShotSolver::CalculateRhoTheta(CConfig *config){
   helper       = myHelper;
 #endif
 
-  rho   = min(max(sqrt(normDeltaNew)/sqrt(normDelta), 0.9*rho_old), 1.0-1.0E-8); // Saturate contractivity
+  rho   = min(max(sqrt(normDeltaNew)/sqrt(normDelta), 0.9*rho_old), 1.0-1.0E-8); // Upper bound 1-(1E-8)
   // theta = min(max(sqrt(fabs(helper)/normDelta*theta_old), 0.9*theta_old), theta_old/0.9);
-  theta = max(sqrt(fabs(helper)/normDelta*theta_old), 0.9*theta_old);
+  theta = max(max(sqrt(fabs(helper)/normDelta*theta_old), 0.9*theta_old), 1.0E-8); // Lower bound 1E-8
 
   /* --- Store rho and theta values for this iteration --- */
   rho_old   = rho;
