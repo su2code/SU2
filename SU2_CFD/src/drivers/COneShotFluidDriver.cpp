44fc5317c154dcad2e8b28db397d28e55dadd38d
bmunguia <bmunguia10@gmail.com>
2019-12-09 16:05:16 -0800

    Lambda_Tilde for PrimalDualStep and BetaTerm

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1169,7 +1169,7 @@ void COneShotFluidDriver::ComputeBetaTerm(){
   /*--- Initialize the adjoint of the objective function with 1.0. ---*/
 
   SetAdj_ObjFunction();
-  SetAdj_ConstrFunction(Lambda);
+  SetAdj_ConstrFunction(Lambda_Tilde);
 
   /*--- Interpret the stored information by calling the corresponding routine of the AD tool. ---*/
 
