97fd739948aee73f6f1ef23fa0c6c1ea96e8da8b
Brian Munguía <bmunguia@stanford.edu>
2019-12-13 08:53:47 -0800

    ShiftLagGrad

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -874,9 +874,9 @@ bool COneShotFluidDriver::CheckFirstWolfe(bool design_update){
   if(design_update) {
     for (unsigned short iDV = 0; iDV < nDV_Total; iDV++){
       /*--- ShiftLagGrad is the gradient at the old iterate. ---*/
-      // admissible_step += DesignVarUpdate[iDV]*ShiftLagGrad[iDV];
+      admissible_step += DesignVarUpdate[iDV]*ShiftLagGrad[iDV];
       /*--- AugLagGrad is the gradient at the old iterate. ---*/
-      admissible_step += DesignVarUpdate[iDV]*AugLagGrad[iDV];
+      // admissible_step += DesignVarUpdate[iDV]*AugLagGrad[iDV];
     }
   }
   else {
@@ -909,9 +909,9 @@ void COneShotFluidDriver::StoreGradDotDir(bool design_update){
   if(design_update) {
     for (unsigned short iDV = 0; iDV < nDV_Total; iDV++){
       /*--- ShiftLagGrad is the gradient at the old iterate. ---*/
-      // GradDotDir += DesignVarUpdate[iDV]*ShiftLagGrad[iDV];
+      GradDotDir += DesignVarUpdate[iDV]*ShiftLagGrad[iDV];
       /*--- AugLagGrad is the gradient at the old iterate. ---*/
-      GradDotDir += DesignVarUpdate[iDV]*AugLagGrad[iDV];
+      // GradDotDir += DesignVarUpdate[iDV]*AugLagGrad[iDV];
     }
   }
   else {
