4ebe84a74930760deb7b4c0d3edeaae08bbfc8f0
Brian Munguía <bmunguia@stanford.edu>
2019-12-15 07:58:36 -0800

    Make initial guess > tol

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -445,7 +445,7 @@ void COneShotFluidDriver::RunOneShot(){
   /*--- Modifiy initial line search guess based on success of line search ---*/
   if(OneShotIter > config->GetOneShotStart()) {
     if(!bool_tol && stepsize < stepsize0) {
-      stepsize0 = stepsize0/2.0;
+      stepsize0 = max(2.0*tol, stepsize0/2.0);
     }
     else if(!bool_tol) {
       stepsize0 = min(1.0, stepsize0*2.0);
