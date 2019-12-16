82360a384b117ba370cec7d21803408c9f9b2255
Brian Munguía <bmunguia@stanford.edu>
2019-12-16 12:29:55 -0800

    Bug fix

--- SU2_CFD/src/solver_one_shot.cpp
+++ SU2_CFD/src/solver_one_shot.cpp
@@ -331,7 +331,7 @@ void COneShotSolver::CalculateGamma(CConfig *config, su2double val_bcheck_norm, 
     // }
       // gamma = max(gamma, config->GetOneShotGammaRate()*config->GetOneShotGamma(iConstr));
     // if(config->GetInnerIter() == config->GetOneShotStart()) {
-      gamma = max(min(gamma, 1.0E4, 1.0E-4);
+      gamma = max(min(gamma, 1.0E4), 1.0E-4);
       config->SetOneShotGamma(gamma, iConstr);
     // }
     // else {
