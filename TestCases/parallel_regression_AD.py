b8f9048e1821992707925c077a4d012a0dfc1295
Steffen Schotthöfer <steffen.schotthoefer@outlook.de>
2019-12-19 18:08:09 +0100

    further configuration of the test case

--- TestCases/parallel_regression_AD.py
+++ TestCases/parallel_regression_AD.py
@@ -183,7 +183,7 @@ def main():
     discadj_cylinder.cfg_file  = "cylinder_Windowing_AD.cfg" 
     discadj_cylinder.test_iter = 6
     discadj_cylinder.test_vals = [3.612556] #last column
-    discadj_cylinder.su2_exec  = "discrete_adjoint.py -f"
+    discadj_cylinder.su2_exec  = "parallel_computation.py -f"
     discadj_cylinder.timeout   = 1600
     discadj_cylinder.tol       = 0.00001
     discadj_cylinder.unsteady  = True
