b9721c3a4cbf788fbc19763245048356cd019707
Brian Munguía <bmunguia@stanford.edu>
2019-12-21 16:51:56 -0500

    New stepsize0

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -453,10 +453,12 @@ void COneShotFluidDriver::RunOneShot(){
   /*--- Modifiy initial line search guess based on success of line search ---*/
   if(OneShotIter > config->GetOneShotStart()) {
     if((!bool_tol) && (ArmijoIter < nArmijoIter) && (stepsize < stepsize0/2.0) && (ArmijoFlag == 0)) {
-      stepsize0 = max(2.0*tol, stepsize0/2.0);
+      // stepsize0 = max(2.0*tol, stepsize0/2.0);
+      stepsize0 = stepsize;
     }
     else if((!bool_tol) && (ArmijoIter < nArmijoIter) && (stepsize < stepsize0/2.0) && (ArmijoFlag == 2)) {
-      stepsize0 = min(1.0, stepsize0*2.0);
+      // stepsize0 = min(1.0, stepsize0*2.0);
+      stepsize0 = min(1.0, stepsize*2.0);
     }
     else {
       stepsize0 = 1.0;
