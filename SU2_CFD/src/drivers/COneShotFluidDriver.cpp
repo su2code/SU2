da0b0246525ffb9d64b9927a4b0a05598fbef96e
Brian Munguía <bmunguia@stanford.edu>
2019-12-21 13:19:38 -0500

    UpdateLambda(stepsize)

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -320,8 +320,8 @@ void COneShotFluidDriver::RunOneShot(){
       }
 
       LoadOldLambda();
-      UpdateLambda(1.0);
-      // UpdateLambda(stepsize);
+      // UpdateLambda(1.0);
+      UpdateLambda(stepsize);
 
       /*--- Compute and store GradL dot p ---*/
       // StoreLambdaGrad();
