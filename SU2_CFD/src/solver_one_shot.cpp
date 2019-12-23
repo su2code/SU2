730bfa75a4130a6d67cb111489c47a5cbd9a527c
Brian Munguía <bmunguia@stanford.edu>
2019-12-22 23:52:50 -0500

    New gamma, bisection method

--- SU2_CFD/src/solver_one_shot.cpp
+++ SU2_CFD/src/solver_one_shot.cpp
@@ -338,7 +338,7 @@ void COneShotSolver::CalculateGamma(CConfig *config, su2double val_bcheck_norm, 
   
   /* --- Estimate gamma value --- */
   for(iConstr = 0; iConstr < nConstr; iConstr++) {
-    su2double gamma = 2./val_bcheck_norm;
+    su2double gamma = 1.01/val_bcheck_norm;
     // if((config->GetKind_ConstrFuncType(iConstr) != EQ_CONSTR) && (val_constr_func[iConstr] <= 0.0)) {
       // gamma = max(min(max(gamma, config->GetOneShotGammaRate()*config->GetOneShotGamma()), config->GetOneShotGammaMax()), 1.0E-6);
     // }
