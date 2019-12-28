8d76c8e5939e560e6ab0c81617ac486439b121e4
Brian Munguía <bmunguia@stanford.edu>
2019-12-27 23:42:23 -0500

    New gamma

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -1332,7 +1332,7 @@ void COneShotFluidDriver::ComputePreconditioner(){
         BCheckInv[0][0] = 1./(BCheck[0][0]+1./config->GetOneShotGamma());
       }
       else {
-        BCheckNorm = 1.0001/gamma;
+        BCheckNorm = 2./gamma;
         BCheckInv[0][0] = gamma;
       }
   } else {
