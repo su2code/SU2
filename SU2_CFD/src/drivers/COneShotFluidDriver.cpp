3cc9436d04afa4e8bb6d2a32106f8d899c9361d8
Brian Munguía <bmunguia@stanford.edu>
2019-12-29 15:58:46 -0500

    Don't calculate alpha/beta/gamma every iteration

--- SU2_CFD/src/drivers/COneShotFluidDriver.cpp
+++ SU2_CFD/src/drivers/COneShotFluidDriver.cpp
@@ -434,9 +434,9 @@ void COneShotFluidDriver::RunOneShot(){
     solver[ADJFLOW_SOL]->CalculateGamma(config, BCheckNorm, ConstrFunc, Lambda);
   }
   else if((OneShotIter > config->GetOneShotStart()) && 
-          (OneShotIter < config->GetOneShotStop())){
-          // (OneShotIter < config->GetOneShotStop()) &&
-          // (ArmijoFlag != 0)){
+          // (OneShotIter < config->GetOneShotStop())){
+          (OneShotIter < config->GetOneShotStop()) &&
+          (ArmijoFlag != 0)){
     solver[ADJFLOW_SOL]->CalculateAlphaBeta(config);
     solver[ADJFLOW_SOL]->CalculateGamma(config, BCheckNorm, ConstrFunc, Lambda);
 
@@ -1331,13 +1331,13 @@ void COneShotFluidDriver::ComputePreconditioner(){
       // const bool active = (LambdaTilde[0] > 0.);
       if(active) {
         BCheckNorm = BCheck[0][0];
-        solver[ADJFLOW_SOL]->CalculateGamma(config, BCheckNorm, ConstrFunc, Lambda);
+        // solver[ADJFLOW_SOL]->CalculateGamma(config, BCheckNorm, ConstrFunc, Lambda);
         // SetAugLagGrad(TOTAL_AUGMENTED_OLD);
         // BCheckNorm = BCheck[0][0] - 1./gamma;
         BCheckInv[0][0] = 1./(BCheck[0][0]+1./config->GetOneShotGamma());
       }
       else {
-        BCheckNorm = 2./gamma;
+        BCheckNorm = 1.01/gamma;
         BCheckInv[0][0] = gamma;
       }
   } else {
