fa5397a195d7f5fc7db0076f3ddb36e4ec6da599
Tim Albring <tim.albring@scicomp.uni-kl.de>
2020-01-05 23:39:26 +0100

    Adding VTK XML filewriter header

--- SU2_CFD/include/output/filewriter/CParaviewXMLFileWriter.hpp
+++ SU2_CFD/include/output/filewriter/CParaviewXMLFileWriter.hpp
@@ -1,4 +1,84 @@
-#ifndef CPARAVIEWXMLFILEWRITER_HPP
-#define CPARAVIEWXMLFILEWRITER_HPP
+/*!
+ * \file CParaviewXMLFileWriter.hpp
+ * \brief Headers fo paraview binary file writer class.
+ * \author T. Albring
+ * \version 7.0.0 "Blackbird"
+ *
+ * SU2 Project Website: https://su2code.github.io
+ *
+ * The SU2 Project is maintained by the SU2 Foundation 
+ * (http://su2foundation.org)
+ *
+ * Copyright 2012-2019, SU2 Contributors (cf. AUTHORS.md)
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#endif // CPARAVIEWXMLFILEWRITER_HPP
+#pragma once
+
+#include "CFileWriter.hpp"
+
+class CParaviewXMLFileWriter final: public CFileWriter{
+
+private:
+  
+  enum class VTKDatatype {
+    FLOAT32,
+    INT32,
+    UINT8
+  };
+  
+  MPI_Offset disp;
+  MPI_File fhw;
+  
+  bool BigEndian;
+  
+  unsigned long dataOffset;
+    
+public:
+
+  /*!
+   * \brief File extension
+   */
+  const static string fileExt;
+
+  /*!
+   * \brief Construct a file writer using field names, dimension.
+   * \param[in] fields - A list of field names
+   * \param[in] nDim - Physical dimension
+   * \param[in] fileName - The name of the file
+   * \param[in] data_sorter - The parallel sorted data to write
+   */
+  CParaviewXMLFileWriter(vector<string> fields, unsigned short nDim,
+                            string fileName, CParallelDataSorter* data_sorter);
+
+  /*!
+   * \brief Destructor
+   */
+  ~CParaviewXMLFileWriter() override;
+
+  /*!
+   * \brief Write sorted data to file in paraview binary file format
+   */
+  void Write_Data() override;
+
+private:
+
+  void WriteString(std::string str, int rank);
+  
+  void AddDataArray(VTKDatatype type, string name, unsigned short nComponents, unsigned long size, unsigned long cumSize);
+  
+  void WriteDataArray(void *data, VTKDatatype type, unsigned long size, unsigned long cumSize, MPI_Offset offset);
+};
+
