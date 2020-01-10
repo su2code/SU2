c0f91b50062452bc50fbb7b91eaca48a5df95251
Clark Pederson <clarkp@utexas.edu>
2020-01-10 16:50:55 -0600

    Fix incorrect naming bug

--- SU2_CFD/src/solver_direct_mean.cpp
+++ SU2_CFD/src/solver_direct_mean.cpp
@@ -7654,7 +7654,7 @@ su2double CEulerSolver::GetInletAtVertex(su2double *val_inlet,
     
     for (iMarker = 0; iMarker < config->GetnMarker_All(); iMarker++) {
       if ((config->GetMarker_All_KindBC(iMarker) == INLET_FLOW ||
-           config->GetMarker_All_KindBC(iMarker) == SUPERSONIC_FLOW) &&
+           config->GetMarker_All_KindBC(iMarker) == SUPERSONIC_INLET) &&
           (config->GetMarker_All_TagBound(iMarker) == val_marker)) {
         
         for (iVertex = 0; iVertex < nVertex[iMarker]; iVertex++){
