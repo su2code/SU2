e1ae25c270df4dfb90af786294254de180682b8a
bmunguia <bmunguia10@gmail.com>
2020-01-17 15:25:27 -0800

    Modifying some Newton parameters

--- SU2_CFD/src/variables/CTNE2EulerVariable.cpp
+++ SU2_CFD/src/variables/CTNE2EulerVariable.cpp
@@ -950,7 +950,7 @@ bool CTNE2EulerVariable::Cons2PrimVar(CConfig *config, su2double *U, su2double *
 
   /*--- Set temperature algorithm paramters ---*/
   NRtol    = 1.0E-8;    // Tolerance for the Newton-Raphson method
-  Btol     = 1.0E-4;    // Tolerance for the Bisection method
+  Btol     = 1.0E-8;    // Tolerance for the Bisection method
   maxNIter = 999;        // Maximum Newton-Raphson iterations
   maxBIter = 999;        // Maximum Bisection method iterations
   scale    = 0.5;       // Scaling factor for Newton-Raphson step
