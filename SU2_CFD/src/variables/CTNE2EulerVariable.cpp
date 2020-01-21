123325ae8bf3abd6f88b0506dcd06c58a9bee14a
bmunguia <bmunguia10@gmail.com>
2020-01-20 16:34:59 -0800

    Increase NR and bisection iterations

--- SU2_CFD/src/variables/CTNE2EulerVariable.cpp
+++ SU2_CFD/src/variables/CTNE2EulerVariable.cpp
@@ -951,8 +951,8 @@ bool CTNE2EulerVariable::Cons2PrimVar(CConfig *config, su2double *U, su2double *
   /*--- Set temperature algorithm paramters ---*/
   NRtol    = 1.0E-8;    // Tolerance for the Newton-Raphson method
   Btol     = 1.0E-6;    // Tolerance for the Bisection method
-  maxNIter = 999;        // Maximum Newton-Raphson iterations
-  maxBIter = 999;        // Maximum Bisection method iterations
+  maxNIter = 9999;        // Maximum Newton-Raphson iterations
+  maxBIter = 9999;        // Maximum Bisection method iterations
   scale    = 0.5;       // Scaling factor for Newton-Raphson step
 
   /*--- Read parameters from config ---*/
