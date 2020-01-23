b6b5c3960f3bca28a5e3ef58976d94ab12c5f7ae
bmunguia <bmunguia10@gmail.com>
2020-01-23 15:34:10 -0800

    dPdU, dTdU, dTvedU if bkup

--- SU2_CFD/src/variables/CTNE2EulerVariable.cpp
+++ SU2_CFD/src/variables/CTNE2EulerVariable.cpp
@@ -913,7 +913,7 @@ bool CTNE2EulerVariable::SetPrimVar_Compressible(unsigned long iPoint, CConfig *
       Prim2ConsVar(config, iPoint, Primitive[iPoint], Solution[iPoint]);
       CalcdPdU(  Primitive[iPoint], eves[iPoint], config, dPdU[iPoint]  );
       CalcdTdU(  Primitive[iPoint], config, dTdU[iPoint]  );
-      CalcdTvedU(Primitive[iPoint], eves[iPoiint], config, dTvedU[iPoint]);
+      CalcdTvedU(Primitive[iPoint], eves[iPoint], config, dTvedU[iPoint]);
     }
   }
 
