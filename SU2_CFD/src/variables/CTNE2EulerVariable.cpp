23126a49e81ef83565e6f65e700406689df6000d
bmunguia <bmunguia10@gmail.com>
2020-01-23 13:05:42 -0800

    New bisection bound

--- SU2_CFD/src/variables/CTNE2EulerVariable.cpp
+++ SU2_CFD/src/variables/CTNE2EulerVariable.cpp
@@ -1097,7 +1097,7 @@ bool CTNE2EulerVariable::Cons2PrimVar(CConfig *config, su2double *U, su2double *
     } else {
 
       // Assign the bounds
-      Tve_o = max(Tvemin, V[T_INDEX]); // Tve should be geq T
+      Tve_o = max(Tvemin, V[T_INDEX]/2.0); // Tve should be geq T
       Tve2  = Tvemax;
       // if (rhoEve_t > rhoEve) {
       //   Tve2  = Tve;
