32d8f11dcd254997c54437afb1c17c61d87f6eaa
bmunguia <bmunguia10@gmail.com>
2020-01-23 15:46:10 -0800

    Reverting Tve bounds

--- SU2_CFD/src/variables/CTNE2EulerVariable.cpp
+++ SU2_CFD/src/variables/CTNE2EulerVariable.cpp
@@ -986,7 +986,7 @@ bool CTNE2EulerVariable::Cons2PrimVar(CConfig *config, su2double *U, su2double *
       nonPhys                = true;
     }
     else V[RHOS_INDEX+iSpecies] = U[iSpecies];
-    
+
     V[RHO_INDEX] += U[iSpecies];
   }
 
@@ -1103,7 +1103,8 @@ bool CTNE2EulerVariable::Cons2PrimVar(CConfig *config, su2double *U, su2double *
     } else {
 
       // Assign the bounds
-      Tve_o = max(Tvemin, V[T_INDEX]/2.0); // Tve should be geq T
+      // Tve_o = max(Tvemin, V[T_INDEX]/2.0); // Tve should be geq T
+      Tve_o = Tvemin;
       Tve2  = Tvemax;
       // if (rhoEve_t > rhoEve) {
       //   Tve2  = Tve;
