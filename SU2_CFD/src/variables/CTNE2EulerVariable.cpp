16406ebf4f442a12a8b4c4c2f0d0a464a3e6761d
bmunguia <bmunguia10@gmail.com>
2020-01-23 16:35:12 -0800

    Solution_Old as last resort

--- SU2_CFD/src/variables/CTNE2EulerVariable.cpp
+++ SU2_CFD/src/variables/CTNE2EulerVariable.cpp
@@ -914,13 +914,21 @@ bool CTNE2EulerVariable::SetPrimVar_Compressible(unsigned long iPoint, CConfig *
       CalcdPdU(  Primitive[iPoint], eves[iPoint], config, dPdU[iPoint]  );
       CalcdTdU(  Primitive[iPoint], config, dTdU[iPoint]  );
       CalcdTvedU(Primitive[iPoint], eves[iPoint], config, dTvedU[iPoint]);
-      bkup = Cons2PrimVar(config, Solution[iPoint], Primitive[iPoint], dPdU[iPoint], dTdU[iPoint],
-                        dTvedU[iPoint], eves[iPoint], Cvves[iPoint]);
-      if(bkup) {
+      nonPhys = Cons2PrimVar(config, Solution[iPoint], Primitive[iPoint], dPdU[iPoint], dTdU[iPoint],
+                             dTvedU[iPoint], eves[iPoint], Cvves[iPoint]);
+      if(nonPhys) {
         for (iVar = 0; iVar < nVar; iVar++)
           Solution(iPoint,iVar) = Solution_Old(iPoint,iVar);
-        bkup = Cons2PrimVar(config, Solution[iPoint], Primitive[iPoint], dPdU[iPoint], dTdU[iPoint],
-                            dTvedU[iPoint], eves[iPoint], Cvves[iPoint]);
+        nonPhys = Cons2PrimVar(config, Solution[iPoint], Primitive[iPoint], dPdU[iPoint], dTdU[iPoint],
+                               dTvedU[iPoint], eves[iPoint], Cvves[iPoint]);
+        if(nonPhys) {
+          Prim2ConsVar(config, iPoint, Primitive[iPoint], Solution[iPoint]);
+          CalcdPdU(  Primitive[iPoint], eves[iPoint], config, dPdU[iPoint]  );
+          CalcdTdU(  Primitive[iPoint], config, dTdU[iPoint]  );
+          CalcdTvedU(Primitive[iPoint], eves[iPoint], config, dTvedU[iPoint]);
+          nonPhys = Cons2PrimVar(config, Solution[iPoint], Primitive[iPoint], dPdU[iPoint], dTdU[iPoint],
+                                 dTvedU[iPoint], eves[iPoint], Cvves[iPoint]);
+        }
       }
     // }
   }
