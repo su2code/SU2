0c54dae4fabd5404cd63de3a14e460e3209b1d0c
Pedro Gomes <pcarruscag@gmail.com>
2020-01-29 09:57:36 +0000

    disable config map declarations when not needed to speedup compilation restructure and clean config_structure

--- SU2_DOT/include/SU2_DOT.hpp
+++ SU2_DOT/include/SU2_DOT.hpp
@@ -32,6 +32,9 @@
 #include "../../Common/include/mpi_structure.hpp"
 #include "../../Common/include/omp_structure.hpp"
 
+#define ENABLE_MAPS
+#include "../../Common/include/config_structure.hpp"
+
 #include <cstdlib>
 #include <iostream>
 #include <iomanip>
@@ -39,7 +42,6 @@
 
 #include "../../Common/include/geometry/CPhysicalGeometry.hpp"
 #include "../../Common/include/fem_geometry_structure.hpp"
-#include "../../Common/include/config_structure.hpp"
 #include "../../Common/include/grid_movement_structure.hpp"
 #include "../../SU2_CFD/include/output/CBaselineOutput.hpp"
 #include "../../SU2_CFD/include/solvers/CBaselineSolver.hpp"
