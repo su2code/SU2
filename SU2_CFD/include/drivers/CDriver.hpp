4b02a33defa9c1818e94853871721536cfc100dc
Tim Albring <tim.albring@scicomp.uni-kl.de>
2020-01-31 14:03:23 +0100

    Updating version number

--- SU2_CFD/include/drivers/CDriver.hpp
+++ SU2_CFD/include/drivers/CDriver.hpp
@@ -3,7 +3,7 @@
  * \brief Headers of the main subroutines for driving single or multi-zone problems.
  *        The subroutines and functions are in the <i>driver_structure.cpp</i> file.
  * \author T. Economon, H. Kline, R. Sanchez
- * \version 7.0.0 "Blackbird"
+ * \version 7.0.1 "Blackbird"
  *
  * SU2 Project Website: https://su2code.github.io
  *
@@ -1095,7 +1095,7 @@ public:
  * \class CDiscAdjFSIDriver
  * \brief Overload: Class for driving a discrete adjoint FSI iteration.
  * \author R. Sanchez.
- * \version 7.0.0 "Blackbird"
+ * \version 7.0.1 "Blackbird"
  */
 class CDiscAdjFSIDriver : public CDriver {
 
--- SU2_CFD/src/solvers/CDiscAdjMeshSolver.cpp
+++ SU2_CFD/src/solvers/CDiscAdjMeshSolver.cpp
@@ -2,7 +2,7 @@
  * \file CDiscAdjMeshSolver.cpp
  * \brief Main subroutines for solving the discrete adjoint mesh problem.
  * \author Ruben Sanchez
- * \version 7.0.0 "Blackbird"
+ * \version 7.0.1 "Blackbird"
  *
  * SU2 Project Website: https://su2code.github.io
  *
