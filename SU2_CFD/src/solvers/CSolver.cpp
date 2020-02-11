983f0fbb596a6498de7a534d8eb29b0de9286158
Aman Baig <baigan@mail.uc.edu>
2020-02-11 11:15:17 -0500

    Inlet_Data_Interpolated size change

--- SU2_CFD/src/solvers/CSolver.cpp
+++ SU2_CFD/src/solvers/CSolver.cpp
@@ -4269,7 +4269,7 @@ void CSolver::LoadInletProfile(CGeometry **geometry,
 
           vector<passivedouble> Inlet_Data = profileReader.GetDataForProfile(jMarker);
           unsigned short nColumns = profileReader.GetNumberOfColumnsInProfile(jMarker);
-          vector<su2double> Inlet_Data_Interpolated (nCol_InletFile);
+          vector<su2double> Inlet_Data_Interpolated (nCol_InletFile*geometry[MESH_0]->nVertex[iMarker]);
 
           /*--- Define Inlet Values vectors before and after interpolation (if needed) ---*/
           vector<su2double> Inlet_Values(nCol_InletFile);
@@ -4393,10 +4393,13 @@ void CSolver::LoadInletProfile(CGeometry **geometry,
 
               /* --- Correcting for Interpolation Type ---*/
               Inlet_Values = CorrectedInletValues(Inlet_Interpolated, Theta, nDim, Coord, nVar_Turb, config);
+              cout<<"Corrected Inlet_Values assigned"<<endl;
               solver[MESH_0][KIND_SOLVER]->SetInletAtVertex(Inlet_Values.data(), iMarker, iVertex);
-
+              cout<<"Inlet_Values assigned to Vertex\n";
               for (unsigned short iVar=0; iVar < nCol_InletFile; iVar++)
                 Inlet_Data_Interpolated[iVertex*nCol_InletFile+iVar] = Inlet_Values[iVar];
+
+              cout<<"Data transferred to Inlet_Data_Interpolated\n";
             }
           }
             if(config->GetPrintInlet_InterpolatedData() == true)
