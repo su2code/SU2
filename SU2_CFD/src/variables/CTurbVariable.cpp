858506826a62fe168521cd0e110c537e0c0f7bf0
bmunguia <bmunguia10@gmail.com>
2020-02-13 13:11:10 -0800

    Always initialize source term

--- SU2_CFD/src/variables/CTurbVariable.cpp
+++ SU2_CFD/src/variables/CTurbVariable.cpp
@@ -66,11 +66,9 @@ CTurbVariable::CTurbVariable(unsigned long npoint, unsigned long ndim, unsigned 
   if (config->GetError_Estimate()) {
     AnisoViscGrad.resize(nPoint,nDim*nVar*nDim) = su2double(0.0);
     AnisoViscHess.resize(nPoint,3*(nDim-1)*nVar*nDim) = su2double(0.0);
-    if(config->GetAdap_Source()) {
-      Source.resize(nPoint,nVar) = su2double(0.0);
-      AnisoSourceGrad.resize(nPoint,nDim*nVar) = su2double(0.0);
-      AnisoSourceHess.resize(nPoint, 3*(nDim-1)*nVar) = su2double(0.0);
-    }
+    Source.resize(nPoint,nVar) = su2double(0.0);
+    AnisoSourceGrad.resize(nPoint,nDim*nVar) = su2double(0.0);
+    AnisoSourceHess.resize(nPoint, 3*(nDim-1)*nVar) = su2double(0.0);
   }
   
 }
