d3dc8ebe00dfc15c84bce9bb81eb1f542d8c6480
Aman Baig <baigan@mail.uc.edu>
2020-02-14 11:20:46 -0500

    resize column data

--- SU2_CFD/include/CMarkerProfileReaderFVM.hpp
+++ SU2_CFD/include/CMarkerProfileReaderFVM.hpp
@@ -166,6 +166,7 @@ public:
   }
   
   inline vector<su2double> &GetColumnForProfile(int val_iProfile, unsigned short iCol) {
+    ColumnData.resize(GetNumberOfRowsInProfile(val_iProfile));
   for (unsigned long iRow = 0; iRow < GetNumberOfRowsInProfile(val_iProfile); iRow++)
     ColumnData[iRow]=profileData[val_iProfile][iRow*GetNumberOfColumnsInProfile(val_iProfile)+iCol];
   return ColumnData;
