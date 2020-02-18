455d65b8447e12fd6b69c3512d69fa1666d8eed8
bmunguia <bmunguia10@gmail.com>
2020-02-18 14:14:27 -0800

    a key

--- SU2_PY/SU2/amginria/interface.py
+++ SU2_PY/SU2/amginria/interface.py
@@ -36,8 +36,8 @@ def prepro_back_mesh(config_cfd, config_amg):
     Tri_Ini = [mesh_ini['Triangles'][0][i] for i in range(3)]
     Tri_Bak = [mesh_bak['Triangles'][0][i] for i in range(3)]
 
-    Ver_Ini = [mesh_ini['xy'][Tri_Ini][i] for i in range(2)]
-    Ver_Bak = [mesh_bak['xy'][Tri_Bak][i] for i in range(2)]
+    Ver_Ini = [mesh_ini['xyz'][Tri_Ini][i] for i in range(2)]
+    Ver_Bak = [mesh_bak['xyz'][Tri_Bak][i] for i in range(2)]
 
     V_Ini = [Ver_Ini[1][i] - Ver_Ini[0][i] for i in range(2)]
     W_Ini = [Ver_Ini[2][i] - Ver_Ini[0][i] for i in range(2)]
