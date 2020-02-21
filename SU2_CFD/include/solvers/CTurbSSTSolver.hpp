235436125c35e169155f893d6c82f245c578a809
bmunguia <bmunguia10@gmail.com>
2020-02-21 14:38:37 -0800

    Declare adaptation functions in SSTSolver header

--- SU2_CFD/include/solvers/CTurbSSTSolver.hpp
+++ SU2_CFD/include/solvers/CTurbSSTSolver.hpp
@@ -309,4 +309,38 @@ public:
    */
   inline su2double GetOmega_Inf(void) const override { return omega_Inf; }
 
+  /*!
+   * \brief Compute the gradient using a L2 Projection method,
+   *        and stores the result in the <i>AnisoGrad</i> variable.
+   * \param[in] geometry - Geometrical definition of the problem.
+   * \param[in] config - Definition of the particular problem.
+   * \param[in] solver_flow - Pointer to the flow solver.
+   */
+  void SetTurbGradient_L2Proj2(CGeometry *geometry, CConfig *config, CSolver *solver_flow) final;
+
+  /*!
+   * \brief Compute the Hessian using a L2 Projection method,
+   *        and stores the result in the <i>AnisoHess</i> variable.
+   * \param[in] geometry - Geometrical definition of the problem.
+   * \param[in] config - Definition of the particular problem.
+   */
+  void SetHessian_L2Proj2(CGeometry *geometry, CConfig *config) final;
+
+  /*!
+   * \brief Compute the gradient using a L2 Projection method,
+   *        and stores the result in the <i>AnisoGrad</i> variable.
+   * \param[in] geometry - Geometrical definition of the problem.
+   * \param[in] config - Definition of the particular problem.
+   * \param[in] solver_flow - Pointer to the flow solver.
+   */
+  void SetTurbGradient_L2Proj3(CGeometry *geometry, CConfig *config, CSolver *solver_flow) final;
+
+  /*!
+   * \brief Compute the Hessian using a L2 Projection method,
+   *        and stores the result in the <i>AnisoHess</i> variable.
+   * \param[in] geometry - Geometrical definition of the problem.
+   * \param[in] config - Definition of the particular problem.
+   */
+  void SetHessian_L2Proj3(CGeometry *geometry, CConfig *config) final;
+
 };
