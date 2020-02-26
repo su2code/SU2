6b97b2268bdeb2307fc094ab0cc45344c3607c71
Tim Albring <tim.albring@scicomp.uni-kl.de>
2020-02-26 11:37:12 +0100

    Adding missing doxygen comment

--- SU2_CFD/include/solvers/CSolverFactory.hpp
+++ SU2_CFD/include/solvers/CSolverFactory.hpp
@@ -39,7 +39,7 @@ private:
    * \param[in] config        - The configuration
    * \param[in] iMGLevel      - The multigrid level
    * \param[in] adjoint       - Boolean indicating whether a primal or adjoint solver should be allocated
-   * \return              - A pointer to the allocated turbulent solver
+   * \return                  - A pointer to the allocated turbulent solver
    */
   static CSolver* createTurbSolver(ENUM_TURB_MODEL kindTurbModel, CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel, int adjoint);
   
@@ -50,7 +50,8 @@ private:
    * \param[in] config        - The configuration
    * \param[in] iMGLevel      - The multigrid level
    * \param[in] adjoint       - Boolean indicating whether a primal or adjoint solver should be allocated
-   * \return              - A pointer to the allocated heat solver
+   * \param[in] standalone    - Boolean indicating whether the heat solver should be allocated as a standalone solver
+   * \return                  - A pointer to the allocated heat solver
    */
   static CSolver* createHeatSolver(CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel, bool adjoint, bool standalone);
   
@@ -61,7 +62,7 @@ private:
    * \param[in] config        - The configuration
    * \param[in] iMGLevel      - The multigrid level
    * \param[in] adjoint       - Boolean indicating whether a primal or adjoint solver should be allocated
-   * \return              - A pointer to the allocated mesh solver
+   * \return                  - A pointer to the allocated mesh solver
    */
   static CSolver* createMeshSolver(CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel, bool adjoint);
   
@@ -71,29 +72,29 @@ private:
    * \param[in] geometry      - The geometry definition
    * \param[in] config        - The configuration
    * \param[in] iMGLevel      - The multigrid level
-   * \return              - A pointer to the allocated DG solver
+   * \return                  - A pointer to the allocated DG solver
    */
   static CSolver* createDGSolver(ENUM_SOLVER kindDGSolver, CGeometry *geometry, CConfig *config, int iMGLevel);
   
   /*!
    * \brief Create a flow solver 
    * \param[in] kindFlowSolver - Kind of flow solver
-   * \param[in] solver        - The solver container 
-   * \param[in] geometry      - The geometry definition
-   * \param[in] config        - The configuration
-   * \param[in] iMGLevel      - The multigrid level
-   * \return              - A pointer to the allocated flow solver
+   * \param[in] solver         - The solver container
+   * \param[in] geometry       - The geometry definition
+   * \param[in] config         - The configuration
+   * \param[in] iMGLevel       - The multigrid level
+   * \return                   - A pointer to the allocated flow solver
    */
   static CSolver* createFlowSolver(ENUM_SOLVER kindFlowSolver, CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel);
   
   /*!
    * \brief Generic routine to create a solver 
-   * \param[in] kindSolver - Kind of solver
+   * \param[in] kindSolver    - Kind of solver
    * \param[in] solver        - The solver container 
    * \param[in] geometry      - The geometry definition
    * \param[in] config        - The configuration
    * \param[in] iMGLevel      - The multigrid level
-   * \return              - A pointer to the allocated solver
+   * \return                  - A pointer to the allocated solver
    */
   static CSolver* createSolver(ENUM_SOLVER kindSolver, CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel);
   
@@ -102,7 +103,7 @@ public:
   /*!
    * \brief Create the solver container by allocating the primary solver 
    * and secondary solvers like heat solver, turbulent solver etc
-   * \param[in] kindSolver - The kind of primary solver 
+   * \param[in] kindSolver    - The kind of primary solver
    * \param[in] config        - The configuration
    * \param[in] geometry      - The geometry definition
    * \param[in] iMGLevel      - The multigrid level
