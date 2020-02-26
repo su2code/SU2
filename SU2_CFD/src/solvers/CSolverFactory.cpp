ab099aca4d92b83504150f6aa0fd63f75ab70d69
Tim Albring <tim.albring@scicomp.uni-kl.de>
2020-02-26 11:34:41 +0100

    Cleaning whitespaces and adding missing file header

--- SU2_CFD/src/solvers/CSolverFactory.cpp
+++ SU2_CFD/src/solvers/CSolverFactory.cpp
@@ -1,3 +1,30 @@
+/*!
+ * \file CSolverFactory.cpp
+ * \brief Main subroutines for CSolverFactoryclass.
+ * \author T. Albring
+ * \version 7.0.1 "Blackbird"
+ *
+ * SU2 Project Website: https://su2code.github.io
+ *
+ * The SU2 Project is maintained by the SU2 Foundation
+ * (http://su2foundation.org)
+ *
+ * Copyright 2012-2019, SU2 Contributors (cf. AUTHORS.md)
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "../../include/solvers/CSolver.hpp"
 #include "../../include/solvers/CSolverFactory.hpp"
 #include "../../include/solvers/CEulerSolver.hpp"
@@ -21,17 +48,17 @@
 #include "../../include/solvers/CDiscAdjMeshSolver.hpp"
 
 CSolver** CSolverFactory::createSolverContainer(ENUM_SOLVER kindSolver, CConfig *config, CGeometry *geometry, int iMGLevel){
-  
+
   CSolver** solver;
-  
+
   solver = new CSolver*[MAX_SOLS];
   for (unsigned int iSol = 0; iSol < MAX_SOLS; iSol++)
     solver[iSol] = nullptr;
-  
+
   const bool allocDirect  = false;
   const bool allocAdjoint = true;
   const bool standAlone   = true;
-  
+
   ENUM_TURB_MODEL kindTurbModel = static_cast<ENUM_TURB_MODEL>(config->GetKind_Turb_Model());
 
   switch (kindSolver) {
@@ -70,7 +97,7 @@ CSolver** CSolverFactory::createSolverContainer(ENUM_SOLVER kindSolver, CConfig 
     case ADJ_NAVIER_STOKES:
       solver[FLOW_SOL]    = createFlowSolver(NAVIER_STOKES, solver, geometry, config, iMGLevel);
       solver[ADJFLOW_SOL] = createSolver(ADJ_NAVIER_STOKES, solver, geometry, config, iMGLevel);
-      break;    
+      break;
     case ADJ_RANS:
       solver[FLOW_SOL]    = createFlowSolver(INC_RANS, solver, geometry, config, iMGLevel);
       solver[ADJFLOW_SOL] = createSolver(ADJ_RANS, solver, geometry, config, iMGLevel);
@@ -138,23 +165,24 @@ CSolver** CSolverFactory::createSolverContainer(ENUM_SOLVER kindSolver, CConfig 
       solver[ADJFLOW_SOL] = createSolver(DISC_ADJ_NAVIER_STOKES, solver, geometry, config, iMGLevel);
       break;
     case DISC_ADJ_FEM_RANS:
-      SU2_MPI::Error("DG RANS Solver not yet implemented.", CURRENT_FUNCTION);
+      solver[FLOW_SOL]    = createDGSolver(FEM_RANS, geometry, config, iMGLevel);
+      solver[ADJFLOW_SOL] = createSolver(DISC_ADJ_RANS, solver, geometry, config, iMGLevel);
       break;
      default:
       solver = nullptr;
   }
-  
+
   solver[MESH_SOL]    = createMeshSolver(solver, geometry, config, iMGLevel, allocDirect);
   solver[ADJMESH_SOL] = createMeshSolver(solver, geometry, config, iMGLevel, allocAdjoint);
-  
+
   return solver;
-  
+
 }
 
 CSolver* CSolverFactory::createSolver(ENUM_SOLVER kindSolver, CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel){
- 
+
   CSolver *genericSolver = nullptr;
-  
+
   switch (kindSolver) {
     case TEMPLATE_SOLVER:
       genericSolver = new CTemplateSolver(geometry, config);
@@ -179,15 +207,15 @@ CSolver* CSolverFactory::createSolver(ENUM_SOLVER kindSolver, CSolver **solver, 
     default:
       SU2_MPI::Error("Solver not found", CURRENT_FUNCTION);
   }
-  
+
   return genericSolver;
-  
+
 }
 
 CSolver* CSolverFactory::createTurbSolver(ENUM_TURB_MODEL kindTurbModel, CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel, int adjoint){
-  
+
   CSolver *turbSolver = nullptr;
-  
+
   if (!adjoint){
     switch (kindTurbModel) {
       case SA: case SA_NEG: case SA_E: case SA_COMP: case SA_E_COMP:
@@ -206,7 +234,7 @@ CSolver* CSolverFactory::createTurbSolver(ENUM_TURB_MODEL kindTurbModel, CSolver
         break;
     }
   } else {
-    
+
     if (config->GetDiscrete_Adjoint()){
       if (!config->GetFrozen_Visc_Disc())
         turbSolver = new CDiscAdjSolver(geometry, config, solver[FLOW_SOL], RUNTIME_TURB_SYS, iMGLevel);
@@ -220,12 +248,12 @@ CSolver* CSolverFactory::createTurbSolver(ENUM_TURB_MODEL kindTurbModel, CSolver
 }
 
 CSolver* CSolverFactory::createHeatSolver(CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel, bool adjoint, bool standalone){
-  
+
   CSolver *heatSolver = nullptr;
-  
-  /*--- Only allocate a heat solver if it should run standalone 
+
+  /*--- Only allocate a heat solver if it should run standalone
    * or if the weakly coupled heat solver is enabled and no energy equation is included ---*/
-  
+
   if ((config->GetWeakly_Coupled_Heat() && !config->GetEnergy_Equation()) || standalone){
     if (adjoint){
       if (config->GetDiscrete_Adjoint()){
@@ -238,27 +266,29 @@ CSolver* CSolverFactory::createHeatSolver(CSolver **solver, CGeometry *geometry,
     }
   }
   return heatSolver;
-  
+
 }
 
 CSolver* CSolverFactory::createMeshSolver(CSolver **solver, CGeometry *geometry, CConfig *config, int iMGLevel, bool adjoint){
-  
+
   CSolver *meshSolver = nullptr;
-  
+
   if (config->GetDeform_Mesh() && iMGLevel == MESH_0){
-    meshSolver = new CMeshSolver(geometry, config);
+    if (!adjoint){
+      meshSolver = new CMeshSolver(geometry, config);
+    }
     if (adjoint && config->GetDiscrete_Adjoint()){
       meshSolver = new CDiscAdjMeshSolver(geometry, config, solver[MESH_SOL]);
     }
   }
   return meshSolver;
-  
+
 }
 
 CSolver* CSolverFactory::createDGSolver(ENUM_SOLVER kindDGSolver, CGeometry *geometry, CConfig *config, int iMGLevel){
-  
+
   CSolver *DGSolver = nullptr;
-  
+
   switch (kindDGSolver) {
     case FEM_EULER:
       if (config->GetKind_FEM_DG_Shock() == PERSSON){
@@ -277,14 +307,14 @@ CSolver* CSolverFactory::createDGSolver(ENUM_SOLVER kindDGSolver, CGeometry *geo
       SU2_MPI::Error("Requested DG solver not found", CURRENT_FUNCTION);
       break;
   }
-  
+
   return DGSolver;
 }
 
 CSolver* CSolverFactory::createFlowSolver(ENUM_SOLVER kindFlowSolver, CSolver **solver,  CGeometry *geometry, CConfig *config, int iMGLevel){
-  
+
   CSolver *flowSolver = nullptr;
-  
+
   switch (kindFlowSolver) {
     case EULER:
       flowSolver = new CEulerSolver(geometry, config, iMGLevel);
@@ -302,11 +332,11 @@ CSolver* CSolverFactory::createFlowSolver(ENUM_SOLVER kindFlowSolver, CSolver **
       SU2_MPI::Error("Flow solver not found", CURRENT_FUNCTION);
       break;
   }
-  
-  if (flowSolver != nullptr){
-    flowSolver->Preprocessing(geometry, solver, config, iMGLevel, NO_RK_ITER, RUNTIME_FLOW_SYS, false);
-  }
- 
+
+  assert(flowSolver != nullptr);
+
+  flowSolver->Preprocessing(geometry, solver, config, iMGLevel, NO_RK_ITER, RUNTIME_FLOW_SYS, false);
+
   return flowSolver;
-  
+
 }
