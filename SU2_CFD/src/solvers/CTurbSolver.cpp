6780e38662e76c98f79734f1fa2e7b6917dcd4a5
bmunguia <bmunguia10@gmail.com>
2020-03-06 00:20:46 -0800

    Less underrelaxation

--- SU2_CFD/src/solvers/CTurbSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSolver.cpp
@@ -505,8 +505,8 @@ void CTurbSolver::ComputeUnderRelaxationFactor(CSolver **solver_container, CConf
        turbulent variables can change over a nonlinear iteration. */
 
       for (unsigned short iVar = 0; iVar < nVar; iVar++) {
-      //   const unsigned long index = iPoint*nVar + iVar;
-      //   su2double ratio = fabs(LinSysSol[index])/(nodes->GetSolution(iPoint, iVar)+EPS);
+        const unsigned long index = iPoint*nVar + iVar;
+        su2double ratio = fabs(LinSysSol[index])/(nodes->GetSolution(iPoint, iVar)+EPS);
       //   if (ratio > allowableRatio) {
       //     localUnderRelaxation = min(allowableRatio/ratio, localUnderRelaxation);
       //   }
