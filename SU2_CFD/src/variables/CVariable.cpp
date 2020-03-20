b47d16b4018ea4a9ee61ead322215e8e1e681396
bmunguia <bmunguia10@gmail.com>
2020-03-19 17:54:44 -0700

    GetDensity

--- SU2_CFD/src/variables/CVariable.cpp
+++ SU2_CFD/src/variables/CVariable.cpp
@@ -137,10 +137,10 @@ void CVariable::RegisterSolution_time_n1() {
 
 void CVariable::RegisterConservativeSolution(CVariable *flowvar, bool input, bool push_index) {
   for (unsigned long iPoint = 0; iPoint < nPoint; ++iPoint) {
-    su2double density = flowvar->GetSolution(iPoint, 0);
+    su2double density = flowvar->GetDensity(iPoint);
     for(unsigned long iVar=0; iVar<nVar; ++iVar) {
+      su2double cons_sol = Solution(iPoint, iVar)*density;
       if(input) {
-        su2double cons_sol = Solution(iPoint, iVar)*SU2_TYPE::GetValue(density);
         if(push_index) {
           AD::RegisterInput(cons_sol);
         }
@@ -151,7 +151,6 @@ void CVariable::RegisterConservativeSolution(CVariable *flowvar, bool input, boo
         Solution(iPoint, iVar) = cons_sol/density;
       }
       else {
-        su2double cons_sol = Solution(iPoint, iVar)*density;
         AD::RegisterOutput(cons_sol);
         if(!push_index)
           AD::SetIndex(AD_OutputIndex(iPoint,iVar), cons_sol);
