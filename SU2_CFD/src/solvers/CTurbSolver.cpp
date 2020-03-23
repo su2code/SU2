60f3247912f397931d603eb36d46d085d0b5c5e5
bmunguia <bmunguia10@gmail.com>
2020-03-23 09:38:03 -0700

    Set omega while density is converging

--- SU2_CFD/src/solvers/CTurbSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSolver.cpp
@@ -452,7 +452,9 @@ void CTurbSolver::ImplicitEuler_Iteration(CGeometry *geometry, CSolver **solver_
             nodes->AddConservative(iPoint, 1, nodes->GetUnderRelaxation(iPoint)*LinSysSol[iPoint*nVar+1], density, lowerlimit[1], upperlimit[1]);
           }
           else{
-            k = nodes->GetSolution(iPoint, 0);
+            su2double muLamInf  = config->GetViscosity_FreeStreamND(),
+                      viscRatio = config->GetTurb2LamViscRatio_FreeStream(),
+                      k         = nodes->GetSolution(iPoint, 0);
             nodes->SetSolution(iPoint, 1, density*kine_Inf/(muLamInf*viscRatio));
           }
 
