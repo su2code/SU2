518fb9c030d07d218652e1f6e7292229112044cc
bmunguia <bmunguia10@gmail.com>
2020-03-22 17:26:37 -0700

    Underrelax omega

--- SU2_CFD/src/solvers/CTurbSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSolver.cpp
@@ -522,16 +522,17 @@ void CTurbSolver::ComputeUnderRelaxationFactor(CSolver **solver_container, CConf
        turbulent variables can change over a nonlinear iteration. */
 
       // for (unsigned short iVar = 0; iVar < nVar; iVar++) {
-      //   const unsigned long index = iPoint*nVar + iVar;
-      //   su2double ratio = fabs(LinSysSol[index])/(nodes->GetSolution(iPoint, iVar)+EPS);
-      //   if (ratio > allowableRatio) {
-      //     localUnderRelaxation = min(allowableRatio, localUnderRelaxation);
-      //   }
-      //   // if (ratio > allowableIncrease) {
-      //   //   localUnderRelaxation = min(allowableIncrease/ratio, localUnderRelaxation);
-      //   // } else if (ratio < allowableDecrease) {
-      //   //   localUnderRelaxation = min(fabs(allowableDecrease)/ratio, localUnderRelaxation);
-      //   // }
+        unsigned short iVar = 1;
+        const unsigned long index = iPoint*nVar + iVar;
+        su2double ratio = fabs(LinSysSol[index])/(nodes->GetSolution(iPoint, iVar)+EPS);
+        if (ratio > allowableRatio) {
+          localUnderRelaxation = min(allowableRatio, localUnderRelaxation);
+        }
+        // if (ratio > allowableIncrease) {
+        //   localUnderRelaxation = min(allowableIncrease/ratio, localUnderRelaxation);
+        // } else if (ratio < allowableDecrease) {
+        //   localUnderRelaxation = min(fabs(allowableDecrease)/ratio, localUnderRelaxation);
+        // }
       // }
 
     }
