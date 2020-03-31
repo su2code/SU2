020fab3ca8bddce866421b865cc02d577505f9b3
bmunguia <bmunguia10@gmail.com>
2020-03-30 17:39:49 -0700

    Bug fix

--- Common/include/geometry/dual_grid/CVertex.hpp
+++ Common/include/geometry/dual_grid/CVertex.hpp
@@ -442,17 +442,11 @@ public:
    * \brief This function does nothing (it comes from a pure virtual function, that implies the
    *        definition of the function in all the derived classes).
    */
-  inline su2double GetPartialVolume(unsigned short val_Point) const override { return 0.0; };
+  inline su2double GetEdgeWeights(unsigned short val_Point) const const override { return 0.0; };
 
   /*!
    * \brief This function does nothing (it comes from a pure virtual function, that implies the
    *        definition of the function in all the derived classes).
    */
-  inline void AddPartialVolume(su2double val_Volume, unsigned short val_Point) { }
-
-  /*!
-   * \brief This function does nothing (it comes from a pure virtual function, that implies the
-   *        definition of the function in all the derived classes).
-   */
-  inline void SetPartialVolume(su2double val_Volume, unsigned short val_Point) { }
+  inline void SetEdgeWeights(su2double val_Weight, unsigned short val_Point) { }
 };
