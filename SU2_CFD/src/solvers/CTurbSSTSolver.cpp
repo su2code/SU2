3f64c4952fd0bfbb3be5577db4a07e6590f1f9cf
bmunguia <bmunguia10@gmail.com>
2020-03-31 19:10:35 -0700

    New limits

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -211,9 +211,9 @@ CTurbSSTSolver::CTurbSSTSolver(CGeometry *geometry, CConfig *config, unsigned sh
   upperlimit = new su2double[nVar];
 
   lowerlimit[0] = 1.0e-10*kine_Inf/kine_Inf_Dim;
-  upperlimit[0] = 1.0e10*kine_Inf/kine_Inf_Dim;
+  upperlimit[0] = 1.0e15*kine_Inf/kine_Inf_Dim;
 
-  lowerlimit[1] = 1.0e-4*omega_Inf/omega_Inf_Dim;
+  lowerlimit[1] = 1.0e-10*omega_Inf/omega_Inf_Dim;
   upperlimit[1] = 1.0e15*omega_Inf/omega_Inf_Dim;
 
   /*--- Initializate quantities for SlidingMesh Interface ---*/
