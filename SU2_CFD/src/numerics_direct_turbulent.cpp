d1a068b17aa7ddecc6c61224ebd5a0b25360c01e
bmunguia <bmunguia10@gmail.com>
2020-04-02 13:39:15 -0700

    0.5, and sign

--- SU2_CFD/src/numerics_direct_turbulent.cpp
+++ SU2_CFD/src/numerics_direct_turbulent.cpp
@@ -1190,11 +1190,11 @@ void CAvgGrad_TurbSST::CorrectJacobian(su2double **Jacobian_i,
       const su2double weight_i = Normal[iDim]*halfOnVol_i;
       const su2double weight_j = Normal[iDim]*halfOnVol_j;
       
-      Jacobian_i[0][0] -= weight_i*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_i[0]/proj_vector_ij;
-      Jacobian_i[1][1] -= weight_i*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_i[1]/proj_vector_ij;
+      Jacobian_i[0][0] -= 0.5*weight_i*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_i[0]/proj_vector_ij;
+      Jacobian_i[1][1] -= 0.5*weight_i*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_i[1]/proj_vector_ij;
       
-      Jacobian_j[0][0] -= weight_j*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_j[0]/proj_vector_ij;
-      Jacobian_j[1][1] -= weight_j*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_j[1]/proj_vector_ij;
+      Jacobian_j[0][0] += 0.5*weight_j*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_j[0]/proj_vector_ij;
+      Jacobian_j[1][1] += 0.5*weight_j*(Normal[iDim] - Edge_Vector[iDim]*proj_vector_ij)*jac_j[1]/proj_vector_ij;
     }
   }
   
