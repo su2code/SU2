2fc37fdefb57f56de38ec2f962d403c9c13bbdf4
bmunguia <bmunguia10@gmail.com>
2020-04-03 18:17:35 -0700

    Directory

--- SU2_PY/SU2/amginria/__init__.py
+++ SU2_PY/SU2/amginria/__init__.py
@@ -1,9 +1,6 @@
 # SU2/amginria/__init__.py
 
 import _amgio
-# from importlib import import_module
-# from distutils.sysconfig import get_config_var
-# sys.modules["_amgio"] = import_module("_amgio"+get_config_var('EXT_SUFFIX'))
 
-from tools     import *
-from interface import *
+from .tools     import *
+from .interface import *
