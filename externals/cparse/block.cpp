23fac9feb157e1bbce0d31df0db8fa8175bba99c
Tim Albring <tim.albring@scicomp.uni-kl.de>
2020-04-11 14:43:47 +0200

    Adding if statement to parser

--- externals/cparse/block.cpp
+++ externals/cparse/block.cpp
@@ -13,24 +13,28 @@ Statement* buildStatement(const char** source, TokenMap scope) {
   uint i;
 
   switch (*code) {
-  case '{':
-    return new BlockStatement(code, source, scope);
-  case 'r':
-    _template = "return";
-    for (i = 1; i < 6; ++i)
-      if (code[i] != _template[i]) break;
+    case '{':
+      return new BlockStatement(code, source, scope);
+    case 'i':
+      if (code[1] == 'f' && !(isalnum(code[2]) || code[2] == '_'))
+        return new IfStatement(code+2, source, scope);
+      break;
+    case 'r':
+      _template = "return";
+      for (i = 1; i < 6; ++i)
+        if (code[i] != _template[i]) break;
 
-    if (i == 6 && !(isalnum(code[i]) || code[i] == '_'))
-      return new ReturnStatement(code+6, source, scope);
-    break;
-  case 'f':
+      if (i == 6 && !(isalnum(code[i]) || code[i] == '_'))
+        return new ReturnStatement(code+6, source, scope);
+      break;
+    case 'f':
 
-    _template = "function";
-    for (i = 1; i < 8; ++i)
-      if (code[i] != _template[i]) break;
+      _template = "function";
+      for (i = 1; i < 8; ++i)
+        if (code[i] != _template[i]) break;
 
-    if (i == 8 && !(isalnum(code[i]) || code[i] == '_'))
-      return new FuncDeclaration(code+8, source, scope);
+      if (i == 8 && !(isalnum(code[i]) || code[i] == '_'))
+        return new FuncDeclaration(code+8, source, scope);
 
   }
 
@@ -109,4 +113,50 @@ returnState BlockStatement::_exec(TokenMap scope) const {
   }
 
   return rs;
+}
+
+void IfStatement::_compile(const char* code, const char** rest,
+                           TokenMap parent_scope) {
+
+  while (isspace(*code)) ++code;
+
+  if (*code != '(') {
+    throw syntax_error("Expected '(' after `if` statement!");
+  }
+
+  // Parse the condition:
+  cond.compile(code+1, parent_scope, ")", &code);
+
+  if (*code != ')') {
+    throw syntax_error("Missing ')' after `if` statement!");
+  }
+
+  _then.compile(code+1, &code, parent_scope);
+
+  while (isspace(*code)) ++code;
+
+  // Check if the next word is else:
+  static const char* str = "else";
+  for (int i = 0; i < 4; ++i) {
+    if (str[i] != code[i]) {
+      if(rest) *rest = code;
+      return;
+    }
+  }
+  if (isalnum(code[4]) || code[4] == '_') {
+    if(rest) *rest = code;
+    return;
+  }
+
+  _else.compile(code+4, &code, parent_scope);
+
+  if(rest) *rest = code;
+}
+
+returnState IfStatement::_exec(TokenMap scope) const {
+  if (cond.eval(scope).asBool()) {
+    return _then.exec(scope);
+  } else {
+    return _else.exec(scope);
+  }
 }
--- externals/cparse/block.h
+++ externals/cparse/block.h
@@ -31,4 +31,25 @@ class BlockStatement : public Statement {
     return new BlockStatement(*this);
   }
 };
+
+
+class IfStatement : public Statement {
+  calculator cond;
+  BlockStatement _then;
+  BlockStatement _else;
+
+ private:
+  void _compile(const char* code, const char** rest, TokenMap parent_scope);
+  returnState _exec(TokenMap scope) const;
+
+ public:
+  IfStatement() {}
+  IfStatement(const char* code, const char** rest = 0,
+              TokenMap parent_scope = &TokenMap::empty) {
+    _compile(code, rest, parent_scope);
+  }
+  virtual Statement* clone() const {
+    return new IfStatement(*this);
+  }
+};
 #endif // BLOCK_H
