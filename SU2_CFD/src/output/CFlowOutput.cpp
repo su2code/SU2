ff5a13863a9d95f22de1a3a41f627bf238029099
flo <floriandmann@gmail.com>
2020-04-12 16:21:15 +0200

    whitespace

--- SU2_CFD/src/output/CFlowOutput.cpp
+++ SU2_CFD/src/output/CFlowOutput.cpp
@@ -276,7 +276,7 @@ void CFlowOutput::SetAnalyzeSurface(CSolver *solver, CGeometry *geometry, CConfi
 
           Surface_StreamVelocity2[iMarker]   += Vn2*Weight;
           Surface_TransvVelocity2[iMarker]   += Vtang2*Weight;
-          
+
         }
       }
     }
