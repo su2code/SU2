01154d05c29f8d71490395b01cdbf14bd21f310c
bmunguia <bmunguia10@gmail.com>
2020-04-21 18:53:55 -0700

    Communicate turb primvar

--- SU2_CFD/src/iteration_structure.cpp
+++ SU2_CFD/src/iteration_structure.cpp
@@ -2194,7 +2194,7 @@ void CDiscAdjFluidIteration::SetDependencies(CSolver *****solver,
   solver[iZone][iInst][MESH_0][FLOW_SOL]->InitiateComms(geometry[iZone][iInst][MESH_0], config[iZone], SOLUTION);
   solver[iZone][iInst][MESH_0][FLOW_SOL]->CompleteComms(geometry[iZone][iInst][MESH_0], config[iZone], SOLUTION);
   solver[iZone][iInst][MESH_0][FLOW_SOL]->InitiateComms(geometry[iZone][iInst][MESH_0], config[iZone], PRIMITIVE);
-  solver[iZone][iInst][MESH_0][FLOW_SOL]->CompleteComms(geometry[iZone][iInst][MESH_0], config[iZone], PRIMTIIVE);
+  solver[iZone][iInst][MESH_0][FLOW_SOL]->CompleteComms(geometry[iZone][iInst][MESH_0], config[iZone], PRIMITIVE);
   if (turbulent && !frozen_visc){
     solver[iZone][iInst][MESH_0][TURB_SOL]->Postprocessing(geometry[iZone][iInst][MESH_0],solver[iZone][iInst][MESH_0], config[iZone], MESH_0);
     solver[iZone][iInst][MESH_0][TURB_SOL]->InitiateComms(geometry[iZone][iInst][MESH_0], config[iZone], SOLUTION_EDDY);
