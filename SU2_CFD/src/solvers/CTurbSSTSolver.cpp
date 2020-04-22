863de65cd2632f4cab7ef1c4477f915aa91bc972
bmunguia <bmunguia10@gmail.com>
2020-04-21 20:10:20 -0700

    Don't reset Jac for discadj

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -289,12 +289,13 @@ void CTurbSSTSolver::Preprocessing(CGeometry *geometry, CSolver **solver_contain
 
   const bool limiter_turb = (config->GetKind_SlopeLimit_Turb() != NO_LIMITER) &&
                             (config->GetInnerIter() <= config->GetLimiterIter());
+  const bool disc_adjoint = config->GetDiscrete_Adjoint();
 
   /*--- Clear residual and system matrix, not needed for
    * reducer strategy as we write over the entire matrix. ---*/
   if (!ReducerStrategy) {
     LinSysRes.SetValZero();
-    Jacobian.SetValZero();
+    if (!disc_adjoint) Jacobian.SetValZero();
   }
 
  //  /*--- Upwind second order reconstruction and gradients ---*/
