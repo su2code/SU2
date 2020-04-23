ed74c1f0fe907e83daa2f900f37ff8cdb142d470
bmunguia <bmunguia10@gmail.com>
2020-04-23 10:25:11 -0700

    Switch FarField coords in viscous term

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -7239,8 +7239,10 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
         /*--- Set the normal vector and the coordinates ---*/
 
         visc_numerics->SetNormal(Normal);
-        visc_numerics->SetCoord(geometry->node[iPoint]->GetCoord(),
-                                geometry->node[Point_Normal]->GetCoord());
+//        visc_numerics->SetCoord(geometry->node[iPoint]->GetCoord(),
+//                                geometry->node[Point_Normal]->GetCoord());
+        visc_numerics->SetCoord(geometry->node[Point_Normal]->GetCoord(),
+                                geometry->node[iPoint]->GetCoord());
 
         /*--- Primitive variables, and gradient ---*/
 
