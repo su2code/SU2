e8381bd54b365c0d2bd72c528448c144994b0416
bmunguia <bmunguia10@gmail.com>
2020-04-23 11:08:53 -0700

    Grads in preprocessing after primvars recalclulated

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -310,30 +310,7 @@ void CTurbSSTSolver::Preprocessing(CGeometry *geometry, CSolver **solver_contain
     LinSysRes.SetValZero();
     Jacobian.SetValZero();
   }
-
- //  /*--- Upwind second order reconstruction and gradients ---*/
-
- // if (config->GetReconstructionGradientRequired()) {
- //   if (config->GetKind_Gradient_Method_Recon() == GREEN_GAUSS)
- //     SetPrimitive_Gradient_GG(geometry, config, true);
- //   if (config->GetKind_Gradient_Method_Recon() == LEAST_SQUARES)
- //     SetPrimitive_Gradient_LS(geometry, config, true);
- //   if (config->GetKind_Gradient_Method_Recon() == WEIGHTED_LEAST_SQUARES)
- //     SetPrimitive_Gradient_LS(geometry, config, true);
- // }
- // if (config->GetKind_Gradient_Method() == GREEN_GAUSS) SetPrimitive_Gradient_GG(geometry, config);
- // if (config->GetKind_Gradient_Method() == WEIGHTED_LEAST_SQUARES) SetPrimitive_Gradient_LS(geometry, config);
-
- // if (limiter_turb) SetPrimitive_Limiter(geometry, config);
-
-}
-
-void CTurbSSTSolver::Postprocessing(CGeometry *geometry, CSolver **solver_container, CConfig *config, unsigned short iMesh) {
-
-  su2double a1 = constants[7];
   
-  bool limiter_turb = (config->GetKind_SlopeLimit_Turb() != NO_LIMITER) && (config->GetInnerIter() <= config->GetLimiterIter());
-
   /*--- Compute mean flow and turbulence gradients ---*/
 
   if (config->GetReconstructionGradientRequired()) {
@@ -349,6 +326,12 @@ void CTurbSSTSolver::Postprocessing(CGeometry *geometry, CSolver **solver_contai
   if (config->GetKind_Gradient_Method() == WEIGHTED_LEAST_SQUARES) SetPrimitive_Gradient_LS(geometry, config);
 
   if (limiter_turb) SetPrimitive_Limiter(geometry, config);
+
+}
+
+void CTurbSSTSolver::Postprocessing(CGeometry *geometry, CSolver **solver_container, CConfig *config, unsigned short iMesh) {
+
+  su2double a1 = constants[7];
 
   SU2_OMP_FOR_STAT(omp_chunk_size)
   for (unsigned long iPoint = 0; iPoint < nPoint; iPoint ++) {
