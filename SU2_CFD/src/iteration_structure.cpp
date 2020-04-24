c2ca3efff3aba9fec69549583eb1f31c46157032
bmunguia <bmunguia10@gmail.com>
2020-04-23 19:46:43 -0700

    Reset in postprocessing

--- SU2_CFD/src/iteration_structure.cpp
+++ SU2_CFD/src/iteration_structure.cpp
@@ -2146,6 +2146,7 @@ void CDiscAdjFluidIteration::SetRecording(CSolver *****solver,
                                           unsigned short kind_recording) {
 
   bool frozen_visc = config[iZone]->GetFrozen_Visc_Disc();
+  bool sst = (config[iZone]->GetKind_Turb_Model() == SST) || (config[iZone]->GetKind_Turb_Model() == SST);
 
   /*--- Prepare for recording by resetting the solution to the initial converged solution ---*/
 
@@ -2158,6 +2159,7 @@ void CDiscAdjFluidIteration::SetRecording(CSolver *****solver,
   }
   if (turbulent && !frozen_visc) {
     solver[iZone][iInst][MESH_0][ADJTURB_SOL]->SetRecording(geometry[iZone][iInst][MESH_0], config[iZone]);
+    if (sst) solver[iZone][iInst][MESH_0][TURB_SOL]->Postprocessing(geometry[iZone][iInst][MESH_0],solver[iZone][iInst][MESH_0], config[iZone], MESH_0);
   }
   if (config[iZone]->GetWeakly_Coupled_Heat()) {
     solver[iZone][iInst][MESH_0][ADJHEAT_SOL]->SetRecording(geometry[iZone][iInst][MESH_0], config[iZone]);
