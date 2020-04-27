d0e5b6e7f3788f537fd0604928d78349389d42ab
bmunguia <bmunguia10@gmail.com>
2020-04-27 12:45:21 -0700

    Switch for k_inf and omega_inf based on flow direction

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -7245,7 +7245,10 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
 
         /*--- Set laminar and eddy viscosity at the infinity ---*/
         
-        const su2double StaticEnergy = Energy - 0.5*Velocity2 - GetTke_Inf();
+        su2double StaticEnergy = Energy - 0.5*Velocity2;
+        if (tkeNeeded) {
+          if (Qn_Infty < 0.0) StaticEnergy -= GetTke_Inf();
+          else StaticEnergy -= solver_container[TURB_SOL]->GetNodes()->GetPrimitive(iPoint,0);
 //        const su2double StaticEnergy = Energy - 0.5*Velocity2 - Kine_Infty;
         GetFluidModel()->SetTDState_rhoe(Density, StaticEnergy);
 
