a28e78437633c660dce9efc98dc99b6a31022426
bmunguia <bmunguia10@gmail.com>
2020-04-28 14:49:38 -0700

    Post pre post

--- SU2_CFD/src/iteration_structure.cpp
+++ SU2_CFD/src/iteration_structure.cpp
@@ -2207,8 +2207,8 @@ void CDiscAdjFluidIteration::SetDependencies(CSolver *****solver,
 
   if (turbulent && !frozen_visc){
     solver[iZone][iInst][MESH_0][TURB_SOL]->Postprocessing(geometry[iZone][iInst][MESH_0],solver[iZone][iInst][MESH_0], config[iZone], MESH_0);
-    solver[iZone][iInst][MESH_0][TURB_SOL]->InitiateComms(geometry[iZone][iInst][MESH_0], config[iZone], SOLUTION_EDDY);
-    solver[iZone][iInst][MESH_0][TURB_SOL]->CompleteComms(geometry[iZone][iInst][MESH_0], config[iZone], SOLUTION_EDDY);
+    solver[iZone][iInst][MESH_0][TURB_SOL]->InitiateComms(geometry[iZone][iInst][MESH_0], config[iZone], SOLUTION);
+    solver[iZone][iInst][MESH_0][TURB_SOL]->CompleteComms(geometry[iZone][iInst][MESH_0], config[iZone], SOLUTION);
   }
 
   if (heat){
