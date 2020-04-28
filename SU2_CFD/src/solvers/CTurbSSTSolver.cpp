94616e9e2c35a741f1ef327f09cccabf15e183a4
bmunguia <bmunguia10@gmail.com>
2020-04-27 17:41:27 -0700

    Revert

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -303,6 +303,9 @@ void CTurbSSTSolver::Preprocessing(CGeometry *geometry, CSolver **solver_contain
     nodes->SetPrimitive(iPoint, 0, rhokine/rho);
     nodes->SetPrimitive(iPoint, 1, rhoomega/rho);
   }
+  
+  InitiateComms(geometry, config, PRIMITIVE);
+  CompleteComms(geometry, config, PRIMITIVE);
 
   /*--- Clear residual and system matrix, not needed for
    * reducer strategy as we write over the entire matrix. ---*/
@@ -346,6 +349,9 @@ void CTurbSSTSolver::Postprocessing(CGeometry *geometry, CSolver **solver_contai
     nodes->SetPrimitive(iPoint, 0, rhokine/rho);
     nodes->SetPrimitive(iPoint, 1, rhoomega/rho);
   }
+  
+  InitiateComms(geometry, config, PRIMITIVE);
+  CompleteComms(geometry, config, PRIMITIVE);
   
   /*--- Compute mean flow and turbulence gradients ---*/
 
