576a4c6911b6fcc5ea6d04a53949ce68e5e9d851
CatarinaGarbacz <ana.g.gomes@ist.utl.pt>
2020-04-30 16:30:32 +0100

    chemical and thermal nonequilibrium bath running.

--- SU2_CFD/src/solvers/CNEMOEulerSolver.cpp
+++ SU2_CFD/src/solvers/CNEMOEulerSolver.cpp
@@ -2003,6 +2003,7 @@ void CNEMOEulerSolver::Source_Residual(CGeometry *geometry, CSolver **solution_c
 
     /*--- Compute the non-equilibrium chemistry ---*/
     numerics->ComputeChemistry(Residual, Source, Jacobian_i, config);
+
     /*--- Check for errors before applying source to the linear system ---*/
     err = false;
     for (iVar = 0; iVar < nVar; iVar++)
@@ -2024,12 +2025,6 @@ void CNEMOEulerSolver::Source_Residual(CGeometry *geometry, CSolver **solution_c
 
     numerics->ComputeVibRelaxation(Residual, Source, Jacobian_i, config);
 
-   //std::ofstream outfile;
-   //outfile.open("prints.txt", std::ios_base::app); // append instead of overwrite
-   //for(iVar = 0; iVar < nVar; iVar++){
-   // outfile << "Residual[" << iVar << "]=" << Residual[iVar] << endl; 
-   //}
-    
     /*--- Check for errors before applying source to the linear system ---*/
     err = false;
     for (iVar = 0; iVar < nVar; iVar++)
