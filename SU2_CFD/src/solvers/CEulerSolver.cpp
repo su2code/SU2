c278c2d55e3b6554c8ffe2038dbc53cdc0fafa44
bmunguia <bmunguia10@gmail.com>
2020-05-05 16:20:05 -0700

    Only interior state for visc numerics

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -7273,7 +7273,7 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
 
         /*--- Primitive variables, and gradient ---*/
 
-        visc_numerics->SetPrimitive(V_domain, V_infty);
+        visc_numerics->SetPrimitive(V_domain, V_domain);
         visc_numerics->SetPrimVarGradient(nodes->GetGradient_Primitive(iPoint),
                                           nodes->GetGradient_Primitive(iPoint));
 
@@ -7281,7 +7281,7 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
 
         if ((config->GetKind_Turb_Model() == SST) || (config->GetKind_Turb_Model() == SST_SUST)) {
           visc_numerics->SetTurbKineticEnergy(solver_container[TURB_SOL]->GetNodes()->GetPrimitive(iPoint,0),
-                                              Kine_Infty);
+                                              solver_container[TURB_SOL]->GetNodes()->GetPrimitive(iPoint,0));
 
           visc_numerics->SetTurbVarGradient(solver_container[TURB_SOL]->GetNodes()->GetGradient(iPoint),
                                             solver_container[TURB_SOL]->GetNodes()->GetGradient(iPoint));
