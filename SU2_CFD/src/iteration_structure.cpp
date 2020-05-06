9dba456806fa376fd6eca4328187c74a2d090a64
bmunguia <bmunguia10@gmail.com>
2020-05-05 18:10:33 -0700

    Preprocess after NS MG iteration

--- SU2_CFD/src/iteration_structure.cpp
+++ SU2_CFD/src/iteration_structure.cpp
@@ -497,17 +497,7 @@ void CFluidIteration::Iterate(COutput *output,
     }
 
   }
-  
-  if (config[val_iZone]->GetDiscrete_Adjoint()) {
-    su2double monitor = 1.0;
-    integration[val_iZone][val_iInst][FLOW_SOL]->NonDimensional_Parameters(geometry[val_iZone][val_iInst],
-                                                                           solver[val_iZone][val_iInst],
-                                                                           numerics[val_iZone][val_iInst],
-                                                                           config[val_iZone],
-                                                                           MESH_0,
-                                                                           RUNTIME_FLOW_SYS,
-                                                                           &monitor);
-  }
+
 }
 
 void CFluidIteration::Update(COutput *output,
