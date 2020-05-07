6efd572b60af5360c61ea7163ae66cbc55d13bd5
bmunguia <bmunguia10@gmail.com>
2020-05-06 18:05:34 -0700

    Partially nonreflective e

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -7191,9 +7191,9 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
       for (iDim = 0; iDim < nDim; iDim++) {
         Velocity2 += Velocity[iDim]*Velocity[iDim];
       }
-//      Pressure = Density*SoundSpeed*SoundSpeed/Gamma;
-      Pressure = 2.0*(Density*SoundSpeed*SoundSpeed/Gamma) - nodes->GetPressure(iPoint);
-      Energy   = Pressure/(Gamma_Minus_One*Density) + 0.5*Velocity2;
+      Pressure = Density*SoundSpeed*SoundSpeed/Gamma;
+//      Energy   = Pressure/(Gamma_Minus_One*Density) + 0.5*Velocity2;
+      Energy   = 2.*(Pressure-nodes->GetPressure(iPoint))/(Gamma_Minus_One*Density) + 0.5*Velocity2;
       if (tkeNeeded) {
 //        /*--- Inflow ---*/
 //        if (Qn_Infty < 0.0) Kine_Infty = GetTke_Inf();
