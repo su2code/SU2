f78b2b3e4150241201bc7b7bed20b1a885bb1942
bmunguia <bmunguia10@gmail.com>
2020-05-07 14:01:30 -0700

    hindexed

--- SU2_CFD/src/solvers/CSolver.cpp
+++ SU2_CFD/src/solvers/CSolver.cpp
@@ -4245,12 +4245,14 @@ void CSolver::Read_SU2_Restart_Binary(CGeometry *geometry, CConfig *config, stri
    points which are distributed throughout the file in blocks of nVar_Restart data. ---*/
 
   int *blocklen = new int[geometry->GetnPointDomain()];
-  int *displace = new int[geometry->GetnPointDomain()];
+//  int *displace = new int[geometry->GetnPointDomain()];
+  MPI_Aint *displace = new MPI_Aint[geometry->GetnPointDomain()];
   int counter = 0;
   for (iPoint_Global = 0; iPoint_Global < geometry->GetGlobal_nPointDomain(); iPoint_Global++ ) {
     if (geometry->GetGlobal_to_Local_Point(iPoint_Global) > -1) {
       blocklen[counter] = nFields;
-      displace[counter] = iPoint_Global*nFields;
+      displace[counter] = iPoint_Global*nFields*sizeof(passivedouble);
+//      displace[counter] = iPoint_Global*nFields;
       counter++;
     }
   }
