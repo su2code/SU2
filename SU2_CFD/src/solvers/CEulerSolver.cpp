a5f98068caaed13bdbacac1459ea7b2594e150cf
bmunguia <bmunguia10@gmail.com>
2020-05-06 18:07:16 -0700

    Extrapolate for convection

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -7192,8 +7192,7 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
         Velocity2 += Velocity[iDim]*Velocity[iDim];
       }
       Pressure = Density*SoundSpeed*SoundSpeed/Gamma;
-//      Energy   = Pressure/(Gamma_Minus_One*Density) + 0.5*Velocity2;
-      Energy   = 2.*(Pressure-nodes->GetPressure(iPoint))/(Gamma_Minus_One*Density) + 0.5*Velocity2;
+      Energy   = Pressure/(Gamma_Minus_One*Density) + 0.5*Velocity2;
       if (tkeNeeded) {
 //        /*--- Inflow ---*/
 //        if (Qn_Infty < 0.0) Kine_Infty = GetTke_Inf();
@@ -7201,7 +7200,6 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
 //        else {
           const su2double Intensity = config->GetTurbulenceIntensity_FreeStream();
           Kine_Infty = 3.0/2.0*(Velocity2*Intensity*Intensity);
-          Kine_Infty = 2.0*Kine_Infty - solver_container[TURB_SOL]->GetNodes()->GetPrimitive(iPoint,0);
 //        }
         Energy += Kine_Infty;
       }
