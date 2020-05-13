8126cc51d682a12b709055b3773148213dadfdb5
bmunguia <bmunguia10@gmail.com>
2020-05-13 14:54:00 -0700

    Subtract divergence^2 from source term

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -843,6 +843,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    }
    else {
      pk = Eddy_Viscosity_i*StrainMag_i*StrainMag_i - 2.0/3.0*Density_i*TurbVar_i[0]*diverg;
+     if (nDim == 2) pk -= 1./3.*Eddy_Viscosity_i*diverg*diverg;
      if ((pk > 0) && (pk < 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
        Jacobian_i[0][0] = min(-2./3.*diverg*Volume, 0.);
        if (TurbVar_i[1] > VorticityMag*F2_i/a1) Jacobian_i[1][1] = min(-2./3.*alfa_blended*diverg*Volume, 0.);
@@ -863,6 +864,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    else {
 //      pw = StrainMag_i*StrainMag_i - 2.0/3.0*zeta*diverg;
 //     pw = StrainMag - 2.0/3.0*diverg*diverg - 2.0/3.0*zeta*diverg;
+//     if (nDim == 2) pw -= 1./3.*diverg*diverg;
 //     if ((pw > 0.) && (TurbVar_i[1] > VorticityMag*F2_i/a1)) Jacobian_i[1][1] = -2./3.*alfa_blended*diverg*Volume;
     pw = pk*alfa_blended*Density_i/Eddy_Viscosity_i;
    }
