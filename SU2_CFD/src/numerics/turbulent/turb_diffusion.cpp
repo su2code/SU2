ca330335bab125c542293bc697e8e2896872425a
bmunguia <bmunguia10@gmail.com>
2020-05-14 12:16:02 -0700

    Bug in diffusion Jac

--- SU2_CFD/src/numerics/turbulent/turb_diffusion.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_diffusion.cpp
@@ -308,12 +308,12 @@ void CAvgGrad_TurbSST::FinishResidualCalc(const CConfig* config) {
     if (TurbVar_i[1] > VorticityMag*F2_i/a1) {
       Jacobian_i[0][0] += 0.5*sigma_kine_i/TurbVar_i[1]*Proj_Mean_GradTurbVar[0];
       Jacobian_i[0][1] += -0.5*sigma_kine_i*TurbVar_i[0]/pow(TurbVar_i[1],2.0)*Proj_Mean_GradTurbVar[0];
-      Jacobian_i[1][0] += 0.5*sigma_omega_i/TurbVar_i[1]*Proj_Mean_GradTurbVar[0];
-      Jacobian_i[1][1] += -0.5*sigma_omega_i*TurbVar_i[0]/pow(TurbVar_i[1],2.0)*Proj_Mean_GradTurbVar[0];
+      Jacobian_i[1][0] += 0.5*sigma_omega_i/TurbVar_i[1]*Proj_Mean_GradTurbVar[1];
+      Jacobian_i[1][1] += -0.5*sigma_omega_i*TurbVar_i[0]/pow(TurbVar_i[1],2.0)*Proj_Mean_GradTurbVar[1];
     }
     else {
       Jacobian_i[0][0] += 0.5*sigma_kine_i*a1/(VorticityMag*F2_i)*Proj_Mean_GradTurbVar[0];
-      Jacobian_i[1][0] += 0.5*sigma_omega_i*a1/(VorticityMag*F2_i)*Proj_Mean_GradTurbVar[0];
+      Jacobian_i[1][0] += 0.5*sigma_omega_i*a1/(VorticityMag*F2_i)*Proj_Mean_GradTurbVar[1];
     }
     
     VorticityMag = sqrt(Vorticity_j[0]*Vorticity_j[0] +
@@ -323,7 +323,7 @@ void CAvgGrad_TurbSST::FinishResidualCalc(const CConfig* config) {
     if (TurbVar_j[1] > VorticityMag*F2_j/a1) {
       Jacobian_j[0][0] += 0.5*sigma_kine_j/TurbVar_j[1]*Proj_Mean_GradTurbVar[0];
       Jacobian_j[0][1] += -0.5*sigma_kine_j*TurbVar_j[0]/pow(TurbVar_j[1],2.0)*Proj_Mean_GradTurbVar[0];
-      Jacobian_j[1][0] += 0.5*sigma_omega_j/TurbVar_j[1]*Proj_Mean_GradTurbVar[0];
+      Jacobian_j[1][0] += 0.5*sigma_omega_j/TurbVar_j[1]*Proj_Mean_GradTurbVar[1];
       Jacobian_j[1][1] += -0.5*sigma_omega_j*TurbVar_j[0]/pow(TurbVar_j[1],2.0)*Proj_Mean_GradTurbVar[1];
     }
     else {
