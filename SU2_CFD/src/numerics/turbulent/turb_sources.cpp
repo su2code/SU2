21cc67b7756788dda6e25ec5eecd3523006875c1
bmunguia <bmunguia10@gmail.com>
2020-05-14 19:51:15 -0700

    Get rid of source Jacobians

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -864,7 +864,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
 //       else {
 //         Jacobian_i[0][0] = (factor*a1/(VorticityMag*F2_i)-2./3.*diverg)*Volume;
 //       }
-       Jacobian_i[0][0] = min(-2./3.*diverg)*Volume, 0.0);
+       Jacobian_i[0][0] = min(-2./3.*diverg*Volume, 0.0);
        Jacobian_i[1][1] = min(-2./3.*alfa_blended*diverg*Volume, 0.0);
      }
 //     else if (pk > 0) {
