af4076d936fe579b953a3a921ff7859dcaff2fe1
bmunguia <bmunguia10@gmail.com>
2020-05-14 19:50:30 -0700

    Get rid of source Jacobians

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -856,14 +856,16 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      }
      pk += Eddy_Viscosity_i*factor;
      if ((pk > 0) && (pk < 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
-       if (TurbVar_i[1] > VorticityMag*F2_i/a1) {
-         Jacobian_i[0][0] = (factor/TurbVar_i[1]-2./3.*diverg)*Volume;
-         Jacobian_i[0][1] = -factor*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
-         Jacobian_i[1][1] = -2./3.*alfa_blended*diverg*Volume;
-       }
-       else {
-         Jacobian_i[0][0] = (factor*a1/(VorticityMag*F2_i)-2./3.*diverg)*Volume;
-       }
+//       if (TurbVar_i[1] > VorticityMag*F2_i/a1) {
+//         Jacobian_i[0][0] = (factor/TurbVar_i[1]-2./3.*diverg)*Volume;
+//         Jacobian_i[0][1] = -factor*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
+//         Jacobian_i[1][1] = -2./3.*alfa_blended*diverg*Volume;
+//       }
+//       else {
+//         Jacobian_i[0][0] = (factor*a1/(VorticityMag*F2_i)-2./3.*diverg)*Volume;
+//       }
+       Jacobian_i[0][0] = min(-2./3.*diverg)*Volume, 0.0);
+       Jacobian_i[1][1] = min(-2./3.*alfa_blended*diverg*Volume, 0.0);
      }
 //     else if (pk > 0) {
 //       Jacobian_i[0][0] = 20.0*beta_star*TurbVar_i[1]*Volume;
@@ -928,7 +930,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    Jacobian_i[1][1] += -2.0*beta_blended*TurbVar_i[1]*Volume;
 
 //   if (CDkw_i > 1e-20) Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
-    Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
+//    Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
   }
 
   AD::SetPreaccOut(Residual, nVar);
