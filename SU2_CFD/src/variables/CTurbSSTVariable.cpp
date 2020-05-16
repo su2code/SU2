5e7042db1b9777de08afb8bc52bd8ee36d5fab40
bmunguia <bmunguia10@gmail.com>
2020-05-16 14:21:34 -0700

    Machine epsilon

--- SU2_CFD/src/variables/CTurbSSTVariable.cpp
+++ SU2_CFD/src/variables/CTurbSSTVariable.cpp
@@ -79,10 +79,10 @@ void CTurbSSTVariable::SetBlendingFunc(unsigned long iPoint, su2double val_visco
 
   /*--- F1 ---*/
 
-  arg2A = sqrt(Primitive(iPoint,0))/(beta_star*Primitive(iPoint,1)*val_dist+EPS*EPS);
-  arg2B = 500.0*val_viscosity / (val_density*val_dist*val_dist*Primitive(iPoint,1)+EPS*EPS);
+  arg2A = sqrt(Primitive(iPoint,0))/(beta_star*Primitive(iPoint,1)*val_dist+eps*eps);
+  arg2B = 500.0*val_viscosity / (val_density*val_dist*val_dist*Primitive(iPoint,1)+eps*eps);
   arg2 = max(arg2A, arg2B);
-  arg1 = min(arg2, 4.0*val_density*sigma_om2*Primitive(iPoint,0) / (CDkw(iPoint)*val_dist*val_dist+EPS*EPS));
+  arg1 = min(arg2, 4.0*val_density*sigma_om2*Primitive(iPoint,0) / (CDkw(iPoint)*val_dist*val_dist+eps*eps));
 //  arg1 = min(arg2, 4.0*val_density*sigma_om2*Primitive(iPoint,0) / (max(CDkw(iPoint),pow(10.0,-20.0))*val_dist*val_dist+EPS*EPS));
   F1(iPoint) = tanh(pow(arg1, 4.0));
 
