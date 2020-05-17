ae89dcdc7e05f68434ffd470a7f22f5d4bae1bfb
bmunguia <bmunguia10@gmail.com>
2020-05-16 20:41:14 -0700

    Debug print

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -895,7 +895,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
     
    if (isnan(pk)) cout << "pk" << endl;
    if (isnan(alfa_blended)) cout << "alfa" << endl;
-    if (isnan(Eddy_Viscosity_i)) cout << "Eddy_Viscosity" << endl;
+   if (isnan(Density_i) cout << "Density" << endl;
+   if (isnan(Eddy_Viscosity_i)) cout << "Eddy_Viscosity" << endl;
     
    /*--- Sustaining terms, if desired. Note that if the production terms are
          larger equal than the sustaining terms, the original formulation is
