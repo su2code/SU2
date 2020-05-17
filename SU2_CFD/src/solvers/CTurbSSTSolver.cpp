92274cdd13707ddccec9bae9c02a2fb3a99ff3c7
bmunguia <bmunguia10@gmail.com>
2020-05-16 20:53:11 -0700

    Get muT from turb nodes

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -351,6 +351,7 @@ void CTurbSSTSolver::SetFlowPrimitive(CSolver **solver_container) {
   for (unsigned long iPoint = 0; iPoint < nPoint; iPoint++) {
     for (unsigned short iVar = 0; iVar < nDim+7; iVar++) {
       if (iVar == nDim+2) nodes->SetFlowPrimitive(iPoint,iVar,flowNodes->GetDensity(iPoint));
+      else if (iVar == nDim+6) nodes->SetFlowPrimitive(iPoint,iVar,nodes->GetmuT(iPoint));
       else nodes->SetFlowPrimitive(iPoint,iVar,flowNodes->GetPrimitive(iPoint,iVar));
     }
   }
