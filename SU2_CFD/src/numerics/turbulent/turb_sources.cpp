af390cac8752cfdda658ce03b4148d5877dcbff3
bmunguia <bmunguia10@gmail.com>
2020-05-19 12:35:08 -0700

    Move max(CDkw,eps) to numerics

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -910,7 +910,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
 
    /*--- Cross diffusion ---*/
 
-   Residual[1] += (1.0 - F1_i)*CDkw_i*Volume;
+   const su2double eps = numeric_limits<passivedouble>::epsilon();
+   Residual[1] += (1.0 - F1_i)*max(CDkw_i,eps)*Volume;
 
    /*--- Implicit part ---*/
 
@@ -919,7 +920,6 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    Jacobian_i[1][0] += 0.0;
    Jacobian_i[1][1] += -2.0*beta_blended*TurbVar_i[1]*Volume;
 
-   const su2double eps = numeric_limits<passivedouble>::epsilon();
    if (CDkw_i > eps) Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
   }
   
