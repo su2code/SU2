5ad137793a976a3c2fcf18bb5baa0b03f757d852
Pedro Gomes <pcarruscag@gmail.com>
2020-05-20 19:31:50 +0100

    move AD inlines to hpp, remove inl

--- Common/include/ad_structure.hpp
+++ Common/include/ad_structure.hpp
@@ -6,7 +6,7 @@
  *
  * SU2 Project Website: https://su2code.github.io
  *
- * The SU2 Project is maintained by the SU2 Foundation 
+ * The SU2 Project is maintained by the SU2 Foundation
  * (http://su2foundation.org)
  *
  * Copyright 2012-2020, SU2 Contributors (cf. AUTHORS.md)
@@ -27,105 +27,108 @@
 
 #pragma once
 
-#include "../include/datatype_structure.hpp"
+#include "datatype_structure.hpp"
 
 /*!
  * \namespace AD
  * \brief Contains routines for the reverse mode of AD.
- * In case there is no reverse type configured, they have no effect at all.
+ * In case there is no reverse type configured, they have no effect at all,
+ * and so the real versions of the routined are after #else.
  */
-
 namespace AD{
+#ifndef CODI_REVERSE_TYPE
   /*!
    * \brief Start the recording of the operations and involved variables.
    * If called, the computational graph of all operations occuring after the call will be stored,
    * starting with the variables registered with RegisterInput.
    */
-  void StartRecording();
+  inline void StartRecording() {}
 
   /*!
    * \brief Stops the recording of the operations and variables.
    */
-  void StopRecording();
+  inline void StopRecording() {}
 
   /*!
    * \brief Check if the tape is active
    * \param[out] Boolean which determines whether the tape is active.
    */
-  bool TapeActive();
+  inline bool TapeActive() {return false;}
 
   /*!
    * \brief Prints out tape statistics.
    */
-  void PrintStatistics();
+  inline void PrintStatistics() {}
 
   /*!
    * \brief Registers the variable as an input and saves internal data (indices). I.e. as a leaf of the computational graph.
    * \param[in] data - The variable to be registered as input.
    * \param[in] push_index - boolean whether we also want to push the index.
    */
-  void RegisterInput(su2double &data, bool push_index = true);
+  inline void RegisterInput(su2double &data, bool push_index = true) {}
 
   /*!
    * \brief Registers the variable as an output. I.e. as the root of the computational graph.
    * \param[in] data - The variable to be registered as output.
    */
-  void RegisterOutput(su2double &data);
+  inline void RegisterOutput(su2double &data) {}
 
   /*!
    * \brief Sets the adjoint value at index to val
    * \param[in] index - Position in the adjoint vector.
    * \param[in] val - adjoint value to be set.
    */
-  void SetDerivative(int index, const double val);
+  inline void SetDerivative(int index, const double val) {}
 
   /*!
    * \brief Extracts the adjoint value at index
    * \param[in] index - position in the adjoint vector where the derivative will be extracted.
    * \return Derivative value.
    */
-  double GetDerivative(int index);
+  inline double GetDerivative(int index) {return 0.0;}
 
   /*!
    * \brief Clears the currently stored adjoints but keeps the computational graph.
    */
-  void ClearAdjoints();
+  inline void ClearAdjoints() {}
 
   /*!
    * \brief Computes the adjoints, i.e. the derivatives of the output with respect to the input variables.
    */
-  void ComputeAdjoint();  
-  
+  inline void ComputeAdjoint() {}
+
   /*!
    * \brief Computes the adjoints, i.e. the derivatives of the output with respect to the input variables.
    * \param[in] enter - Position where we start evaluating the tape.
    * \param[in] leave - Position where we stop evaluating the tape.
    */
-  void ComputeAdjoint(unsigned short enter, unsigned short leave);
+  inline void ComputeAdjoint(unsigned short enter, unsigned short leave) {}
 
   /*!
    * \brief Reset the tape structure to be ready for a new recording.
    */
-  void Reset();
+  inline void Reset() {}
 
   /*!
    * \brief Reset the variable (set index to zero).
    * \param[in] data - the variable to be unregistered from the tape.
    */
-  void ResetInput(su2double &data);
+  inline void ResetInput(su2double &data) {}
 
   /*!
-   * \brief Sets the scalar input of a preaccumulation section.
-   * \param[in] data - the scalar input variable.
+   * \brief Sets the scalar inputs of a preaccumulation section.
+   * \param[in] data - the scalar input variables.
    */
-  void SetPreaccIn(const su2double &data);
+  template<class... Ts>
+  inline void SetPreaccIn(Ts&&... data) {}
 
   /*!
    * \brief Sets the input variables of a preaccumulation section using a 1D array.
    * \param[in] data - the input 1D array.
    * \param[in] size - size of the array.
    */
-  void SetPreaccIn(const su2double* data, const int size);
+  template<class T>
+  inline void SetPreaccIn(const T& data, const int size) {}
 
   /*!
    * \brief Sets the input variables of a preaccumulation section using a 2D array.
@@ -133,7 +136,8 @@ namespace AD{
    * \param[in] size_x - size of the array in x dimension.
    * \param[in] size_y - size of the array in y dimension.
    */
-  void SetPreaccIn(const su2double* const *data, const int size_x, const int size_y);
+  template<class T>
+  inline void SetPreaccIn(const T& data, const int size_x, const int size_y) {}
 
   /*!
    * \brief Starts a new preaccumulation section and sets the input variables.
@@ -145,73 +149,79 @@ namespace AD{
    *
    * Note: the call of this routine must be followed by a call of EndPreacc() and the end of the code section.
    */
-  void StartPreacc();
+  inline void StartPreacc() {}
 
   /*!
-   * \brief Sets the scalar output of a preaccumulation section.
-   * \param[in] data - the scalar output variable.
+   * \brief Sets the scalar outputs of a preaccumulation section.
+   * \param[in] data - the scalar output variables.
    */
-  void SetPreaccOut(su2double &data);
+  template<class... Ts>
+  inline void SetPreaccOut(Ts&&... data) {}
 
   /*!
    * \brief Sets the output variables of a preaccumulation section using a 1D array.
    * \param[in] data - the output 1D array.
    */
-  void SetPreaccOut(su2double* data, const int size);
+  template<class T>
+  inline void SetPreaccOut(T&& data, const int size) {}
 
   /*!
    * \brief Sets the input variables of a preaccumulation section using a 2D array.
    * \param[in] data - the output 1D array.
    */
-  void SetPreaccOut(su2double** data, const int size_x, const int size_y);
+  template<class T>
+  inline void SetPreaccOut(T&& data, const int size_x, const int size_y) {}
 
   /*!
    * \brief Ends a preaccumulation section and computes the local Jacobi matrix
    * of a code section using the variables set with SetLocalInput(), SetLocalOutput() and pushes a statement
    * for each output variable to the AD tape.
    */
-  void EndPreacc();
-  
+  inline void EndPreacc() {}
+
   /*!
    * \brief Initializes an externally differentiated function. Input and output variables are set with SetExtFuncIn/SetExtFuncOut
    * \param[in] storePrimalInput - Specifies whether the primal input values are stored for the reverse call of the external function.
    * \param[in] storePrimalOutput - Specifies whether the primal output values are stored for the reverse call of the external function.
    */
-  void StartExtFunc(bool storePrimalInput, bool storePrimalOutput);
-  
+  inline void StartExtFunc(bool storePrimalInput, bool storePrimalOutput) {}
+
   /*!
    * \brief Sets the scalar input of a externally differentiated function.
    * \param[in] data - the scalar input variable.
    */
-  void SetExtFuncIn(su2double &data);
-  
+  inline void SetExtFuncIn(su2double &data) {}
+
   /*!
    * \brief Sets the input variables of a externally differentiated function using a 1D array.
    * \param[in] data - the input 1D array.
    * \param[in] size - number of rows.
    */
-  void SetExtFuncIn(const su2double* data, const int size);
-  
+  template<class T>
+  inline void SetExtFuncIn(const T& data, const int size) {}
+
   /*!
   * \brief  Sets the input variables of a externally differentiated function using a 2D array.
   * \param[in] data - the input 2D array.
   * \param[in] size_x - number of rows.
   * \param[in] size_y - number of columns.
   */
-  void SetExtFuncIn(const su2double* const *data, const int size_x, const int size_y);
+  template<class T>
+  inline void SetExtFuncIn(const T& data, const int size_x, const int size_y) {}
 
   /*!
    * \brief Sets the scalar output of a externally differentiated function.
    * \param[in] data - the scalar output variable.
    */
-  void SetExtFuncOut(su2double &data);
+  inline void SetExtFuncOut(su2double &data) {}
 
   /*!
    * \brief Sets the output variables of a externally differentiated function using a 1D array.
    * \param[in] data - the output 1D array.
    * \param[in] size - number of rows.
    */
-  void SetExtFuncOut(su2double* data, const int size);
+  template<class T>
+  inline void SetExtFuncOut(T&& data, const int size) {}
 
   /*!
   * \brief  Sets the output variables of a externally differentiated function using a 2D array.
@@ -219,25 +229,268 @@ namespace AD{
   * \param[in] size_x - number of rows.
   * \param[in] size_y - number of columns.
   */
-  void SetExtFuncOut(su2double** data, const int size_x, const int size_y);
-  
+  template<class T>
+  inline void SetExtFuncOut(T&& data, const int size_x, const int size_y) {}
+
   /*!
    * \brief Ends an external function section by deleting the structures.
    */
-  void EndExtFunc();
+  inline void EndExtFunc() {}
 
   /*!
    * \brief Evaluates and saves gradient data from a variable.
    * \param[in] data - variable whose gradient information will be extracted.
    * \param[in] index - where obtained gradient information will be stored.
    */
-  void SetIndex(int &index, const su2double &data);
+  inline void SetIndex(int &index, const su2double &data) {}
 
   /*!
    * \brief Pushes back the current tape position to the tape position's vector.
    */
-  void Push_TapePosition();
-}
+  inline void Push_TapePosition() {}
+
+#else
+  typedef codi::DataStore CheckpointHandler;
+
+  typedef su2double::TapeType Tape;
+
+  typedef codi::ExternalFunctionHelper<su2double> ExtFuncHelper;
+
+  extern ExtFuncHelper* FuncHelper;
+
+  /*--- Stores the indices of the input variables (they might be overwritten) ---*/
+
+  extern std::vector<su2double::GradientData> inputValues;
+
+  /*--- Current position inside the adjoint vector ---*/
+
+  extern int adjointVectorPosition;
+
+  /*--- Reference to the tape ---*/
+
+  extern su2double::TapeType& globalTape;
+
+  extern bool Status;
+
+  extern bool PreaccActive;
+
+  extern bool PreaccEnabled;
+
+  extern su2double::TapeType::Position StartPosition, EndPosition;
+
+  extern std::vector<su2double::TapeType::Position> TapePositions;
+
+  extern std::vector<su2double::GradientData> localInputValues;
+
+  extern std::vector<su2double*> localOutputValues;
+
+  extern codi::PreaccumulationHelper<su2double> PreaccHelper;
+
+  FORCEINLINE void RegisterInput(su2double &data, bool push_index = true) {
+    AD::globalTape.registerInput(data);
+    if (push_index) {
+      inputValues.push_back(data.getGradientData());
+    }
+  }
+
+  FORCEINLINE void RegisterOutput(su2double& data) {AD::globalTape.registerOutput(data);}
+
+  FORCEINLINE void ResetInput(su2double &data) {data.getGradientData() = su2double::GradientData();}
+
+  FORCEINLINE void StartRecording() {AD::globalTape.setActive();}
+
+  FORCEINLINE void StopRecording() {AD::globalTape.setPassive();}
+
+  FORCEINLINE bool TapeActive() { return AD::globalTape.isActive(); }
+
+  FORCEINLINE void PrintStatistics() {AD::globalTape.printStatistics();}
+
+  FORCEINLINE void ClearAdjoints() {AD::globalTape.clearAdjoints(); }
+
+  FORCEINLINE void ComputeAdjoint() {AD::globalTape.evaluate(); adjointVectorPosition = 0;}
+
+  FORCEINLINE void ComputeAdjoint(unsigned short enter, unsigned short leave) {
+    AD::globalTape.evaluate(TapePositions[enter], TapePositions[leave]);
+    if (leave == 0)
+      adjointVectorPosition = 0;
+  }
+
+  FORCEINLINE void Reset() {
+    globalTape.reset();
+    if (inputValues.size() != 0) {
+      adjointVectorPosition = 0;
+      inputValues.clear();
+    }
+    if (TapePositions.size() != 0) {
+      TapePositions.clear();
+    }
+  }
+
+  FORCEINLINE void SetIndex(int &index, const su2double &data) {
+    index = data.getGradientData();
+  }
+
+  FORCEINLINE void SetDerivative(int index, const double val) {
+    AD::globalTape.setGradient(index, val);
+  }
+
+  FORCEINLINE double GetDerivative(int index) {
+    return AD::globalTape.getGradient(index);
+  }
+
+  /*--- Base case for parameter pack expansion. ---*/
+  FORCEINLINE void SetPreaccIn() {}
+
+  template<class T, class... Ts,
+           typename std::enable_if<std::is_same<T,su2double>::value,bool>::type = 0>
+  FORCEINLINE void SetPreaccIn(const T& data, Ts&&... moreData) {
+    if (!PreaccActive) return;
+    if (data.isActive())
+      PreaccHelper.addInput(data);
+    SetPreaccIn(moreData...);
+  }
+
+  template<class T>
+  FORCEINLINE void SetPreaccIn(const T& data, const int size) {
+    if (PreaccActive) {
+      for (int i = 0; i < size; i++) {
+        if (data[i].isActive()) {
+          PreaccHelper.addInput(data[i]);
+        }
+      }
+    }
+  }
+
+  template<class T>
+  FORCEINLINE void SetPreaccIn(const T& data, const int size_x, const int size_y) {
+    if (!PreaccActive) return;
+    for (int i = 0; i < size_x; i++) {
+      for (int j = 0; j < size_y; j++) {
+        if (data[i][j].isActive()) {
+          PreaccHelper.addInput(data[i][j]);
+        }
+      }
+    }
+  }
+
+  FORCEINLINE void StartPreacc() {
+    if (globalTape.isActive() && PreaccEnabled) {
+      PreaccHelper.start();
+      PreaccActive = true;
+    }
+  }
+
+  /*--- Base case for parameter pack expansion. ---*/
+  FORCEINLINE void SetPreaccOut() {}
+
+  template<class T, class... Ts,
+           typename std::enable_if<std::is_same<T,su2double>::value,bool>::type = 0>
+  FORCEINLINE void SetPreaccOut(T& data, Ts&&... moreData) {
+    if (!PreaccActive) return;
+    if (data.isActive())
+      PreaccHelper.addOutput(data);
+    SetPreaccOut(moreData...);
+  }
+
+  template<class T>
+  FORCEINLINE void SetPreaccOut(T&& data, const int size) {
+    if (PreaccActive) {
+      for (int i = 0; i < size; i++) {
+        if (data[i].isActive()) {
+          PreaccHelper.addOutput(data[i]);
+        }
+      }
+    }
+  }
+
+  template<class T>
+  FORCEINLINE void SetPreaccOut(T&& data, const int size_x, const int size_y) {
+    if (!PreaccActive) return;
+    for (int i = 0; i < size_x; i++) {
+      for (int j = 0; j < size_y; j++) {
+        if (data[i][j].isActive()) {
+          PreaccHelper.addOutput(data[i][j]);
+        }
+      }
+    }
+  }
+
+  FORCEINLINE void Push_TapePosition() {
+    TapePositions.push_back(AD::globalTape.getPosition());
+  }
+
+  FORCEINLINE void EndPreacc(){
+    if (PreaccActive) {
+      PreaccHelper.finish(false);
+    }
+  }
+
+  FORCEINLINE void StartExtFunc(bool storePrimalInput, bool storePrimalOutput){
+    FuncHelper = new ExtFuncHelper(true);
+    if (!storePrimalInput){
+      FuncHelper->disableInputPrimalStore();
+    }
+    if (!storePrimalOutput){
+      FuncHelper->disableOutputPrimalStore();
+    }
+  }
+
+  FORCEINLINE void SetExtFuncIn(const su2double &data) {
+    FuncHelper->addInput(data);
+  }
+
+  template<class T>
+  FORCEINLINE void SetExtFuncIn(const T& data, const int size) {
+    for (int i = 0; i < size; i++) {
+      FuncHelper->addInput(data[i]);
+    }
+  }
+
+  template<class T>
+  FORCEINLINE void SetExtFuncIn(const T& data, const int size_x, const int size_y) {
+    for (int i = 0; i < size_x; i++) {
+      for (int j = 0; j < size_y; j++) {
+        FuncHelper->addInput(data[i][j]);
+      }
+    }
+  }
+
+  FORCEINLINE void SetExtFuncOut(su2double& data) {
+    if (globalTape.isActive()) {
+      FuncHelper->addOutput(data);
+    }
+  }
+
+  template<class T>
+  FORCEINLINE void SetExtFuncOut(T&& data, const int size) {
+    for (int i = 0; i < size; i++) {
+      if (globalTape.isActive()) {
+        FuncHelper->addOutput(data[i]);
+      }
+    }
+  }
+
+  template<class T>
+  FORCEINLINE void SetExtFuncOut(T&& data, const int size_x, const int size_y) {
+    for (int i = 0; i < size_x; i++) {
+      for (int j = 0; j < size_y; j++) {
+        if (globalTape.isActive()) {
+          FuncHelper->addOutput(data[i][j]);
+        }
+      }
+    }
+  }
+
+  FORCEINLINE void delete_handler(void *handler) {
+    CheckpointHandler *checkpoint = static_cast<CheckpointHandler*>(handler);
+    checkpoint->clear();
+  }
+
+  FORCEINLINE void EndExtFunc() { delete FuncHelper; }
+
+#endif // CODI_REVERSE_TYPE
+
+} // namespace AD
 
 /*--- Macro to begin and end sections with a passive tape ---*/
 
@@ -257,6 +510,7 @@ namespace AD{
 #define AD_END_PASSIVE
 #endif
 
+
 /*--- If we compile under OSX we have to overload some of the operators for
  *   complex numbers to avoid the use of the standard operators
  *  (they use a lot of functions that are only defined for doubles) ---*/
@@ -264,14 +518,35 @@ namespace AD{
 #ifdef __APPLE__
 
 namespace std{
-  template<>
-  su2double abs(const complex<su2double>& x);
 
   template<>
-  complex<su2double> operator/(const complex<su2double>& x, const complex<su2double>& y);
+  inline su2double abs(const complex<su2double>& x){
+
+    return sqrt(x.real()*x.real() + x.imag()*x.imag());
+
+  }
 
   template<>
-  complex<su2double> operator*(const complex<su2double>& x, const complex<su2double>& y);
+  inline complex<su2double> operator/(const complex<su2double>& x,
+                                      const complex<su2double>& y){
+
+    su2double d    = (y.real()*y.real() + y.imag()*y.imag());
+    su2double real = (x.real()*y.real() + x.imag()*y.imag())/d;
+    su2double imag = (x.imag()*y.real() - x.real()*y.imag())/d;
+
+    return complex<su2double>(real, imag);
+
+  }
+
+  template<>
+  inline complex<su2double> operator*(const complex<su2double>& x,
+                                      const complex<su2double>& y){
+
+    su2double real = (x.real()*y.real() - x.imag()*y.imag());
+    su2double imag = (x.imag()*y.real() + x.real()*y.imag());
+
+    return complex<su2double>(real, imag);
+
+  }
 }
 #endif
-#include "ad_structure.inl"
