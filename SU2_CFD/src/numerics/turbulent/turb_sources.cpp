794c1fc9cdc01653087fbd5a678fb0fef16ba0a0
bmunguia <bmunguia10@gmail.com>
2020-05-19 22:00:35 -0700

    Stress limiter in cross diffusion

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -915,7 +915,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
 
    /*--- Cross diffusion ---*/
 
-    Residual[1] += (1.0 - F1_i)*max(CDkw_i,0.0)*Volume;
+    Residual[1] += (1.0 - F1_i)*max(CDkw_i,0.0)*TurbVar_i[1]*Eddy_Viscosity_i/(Density_i*TurbVar_i[0])*Volume;
 
    /*--- Implicit part ---*/
 
@@ -924,7 +924,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    Jacobian_i[1][0] += 0.0;
    Jacobian_i[1][1] += -2.0*beta_blended*TurbVar_i[1]*Volume;
 
-    if (CDkw_i > 0.0) Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
+    if (CDkw_i > 0.0 && TurbVar_i[1] > VorticityMag_i*F2_i/a1) Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
   }
   
   AD::SetPreaccOut(Residual, nVar);
