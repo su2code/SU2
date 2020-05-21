561d45bdc2dc1e0188bf972bd7fea468f87887cd
bmunguia <bmunguia10@gmail.com>
2020-05-20 22:37:55 -0700

    Don't extrapolate k/omega

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -7194,14 +7194,15 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver_container,
       Pressure = Density*SoundSpeed*SoundSpeed/Gamma;
       Energy   = Pressure/(Gamma_Minus_One*Density) + 0.5*Velocity2;
       if (tkeNeeded) {
-//        if (Qn_Infty > 0.0) {
-//          /*--- Outflow conditions ---*/
-          const su2double Intensity = config->GetTurbulenceIntensity_FreeStream();
-          Kine_Infty = 3.0/2.0*(Velocity2*Intensity*Intensity);
-//        }
-//        else {
-//          Kine_Infty = solver_container[TURB_SOL]->GetNodes()->GetPrimitive(iPoint,0);
-//        }
+        if (Qn_Infty > 0.0) {
+          /*--- Outflow conditions ---*/
+//          const su2double Intensity = config->GetTurbulenceIntensity_FreeStream();
+//          Kine_Infty = 3.0/2.0*(Velocity2*Intensity*Intensity);
+          Kine_Infty = GetTke_Inf();
+        }
+        else {
+          Kine_Infty = solver_container[TURB_SOL]->GetNodes()->GetPrimitive(iPoint,0);
+        }
         Energy += Kine_Infty;
       }
 
