ffe8225db4e3563dfcf86cd7716ddbd0f4d268b8
bmunguia <bmunguia10@gmail.com>
2020-05-21 12:07:29 -0700

    No production limiter on omega

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -854,11 +854,12 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
        }
      }
      pk += Eddy_Viscosity_i*factor;
+     pw = pk*alfa_blended*Density_i/Eddy_Viscosity_i;
      if ((pk > 0) && (pk < 10.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
        if (TurbVar_i[1] > VorticityMag_i*F2_i/a1) {
          Jacobian_i[0][0] = (factor/TurbVar_i[1]-2./3.*diverg)*Volume;
          Jacobian_i[0][1] = -factor*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
-         Jacobian_i[1][1] = -2./3.*alfa_blended*diverg*Volume;
+//         Jacobian_i[1][1] = -2./3.*alfa_blended*diverg*Volume;
        }
        else {
          Jacobian_i[0][0] = (factor*a1/(VorticityMag_i*F2_i)-2./3.*diverg)*Volume;
@@ -867,9 +868,12 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      else if (pk > 0) {
        Jacobian_i[0][0] = 10.0*beta_star*TurbVar_i[1]*Volume;
        Jacobian_i[0][1] = 10.0*beta_star*TurbVar_i[0]*Volume;
-       if (TurbVar_i[1] > VorticityMag_i*F2_i/a1) {
-         Jacobian_i[1][1] = 20.0*alfa_blended*beta_star*TurbVar_i[1]*Volume;
-       }
+//       if (TurbVar_i[1] > VorticityMag_i*F2_i/a1) {
+//         Jacobian_i[1][1] = 20.0*alfa_blended*beta_star*TurbVar_i[1]*Volume;
+//       }
+     }
+     if (TurbVar_i[1] > VorticityMag_i*F2_i/a1) {
+       Jacobian_i[1][1] = -2./3.*alfa_blended*diverg*Volume;
      }
    }
 
@@ -883,9 +887,9 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      pw = PerturbedStrainMag * PerturbedStrainMag - 2.0/3.0*zeta*diverg;
      pw = alfa_blended*Density_i*max(pw,0.0);
    }
-   else {
-    pw = pk*alfa_blended*Density_i/Eddy_Viscosity_i;
-   }
+//   else {
+//    pw = pk*alfa_blended*Density_i/Eddy_Viscosity_i;
+//   }
     
    /*--- Sustaining terms, if desired. Note that if the production terms are
          larger equal than the sustaining terms, the original formulation is
