220a5dd71b4b1f488c151a6acf1c7d2c50a1e199
bmunguia <bmunguia10@gmail.com>
2020-05-26 17:04:06 -0700

    SST 1993 constants

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -855,7 +855,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      }
      pk += Eddy_Viscosity_i*factor;
      pw = pk*alfa_blended*Density_i/Eddy_Viscosity_i;
-     if ((pk > 0) && (pk <= 10.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
+     if ((pk > 0) && (pk <= 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
        if (TurbVar_i[1] > VorticityMag_i*F2_i/a1) {
          Jacobian_i[0][0] = (factor/TurbVar_i[1]-2./3.*diverg)*Volume;
          Jacobian_i[0][1] = -factor*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
