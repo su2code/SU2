56ccaed77ad369fe9c0706c3a8511603700e998d
bmunguia <bmunguia10@gmail.com>
2020-05-29 13:08:51 -0700

    No cross-diffusion limiter

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -923,8 +923,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
 
    /*--- Cross diffusion ---*/
 
-   Residual[1] += (1.0 - F1_i)*max(CDkw_i,0.)*Volume;
-//    Residual[1] += (1.0 - F1_i)*CDkw_i*Volume;
+//   Residual[1] += (1.0 - F1_i)*max(CDkw_i,0.)*Volume;
+    Residual[1] += (1.0 - F1_i)*CDkw_i*Volume;
 
    /*--- Implicit part ---*/
 
@@ -933,8 +933,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    Jacobian_i[1][0] += 0.0;
    Jacobian_i[1][1] += -2.0*beta_blended*TurbVar_i[1]*Volume;
 
-    if (CDkw_i > 0.) Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
-//    Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
+//    if (CDkw_i > 0.) Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
+    Jacobian_i[1][1] += -(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
   }
   
   AD::SetPreaccOut(Residual, nVar);
