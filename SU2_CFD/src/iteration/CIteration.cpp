571a0ac1f9f265db78e0d5401473fccf08a3ec7c
Tim Albring <tim.albring@scicomp.uni-kl.de>
2020-06-03 15:27:06 +0200

    Clang format CIteration.cpp

--- SU2_CFD/src/iteration/CIteration.cpp
+++ SU2_CFD/src/iteration/CIteration.cpp
@@ -25,12 +25,12 @@
  * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include "../include/iteration/CIteration.hpp"
-#include "../include/solvers/CFEASolver.hpp"
-#include "../include/output/COutput.hpp"
 
-CIteration::CIteration(const CConfig *config) {
+#include "../include/output/COutput.hpp"
+#include "../include/solvers/CFEASolver.hpp"
+
+CIteration::CIteration(const CConfig* config) {
   rank = SU2_MPI::GetRank();
   size = SU2_MPI::GetSize();
 
@@ -39,19 +39,13 @@ CIteration::CIteration(const CConfig *config) {
 
   multizone = config->GetMultizone_Problem();
   singlezone = !(config->GetMultizone_Problem());
-
 }
 
-CIteration::~CIteration(void) { }
+CIteration::~CIteration(void) {}
 
-void CIteration::SetGrid_Movement(CGeometry **geometry,
-          CSurfaceMovement *surface_movement,
-          CVolumetricMovement *grid_movement,
-          CSolver ***solver,
-          CConfig *config,
-          unsigned long IntIter,
-          unsigned long TimeIter)   {
-
+void CIteration::SetGrid_Movement(CGeometry** geometry, CSurfaceMovement* surface_movement,
+                                  CVolumetricMovement* grid_movement, CSolver*** solver, CConfig* config,
+                                  unsigned long IntIter, unsigned long TimeIter) {
   unsigned short Kind_Grid_Movement = config->GetKind_GridMovement();
   unsigned long nIterMesh;
   bool stat_mesh = true;
@@ -64,44 +58,37 @@ void CIteration::SetGrid_Movement(CGeometry **geometry,
 
   /*--- Perform mesh movement depending on specified type ---*/
   switch (Kind_Grid_Movement) {
+    case RIGID_MOTION:
 
-  case RIGID_MOTION:
+      if (rank == MASTER_NODE) cout << endl << " Performing rigid mesh transformation." << endl;
 
-    if (rank == MASTER_NODE)
-      cout << endl << " Performing rigid mesh transformation." << endl;
+      /*--- Move each node in the volume mesh using the specified type
+       of rigid mesh motion. These routines also compute analytic grid
+       velocities for the fine mesh. ---*/
 
-    /*--- Move each node in the volume mesh using the specified type
-     of rigid mesh motion. These routines also compute analytic grid
-     velocities for the fine mesh. ---*/
+      grid_movement->Rigid_Translation(geometry[MESH_0], config, val_iZone, TimeIter);
+      grid_movement->Rigid_Plunging(geometry[MESH_0], config, val_iZone, TimeIter);
+      grid_movement->Rigid_Pitching(geometry[MESH_0], config, val_iZone, TimeIter);
+      grid_movement->Rigid_Rotation(geometry[MESH_0], config, val_iZone, TimeIter);
 
-    grid_movement->Rigid_Translation(geometry[MESH_0], config, val_iZone, TimeIter);
-    grid_movement->Rigid_Plunging(geometry[MESH_0], config, val_iZone, TimeIter);
-    grid_movement->Rigid_Pitching(geometry[MESH_0], config, val_iZone, TimeIter);
-    grid_movement->Rigid_Rotation(geometry[MESH_0], config, val_iZone, TimeIter);
+      /*--- Update the multigrid structure after moving the finest grid,
+       including computing the grid velocities on the coarser levels. ---*/
 
-    /*--- Update the multigrid structure after moving the finest grid,
-     including computing the grid velocities on the coarser levels. ---*/
+      grid_movement->UpdateMultiGrid(geometry, config);
 
-    grid_movement->UpdateMultiGrid(geometry, config);
+      break;
 
-    break;
-
- 	/*--- Already initialized in the static mesh movement routine at driver level. ---*/
-  case STEADY_TRANSLATION:
-  case ROTATING_FRAME:
-    break;
-
+      /*--- Already initialized in the static mesh movement routine at driver level. ---*/
+    case STEADY_TRANSLATION:
+    case ROTATING_FRAME:
+      break;
   }
 
-  if (config->GetSurface_Movement(AEROELASTIC) ||
-      config->GetSurface_Movement(AEROELASTIC_RIGID_MOTION)) {
-
+  if (config->GetSurface_Movement(AEROELASTIC) || config->GetSurface_Movement(AEROELASTIC_RIGID_MOTION)) {
     /*--- Apply rigid mesh transformation to entire grid first, if necessary ---*/
     if (IntIter == 0) {
       if (Kind_Grid_Movement == AEROELASTIC_RIGID_MOTION) {
-
-        if (rank == MASTER_NODE)
-          cout << endl << " Performing rigid mesh transformation." << endl;
+        if (rank == MASTER_NODE) cout << endl << " Performing rigid mesh transformation." << endl;
 
         /*--- Move each node in the volume mesh using the specified type
          of rigid mesh motion. These routines also compute analytic grid
@@ -122,9 +109,7 @@ void CIteration::SetGrid_Movement(CGeometry **geometry,
 
     /*--- Use the if statement to move the grid only at selected dual time step iterations. ---*/
     else if (IntIter % config->GetAeroelasticIter() == 0) {
-
-      if (rank == MASTER_NODE)
-        cout << endl << " Solving aeroelastic equations and updating surface positions." << endl;
+      if (rank == MASTER_NODE) cout << endl << " Solving aeroelastic equations and updating surface positions." << endl;
 
       /*--- Solve the aeroelastic equations for the new node locations of the moving markers(surfaces) ---*/
 
@@ -132,15 +117,13 @@ void CIteration::SetGrid_Movement(CGeometry **geometry,
 
       /*--- Deform the volume grid around the new boundary locations ---*/
 
-      if (rank == MASTER_NODE)
-        cout << " Deforming the volume grid due to the aeroelastic movement." << endl;
+      if (rank == MASTER_NODE) cout << " Deforming the volume grid due to the aeroelastic movement." << endl;
       grid_movement->SetVolume_Deformation(geometry[MESH_0], config, true);
 
       /*--- Update the grid velocities on the fine mesh using finite
        differencing based on node coordinates at previous times. ---*/
 
-      if (rank == MASTER_NODE)
-        cout << " Computing grid velocities by finite differencing." << endl;
+      if (rank == MASTER_NODE) cout << " Computing grid velocities by finite differencing." << endl;
       geometry[MESH_0]->SetGridVelocity(config, TimeIter);
 
       /*--- Update the multigrid structure after moving the finest grid,
@@ -148,7 +131,6 @@ void CIteration::SetGrid_Movement(CGeometry **geometry,
 
       grid_movement->UpdateMultiGrid(geometry, config);
     }
-
   }
 
   if (config->GetSurface_Movement(FLUID_STRUCTURE)) {
@@ -157,59 +139,49 @@ void CIteration::SetGrid_Movement(CGeometry **geometry,
 
     /*--- Deform the volume grid around the new boundary locations ---*/
 
-    if (rank == MASTER_NODE && Screen_Output)
-      cout << "Deforming the volume grid." << endl;
+    if (rank == MASTER_NODE && Screen_Output) cout << "Deforming the volume grid." << endl;
     grid_movement->SetVolume_Deformation(geometry[MESH_0], config, true, false);
 
     nIterMesh = grid_movement->Get_nIterMesh();
     stat_mesh = (nIterMesh == 0);
 
     if (!adjoint && !stat_mesh) {
-      if (rank == MASTER_NODE && Screen_Output)
-        cout << "Computing grid velocities by finite differencing." << endl;
+      if (rank == MASTER_NODE && Screen_Output) cout << "Computing grid velocities by finite differencing." << endl;
       geometry[MESH_0]->SetGridVelocity(config, TimeIter);
-    }
-    else if (stat_mesh) {
-        if (rank == MASTER_NODE && Screen_Output)
-          cout << "The mesh is up-to-date. Using previously stored grid velocities." << endl;
+    } else if (stat_mesh) {
+      if (rank == MASTER_NODE && Screen_Output)
+        cout << "The mesh is up-to-date. Using previously stored grid velocities." << endl;
     }
 
     /*--- Update the multigrid structure after moving the finest grid,
      including computing the grid velocities on the coarser levels. ---*/
 
     grid_movement->UpdateMultiGrid(geometry, config);
-
   }
 
-  if (config->GetSurface_Movement(EXTERNAL) ||
-      config->GetSurface_Movement(EXTERNAL_ROTATION)) {
-
+  if (config->GetSurface_Movement(EXTERNAL) || config->GetSurface_Movement(EXTERNAL_ROTATION)) {
     /*--- Apply rigid rotation to entire grid first, if necessary ---*/
 
     if (Kind_Grid_Movement == EXTERNAL_ROTATION) {
-      if (rank == MASTER_NODE)
-        cout << " Updating node locations by rigid rotation." << endl;
+      if (rank == MASTER_NODE) cout << " Updating node locations by rigid rotation." << endl;
       grid_movement->Rigid_Rotation(geometry[MESH_0], config, val_iZone, TimeIter);
     }
 
     /*--- Load new surface node locations from external files ---*/
 
-    if (rank == MASTER_NODE)
-      cout << " Updating surface locations from file." << endl;
+    if (rank == MASTER_NODE) cout << " Updating surface locations from file." << endl;
     surface_movement->SetExternal_Deformation(geometry[MESH_0], config, val_iZone, TimeIter);
 
     /*--- Deform the volume grid around the new boundary locations ---*/
 
-    if (rank == MASTER_NODE)
-      cout << " Deforming the volume grid." << endl;
+    if (rank == MASTER_NODE) cout << " Deforming the volume grid." << endl;
     grid_movement->SetVolume_Deformation(geometry[MESH_0], config, true);
 
     /*--- Update the grid velocities on the fine mesh using finite
        differencing based on node coordinates at previous times. ---*/
 
     if (!adjoint) {
-      if (rank == MASTER_NODE)
-        cout << " Computing grid velocities by finite differencing." << endl;
+      if (rank == MASTER_NODE) cout << " Computing grid velocities by finite differencing." << endl;
       geometry[MESH_0]->SetGridVelocity(config, TimeIter);
     }
 
@@ -217,17 +189,11 @@ void CIteration::SetGrid_Movement(CGeometry **geometry,
        including computing the grid velocities on the coarser levels. ---*/
 
     grid_movement->UpdateMultiGrid(geometry, config);
-
   }
-
 }
 
-void CIteration::SetMesh_Deformation(CGeometry **geometry,
-                                     CSolver **solver,
-                                     CNumerics ***numerics,
-                                     CConfig *config,
+void CIteration::SetMesh_Deformation(CGeometry** geometry, CSolver** solver, CNumerics*** numerics, CConfig* config,
                                      unsigned short kind_recording) {
-
   if (!config->GetDeform_Mesh()) return;
 
   /*--- Perform the elasticity mesh movement ---*/
@@ -250,113 +216,45 @@ void CIteration::SetMesh_Deformation(CGeometry **geometry,
 
   /*--- Continue recording. ---*/
   if (ActiveTape) AD::StartRecording();
-
 }
 
-void CIteration::Preprocess(COutput *output,
-                            CIntegration ****integration,
-                            CGeometry ****geometry,
-                            CSolver *****solver,
-                            CNumerics ******numerics,
-                            CConfig **config,
-                            CSurfaceMovement **surface_movement,
-                            CVolumetricMovement ***grid_movement,
-                            CFreeFormDefBox*** FFDBox,
-                            unsigned short val_iZone,
-                            unsigned short val_iInst) { }
-void CIteration::Iterate(COutput *output,
-                         CIntegration ****integration,
-                         CGeometry ****geometry,
-                         CSolver *****solver,
-                         CNumerics ******numerics,
-                         CConfig **config,
-                         CSurfaceMovement **surface_movement,
-                         CVolumetricMovement ***grid_movement,
-                         CFreeFormDefBox*** FFDBox,
-                         unsigned short val_iZone,
-                         unsigned short val_iInst) { }
-void CIteration::Solve(COutput *output,
-                         CIntegration ****integration,
-                         CGeometry ****geometry,
-                         CSolver *****solver,
-                         CNumerics ******numerics,
-                         CConfig **config,
-                         CSurfaceMovement **surface_movement,
-                         CVolumetricMovement ***grid_movement,
-                         CFreeFormDefBox*** FFDBox,
-                         unsigned short val_iZone,
-                         unsigned short val_iInst) { }
-void CIteration::Update(COutput *output,
-                        CIntegration ****integration,
-                        CGeometry ****geometry,
-                        CSolver *****solver,
-                        CNumerics ******numerics,
-                        CConfig **config,
-                        CSurfaceMovement **surface_movement,
-                        CVolumetricMovement ***grid_movement,
-                        CFreeFormDefBox*** FFDBox,
-                        unsigned short val_iZone,
-                        unsigned short val_iInst)      { }
-void CIteration::Predictor(COutput *output,
-                        CIntegration ****integration,
-                        CGeometry ****geometry,
-                        CSolver *****solver,
-                        CNumerics ******numerics,
-                        CConfig **config,
-                        CSurfaceMovement **surface_movement,
-                        CVolumetricMovement ***grid_movement,
-                        CFreeFormDefBox*** FFDBox,
-                        unsigned short val_iZone,
-                        unsigned short val_iInst)      { }
-void CIteration::Relaxation(COutput *output,
-                        CIntegration ****integration,
-                        CGeometry ****geometry,
-                        CSolver *****solver,
-                        CNumerics ******numerics,
-                        CConfig **config,
-                        CSurfaceMovement **surface_movement,
-                        CVolumetricMovement ***grid_movement,
-                        CFreeFormDefBox*** FFDBox,
-                        unsigned short val_iZone,
-                        unsigned short val_iInst)      { }
-bool CIteration::Monitor(COutput *output,
-                        CIntegration ****integration,
-                        CGeometry ****geometry,
-                        CSolver *****solver,
-                        CNumerics ******numerics,
-                        CConfig **config,
-                        CSurfaceMovement **surface_movement,
-                        CVolumetricMovement ***grid_movement,
-                        CFreeFormDefBox*** FFDBox,
-                        unsigned short val_iZone,
-                        unsigned short val_iInst) { return false; }
+void CIteration::Preprocess(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                            CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                            CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                            unsigned short val_iInst) {}
+void CIteration::Iterate(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                         CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                         CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                         unsigned short val_iInst) {}
+void CIteration::Solve(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                       CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                       CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                       unsigned short val_iInst) {}
+void CIteration::Update(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                        CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                        CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                        unsigned short val_iInst) {}
+void CIteration::Predictor(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                           CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                           CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                           unsigned short val_iInst) {}
+void CIteration::Relaxation(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                            CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                            CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                            unsigned short val_iInst) {}
+bool CIteration::Monitor(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                         CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                         CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                         unsigned short val_iInst) {
+  return false;
+}
 
-void CIteration::Output(COutput *output,
-                        CGeometry ****geometry,
-                        CSolver *****solver,
-                        CConfig **config,
-                        unsigned long InnerIter,
-                        bool StopCalc,
-                        unsigned short val_iZone,
-                        unsigned short val_iInst) {
-
-  output->SetResult_Files(geometry[val_iZone][INST_0][MESH_0],
-                          config[val_iZone],
-                          solver[val_iZone][INST_0][MESH_0],
+void CIteration::Output(COutput* output, CGeometry**** geometry, CSolver***** solver, CConfig** config,
+                        unsigned long InnerIter, bool StopCalc, unsigned short val_iZone, unsigned short val_iInst) {
+  output->SetResult_Files(geometry[val_iZone][INST_0][MESH_0], config[val_iZone], solver[val_iZone][INST_0][MESH_0],
                           InnerIter);
-
 }
-void CIteration::Postprocess(COutput *output,
-                             CIntegration ****integration,
-                             CGeometry ****geometry,
-                             CSolver *****solver,
-                             CNumerics ******numerics,
-                             CConfig **config,
-                             CSurfaceMovement **surface_movement,
-                             CVolumetricMovement ***grid_movement,
-                             CFreeFormDefBox*** FFDBox,
-                             unsigned short val_iZone,
-                             unsigned short val_iInst) { }
-
-
-
+void CIteration::Postprocess(COutput* output, CIntegration**** integration, CGeometry**** geometry, CSolver***** solver,
+                             CNumerics****** numerics, CConfig** config, CSurfaceMovement** surface_movement,
+                             CVolumetricMovement*** grid_movement, CFreeFormDefBox*** FFDBox, unsigned short val_iZone,
+                             unsigned short val_iInst) {}
