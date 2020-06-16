5660bd4d242ea5c75ea0f959f05bc9259f6a09e6
bmunguia <bmunguia10@gmail.com>
2020-06-16 13:31:36 -0700

    Convergence criteria

--- SU2_CFD/src/solvers/CNSSolver.cpp
+++ SU2_CFD/src/solvers/CNSSolver.cpp
@@ -1906,8 +1906,8 @@ void CNSSolver::ComputeWallFunction(CGeometry *geometry, CSolver **solver, CConf
   const su2double Gas_Constant = config->GetGas_ConstantND();
   const su2double Cp = (Gamma / Gamma_Minus_One) * Gas_Constant;
 
-  const unsigned short max_iter = 1000;
-  const su2double tol = 1e-12;
+  const unsigned short max_iter = 50;
+  const su2double tol = 1e-6;
   bool converged = true;
   
   const bool tkeNeeded = (config->GetKind_Turb_Model() == SST) || (config->GetKind_Turb_Model() == SST_SUST);
