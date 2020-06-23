77bdfab5a5e037d3e189fe55e54bb5306fd9b200
auzbaig <46273732+auzbaig@users.noreply.github.com>
2020-06-23 17:38:22 -0400

    AD convegence:4

--- Common/src/config_structure.cpp
+++ Common/src/config_structure.cpp
@@ -1210,16 +1210,15 @@ void CConfig::SetConfig_Options(unsigned short val_iZone, unsigned short val_nZo
    *  \n DESCRIPTION: Convergence criteria \n OPTIONS: see \link Converge_Crit_Map \endlink \n DEFAULT: RESIDUAL \ingroup Config*/
   addEnumOption("CONV_CRITERIA", ConvCriteria, Converge_Crit_Map, RESIDUAL);
   /*!\brief RESIDUAL_REDUCTION \n DESCRIPTION: Residual reduction (order of magnitude with respect to the initial value)\n DEFAULT: 3.0 \ingroup Config*/
-  addDoubleOption("RESIDUAL_REDUCTION", OrderMagResidual, 5.0);
+  addDoubleOption("RESIDUAL_REDUCTION", OrderMagResidual, 10.0);
   /*!\brief RESIDUAL_MINVAL\n DESCRIPTION: Min value of the residual (log10 of the residual)\n DEFAULT: -14.0 \ingroup Config*/
   
   if(DiscreteAdjoint == true){
     addDoubleOption("RESIDUAL_MINVAL", MinLogResidual, -5.0);
-    MinLogResidual=-5.0;
   }
   else
   {
-    addDoubleOption("RESIDUAL_MINVAL", MinLogResidual, -14.0);
+    addDoubleOption("RESIDUAL_MINVAL", MinLogResidual, -12.0);
   }
   
   /* DESCRIPTION: Residual reduction (order of magnitude with respect to the initial value) */
