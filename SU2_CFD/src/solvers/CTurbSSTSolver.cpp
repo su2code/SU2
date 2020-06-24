bb708c6b5deea832f805e3cd691f0af4eb1142b4
bmunguia <bmunguia10@gmail.com>
2020-06-23 21:05:34 -0700

    Don't clip CDkw

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -550,11 +550,10 @@ void CTurbSSTSolver::Cross_Diffusion_Jacobian(CGeometry *geometry,
   
   const CVariable* flowNodes = solver_container[FLOW_SOL]->GetNodes();
   const su2double sigma_om2 = constants[3];
-  const su2double CDkw = nodes->GetCrossDiff(iPoint);
   
   if (config->GetKind_Gradient_Method() == GREEN_GAUSS) {
     
-    if ((geometry->node[iPoint]->GetWall_Distance() > 1.0e-10) && (CDkw > 1.0e-20)) {
+    if (geometry->node[iPoint]->GetWall_Distance() > 1.0e-10) {
       const su2double F1_i     = nodes->GetF1blending(iPoint);
       const su2double r_i      = flowNodes->GetDensity(iPoint);
       // const su2double r_i      = flowNodes->GetPrimitive(iPoint, nDim+2);
