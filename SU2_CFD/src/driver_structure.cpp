b4631e17229562d0500cf5920e58eb1f82268ab5
auzbaig <46273732+auzbaig@users.noreply.github.com>
2020-06-24 21:05:42 -0400

    flowangleout changed to alpha_m in definition

--- SU2_CFD/src/driver_structure.cpp
+++ SU2_CFD/src/driver_structure.cpp
@@ -3336,9 +3336,10 @@ void CDriver::TurbomachineryPreprocessing(){
       flowAngleIn /= solver_container[iZone][MESH_0][FLOW_SOL]->GetTurboVelocityIn(iZone, config_container[iZone]->GetnSpanWiseSections())[0];
       flowAngleIn = atan(flowAngleIn)*180.0/PI_NUMBER;
       cout << "Inlet flow angle for Row "<< iZone + 1<< ": "<< flowAngleIn <<"°."  <<endl;
-      flowAngleOut = solver_container[iZone][MESH_0][FLOW_SOL]->GetTurboVelocityOut(iZone, config_container[iZone]->GetnSpanWiseSections())[1];
-      flowAngleOut /= solver_container[iZone][MESH_0][FLOW_SOL]->GetTurboVelocityOut(iZone, config_container[iZone]->GetnSpanWiseSections())[0];
-      flowAngleOut = atan(flowAngleOut)*180.0/PI_NUMBER;
+      flowAngleOut = pow(solver_container[iZone][MESH_0][FLOW_SOL]->GetTurboVelocityOut(iZone, config_container[iZone]->GetnSpanWiseSections())[0],2);
+      flowAngleOut += pow(solver_container[iZone][MESH_0][FLOW_SOL]->GetTurboVelocityOut(iZone, config_container[iZone]->GetnSpanWiseSections())[2],2);
+      flowAngleOut = sqrt(flowAngleOut)/solver_container[iZone][MESH_0][FLOW_SOL]->GetTurboVelocityOut(iZone, config_container[iZone]->GetnSpanWiseSections())[1];
+      flowAngleOut = atan(1/flowAngleOut)*180.0/PI_NUMBER;
       cout << "Outlet flow angle for Row "<< iZone + 1<< ": "<< flowAngleOut <<"°."  <<endl;
 
     }
