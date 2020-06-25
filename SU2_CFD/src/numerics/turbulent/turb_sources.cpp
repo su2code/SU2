b0029b2b9847e77d89cc8c33fa6d85c89d5898f3
bmunguia <bmunguia10@gmail.com>
2020-06-25 15:10:54 -0700

    Let's test with negative production

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -855,15 +855,11 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      pw = alfa_blended*Density_i*(factor - 2./3.*zeta*diverg);
        
      /*--- k production Jacobian ---*/
-     if ((pk > 0.) && (pk <= 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
-       Jacobian_i[0][0] = -2./3.*diverg*Volume;
-       if (TurbVar_i[1] > VorticityMag*F2_i/a1) {
-         Jacobian_i[0][0] += factor/TurbVar_i[1]*Volume;
+     // if ((pk > 0.) && (pk <= 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
+     if (pk <= 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
+       Jacobian_i[0][0] = (factor/zeta-2./3.*diverg)*Volume;
+       if (TurbVar_i[1] > VorticityMag*F2_i/a1)
          Jacobian_i[0][1] += -factor*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
-       }
-       else {
-         Jacobian_i[0][0] += factor*a1/(VorticityMag*F2_i)*Volume;
-       }
      }
      else if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
        Jacobian_i[0][0] = 20.*beta_star*TurbVar_i[1]*Volume;
@@ -871,14 +867,15 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      }
      
      /*--- omega production Jacobian ---*/
-     if ((pw > 0.) && (pw <= 20.*beta_star*alfa_blended*Density_i*TurbVar_i[1]*zeta)) {
-       if (TurbVar_i[1] > VorticityMag*F2_i/a1) {
+     // if ((pw > 0.) && (pw <= 20.*beta_star*alfa_blended*Density_i*TurbVar_i[1]*zeta)) {
+     if (pw <= 20.*beta_star*alfa_blended*Density_i*TurbVar_i[1]*zeta) {
+       if (TurbVar_i[1] > VorticityMag*F2_i/a1)
          Jacobian_i[1][1] = -2./3.*alfa_blended*diverg*Volume;
-       }
      }
      else if (pw > 20.*beta_star*alfa_blended*Density_i*TurbVar_i[1]*zeta) {
        Jacobian_i[1][1] = 20.*beta_star*alfa_blended*zeta*Volume;
-       if (TurbVar_i[1] > VorticityMag*F2_i/a1) Jacobian_i[1][1] *= 2.;
+       if (TurbVar_i[1] > VorticityMag*F2_i/a1) 
+        Jacobian_i[1][1] *= 2.;
      }
    }
     
@@ -894,8 +891,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      pw = min(pw, 20.*beta_star*alfa_blended*Density_i*TurbVar_i[1]*zeta);
    }
     
-   pk = max(pk, 0.0);
-   pw = max(pw, 0.0);
+   // pk = max(pk, 0.0);
+   // pw = max(pw, 0.0);
     
    /*--- Sustaining terms, if desired. Note that if the production terms are
          larger equal than the sustaining terms, the original formulation is
