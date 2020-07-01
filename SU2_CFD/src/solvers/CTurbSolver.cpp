98ffa2a7f4fa66537c45381b6bf2f673d6ae2f38
Brian Munguía <bmunguia@stanford.edu>
2020-07-01 15:28:54 -0700

    Underrelax decrease in omega

--- SU2_CFD/src/solvers/CTurbSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSolver.cpp
@@ -608,17 +608,18 @@ void CTurbSolver::ComputeUnderRelaxationFactor(CSolver **solver_container, CConf
     }
 
     else if (sst_model) {
+      for (unsigned short iVar = 0; iVar < nVar; iVar++) {
 
-      /* We impose a limit on the maximum percentage that the
-       turbulence variables can change over a nonlinear iteration. */
+        /* We impose a limit on the maximum percentage that the
+         turbulence variables can change over a nonlinear iteration. */
 
-      const unsigned short iVar = 0;
-      const unsigned long index = iPoint*nVar + iVar;
-      su2double ratio = LinSysSol[index]/(nodes->GetSolution(iPoint, iVar)+EPS);
-      if (ratio < allowableDecrease) {
-        localUnderRelaxation = min(fabs(allowableDecrease)/ratio, localUnderRelaxation);
+        const unsigned long index = iPoint*nVar + iVar;
+        su2double ratio = LinSysSol[index]/(nodes->GetSolution(iPoint, iVar)+EPS);
+        if (ratio < allowableDecrease) {
+          localUnderRelaxation = min(fabs(allowableDecrease)/ratio, localUnderRelaxation);
+        }
+
       }
-
     }
 
     /* Choose the minimum factor between mean flow and turbulence. */
