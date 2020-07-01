d859221cc215b8e568d375b599934c06103ca99e
Brian Munguía <bmunguia@stanford.edu>
2020-07-01 10:03:10 -0700

    Test without tke in flux Jacobian

--- SU2_CFD/src/numerics/CNumerics.cpp
+++ SU2_CFD/src/numerics/CNumerics.cpp
@@ -382,7 +382,8 @@ void CNumerics::GetInviscidProjJac(su2double *val_velocity, su2double *val_energ
   }
 
   phi = 0.5*Gamma_Minus_One*sqvel;
-  a1 = Gamma*(*val_energy)-phi-Gamma_Minus_One*(*val_tke);
+  // a1 = Gamma*(*val_energy)-phi-Gamma_Minus_One*(*val_tke);
+  a1 = Gamma*(*val_energy)-phi;
   a2 = Gamma-1.0;
 
   val_Proj_Jac_Tensor[0][0] = 0.0;
