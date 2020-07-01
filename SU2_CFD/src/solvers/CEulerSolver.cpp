41d82d0ec2da2e7ab071f38dba71bd6594f8cde4
Brian Munguía <bmunguia@stanford.edu>
2020-07-01 15:15:43 -0700

    Updating min underrelaxation

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -4829,7 +4829,7 @@ void CEulerSolver::ComputeUnderRelaxationFactor(CSolver **solver_container, CCon
      a very small value. This helps avoid catastrophic crashes due
      to non-realizable states by canceling the update. */
 
-    if (localUnderRelaxation < 0.1) localUnderRelaxation = 0.0;
+    if (localUnderRelaxation < 0.01) localUnderRelaxation = 0.0;
 
     /* Store the under-relaxation factor for this point. */
 
--- SU2_CFD/src/solvers/CTurbSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSolver.cpp
@@ -615,7 +615,7 @@ void CTurbSolver::ComputeUnderRelaxationFactor(CSolver **solver_container, CConf
      a very small value. This helps avoid catastrophic crashes due
      to non-realizable states by canceling the update. */
 
-    if (localUnderRelaxation < 0.1) localUnderRelaxation = 0.0;
+    if (localUnderRelaxation < 0.01) localUnderRelaxation = 0.0;
 
     /* Store the under-relaxation factor for this point. */
 
