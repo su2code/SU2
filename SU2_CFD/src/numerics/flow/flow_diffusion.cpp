a0970fb585a68c4324d09ef3fcf3600db73ca765
bmunguia <bmunguia10@gmail.com>
2020-07-03 15:18:47 -0700

    Get rid of redundant zeroing of density viscous Jacobians

--- SU2_CFD/src/numerics/flow/flow_diffusion.cpp
+++ SU2_CFD/src/numerics/flow/flow_diffusion.cpp
@@ -537,10 +537,6 @@ void CAvgGrad_Base::GetViscousProjJacs(const su2double *val_Mean_PrimVar,
 
   if (nDim == 2) {
 
-    Jacobian_i[0][0] = 0.0;
-    Jacobian_i[0][1] = 0.0;
-    Jacobian_i[0][2] = 0.0;
-    Jacobian_i[0][3] = 0.0;
     Jacobian_i[1][0] = tau_jacobian_i[0][0];
     Jacobian_i[1][1] = tau_jacobian_i[0][1];
     Jacobian_i[1][2] = tau_jacobian_i[0][2];
@@ -549,11 +545,7 @@ void CAvgGrad_Base::GetViscousProjJacs(const su2double *val_Mean_PrimVar,
     Jacobian_i[2][1] = tau_jacobian_i[1][1];
     Jacobian_i[2][2] = tau_jacobian_i[1][2];
     Jacobian_i[2][3] = tau_jacobian_i[1][3];
-    
-    Jacobian_j[0][0] = 0.0;
-    Jacobian_j[0][1] = 0.0;
-    Jacobian_j[0][2] = 0.0;
-    Jacobian_j[0][3] = 0.0;
+
     Jacobian_j[1][0] = tau_jacobian_j[0][0];
     Jacobian_j[1][1] = tau_jacobian_j[0][1];
     Jacobian_j[1][2] = tau_jacobian_j[0][2];
@@ -596,12 +588,7 @@ void CAvgGrad_Base::GetViscousProjJacs(const su2double *val_Mean_PrimVar,
     Jacobian_j[3][2] += factor_j*val_Proj_Visc_Flux[2];
 
   } else {
-
-    Jacobian_i[0][0] = 0.0;
-    Jacobian_i[0][1] = 0.0;
-    Jacobian_i[0][2] = 0.0;
-    Jacobian_i[0][3] = 0.0;
-    Jacobian_i[0][4] = 0.0;
+    
     Jacobian_i[1][0] = tau_jacobian_i[0][0];
     Jacobian_i[1][1] = tau_jacobian_i[0][1];
     Jacobian_i[1][2] = tau_jacobian_i[0][2];
@@ -617,12 +604,7 @@ void CAvgGrad_Base::GetViscousProjJacs(const su2double *val_Mean_PrimVar,
     Jacobian_i[3][2] = tau_jacobian_i[2][2];
     Jacobian_i[3][3] = tau_jacobian_i[2][3];
     Jacobian_i[3][4] = tau_jacobian_i[2][4];
-    
-    Jacobian_j[0][0] = 0.0;
-    Jacobian_j[0][1] = 0.0;
-    Jacobian_j[0][2] = 0.0;
-    Jacobian_j[0][3] = 0.0;
-    Jacobian_j[0][4] = 0.0;
+
     Jacobian_j[1][0] = tau_jacobian_j[0][0];
     Jacobian_j[1][1] = tau_jacobian_j[0][1];
     Jacobian_j[1][2] = tau_jacobian_j[0][2];
