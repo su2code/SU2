db2e381383290f6825f2e6ef978dbad6b6a0b0fc
fmpmorgado <fabio.pereira-morgado@strath.ac.uk>
2020-07-07 12:35:36 +0100

    Change in CNEMOCompOutput.cpp to output mass fractions

--- SU2_CFD/src/output/CNEMOCompOutput.cpp
+++ SU2_CFD/src/output/CNEMOCompOutput.cpp
@@ -72,6 +72,7 @@ CNEMOCompOutput::CNEMOCompOutput(CConfig *config, unsigned short nDim) : CFlowOu
     requestedVolumeFields.emplace_back("COORDINATES");
     requestedVolumeFields.emplace_back("SOLUTION");
     requestedVolumeFields.emplace_back("PRIMITIVE");
+    requestedVolumeFields.emplace_back("AUXILIARY");
     nRequestedVolumeFields = requestedVolumeFields.size();
   }
 
@@ -320,6 +321,7 @@ void CNEMOCompOutput::SetVolumeOutputFields(CConfig *config){
     AddVolumeOutput("DENSITY_N2",  "Density_N2",  "SOLUTION", "Density_N2");
     AddVolumeOutput("DENSITY_N",   "Density_N",   "SOLUTION", "Density_N");
   }
+ 
   if (nSpecies == 5){
     AddVolumeOutput("DENSITY_N2",  "Density_N2",  "SOLUTION", "Density_N2");
     AddVolumeOutput("DENSITY_O2",  "Density_O2",  "SOLUTION", "Density_N");
@@ -327,6 +329,7 @@ void CNEMOCompOutput::SetVolumeOutputFields(CConfig *config){
     AddVolumeOutput("DENSITY_N",   "Density_N",   "SOLUTION", "Density_N");
     AddVolumeOutput("DENSITY_O",   "Density_O",   "SOLUTION", "Density_O");
   }
+
 
   AddVolumeOutput("MOMENTUM-X", "Momentum_x", "SOLUTION", "x-component of the momentum vector");
   AddVolumeOutput("MOMENTUM-Y", "Momentum_y", "SOLUTION", "y-component of the momentum vector");
@@ -347,6 +350,20 @@ void CNEMOCompOutput::SetVolumeOutputFields(CConfig *config){
   case NONE:
     break;
   }
+
+  //Auxiliary variables for post-processment
+  if (nSpecies == 2){
+    AddVolumeOutput("MASS_N2",  "MassFrac_N2",  "AUXILIARY", "MassFrac_N2");
+    AddVolumeOutput("MASS_N",   "MassFrac_N",   "AUXILIARY", "MassFrac_N");  
+  }
+
+  if (nSpecies == 5){
+    AddVolumeOutput("MASS_N2",  "MassFrac_N2",  "AUXILIARY", "MassFrac_N2");
+    AddVolumeOutput("MASS_O2",  "MassFrac_O2",  "AUXILIARY", "MassFrac_O2");
+    AddVolumeOutput("MASS_NO",  "Massfrac_NO",  "AUXILIARY", "MassFrac_NO");
+    AddVolumeOutput("MASS_N",   "MassFrac_N",   "AUXILIARY", "MassFrac_N");
+    AddVolumeOutput("MASS_O",   "MassFrac_O",   "AUXILIARY", "MassFrac_NO");
+  } 
 
   // Grid velocity
   if (config->GetGrid_Movement()){
@@ -478,6 +495,8 @@ void CNEMOCompOutput::LoadVolumeData(CConfig *config, CGeometry *geometry, CSolv
     SetVolumeOutputValue("DENSITY_N2",   iPoint, Node_Flow->GetSolution(iPoint, 0));
     SetVolumeOutputValue("DENSITY_N",    iPoint, Node_Flow->GetSolution(iPoint, 1));
   }
+  
+
   if (nSpecies == 5){
     SetVolumeOutputValue("DENSITY_N2",   iPoint, Node_Flow->GetSolution(iPoint, 0));
     SetVolumeOutputValue("DENSITY_O2",   iPoint, Node_Flow->GetSolution(iPoint, 1));
@@ -485,6 +504,22 @@ void CNEMOCompOutput::LoadVolumeData(CConfig *config, CGeometry *geometry, CSolv
     SetVolumeOutputValue("DENSITY_N",    iPoint, Node_Flow->GetSolution(iPoint, 3));
     SetVolumeOutputValue("DENSITY_O",    iPoint, Node_Flow->GetSolution(iPoint, 4));
   }
+
+
+  if (nSpecies == 2){
+    SetVolumeOutputValue("MASS_N2",   iPoint, Node_Flow->GetSolution(iPoint, 0)/Node_Flow->GetDensity(iPoint));
+    SetVolumeOutputValue("MASS_N",    iPoint, Node_Flow->GetSolution(iPoint, 1)/Node_Flow->GetDensity(iPoint));
+  }
+  
+  if (nSpecies == 5){
+    SetVolumeOutputValue("MASS_N2",   iPoint, Node_Flow->GetSolution(iPoint, 0)/Node_Flow->GetDensity(iPoint));
+    SetVolumeOutputValue("MASS_O2",   iPoint, Node_Flow->GetSolution(iPoint, 1)/Node_Flow->GetDensity(iPoint));
+    SetVolumeOutputValue("MASS_NO",   iPoint, Node_Flow->GetSolution(iPoint, 2)/Node_Flow->GetDensity(iPoint));
+    SetVolumeOutputValue("MASS_N",    iPoint, Node_Flow->GetSolution(iPoint, 3)/Node_Flow->GetDensity(iPoint));
+    SetVolumeOutputValue("MASS_O",    iPoint, Node_Flow->GetSolution(iPoint, 4)/Node_Flow->GetDensity(iPoint));
+  } 
+
+
   SetVolumeOutputValue("MOMENTUM-X", iPoint, Node_Flow->GetSolution(iPoint, nSpecies));
   SetVolumeOutputValue("MOMENTUM-Y", iPoint, Node_Flow->GetSolution(iPoint, nSpecies+1));
   if (nDim == 3){
