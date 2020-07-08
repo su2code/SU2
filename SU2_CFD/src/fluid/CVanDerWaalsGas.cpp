50e309f6a5b0ce152952701af91f6a831dbe9b1f
GitHub Action <action@github.com>
2020-07-08 10:47:19 +0000

    Changing version number to 7.0.6

--- SU2_CFD/src/fluid/CVanDerWaalsGas.cpp
+++ SU2_CFD/src/fluid/CVanDerWaalsGas.cpp
@@ -2,7 +2,7 @@
  * \file CVanDerWaalsGas.cpp
  * \brief Source of the Polytropic Van der Waals model.
  * \author S. Vitale, G. Gori, M. Pini, A. Guardone, P. Colonna
- * \version 7.0.5 "Blackbird"
+ * \version 7.0.6 "Blackbird"
  *
  * SU2 Project Website: https://su2code.github.io
  *
