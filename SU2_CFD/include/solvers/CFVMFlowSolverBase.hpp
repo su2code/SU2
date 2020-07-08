3ad6b26b743f0e731e6c09ca192a1b976480f1f6
Pedro Gomes <pcarruscag@gmail.com>
2020-07-08 23:04:44 +0100

    update versions

--- SU2_CFD/include/solvers/CFVMFlowSolverBase.hpp
+++ SU2_CFD/include/solvers/CFVMFlowSolverBase.hpp
@@ -1,7 +1,7 @@
 /*!
  * \file CFVMFlowSolverBase.hpp
  * \brief Base class template for all FVM flow solvers.
- * \version 7.0.5 "Blackbird"
+ * \version 7.0.6 "Blackbird"
  *
  * SU2 Project Website: https://su2code.github.io
  *
