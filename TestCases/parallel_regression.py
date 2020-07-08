1dbd6fb1bfeea82400920d0f1b9c508f4a0296b4
SaettaE <et.saetta@studenti.unina.it>
2020-07-08 16:01:10 +0200

    Reference values changed

--- TestCases/parallel_regression.py
+++ TestCases/parallel_regression.py
@@ -287,7 +287,7 @@ def main():
     propeller_var_load.cfg_dir   = "rans/actuatordisk_variable_load"
     propeller_var_load.cfg_file  = "propeller_variable_load.cfg"
     propeller_var_load.test_iter = 10
-    propeller_var_load.test_vals = [-1.536168, 1.550551, 0.809250, 3.902940] #last 4 columns
+    propeller_var_load.test_vals = [-1.535982, -4.565925, 0.000294, 0.334161] #last 4 columns
     propeller_var_load.su2_exec  = "parallel_computation.py -f"
     propeller_var_load.timeout   = 3200
     propeller_var_load.tol       = 0.00001
