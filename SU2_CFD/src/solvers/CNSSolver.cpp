8da0c191937462223ce32df488f33fed4f1cb924
bmunguia <bmunguia10@gmail.com>
2020-07-09 15:00:29 -0700

    Don't check if domain when calling CorrectJacobian

--- SU2_CFD/src/solvers/CNSSolver.cpp
+++ SU2_CFD/src/solvers/CNSSolver.cpp
@@ -416,10 +416,8 @@ void CNSSolver::Viscous_Residual(unsigned long iEdge, CGeometry *geometry, CSolv
       Jacobian.UpdateBlocksSub(iEdge, iPoint, jPoint, residual.jacobian_i, residual.jacobian_j);
       
       /*--- Compute Jacobian correction for influence from all neighbors ---*/
-      if (geometry->node[iPoint]->GetDomain())
-        CorrectJacobian(geometry, solver, config, iPoint, jPoint, geometry->edge[iEdge]->GetNormal(), 1.0);
-      if (geometry->node[jPoint]->GetDomain())
-        CorrectJacobian(geometry, solver, config, jPoint, iPoint, geometry->edge[iEdge]->GetNormal(), -1.0);
+      CorrectJacobian(geometry, solver, config, iPoint, jPoint, geometry->edge[iEdge]->GetNormal(), 1.0);
+      CorrectJacobian(geometry, solver, config, jPoint, iPoint, geometry->edge[iEdge]->GetNormal(), -1.0);
     }
   }
 
--- SU2_CFD/src/solvers/CTurbSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSolver.cpp
@@ -334,10 +334,8 @@ void CTurbSolver::Viscous_Residual(unsigned long iEdge, CGeometry *geometry, CSo
   }
   
   /*--- Compute Jacobian correction for influence from all neighbors ---*/
-  if (geometry->node[iPoint]->GetDomain())
-    CorrectJacobian(geometry, solver, config, iPoint, jPoint, 1.0, residual.jacobian_ic);
-  if (geometry->node[jPoint]->GetDomain())
-    CorrectJacobian(geometry, solver, config, jPoint, iPoint, -1.0, residual.jacobian_jc);
+  CorrectJacobian(geometry, solver, config, iPoint, jPoint, 1.0, residual.jacobian_ic);
+  CorrectJacobian(geometry, solver, config, jPoint, iPoint, -1.0, residual.jacobian_jc);
   
 }
 
