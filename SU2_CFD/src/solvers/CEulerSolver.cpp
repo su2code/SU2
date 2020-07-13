819ee40cd108fa6f3179c1a77cd7c954918f2aae
bmunguia <bmunguia10@gmail.com>
2020-07-13 13:35:34 -0700

    Original form of van albada

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -3334,10 +3334,10 @@ void CEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver,
           if (van_albada) {
             // Limiter_i[iVar] = V_ij*( 2.0*Project_Grad_i + V_ij) / (4*pow(Project_Grad_i, 2) + pow(V_ij, 2) + EPS);
             // Limiter_j[iVar] = V_ij*(-2.0*Project_Grad_j + V_ij) / (4*pow(Project_Grad_j, 2) + pow(V_ij, 2) + EPS);
-            su2double a = Project_Grad_i, 
+            su2double a = 2.0*Project_Grad_i, 
                       b = V_ij;
             Limiter_i[iVar] = max((pow(a,2.0)*b + pow(b,2.0)*a)/(pow(a,2.0) + pow(b,2.0) + EPS), 0.0);
-            a = Project_Grad_j;
+            a = 2.0*Project_Grad_j;
             Limiter_j[iVar] = max((pow(a,2.0)*b + pow(b,2.0)*a)/(pow(a,2.0) + pow(b,2.0) + EPS), 0.0);
             // const su2double R_i = fabs((Project_Grad_j)/(Project_Grad_i+EPS));
             // const su2double R_j = fabs((Project_Grad_i)/(Project_Grad_j+EPS));
