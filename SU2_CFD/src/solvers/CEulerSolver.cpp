bd6f9405a0af73e0f1d3bb42edb817823511ae70
bmunguia <bmunguia10@gmail.com>
2020-07-14 16:56:13 -0700

    Factor of 1/2

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -3251,7 +3251,7 @@ void CEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver,
 
       for (iVar = 0; iVar < nPrimVarGrad; iVar++) {
         const su2double V_ij = (V_j[iVar] - V_i[iVar]);
-        su2double ProjGrad_i = Kappa*V_ij, ProjGrad_j = Kappa*V_ij;
+        su2double ProjGrad_i = 0.5*Kappa*V_ij, ProjGrad_j = 0.5*Kappa*V_ij;
         for (iDim = 0; iDim < nDim; iDim++) {
           ProjGrad_i += (1.0-Kappa)*Gradient_i[iVar][iDim]*Vector_ij[iDim];
           ProjGrad_j += (1.0-Kappa)*Gradient_j[iVar][iDim]*Vector_ij[iDim];
