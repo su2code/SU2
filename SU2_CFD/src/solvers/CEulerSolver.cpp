91e558c1e745d1560013ba767a335f431ba992be
bmunguia <bmunguia10@gmail.com>
2020-07-13 20:38:06 -0700

    Slight tweak to upwind gradient

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -3151,12 +3151,12 @@ void CEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver,
         }
         if (limiter) {
           if (van_albada) {
-            su2double a = 2.0*Project_Grad_i;
+            su2double a = 2.0*Project_Grad_i - T_ij;
             su2double b = T_ij;
             Project_Grad_i = 0.5*((pow(a,2.0)+pow(EPS,2.0))*b
                          + (pow(b,2.0)+pow(EPS,2.0))*a) 
                          / (pow(a,2.0) + pow(b,2.0) + pow(EPS,2.0));
-            a = 2.0*Project_Grad_j;
+            a = 2.0*Project_Grad_j - T_ij;
             Project_Grad_j = 0.5*((pow(a,2.0)+pow(EPS,2.0))*b
                          + (pow(b,2.0)+pow(EPS,2.0))*a) 
                          / (pow(a,2.0) + pow(b,2.0) + pow(EPS,2.0));
@@ -3209,12 +3209,12 @@ void CEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver,
 
         if (limiter) {
           if (van_albada) {
-            su2double a = 2.0*Project_Grad_i;
+            su2double a = 2.0*Project_Grad_i - V_ij;
             su2double b = V_ij;
             Project_Grad_i = 0.5*((pow(a,2.0)+pow(EPS,2.0))*b
                             + (pow(b,2.0)+pow(EPS,2.0))*a) 
                             / (pow(a,2.0) + pow(b,2.0) + pow(EPS,2.0));
-            a = 2.0*Project_Grad_j;
+            a = 2.0*Project_Grad_j - V_ij;
             Project_Grad_j = 0.5*((pow(a,2.0)+pow(EPS,2.0))*b
                             + (pow(b,2.0)+pow(EPS,2.0))*a) 
                             / (pow(a,2.0) + pow(b,2.0) + pow(EPS,2.0));
