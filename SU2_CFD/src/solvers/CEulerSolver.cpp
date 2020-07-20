eac9c52494d1b01e69ecd5e5bbfcd9ed7442c628
bmunguia <bmunguia10@gmail.com>
2020-07-20 14:40:37 -0700

    Rewrite in terms of 1+k and 1-k

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -3149,7 +3149,7 @@ void CEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver,
           Limiter_j = turbNodes->GetLimiter(jPoint);
         }
 
-        const su2double Kappa = (piperno) ? 2.0/3.0 : config->GetMUSCL_Kappa();
+        const su2double Kappa = (piperno) ? 1.0/3.0 : config->GetMUSCL_Kappa();
 
         const su2double T_ij = 0.5*(tke_j - tke_i);
 
@@ -3213,7 +3213,7 @@ void CEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver,
         Limiter_j = nodes->GetLimiter_Primitive(jPoint);
       }
 
-      const su2double Kappa = (piperno) ? 2.0/3.0 : config->GetMUSCL_Kappa();
+      const su2double Kappa = (piperno) ? 1.0/3.0 : config->GetMUSCL_Kappa();
 
       for (iVar = 0; iVar < nPrimVarGrad; iVar++) {
 
