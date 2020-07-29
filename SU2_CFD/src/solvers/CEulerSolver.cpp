96f1da840e80db5c2184037baf1017011f525075
bmunguia <bmunguia10@gmail.com>
2020-07-29 15:31:08 -0700

    Revert

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -7399,7 +7399,7 @@ void CEulerSolver::BC_Sym_Plane(CGeometry      *geometry,
           Jacobian.SubtractBlock2Diag(iPoint, residual.jacobian_i);
           
           /*--- Compute Jacobian correction for influence from all neighbors ---*/
-          CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
+          // CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
         }
       }//if viscous
     }//if GetDomain
@@ -7703,7 +7703,7 @@ void CEulerSolver::BC_Far_Field(CGeometry *geometry, CSolver **solver, CNumerics
           Jacobian.SubtractBlock2Diag(iPoint, visc_residual.jacobian_i);
           
           /*--- Compute Jacobian correction for influence from all neighbors ---*/
-          CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
+          // CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
         }
         
       }
@@ -9939,7 +9939,7 @@ void CEulerSolver::BC_Inlet(CGeometry *geometry, CSolver **solver,
           Jacobian.SubtractBlock2Diag(iPoint, residual.jacobian_i);
           
           /*--- Compute Jacobian correction for influence from all neighbors ---*/
-          CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
+          // CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
         }
 
       }
@@ -10149,7 +10149,7 @@ void CEulerSolver::BC_Outlet(CGeometry *geometry, CSolver **solver,
           Jacobian.SubtractBlock2Diag(iPoint, residual.jacobian_i);
           
           /*--- Compute Jacobian correction for influence from all neighbors ---*/
-          CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
+          // CorrectJacobian(geometry, solver, config, iPoint, iPoint, Normal, 1.0);
         }
 
       }
