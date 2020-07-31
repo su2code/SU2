06522efe15a4251e0df74444cb29f30074b2bc75
bmunguia <bmunguia10@gmail.com>
2020-07-31 16:07:24 -0700

    CDkwmin

--- SU2_CFD/src/variables/CTurbSSTVariable.cpp
+++ SU2_CFD/src/variables/CTurbSSTVariable.cpp
@@ -79,13 +79,14 @@ void CTurbSSTVariable::SetBlendingFunc(unsigned long iPoint, su2double val_visco
   for (unsigned long iDim = 0; iDim < nDim; iDim++)
     CDkw(iPoint) += Gradient(iPoint,0,iDim)*Gradient(iPoint,1,iDim);
   CDkw(iPoint) *= 2.0*val_density*sigma_om2/(Primitive(iPoint,1)+eps);
+  CDkw(iPoint) = max(CDkw(iPoint), CDKwmin);
 
   /*--- F1 ---*/
 
   arg2A = sqrt(Primitive(iPoint,0))/(beta_star*Primitive(iPoint,1)*val_dist+eps);
   arg2B = 500.0*val_viscosity / (val_density*val_dist*val_dist*Primitive(iPoint,1)+eps);
   arg2 = max(arg2A, arg2B);
-  arg1 = min(arg2, 4.0*val_density*sigma_om2*Primitive(iPoint,0) / (max(CDkw(iPoint),CDkwmin)*val_dist*val_dist+eps));
+  arg1 = min(arg2, 4.0*val_density*sigma_om2*Primitive(iPoint,0) / (CDkw(iPoint)*val_dist*val_dist+eps));
   F1(iPoint) = tanh(pow(arg1, 4.0));
 
   if (F1(iPoint) != F1(iPoint)) cout << "k= " << Primitive(iPoint,0) << ", Omega= " << Primitive(iPoint,1) << ", Density= " << val_density << ", UR= " << UnderRelaxation(iPoint) << ", distance= " << val_dist << endl;
