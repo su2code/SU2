bdfa4639b92cdb135a252118e175ae43e2699f86
TobiKattmann <Tobias.Kattmann@de.bosch.com>
2020-08-04 19:39:56 +0200

    Little changes for streamwise regression tests.

--- TestCases/streamwise_periodic_regression.py
+++ TestCases/streamwise_periodic_regression.py
@@ -6,8 +6,8 @@
 #  \version 7.0.4 "Blackbird"
 #
 # SU2 Project Website: https://su2code.github.io
-# 
-# The SU2 Project is maintained by the SU2 Foundation 
+#
+# The SU2 Project is maintained by the SU2 Foundation
 # (http://su2foundation.org)
 #
 # Copyright 2012-2020, SU2 Contributors (cf. AUTHORS.md)
@@ -16,7 +16,7 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # SU2 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -30,12 +30,12 @@ import sys
 from TestCase import TestCase
 
 def main():
-    '''This program runs SU2 and ensures that the output matches specified values. 
-       This will be used to do checks when code is pushed to github 
+    '''This program runs SU2 and ensures that the output matches specified values.
+       This will be used to do checks when code is pushed to github
        to make sure nothing is broken. '''
 
     test_list = []
-    
+
     #################################
     ## Streamwise Periodic primal ###
     #################################
@@ -62,7 +62,7 @@ def main():
     sp_pipeSlice_3d_dp_hf_tp.tol       = 0.00001
     test_list.append(sp_pipeSlice_3d_dp_hf_tp)
 
-    # create 2D pin case pressure drop periodic with heatflux BC and temperature periodicity 
+    # create 2D pin case pressure drop periodic with heatflux BC and temperature periodicity
     sp_pinArray_2d_dp_hf_tp           = TestCase('sp_pinArray_2d_dp_hf_tp')
     sp_pinArray_2d_dp_hf_tp.cfg_dir   = "incomp_navierstokes/streamwise_periodic/pinArray_2d"
     sp_pinArray_2d_dp_hf_tp.cfg_file  = "sp_pinArray_2d_dp_hf_tp.cfg"
@@ -84,18 +84,7 @@ def main():
     sp_pinArray_2d_mf_hf.tol       = 0.00001
     test_list.append(sp_pinArray_2d_mf_hf)
 
-    # create simple small 3D pin case massflow periodic with heatflux BC and temperature periodicity (without turbulence model for now)
-    sp_pinArray_3d_mf_hf_tp           = TestCase('sp_pinArray_3d_mf_hf_tp')
-    sp_pinArray_3d_mf_hf_tp.cfg_dir   = "incomp_navierstokes/streamwise_periodic/sp_pinArray_3d_mf_hf_tp"
-    sp_pinArray_3d_mf_hf_tp.cfg_file  = "sp_pinArray_3d_mf_hf_tp.cfg"
-    sp_pinArray_3d_mf_hf_tp.test_iter = 10
-    sp_pinArray_3d_mf_hf_tp.test_vals = [10, -10.352122, -10.185237, -10.185237] #last 4 lines
-    sp_pinArray_3d_mf_hf_tp.su2_exec  = "parallel_computation.py -f"
-    sp_pinArray_3d_mf_hf_tp.timeout   = 1600
-    sp_pinArray_3d_mf_hf_tp.tol       = 0.00001
-    #test_list.append(sp_pinArray_3d_mf_hf_tp)
-
-    # create 2D CHT case with HF BC and  
+    # create 2D CHT case with HF BC and
     sp_pinArray_cht_2d_mf_hf           = TestCase('sp_pinArray_cht_2d_mf_hf')
     sp_pinArray_cht_2d_mf_hf.cfg_dir   = "incomp_navierstokes/streamwise_periodic/chtPinArray_2d"
     sp_pinArray_cht_2d_mf_hf.cfg_file  = "configMaster.cfg"
@@ -106,6 +95,17 @@ def main():
     sp_pinArray_cht_2d_mf_hf.tol       = 0.00001
     sp_pinArray_cht_2d_mf_hf.multizone = True
     test_list.append(sp_pinArray_cht_2d_mf_hf)
+
+    # create simple small 3D pin case massflow periodic with heatflux BC and temperature periodicity (without turbulence model for now)
+    sp_pinArray_3d_cht_mf_hf_tp           = TestCase('sp_pinArray_3d_cht_mf_hf_tp')
+    sp_pinArray_3d_cht_mf_hf_tp.cfg_dir   = "incomp_navierstokes/streamwise_periodic/chtPinArray_3d"
+    sp_pinArray_3d_cht_mf_hf_tp.cfg_file  = "configMaster.cfg"
+    sp_pinArray_3d_cht_mf_hf_tp.test_iter = 30
+    sp_pinArray_3d_cht_mf_hf_tp.test_vals = [10, -10.352122, -10.185237, -10.185237] #last 4 lines
+    sp_pinArray_3d_cht_mf_hf_tp.su2_exec  = "parallel_computation.py -f"
+    sp_pinArray_3d_cht_mf_hf_tp.timeout   = 1600
+    sp_pinArray_3d_cht_mf_hf_tp.tol       = 0.00001
+    test_list.append(sp_pinArray_3d_cht_mf_hf_tp)
 
     ##################################
     ## Streamwise Periodic adjoint ###
@@ -128,7 +128,7 @@ def main():
     ######################################
 
     pass_list = [ test.run_test() for test in test_list ]
-    
+
     # 2D DA case cht pressure drop, heat obj function
     fd_sp_pinArray_cht_2d_dp_hf                = TestCase('fd_sp_pinArray_cht_2d_dp_hf')
     fd_sp_pinArray_cht_2d_dp_hf.cfg_dir        = "incomp_navierstokes/streamwise_periodic/chtPinArray_2d"
@@ -151,7 +151,7 @@ def main():
             print('  passed - %s'%test.tag)
         else:
             print('* FAILED - %s'%test.tag)
-    
+
     if all(pass_list):
         sys.exit(0)
     else:
