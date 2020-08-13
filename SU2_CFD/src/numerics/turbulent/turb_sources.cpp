56c13b4ac9f9406513fddb30ab0e20cfcf4a57a4
bmunguia <bmunguia10@gmail.com>
2020-08-13 12:57:03 -0700

    Use strain mag in turb sources

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -844,14 +844,14 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      pw = PerturbedStrainMag * PerturbedStrainMag - TWO3*zeta*diverg;
    }
    else {
-    su2double factor = 0.;
-     for (iDim = 0; iDim < nDim; iDim++) {
-       for (jDim = 0; jDim < nDim; jDim++) {
-         factor += (PrimVar_Grad_i[iDim+1][jDim]+PrimVar_Grad_i[jDim+1][iDim]
-                  - TWO3*diverg*delta[iDim][jDim])*PrimVar_Grad_i[iDim+1][jDim];
-       }
-     }
-     // su2double factor = StrainMag_i*StrainMag_i;
+    // su2double factor = 0.;
+    //  for (iDim = 0; iDim < nDim; iDim++) {
+    //    for (jDim = 0; jDim < nDim; jDim++) {
+    //      factor += (PrimVar_Grad_i[iDim+1][jDim]+PrimVar_Grad_i[jDim+1][iDim]
+    //               - TWO3*diverg*delta[iDim][jDim])*PrimVar_Grad_i[iDim+1][jDim];
+    //    }
+    //  }
+     const su2double factor = StrainMag_i*StrainMag_i;
      pk = Eddy_Viscosity_i*factor - TWO3*Density_i*TurbVar_i[0]*diverg;
      pw = factor - TWO3*zeta*diverg;
        
