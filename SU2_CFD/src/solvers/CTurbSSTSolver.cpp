e42e769a91dc116432aae295c82693062019b3fa
bmunguia <bmunguia10@gmail.com>
2020-08-12 21:12:00 -0700

    Limiter on cross-diffusion

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -555,7 +555,7 @@ void CTurbSSTSolver::CrossDiffusionJacobian(CGeometry *geometry,
     const su2double F1_i = nodes->GetF1blending(iPoint);
     const su2double F2_i = nodes->GetF2blending(iPoint);
     const su2double r_i  = flowNodes->GetDensity(iPoint);
-    const su2double om_i = max(nodes->GetPrimitive(iPoint,1);
+    const su2double om_i = nodes->GetPrimitive(iPoint,1);
     const su2double z_i  = max(om_i, VorticityMag*F2_i/a1);
     
     Jacobian_i[0][0] = 0.; Jacobian_i[0][1] = 0.;
