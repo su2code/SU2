33ddbdc548496f036bf52f7c8398f8f490d96c35
cvencro <cr109@ic.ac.uk>
2020-08-15 08:59:21 +0100

    Update dual time Jacobian for dynamic grids too

--- SU2_CFD/src/solvers/CIncEulerSolver.cpp
+++ SU2_CFD/src/solvers/CIncEulerSolver.cpp
@@ -3292,7 +3292,24 @@ void CIncEulerSolver::SetResidual_DualTime(CGeometry *geometry, CSolver **solver
        to the dual time source term. ---*/
       if (!energy) Residual[nDim+1] = 0.0;
       LinSysRes.AddBlock(iPoint, Residual);
+
+      // ADD old version
       if (implicit) {
+        for (iVar = 1; iVar < nVar; iVar++) {
+          if (config->GetTime_Marching() == DT_STEPPING_1ST)
+            Jacobian_i[iVar][iVar] = Volume_nP1 / TimeStep;
+          if (config->GetTime_Marching() == DT_STEPPING_2ND)
+            Jacobian_i[iVar][iVar] = (Volume_nP1*3.0)/(2.0*TimeStep);
+        }
+        for (iDim = 0; iDim < nDim; iDim++)
+          Jacobian_i[iDim+1][iDim+1] = Density*Jacobian_i[iDim+1][iDim+1];
+        Jacobian_i[nDim+1][nDim+1] = Density*Cp*Jacobian_i[nDim+1][nDim+1];
+
+        Jacobian.AddBlock2Diag(iPoint, Jacobian_i);
+      }
+      // ADD old version
+
+      if (implicit && false) {
         SetPreconditioner(config, iPoint);
         for (iVar = 0; iVar < nVar; iVar++) {
           for (jVar = 0; jVar < nVar; jVar++) {
