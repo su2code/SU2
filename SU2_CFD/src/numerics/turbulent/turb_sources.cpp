fd0ff04bc92775e5a6254dc5b4cfd240a298a2e5
bmunguia <bmunguia10@gmail.com>
2020-08-18 15:16:12 -0700

    Use strain magnitude for production

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -844,14 +844,14 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      pw = PerturbedStrainMag * PerturbedStrainMag - TWO3*zeta*diverg;
    }
    else {
-    su2double S2 = 0.;
-     for (iDim = 0; iDim < nDim; iDim++) {
-       for (jDim = 0; jDim < nDim; jDim++) {
-         S2 += (PrimVar_Grad_i[iDim+1][jDim]+PrimVar_Grad_i[jDim+1][iDim]
-                  - TWO3*diverg*delta[iDim][jDim])*PrimVar_Grad_i[iDim+1][jDim];
-       }
-     }
-     // const su2double S2 = StrainMag_i*StrainMag_i;
+    // su2double S2 = 0.;
+    //  for (iDim = 0; iDim < nDim; iDim++) {
+    //    for (jDim = 0; jDim < nDim; jDim++) {
+    //      S2 += (PrimVar_Grad_i[iDim+1][jDim]+PrimVar_Grad_i[jDim+1][iDim]
+    //               - TWO3*diverg*delta[iDim][jDim])*PrimVar_Grad_i[iDim+1][jDim];
+    //    }
+    //  }
+     const su2double S2 = StrainMag_i*StrainMag_i;
      pk = Eddy_Viscosity_i*S2 - TWO3*Density_i*TurbVar_i[0]*diverg;
      pw = S2 - TWO3*zeta*diverg;
        
@@ -909,7 +909,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    Jacobian_i[0][1] += -beta_star*TurbVar_i[0]*Volume;
    Jacobian_i[1][1] += -2.*beta_blended*TurbVar_i[1]*Volume;
 
-   Jacobian_i[1][1] += min(-(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume ,0.0);
+   Jacobian_i[1][1] += min(-(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume, 0.0);
 
   }
   
