701fc306a0cef8e397966088d2338bbcd8f6079d
bmunguia <bmunguia10@gmail.com>
2020-08-17 17:55:35 -0700

    Maintain diagonal dominance

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -872,6 +872,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      /*--- k production Jacobian ---*/
      if ((pk > 0.) && (pk <= 10.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
        Jacobian_i[0][0] = min(-TWO3*diverg*Volume, 0.0);
+       if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
+         Jacobian_i[0][1] = -S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
      }
      
      /*--- omega production Jacobian ---*/
