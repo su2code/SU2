77617bb8d70db2689c2fd387a2d44eace7b29f22
bmunguia <bmunguia10@gmail.com>
2020-08-18 19:00:28 -0700

    Revert

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -851,8 +851,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      /*--- k production Jacobian ---*/
      if ((pk > 0.)) {
        Jacobian_i[0][0] = min(-TWO3*diverg*Volume,0.0);
-       // if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
-       //   Jacobian_i[0][1] = -S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
+       if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
+         Jacobian_i[0][1] = -S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
      }
      // else if (pk > 10.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
      //   Jacobian_i[0][0] = 10.*beta_star*TurbVar_i[1]*Volume;
@@ -908,7 +908,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    /*--- Implicit part ---*/
 
    Jacobian_i[0][0] += -beta_star*TurbVar_i[1]*Volume;
-   // Jacobian_i[0][1] += -beta_star*TurbVar_i[0]*Volume;
+   Jacobian_i[0][1] += -beta_star*TurbVar_i[0]*Volume;
    Jacobian_i[1][1] += -2.*beta_blended*TurbVar_i[1]*Volume;
 
    if (CDkw_i > CDKW_MIN) Jacobian_i[1][1] += min(-(1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume, 0.0);
