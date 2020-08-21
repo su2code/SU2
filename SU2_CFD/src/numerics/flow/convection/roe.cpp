ab7ef4ef1c559461de1a6e1c2b5db883b90e24e1
bmunguia <bmunguia10@gmail.com>
2020-08-20 20:21:29 -0700

    Include k column in P_Tensor and row in invP_Tensor

--- SU2_CFD/src/numerics/flow/convection/roe.cpp
+++ SU2_CFD/src/numerics/flow/convection/roe.cpp
@@ -447,7 +447,7 @@ void CUpwRoe_Flow::FinalizeResidual(su2double *val_residual, su2double **val_Jac
   GetPMatrix_inv(&RoeDensity, RoeVelocity, &RoeTke, &RoeSoundSpeed, UnitNormal, invP_Tensor);
 
   if (tkeNeeded) {
-    invP_Tensor[nVar][nVar-1] = RoeTkes;
+    invP_Tensor[nVar][nVar-1] = RoeTke;
   }
 
   /*--- Diference between conservative variables at jPoint and iPoint ---*/
