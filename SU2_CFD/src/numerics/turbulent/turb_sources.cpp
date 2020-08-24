7b67190005ed0bff06b90783a9d1499a5821d037
bmunguia <bmunguia10@gmail.com>
2020-08-24 12:04:51 -0700

    modification to omega source jac

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -850,7 +850,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
        
      /*--- k production Jacobian ---*/
      Jacobian_i[0][0] -= TWO3*Volume*max(diverg,0.0);
-     if ((TurbVar_i[1] > StrainMag_i*F2_i/a1) && (diverg > 0))
+     // if ((TurbVar_i[1] > StrainMag_i*F2_i/a1) && (diverg > 0))
+     if (diverg > 0)
        Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
 
      // if ((pk >= 0) && (pk <= 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
@@ -865,8 +866,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      // }
      
      /*--- omega production Jacobian ---*/
-     if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
-       Jacobian_i[1][1] -= TWO3*alfa_blended*Volume*max(diverg,0.0);
+     // if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
+     Jacobian_i[1][1] -= TWO3*alfa_blended*Volume*max(diverg,0.0);
 
      // if ((pw >= 0) && (pw <= 20.*beta_star*TurbVar_i[1]*zeta) && (TurbVar_i[1] > StrainMag_i*F2_i/a1)) {
      // if (pw >= 0) {
