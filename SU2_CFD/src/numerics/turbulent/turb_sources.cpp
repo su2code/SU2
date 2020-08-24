5e4d8fb05640205cee60f2cef8b7efa1f4d511f4
bmunguia <bmunguia10@gmail.com>
2020-08-24 11:33:46 -0700

    no omega production limiter

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -868,21 +868,22 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      // if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
      //   Jacobian_i[1][1] -= TWO3*alfa_blended*Volume*max(diverg,0.0);
 
-     if ((pw >= 0) && (pw <= 20.*beta_star*TurbVar_i[1]*zeta) && (TurbVar_i[1] > StrainMag_i*F2_i/a1)) {
+     // if ((pw >= 0) && (pw <= 20.*beta_star*TurbVar_i[1]*zeta) && (TurbVar_i[1] > StrainMag_i*F2_i/a1)) {
+     if (pw >= 0) {
        Jacobian_i[1][1] -= TWO3*alfa_blended*diverg*Volume;
      }
-     else if (pw > 20.*beta_star*TurbVar_i[1]*zeta) {
-       Jacobian_i[1][1] = 20.*beta_star*alfa_blended*zeta*Volume;
-       if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
-        Jacobian_i[1][1] *= 2.;
-     }
+     // else if (pw > 20.*beta_star*TurbVar_i[1]*zeta) {
+     //   Jacobian_i[1][1] = 20.*beta_star*alfa_blended*zeta*Volume;
+     //   if (TurbVar_i[1] > StrainMag_i*F2_i/a1)
+     //    Jacobian_i[1][1] *= 2.;
+     // }
    }
     
     pk = min(pk, 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]);
-    pw = min(pw, 20.*beta_star*TurbVar_i[1]*zeta)*alfa_blended*Density_i;
+    // pw = min(pw, 20.*beta_star*TurbVar_i[1]*zeta)*alfa_blended*Density_i;
     
     pk = max(pk, 0.0);
-    pw = max(pw, 0.0);
+    pw = alfa_blended*Density_i*max(pw, 0.0);
     
    /*--- Sustaining terms, if desired. Note that if the production terms are
          larger equal than the sustaining terms, the original formulation is
