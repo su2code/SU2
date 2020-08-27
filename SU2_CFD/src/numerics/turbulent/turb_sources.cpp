f065b25e83ac9b479783f2990d9f0b3f57a42fef
bmunguia <bmunguia10@gmail.com>
2020-08-27 16:37:08 -0700

    cdkw_min

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -908,7 +908,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
 
    /*--- Cross diffusion ---*/
 
-   Residual[1] += (1.0 - F1_i)*CDkw_i*Volume;
+   // Residual[1] += (1.0 - F1_i)*CDkw_i*Volume;
+   Residual[1] += (1.0 - F1_i)*max(CDkw_i,CDKW_MIN)*Volume;
 
    /*--- Implicit part ---*/
 
@@ -917,7 +918,7 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    Jacobian_i[1][1] -= 2.*beta_blended*TurbVar_i[1]*Volume;
 
    // Jacobian_i[1][1] -= (1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
-   Jacobian_i[1][1] -= (1. - F1_i)/(Density_i*TurbVar_i[1])*Volume*max(CDkw_i,0.0);
+   Jacobian_i[1][1] -= (1. - F1_i)/(Density_i*TurbVar_i[1])*Volume*(CDkw_i > CDKW_MIN);
 
   }
   
