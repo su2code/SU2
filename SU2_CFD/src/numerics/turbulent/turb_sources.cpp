878d7075cd928071c78351a70ab9d54436770dee
bmunguia <bmunguia10@gmail.com>
2020-09-03 15:25:28 -0700

    cdkw jac modifications

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -920,8 +920,8 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
    Jacobian_i[1][1] -= 2.*beta_blended*TurbVar_i[1]*Volume;
 
    // Jacobian_i[1][1] -= (1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
-   // Jacobian_i[1][1] -= (1. - F1_i)/(Density_i*TurbVar_i[1])*Volume*max(CDkw_i,0.0);
-   Jacobian_i[1][1] -= (1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume*(CDkw_i > CDKW_MIN);
+   Jacobian_i[1][1] -= (1. - F1_i)/(Density_i*TurbVar_i[1])*Volume*max(CDkw_i,0.0);
+   // Jacobian_i[1][1] -= (1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume*(CDkw_i > CDKW_MIN);
 
   }
   
