b8cec25d1dd9e724be46496429fdebf9bcf6fb62
bmunguia <bmunguia10@gmail.com>
2020-09-02 17:18:32 -0700

    modify source jac based on Levy paper (JCP 2006)

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -842,10 +842,11 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      pw = S2 - TWO3*zeta*diverg;
        
      /*--- k production Jacobian ---*/
-     if (pk >= 0)
-       Jacobian_i[0][0] -= TWO3*Volume*max(diverg,0.0);
-     if ((pk >= 0) && (TurbVar_i[1] > StrainMag_i*F2_i/a1) && (diverg > 0))
-       Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
+     Jacobian_i[0][0] = (S2/zeta-TWO3*diverg)*Volume;
+     // if (pk >= 0)
+     //   Jacobian_i[0][0] -= TWO3*Volume*max(diverg,0.0);
+     // if ((pk >= 0) && (TurbVar_i[1] > StrainMag_i*F2_i/a1) && (diverg > 0))
+     //   Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
 
      // if ((pk >= 0) && (pk <= 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0])) {
      //   Jacobian_i[0][0] += (S2/zeta-TWO3*diverg)*Volume;
@@ -858,8 +859,9 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      // }
      
      /*--- omega production Jacobian ---*/
-     if ((pw >= 0) && (TurbVar_i[1] > StrainMag_i*F2_i/a1))
-       Jacobian_i[1][1] -= TWO3*alfa_blended*Volume*max(diverg,0.0);
+     Jacobian_i[1][1] -= TWO3*alfa_blended*diver*Volume;
+     // if ((pw >= 0) && (TurbVar_i[1] > StrainMag_i*F2_i/a1))
+     //   Jacobian_i[1][1] -= TWO3*alfa_blended*Volume*max(diverg,0.0);
 
      // if ((pw >= 0) && (TurbVar_i[1] > StrainMag_i*F2_i/a1))
      //   Jacobian_i[1][1] -= TWO3*alfa_blended*diverg*Volume;
@@ -912,13 +914,14 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
 
    /*--- Implicit part ---*/
 
-   Jacobian_i[0][0] -= beta_star*TurbVar_i[1]*Volume;
-   Jacobian_i[0][1] -= beta_star*TurbVar_i[0]*Volume;
+   // Jacobian_i[0][0] -= beta_star*TurbVar_i[1]*Volume;
+   // Jacobian_i[0][1] -= beta_star*TurbVar_i[0]*Volume;
+   Jacobian_i[0][0] -= 2.*beta_star*TurbVar_i[1]*Volume;
    Jacobian_i[1][1] -= 2.*beta_blended*TurbVar_i[1]*Volume;
 
    // Jacobian_i[1][1] -= (1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume;
    // Jacobian_i[1][1] -= (1. - F1_i)/(Density_i*TurbVar_i[1])*Volume*max(CDkw_i,0.0);
-   Jacobian_i[1][1] -= (1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume*(CDkw_i > CDKW_MIN);
+   // Jacobian_i[1][1] -= (1. - F1_i)*CDkw_i/(Density_i*TurbVar_i[1])*Volume*(CDkw_i > CDKW_MIN);
 
   }
   
