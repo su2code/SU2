1cf3049a7f5c81034ec7a032756fd13da4618a25
bmunguia <bmunguia10@gmail.com>
2020-09-25 18:20:05 -0700

    jac

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -854,10 +854,10 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
        if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
          Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
      }
-     // else if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
-     //   Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
-     //   Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
-     // }
+     else if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
+       Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
+       Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
+     }
      
      /*--- omega production Jacobian ---*/
      // if ((pw >= 0) && (TurbVar_i[1] > VorticityMag_i*F2_i/a1))
