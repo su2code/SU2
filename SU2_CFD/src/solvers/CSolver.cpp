0d15680e330db6b0df59f7552bea727e38d4b580
bmunguia <bmunguia10@gmail.com>
2020-09-25 18:06:14 -0700

    reset adaptive CFL vars if maxLinRes

--- SU2_CFD/src/solvers/CSolver.cpp
+++ SU2_CFD/src/solvers/CSolver.cpp
@@ -2575,7 +2575,7 @@ void CSolver::AdaptCFLNumber(CGeometry **geometry,
      rate. In this situation, we force a reduction of the CFL in all cells.
      Reset the array so that we delay the next decrease for some iterations. */
 
-    if (NonLinRes_Value > -0.1*New_Func && NonLinRes_Counter >= Res_Count) {
+    if ((NonLinRes_Value > -0.1*New_Func && NonLinRes_Counter >= Res_Count) || reduceCFL) {
       for (auto iCounter = 0; iCounter < Res_Count; iCounter++)
         NonLinRes_Series[iCounter] = 1.0;
       for (auto iVar = 0; iVar < nVar; iVar++)
@@ -2590,7 +2590,7 @@ void CSolver::AdaptCFLNumber(CGeometry **geometry,
     } /* End SU2_OMP_MASTER, now all threads update the CFL number. */
     SU2_OMP_BARRIER
 
-    if (NonLinRes_Value > -0.1*New_Func && NonLinRes_Counter >= Res_Count) {
+    if ((NonLinRes_Value > -0.1*New_Func && NonLinRes_Counter >= Res_Count) || reduceCFL) {
       NonLinRes_Counter = 0;
       reduceCFL = true;
       New_Func = 1.0;
