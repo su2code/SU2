3d73d7b2696bbd543b20ddc54a8e1be0fe59b829
cvencro <cr109@ic.ac.uk>
2020-10-05 22:49:32 +0100

    re-add missed structural markers

--- Common/src/grid_movement/CVolumetricMovement.cpp
+++ Common/src/grid_movement/CVolumetricMovement.cpp
@@ -312,8 +312,10 @@ void CVolumetricMovement::ComputeSolid_Wall_Distance(CGeometry *geometry, CConfi
   for(iMarker=0; iMarker<config->GetnMarker_All(); ++iMarker) {
     if( (config->GetMarker_All_KindBC(iMarker) == EULER_WALL ||
          config->GetMarker_All_KindBC(iMarker) == HEAT_FLUX)  ||
-       (config->GetMarker_All_KindBC(iMarker) == ISOTHERMAL) ||
-        (config->GetMarker_All_KindBC(iMarker) == CHT_WALL_INTERFACE)) {
+        (config->GetMarker_All_KindBC(iMarker) == ISOTHERMAL) ||
+        (config->GetMarker_All_KindBC(iMarker) == CHT_WALL_INTERFACE) ||
+        (config->GetMarker_All_KindBC(iMarker) == CLAMPED_BOUNDARY ) ||
+        (config->GetMarker_All_KindBC(iMarker) == LOAD_BOUNDARY )) {
       nVertex_SolidWall += geometry->GetnVertex(iMarker);
     }
   }
@@ -331,8 +333,10 @@ void CVolumetricMovement::ComputeSolid_Wall_Distance(CGeometry *geometry, CConfi
   for (iMarker=0; iMarker<config->GetnMarker_All(); ++iMarker) {
     if ( (config->GetMarker_All_KindBC(iMarker) == EULER_WALL ||
          config->GetMarker_All_KindBC(iMarker) == HEAT_FLUX)  ||
-       (config->GetMarker_All_KindBC(iMarker) == ISOTHERMAL)  ||
-         (config->GetMarker_All_KindBC(iMarker) == CHT_WALL_INTERFACE)) {
+         (config->GetMarker_All_KindBC(iMarker) == ISOTHERMAL)  ||
+         (config->GetMarker_All_KindBC(iMarker) == CHT_WALL_INTERFACE) ||
+         (config->GetMarker_All_KindBC(iMarker) == CLAMPED_BOUNDARY ) ||
+         (config->GetMarker_All_KindBC(iMarker) == LOAD_BOUNDARY )) {
       for (iVertex=0; iVertex<geometry->GetnVertex(iMarker); ++iVertex) {
         iPoint = geometry->vertex[iMarker][iVertex]->GetNode();
         PointIDs[jj++] = iPoint;
