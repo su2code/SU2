f4a4f306f6b945da95736e89d30a89da6df66645
bmunguia <bmunguia10@gmail.com>
2020-10-14 16:47:57 -0700

    allow gva to be negative

--- SU2_CFD/include/limiters/CLimiterDetails.hpp
+++ SU2_CFD/include/limiters/CLimiterDetails.hpp
@@ -82,7 +82,8 @@ namespace LimiterHelpers
     const su2double beta = (1.0 + kappa)/(1.0 - kappa);
     const su2double psi = R*(R + beta)/(pow(R, 2.0) + beta);
 
-    return psi * (R >= 0);
+    // return psi * (R >= 0);
+    return psi;
   }
 
   inline su2double pipernoFunction(su2double proj, su2double delta)
