06a18362d6bdac14aa2066d0859c14e0940d9a87
bmunguia <bmunguia10@gmail.com>
2020-10-25 16:34:07 -0700

    bug fix

--- SU2_CFD/src/numerics/flow/flow_diffusion.cpp
+++ SU2_CFD/src/numerics/flow/flow_diffusion.cpp
@@ -748,7 +748,6 @@ void CAvgGrad_Flow::SetLaminarViscosityJacobian(const su2double *val_Mean_PrimVa
     proj_stress[iDim] = -TWO3*div_vel*Normal[iDim];
     for (auto jDim = 0; jDim < nDim; jDim++)
       proj_stress[iDim] += WF_Factor*(Mean_GradPrimVar[jDim+1][iDim] + Mean_GradPrimVar[iDim+1][jDim])*Normal[jDim];
-
     proj_stress_dot_v += proj_stress[iDim]*val_Mean_PrimVar[iDim+1];
     proj_heat_flux    += heat_flux_factor*Mean_GradPrimVar[0][iDim]*Normal[iDim];
     proj_tke_flux     += Mean_GradTurbVar[iDim]*Normal[iDim];
@@ -814,10 +813,10 @@ void CAvgGrad_Flow::SetEddyViscosityJacobian(const su2double *val_Mean_PrimVar,
       proj_stress[iDim] = -TWO3*div_vel*Normal[iDim];
       for (auto jDim = 0; jDim < nDim; jDim++)
         proj_stress[iDim] += WF_Factor*(Mean_GradPrimVar[jDim+1][iDim] + Mean_GradPrimVar[iDim+1][jDim])*Normal[jDim];
-
       proj_stress_dot_v += proj_stress[iDim]*val_Mean_PrimVar[iDim+1];
       proj_heat_flux    += heat_flux_factor*Mean_GradPrimVar[0][iDim]*Normal[iDim];
       proj_tke_flux     += Mean_GradTurbVar[iDim]*Normal[iDim];
+    }
 
     if (turb_omega_i > VorticityMag_i*F2_i/a1) {
       const su2double factor = 0.5*turb_ke_i/turb_omega_i;
