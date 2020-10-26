d9375982137c2c4eaa24751d4e9056f195a1e80d
bmunguia <bmunguia10@gmail.com>
2020-10-26 15:09:10 -0700

    modifying source term

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -849,6 +849,17 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
          Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
      }
 
+     if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
+       Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
+       Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
+     }
+     // else if (pk >= 0) {
+     else {
+       Jacobian_i[0][0] += (S2/zeta-TWO3*diverg)*Volume;
+       if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
+         Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
+     }
+
      // if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
      //   Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
      //   Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
@@ -861,11 +872,12 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      
      /*--- omega production Jacobian ---*/
      // if ((pw >= 0) && (TurbVar_i[1] > VorticityMag_i*F2_i/a1))
-     if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
-       Jacobian_i[1][1] -= TWO3*alfa_blended*Volume*max(diverg,0.0);
+     // if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
+     //   Jacobian_i[1][1] -= TWO3*alfa_blended*Volume*max(diverg,0.0);
 
      // if ((pw >= 0) && (TurbVar_i[1] > VorticityMag_i*F2_i/a1))
-     //   Jacobian_i[1][1] -= TWO3*alfa_blended*diverg*Volume;
+     if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
+       Jacobian_i[1][1] -= TWO3*alfa_blended*diverg*Volume;
    }
     
     pk  = min(pk, 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]);
