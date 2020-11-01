f77c4172cb8d919a404ec7f9a7ac264dcb85d2c0
bmunguia <bmunguia10@gmail.com>
2020-10-31 17:05:09 -0700

    omega prod limiter

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -843,13 +843,13 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
        
      /*--- k production Jacobian ---*/
 
-     // if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
-     //   Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
-     //   Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
-     // }
-     // else if (pk >= 0) {
+     if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
+       Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
+       Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
+     }
+     else if (pk >= 0) {
      // else {
-     if (pk >= 0) {
+     // if (pk >= 0) {
        Jacobian_i[0][0] += (S2/zeta-TWO3*diverg)*Volume;
        if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
          Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
