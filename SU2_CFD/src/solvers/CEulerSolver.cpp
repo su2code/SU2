1eaaad1f1a0659230b6af1a707cd871ff728d522
bmunguia <bmunguia10@gmail.com>
2020-11-04 13:35:56 -0800

    one way ur coupling

--- SU2_CFD/src/solvers/CEulerSolver.cpp
+++ SU2_CFD/src/solvers/CEulerSolver.cpp
@@ -5230,6 +5230,10 @@ void CEulerSolver::ComputeUnderRelaxationFactor(CSolver **solver, CConfig *confi
       }
     }
 
+    /* Choose the minimum factor between mean flow and turbulence. */
+
+    localUnderRelaxation = min(localUnderRelaxation, solver[TURB_SOL]->GetNodes()->GetUnderRelaxation(iPoint));
+
     /* Threshold the relaxation factor in the event that there is
      a very small value. This helps avoid catastrophic crashes due
      to non-realizable states by canceling the update. */
