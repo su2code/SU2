cbe9c6a0eb48c4347b015f284341f3257a77ae83
bmunguia <bmunguia10@gmail.com>
2020-11-04 18:46:50 -0800

    revert production limiter, but don't require to be positive

--- SU2_CFD/src/numerics/turbulent/turb_sources.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_sources.cpp
@@ -843,13 +843,13 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
        
      /*--- k production Jacobian ---*/
 
-     // if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
-     //   Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
-     //   Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
-     // }
+     if (pk > 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]) {
+       Jacobian_i[0][0] += 20.*beta_star*TurbVar_i[1]*Volume;
+       Jacobian_i[0][1] += 20.*beta_star*TurbVar_i[0]*Volume;
+     }
      // else if (pk >= 0) {
-     // else {
-     if (pk >= 0) {
+     else {
+     // if (pk >= 0) {
        Jacobian_i[0][0] += (S2/zeta-TWO3*diverg)*Volume;
        if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
          Jacobian_i[0][1] -= S2*TurbVar_i[0]/pow(TurbVar_i[1],2.)*Volume;
@@ -857,15 +857,15 @@ CNumerics::ResidualType<> CSourcePieceWise_TurbSST::ComputeResidual(const CConfi
      
      /*--- omega production Jacobian ---*/
 
-     if ((pw >= 0) && (TurbVar_i[1] > VorticityMag_i*F2_i/a1))
-     // if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
+     // if ((pw >= 0) && (TurbVar_i[1] > VorticityMag_i*F2_i/a1))
+     if (TurbVar_i[1] > VorticityMag_i*F2_i/a1)
        Jacobian_i[1][1] -= TWO3*alfa_blended*diverg*Volume;
    }
     
-    // pk = min(pk, 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]);
+    pk = min(pk, 20.*beta_star*Density_i*TurbVar_i[1]*TurbVar_i[0]);
     
-    pk = max(pk, 0.0);
-    pw = max(pw, 0.0);
+    // pk = max(pk, 0.0);
+    // pw = max(pw, 0.0);
 
    /*--- Sustaining terms, if desired. Note that if the production terms are
          larger equal than the sustaining terms, the original formulation is
