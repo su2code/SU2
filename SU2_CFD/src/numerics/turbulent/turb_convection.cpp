04d5495c1a7ed3f757e23072982ae7e96aa8a46e
bmunguia <bmunguia10@gmail.com>
2020-11-18 15:58:06 -0800

    revert convection

--- SU2_CFD/src/numerics/turbulent/turb_convection.cpp
+++ SU2_CFD/src/numerics/turbulent/turb_convection.cpp
@@ -134,14 +134,9 @@ void CUpwSca_TurbSST::ExtraADPreaccIn() {
 
 void CUpwSca_TurbSST::FinishResidualCalc(const CConfig* config) {
 
-  // Flux[0] = a_i*Density_i*TurbVar_i[0]+a_j*Density_j*TurbVar_j[0];
-  // Flux[1] = a_i*Density_i*TurbVar_i[1]+a_j*Density_j*TurbVar_j[1];
+  Flux[0] = a_i*Density_i*TurbVar_i[0]+a_j*Density_j*TurbVar_j[0];
+  Flux[1] = a_i*Density_i*TurbVar_i[1]+a_j*Density_j*TurbVar_j[1];
 
-  // Jacobian_i[0][0] = Jacobian_i[1][1] = a_i;
-  // Jacobian_j[0][0] = Jacobian_j[1][1] = a_j;
-  Flux[0] = 0.5*(Density_i*TurbVar_i[0]+Density_j*TurbVar_j[0])*a_ij;
-  Flux[1] = 0.5*(Density_i*TurbVar_i[1]+Density_j*TurbVar_j[1])*a_ij;
-
-  Jacobian_i[0][0] = Jacobian_i[1][1] = 0.5*a_ij;
-  Jacobian_j[0][0] = Jacobian_j[1][1] = 0.5*a_ij;
+  Jacobian_i[0][0] = Jacobian_i[1][1] = a_i;
+  Jacobian_j[0][0] = Jacobian_j[1][1] = a_j;
 }
