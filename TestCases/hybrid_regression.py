00f537e13699e6576af8361761143b975e98577b
Pedro Gomes <pcarruscag@gmail.com>
2020-12-18 22:44:00 +0000

    final update

--- TestCases/hybrid_regression.py
+++ TestCases/hybrid_regression.py
@@ -378,7 +378,7 @@ def main():
     Jones_tc.cfg_dir   = "turbomachinery/APU_turbocharger"
     Jones_tc.cfg_file  = "Jones.cfg"
     Jones_tc.test_iter = 5
-    Jones_tc.test_vals = [-5.280316, 0.379651, 72.207590, 1.277638]
+    Jones_tc.test_vals = [-5.280316, 0.379651, 72.212090, 1.277440]
     Jones_tc.new_output = False
     test_list.append(Jones_tc)
 
@@ -387,7 +387,7 @@ def main():
     Jones_tc_rst.cfg_dir   = "turbomachinery/APU_turbocharger"
     Jones_tc_rst.cfg_file  = "Jones_rst.cfg"
     Jones_tc_rst.test_iter = 5
-    Jones_tc_rst.test_vals = [-4.625330, -1.568896, 33.995140, 10.181610]
+    Jones_tc_rst.test_vals = [-4.625248, -1.568821, 33.995140, 10.181940]
     Jones_tc_rst.new_output = False
     test_list.append(Jones_tc_rst)
 
