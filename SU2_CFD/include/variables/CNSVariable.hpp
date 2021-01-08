e19dbe44e2b4d11088df6f8d2d5ce5a74f194593
bmunguia <bmunguia10@gmail.com>
2021-01-08 15:20:54 -0800

    index bug fix

--- SU2_CFD/include/variables/CNSVariable.hpp
+++ SU2_CFD/include/variables/CNSVariable.hpp
@@ -304,7 +304,7 @@ public:
   inline bool GetUseWallFunction(unsigned long iPoint) override {
     bool UseWallFunction = true;
     for (auto iNode = 0; iNode < 4; iNode++) {
-      if (WallUTau(iPoint, iNode) < 0) {
+      if (WallUTau(WallMap(iPoint), iNode) < 0) {
         UseWallFunction = false;
         break;
       }
