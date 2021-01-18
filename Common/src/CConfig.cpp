db4d1750c9d3fd002d3af83384be3d71382a30d5
cvencro <cr109@ic.ac.uk>
2021-01-18 18:47:41 +0000

    remove error

--- Common/src/CConfig.cpp
+++ Common/src/CConfig.cpp
@@ -4830,15 +4830,9 @@ void CConfig::SetPostprocessing(unsigned short val_software, unsigned short val_
     Kind_Linear_Solver = Kind_DiscAdj_Linear_Solver;
     Kind_Linear_Solver_Prec = Kind_DiscAdj_Linear_Prec;
 
-    if (TimeMarching) {
+    if (Time_Domain) {
 
       Restart_Flow = false;
-
-      if (GetKind_GridMovement() != RIGID_MOTION &&
-          GetKind_GridMovement() != NO_MOVEMENT) {
-        SU2_MPI::Error(string("Dynamic mesh movement currently only supported for the discrete adjoint solver for\n") +
-                       string("GRID_MOVEMENT = RIGID_MOTION."), CURRENT_FUNCTION);
-      }
 
       if (Unst_AdjointIter- long(nTimeIter) < 0){
         SU2_MPI::Error(string("Invalid iteration number requested for unsteady adjoint.\n" ) +
