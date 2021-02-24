b8f45000b0944985b6742e5414cef321064f361f
TobiKattmann <Tobias.Kattmann@de.bosch.com>
2021-02-24 15:38:40 +0100

    Cleaning regression test files

--- TestCases/parallel_regression.py
+++ TestCases/parallel_regression.py
@@ -452,6 +452,17 @@ def main():
     inc_lam_bend.tol       = 0.00001
     test_list.append(inc_lam_bend)
 
+    # 3D laminar channnel with 1 cell in flow direction, streamwise periodic
+    sp_pipeSlice_3d_dp_hf_tp           = TestCase('sp_pipeSlice_3d_dp_hf_tp')
+    sp_pipeSlice_3d_dp_hf_tp.cfg_dir   = "incomp_navierstokes/streamwise_periodic/pipeSlice_3d"
+    sp_pipeSlice_3d_dp_hf_tp.cfg_file  = "sp_pipeSlice_3d_dp_hf_tp.cfg"
+    sp_pipeSlice_3d_dp_hf_tp.test_iter = 10
+    sp_pipeSlice_3d_dp_hf_tp.test_vals = [-11.119796, -11.234737, -8.694310, -0.000023] #last 4 lines
+    sp_pipeSlice_3d_dp_hf_tp.su2_exec  = "mpirun -n 2 SU2_CFD"
+    sp_pipeSlice_3d_dp_hf_tp.timeout   = 1600
+    sp_pipeSlice_3d_dp_hf_tp.tol       = 0.00001
+    test_list.append(sp_pipeSlice_3d_dp_hf_tp)
+
     ############################
     ### Incompressible RANS  ###
     ############################
@@ -1232,6 +1243,30 @@ def main():
     cht_compressible.tol       = 0.00001
     test_list.append(cht_compressible)
 
+    # 2D CHT case with HF BC and
+    sp_pinArray_cht_2d_mf_hf           = TestCase('sp_pinArray_cht_2d_mf_hf')
+    sp_pinArray_cht_2d_mf_hf.cfg_dir   = "incomp_navierstokes/streamwise_periodic/chtPinArray_2d"
+    sp_pinArray_cht_2d_mf_hf.cfg_file  = "configMaster.cfg"
+    sp_pinArray_cht_2d_mf_hf.test_iter = 100
+    sp_pinArray_cht_2d_mf_hf.test_vals = [0.247022, -0.812199, -0.974877, -0.753315, 208.023676, 349.950000] #last 7 lines
+    sp_pinArray_cht_2d_mf_hf.su2_exec  = "mpirun -n 2 SU2_CFD"
+    sp_pinArray_cht_2d_mf_hf.timeout   = 1600
+    sp_pinArray_cht_2d_mf_hf.tol       = 0.00001
+    sp_pinArray_cht_2d_mf_hf.multizone = True
+    test_list.append(sp_pinArray_cht_2d_mf_hf)
+
+    # simple small 3D pin case massflow periodic with heatflux BC
+    sp_pinArray_3d_cht_mf_hf_tp           = TestCase('sp_pinArray_3d_cht_mf_hf_tp')
+    sp_pinArray_3d_cht_mf_hf_tp.cfg_dir   = "incomp_navierstokes/streamwise_periodic/chtPinArray_3d"
+    sp_pinArray_3d_cht_mf_hf_tp.cfg_file  = "configMaster.cfg"
+    sp_pinArray_3d_cht_mf_hf_tp.test_iter = 30
+    sp_pinArray_3d_cht_mf_hf_tp.test_vals = [0.511984, -3.063453, -0.451732, -0.008477, 214.707868, 365.670000] #last 7 lines
+    sp_pinArray_3d_cht_mf_hf_tp.su2_exec  = "mpirun -n 2 SU2_CFD"
+    sp_pinArray_3d_cht_mf_hf_tp.timeout   = 1600
+    sp_pinArray_3d_cht_mf_hf_tp.tol       = 0.00001
+    sp_pinArray_3d_cht_mf_hf_tp.multizone = True
+    test_list.append(sp_pinArray_3d_cht_mf_hf_tp)
+
     ##########################
     ###   Python wrapper   ###
     ##########################
@@ -1584,6 +1619,20 @@ def main():
 
     pass_list.append(sphere_ffd_def_bspline.run_def())
     test_list.append(sphere_ffd_def_bspline)
+
+    # 2D FD case cht, pressure drop, heat obj function
+    fd_sp_pinArray_cht_2d_dp_hf                = TestCase('fd_sp_pinArray_cht_2d_dp_hf')
+    fd_sp_pinArray_cht_2d_dp_hf.cfg_dir        = "incomp_navierstokes/streamwise_periodic/chtPinArray_2d"
+    fd_sp_pinArray_cht_2d_dp_hf.cfg_file       = "FD_configMaster.cfg"
+    fd_sp_pinArray_cht_2d_dp_hf.test_iter      = 100
+    fd_sp_pinArray_cht_2d_dp_hf.su2_exec       = "finite_differences.py -z 2 -n 2 -f"
+    fd_sp_pinArray_cht_2d_dp_hf.timeout        = 1600
+    fd_sp_pinArray_cht_2d_dp_hf.reference_file = "of_grad_findiff.csv.ref"
+    fd_sp_pinArray_cht_2d_dp_hf.test_file      = "FINDIFF/of_grad_findiff.csv"
+    fd_sp_pinArray_cht_2d_dp_hf.multizone      = True
+
+    pass_list.append(fd_sp_pinArray_cht_2d_dp_hf.run_filediff())
+    test_list.append(fd_sp_pinArray_cht_2d_dp_hf)
 
 
     # Tests summary
