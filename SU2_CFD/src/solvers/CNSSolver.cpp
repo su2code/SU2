644e8c935c4264694a49a3ca11bdf5882213438f
bmunguia <bmunguia10@gmail.com>
2021-03-03 17:27:11 -0800

    make turbulent work with venkat

--- SU2_CFD/src/solvers/CNSSolver.cpp
+++ SU2_CFD/src/solvers/CNSSolver.cpp
@@ -222,7 +222,7 @@ void CNSSolver::Preprocessing(CGeometry *geometry, CSolver **solver, CConfig *co
   /*--- Compute the limiter in case we need it in the turbulence model or to limit the
    *    viscous terms (check this logic with JST and 2nd order turbulence model) ---*/
 
-  if ((iMesh == MESH_0) && (limiter_flow || limiter_turb || limiter_adjflow) && !Output) {
+  if ((iMesh == MESH_0) && (limiter_flow || limiter_adjflow) && !Output) {
     SetPrimitive_Limiter(geometry, config);
   }
 
