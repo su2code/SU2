ed1d185c34d763115e6c1bcca170ab19599ce832
bmunguia <bmunguia10@gmail.com>
2021-03-11 17:00:53 -0800

    clip

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -297,10 +297,10 @@ void CTurbSSTSolver::Postprocessing(CGeometry *geometry, CSolver **solver, CConf
                             
   for (auto iPoint = 0ul; iPoint < nPoint; iPoint++) {
     nodes->SetNon_Physical(iPoint, false);
-    for (auto iVar = 0; iVar < nVar; iVar++) {
-      const su2double sol = max(min(nodes->GetSolution(iPoint, iVar), upperlimit[iVar]), lowerlimit[iVar]);
-      nodes->SetSolution(iPoint, iVar, sol);
-    }
+    // for (auto iVar = 0; iVar < nVar; iVar++) {
+    //   const su2double sol = max(min(nodes->GetSolution(iPoint, iVar), upperlimit[iVar]), lowerlimit[iVar]);
+    //   nodes->SetSolution(iPoint, iVar, sol);
+    // }
   }
   
   SetPrimitive_Variables(solver);
