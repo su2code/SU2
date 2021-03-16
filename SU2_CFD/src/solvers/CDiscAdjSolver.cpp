c53ecdd6caa532e81251715437ac284bc5f37370
bmunguia <bmunguia10@gmail.com>
2021-03-16 11:11:58 -0700

    obnoxious print

--- SU2_CFD/src/solvers/CDiscAdjSolver.cpp
+++ SU2_CFD/src/solvers/CDiscAdjSolver.cpp
@@ -523,7 +523,8 @@ void CDiscAdjSolver::ExtractAdjoint_Solution(CGeometry *geometry, CConfig *confi
 
         cout << "Sol[iPoint=" << geometry->node[iPoint]->GetGlobalIndex() << "][iVar=" << iVar << "]= " << direct_solver->GetNodes()->GetSolution(iPoint,iVar) << ", Res= " << direct_solver->LinSysRes(iPoint,iVar) << ", Domain= " << isdomain << endl;
         for (auto jVar = 0u; jVar < nVar; jVar++) {
-              cout << "Jac[iPoint=" << geometry->node[iPoint]->GetGlobalIndex() << "][iPoint][iVar=" << iVar <<"][jVar=" << jVar << "]= " << direct_solver->Jacobian.GetBlock(iPoint,jPoint,iVar,jVar) << endl;
+          cout << "Jac[iPoint=" << geometry->node[iPoint]->GetGlobalIndex() << "][iPoint][iVar=" << iVar <<"][jVar=" << jVar << "]= " << direct_solver->Jacobian.GetBlock(iPoint,jPoint,iVar,jVar) << endl;
+        }
         for (auto iNeigh = 0; iNeigh < geometry->node[iPoint]->GetnNeighbor(); iNeigh++) {
           const auto jPoint = geometry->node[iPoint]->GetPoint(iNeigh);
             for (auto jVar = 0u; jVar < nVar; jVar++) {
