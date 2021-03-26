128887bbab9f310c6b746f806cfada8bc644aae9
Wally Thomas Maier <wmaier@sh01-ln03.stanford.edu>
2021-03-26 13:17:44 -0700

    fixing protection statement (i think)

--- Common/src/CConfig.cpp
+++ Common/src/CConfig.cpp
@@ -3597,7 +3597,7 @@ void CConfig::SetPostprocessing(unsigned short val_software, unsigned short val_
   if (nemo){
     if (Kind_Upwind_Flow == AUSMPWPLUS)
       SU2_MPI::Error("AUSMPW+ is extremely unstable. Feel free to fix me!", CURRENT_FUNCTION);
-    if ((GetKind_FluidModel() != SU2_NONEQ) || (GetKind_FluidModel() != MUTATIONPP) )
+    if ((GetKind_FluidModel() != SU2_NONEQ) && (GetKind_FluidModel() != MUTATIONPP) )
       SU2_MPI::Error("Only FLUID_MODEL = SU2_NONEQ or FLUID_MODEL = MUTATIONPP can be used with a NEMO solver.", CURRENT_FUNCTION);
   }
 
