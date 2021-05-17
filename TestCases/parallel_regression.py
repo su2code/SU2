0d55909d8eaf29855b284aa067ac114c31be5cc7
Pedro Gomes <pcarruscag@gmail.com>
2021-05-18 00:09:36 +0100

    update regressions

--- TestCases/parallel_regression.py
+++ TestCases/parallel_regression.py
@@ -1175,7 +1175,7 @@ def main():
     fsi2d.cfg_dir   = "fea_fsi/WallChannel_2d"
     fsi2d.cfg_file  = "configFSI.cfg"
     fsi2d.test_iter = 4
-    fsi2d.test_vals = [4, 0, -3.768496, -4.159961] #last 4 columns
+    fsi2d.test_vals = [4, 0, -3.771873, -4.163491] #last 4 columns
     fsi2d.su2_exec  = "parallel_computation_fsi.py -f"
     fsi2d.timeout   = 1600
     fsi2d.multizone= True
@@ -1200,7 +1200,7 @@ def main():
     dyn_fsi.cfg_dir   = "fea_fsi/dyn_fsi"
     dyn_fsi.cfg_file  = "config.cfg"
     dyn_fsi.test_iter = 4
-    dyn_fsi.test_vals = [-4.398550, -4.086739, 0.000000, 117.000000]
+    dyn_fsi.test_vals = [-4.400874, -4.085152, 0.000000, 117]
     dyn_fsi.multizone = True
     dyn_fsi.unsteady  = True
     dyn_fsi.su2_exec  = "mpirun -n 2 SU2_CFD"
@@ -1356,7 +1356,7 @@ def main():
     pywrapper_fsi2d.cfg_dir   = "fea_fsi/WallChannel_2d"
     pywrapper_fsi2d.cfg_file  = "configFSI.cfg"
     pywrapper_fsi2d.test_iter = 4
-    pywrapper_fsi2d.test_vals = [4, 0, -3.768496, -4.159961] #last 4 columns
+    pywrapper_fsi2d.test_vals = [4, 0, -3.771873, -4.163491] #last 4 columns
     pywrapper_fsi2d.su2_exec  = "mpirun -np 2 SU2_CFD.py --nZone 2 --fsi True --parallel -f"
     pywrapper_fsi2d.timeout   = 1600
     pywrapper_fsi2d.unsteady  = True
