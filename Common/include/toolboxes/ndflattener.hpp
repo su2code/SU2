a0fb3d54cf9a43e75983a3aa951a2c7a1f8e4ac8
Max Aehle <max.aehle@scicomp.uni-kl.de>
2021-05-24 17:27:07 +0200

    const reference in parameter packs
    ARGS... args  ->  ARGS const&... args

--- Common/include/toolboxes/ndflattener.hpp
+++ Common/include/toolboxes/ndflattener.hpp
@@ -383,7 +383,7 @@ public:
    *
    */
   template<class... ARGS>
-  NdFlattener(ARGS... args) {
+  NdFlattener(ARGS const&... args) {
     initialize_or_refresh(args...);
   }
 
@@ -395,7 +395,7 @@ public:
    * recollected under the assumption that the indices arrays did not change.
    */
   template<class ...ARGS>
-  void initialize_or_refresh(ARGS... args){
+  void initialize_or_refresh(ARGS const&... args){
     if( initialized() ){
       refresh(args...);
     } else {
@@ -686,12 +686,12 @@ public:
   NdFlattener(void) {}
 
   template<class... ARGS>
-  NdFlattener(ARGS... args) {
+  NdFlattener(ARGS const&... args) {
     initialize_or_refresh(args...);
   }
 
   template<class ...ARGS>
-  void initialize_or_refresh(ARGS... args){
+  void initialize_or_refresh(ARGS const&... args){
     if( initialized() ){
       refresh(args...);
     } else {
