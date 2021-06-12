9142f50a5c13f73384e3f00e5cbbee56f040423f
Pedro Gomes <pcarruscag@gmail.com>
2021-06-12 18:53:43 +0100

    towards less "using namespace std"

--- Common/include/toolboxes/C1DInterpolation.hpp
+++ Common/include/toolboxes/C1DInterpolation.hpp
@@ -177,5 +177,5 @@ std::vector<su2double> CorrectedInletValues(const std::vector<su2double> &Inlet_
  * \param[in] nDim - the dimensions of the problem.
  * \param[in] nColumns - the number of columns in the final interpolated data
  */
-void PrintInletInterpolatedData(const std::vector<su2double>& Inlet_Data_Interpolated, string Marker,
+void PrintInletInterpolatedData(const std::vector<su2double>& Inlet_Data_Interpolated, std::string Marker,
                                 unsigned long nVertex, unsigned short nDim, unsigned short nColumns);
