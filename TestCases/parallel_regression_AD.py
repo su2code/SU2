56424a95a9f456334415d4441fbc6b5752ca0b05
snow54 <yuukiutsumi@yahoo.co.jp>
2021-07-21 14:54:09 +0900

    Define output vectors

--- TestCases/parallel_regression_AD.py
+++ TestCases/parallel_regression_AD.py
@@ -80,7 +80,7 @@ def main():
     ea_naca64206.cfg_dir      = "optimization_euler/equivalentarea_naca64206"
     ea_naca64206.cfg_file     = "NACA64206_adjoint.cfg"
     ea_naca64206.test_iter    = 20
-    ea_naca64206.test_vals    = [-3.111181, -3.501516, 6.8705e-02, 0]
+    ea_naca64206.test_vals    = [3.663941, 3.471570, 0.0000e+00, 1.2218e+02]
     ea_naca64206.su2_exec     = "mpirun -n 2 SU2_CFD_AD"
     ea_naca64206.timeout      = 1600
     ea_naca64206.tol          = 0.00001
