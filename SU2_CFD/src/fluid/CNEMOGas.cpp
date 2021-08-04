e2084156b6e2bb306d48a3e271cd5aa03885569c
unknown <wallymaier@gmail.com>
2021-08-04 14:58:46 -0400

    fixed vel2 bug

--- SU2_CFD/src/fluid/CNEMOGas.cpp
+++ SU2_CFD/src/fluid/CNEMOGas.cpp
@@ -257,7 +257,7 @@ void CNEMOGas::ComputedTdU(su2double *V, su2double *val_dTdU){
 
   /*--- Calculate supporting quantities ---*/
   for (iDim = 0; iDim < nDim; iDim++)
-    Vel[iDim] = V[VEL_INDEX+iDim]*V[VEL_INDEX+iDim];
+    Vel[iDim] = V[VEL_INDEX+iDim];
   su2double v2 = GeometryToolbox::SquaredNorm(nDim,Vel);
 
   /*--- Species density derivatives ---*/
