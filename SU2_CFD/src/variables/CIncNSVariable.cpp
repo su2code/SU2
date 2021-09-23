2a6cf7fef47ebf79ffb362cf8232edbdcccffad2
Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>
2021-09-23 09:30:59 +0100

    Update SU2_CFD/src/variables/CIncNSVariable.cpp
    Co-authored-by: TobiKattmann <31306376+TobiKattmann@users.noreply.github.com>

--- SU2_CFD/src/variables/CIncNSVariable.cpp
+++ SU2_CFD/src/variables/CIncNSVariable.cpp
@@ -37,7 +37,9 @@ CIncNSVariable::CIncNSVariable(su2double pressure, const su2double *velocity, su
   DES_LengthScale.resize(nPoint) = su2double(0.0);
   Max_Lambda_Visc.resize(nPoint);
 
-  /*--- Allocate memory for the AuxVar+gradient of eddy viscosity mu_t ---*/
+  /*--- Allocate memory for the AuxVar and its gradient. See e.g. CIncEulerSolver::Source_Residual:
+   * Axisymmetric: total-viscosity * y-vel / y-coord
+   * Streamwise Periodic: eddy viscosity (mu_t) ---*/
   if (config->GetAxisymmetric() ||
       (config->GetStreamwise_Periodic_Temperature() && (config->GetKind_Turb_Model() != NONE))) {
     nAuxVar = 1;
