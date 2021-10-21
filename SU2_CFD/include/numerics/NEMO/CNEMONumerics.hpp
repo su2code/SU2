770ebf3dd4eda055d57cd24424f281e5dd5233ba
WallyMaier <wallytmaier@gmail.com>
2021-10-20 21:49:15 -0700

    more fixes

--- SU2_CFD/include/numerics/NEMO/CNEMONumerics.hpp
+++ SU2_CFD/include/numerics/NEMO/CNEMONumerics.hpp
@@ -156,19 +156,19 @@ public:
    * \param[out] val_Proj_Jac_Tensor_j - Pointer to the projected viscous Jacobian at point j.
    */
   template <unsigned short NVAR, unsigned short NSPECIES>
-  void CNEMONumerics::GetViscousProjJacs(su2double *val_Mean_PrimVar,
-                                         su2double **val_Mean_GradPrimVar,
-                                         su2double *val_Mean_Eve,
-                                         su2double *val_Mean_Cvve,
-                                         su2double *val_diffusion_coeff,
-                                         su2double val_laminar_viscosity,
-                                         su2double val_eddy_viscosity,
-                                         su2double val_thermal_conductivity,
-                                         su2double val_thermal_conductivity_ve,
-                                         su2double val_dist_ij, su2double *val_normal,
-                                         su2double val_dS, su2double *val_Fv,
-                                         su2double **val_Jac_i, su2double **val_Jac_j,
-                                         const CConfig *config){
+  void GetViscousProjJacs(su2double *val_Mean_PrimVar,
+                          su2double **val_Mean_GradPrimVar,
+                          su2double *val_Mean_Eve,
+                          su2double *val_Mean_Cvve,
+                          su2double *val_diffusion_coeff,
+                          su2double val_laminar_viscosity,
+                          su2double val_eddy_viscosity,
+                          su2double val_thermal_conductivity,
+                          su2double val_thermal_conductivity_ve,
+                          su2double val_dist_ij, su2double *val_normal,
+                          su2double val_dS, su2double *val_Fv,
+                          su2double **val_Jac_i, su2double **val_Jac_j,
+                          const CConfig *config){
 
     /*--- Template matrix creation for code optimization ---*/
     template <int nRows, int nCols>
