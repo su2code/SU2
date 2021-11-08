996ee6a701aafa08dd17fd18cc571074cfd6b76e
jtneedels <jneedels@stanford.edu>
2021-11-07 17:20:59 -0800

    change limiter to use min val, need to still fix minmod
    Signed-off-by: jtneedels <jneedels@stanford.edu>

--- SU2_CFD/src/solvers/CNEMOEulerSolver.cpp
+++ SU2_CFD/src/solvers/CNEMOEulerSolver.cpp
@@ -483,13 +483,14 @@ void CNEMOEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver_con
   CNumerics* numerics = numerics_container[CONV_TERM];
 
   /*--- Static arrays for MUSCL reconstructed variables ---*/
-  su2double Primitive_i[MAXNVAR] = {0.0}, Primitive_j[MAXNVAR] = {0.0};
-  su2double Conserved_i[MAXNVAR] = {0.0}, Conserved_j[MAXNVAR] = {0.0};
-  su2double      dPdU_i[MAXNVAR] = {0.0},      dPdU_j[MAXNVAR] = {0.0};
-  su2double      dTdU_i[MAXNVAR] = {0.0},      dTdU_j[MAXNVAR] = {0.0};
-  su2double    dTvedU_i[MAXNVAR] = {0.0},    dTvedU_j[MAXNVAR] = {0.0};
-  su2double       Eve_i[MAXNVAR] = {0.0},       Eve_j[MAXNVAR] = {0.0};
-  su2double      Cvve_i[MAXNVAR] = {0.0},      Cvve_j[MAXNVAR] = {0.0};
+  su2double     Primitive_i[MAXNVAR] = {0.0},    Primitive_j[MAXNVAR] = {0.0};
+  su2double     Conserved_i[MAXNVAR] = {0.0},    Conserved_j[MAXNVAR] = {0.0};
+  su2double          dPdU_i[MAXNVAR] = {0.0},         dPdU_j[MAXNVAR] = {0.0};
+  su2double          dTdU_i[MAXNVAR] = {0.0},         dTdU_j[MAXNVAR] = {0.0};
+  su2double        dTvedU_i[MAXNVAR] = {0.0},       dTvedU_j[MAXNVAR] = {0.0};
+  su2double           Eve_i[MAXNVAR] = {0.0},          Eve_j[MAXNVAR] = {0.0};
+  su2double          Cvve_i[MAXNVAR] = {0.0},         Cvve_j[MAXNVAR] = {0.0};
+  su2double  Project_Grad_i[MAXNVAR] = {0.0}, Project_Grad_j[MAXNVAR] = {0.0};
   su2double Gamma_i = 0.0, Gamma_j = 0.0;
 
   /*--- Loop over edges and calculate convective fluxes ---*/
@@ -534,32 +535,65 @@ void CNEMOEulerSolver::Upwind_Residual(CGeometry *geometry, CSolver **solver_con
       auto Gradient_i = nodes->GetGradient_Reconstruction(iPoint);
       auto Gradient_j = nodes->GetGradient_Reconstruction(jPoint);
 
-      for (iVar = 0; iVar < nPrimVarGrad; iVar++) {
+      /*--- Set and extract limiters ---*/
+      su2double *Limiter_i = nullptr, *Limiter_j = nullptr;
 
-        su2double Project_Grad_i = 0.0;
-        su2double Project_Grad_j = 0.0;
+      if (limiter){
+        Limiter_i = nodes->GetLimiter_Primitive(iPoint);
+        Limiter_j = nodes->GetLimiter_Primitive(jPoint);
+      }
+
+      su2double lim_i = 2.0;
+      su2double lim_j = 2.0;
+
+      for (iVar = 0; iVar < nPrimVarGrad; iVar++) {
+        Project_Grad_i[iVar] = 0.0; Project_Grad_j[iVar] = 0.0;
 
         for (iDim = 0; iDim < nDim; iDim++) {
-          Project_Grad_i += Vector_ij[iDim]*Gradient_i[iVar][iDim];
-          Project_Grad_j -= Vector_ij[iDim]*Gradient_j[iVar][iDim];
+          Project_Grad_i[iVar] += Vector_ij[iDim]*Gradient_i[iVar][iDim];
+          Project_Grad_j[iVar] -= Vector_ij[iDim]*Gradient_j[iVar][iDim];
         }
 
-        su2double lim_i = 1.0;
-        su2double lim_j = 1.0;
+        if (limiter) {
+          if (van_albada) {
+            su2double V_ij = V_j[iVar] - V_i[iVar];
+            Limiter_i[iVar] = V_ij*( 2.0*Project_Grad_i[iVar] + V_ij) / (4*pow(Project_Grad_i[iVar], 2) + pow(V_ij, 2) + EPS);
+            Limiter_j[iVar] = V_ij*(-2.0*Project_Grad_j[iVar] + V_ij) / (4*pow(Project_Grad_j[iVar], 2) + pow(V_ij, 2) + EPS);
+          }
+          //else if (minmod) {
+          //  su2double r_i=Project_Grad_j[iVar]/Project_Grad_i[iVar];
+          //  su2double r_j=Project_Grad_i[iVar]/Project_Grad_j[iVar];
 
-        if (van_albada) {
-          su2double V_ij = V_j[iVar] - V_i[iVar];
-          lim_i = LimiterHelpers<>::vanAlbadaFunction(Project_Grad_i, V_ij, EPS);
-          lim_j = LimiterHelpers<>::vanAlbadaFunction(-Project_Grad_j, V_ij, EPS);
-        }
-        else if (limiter) {
-          lim_i = nodes->GetLimiter_Primitive(iPoint, iVar);
-          lim_j = nodes->GetLimiter_Primitive(jPoint, iVar);
-        }
-        su2double lim_ij = min(lim_i, lim_j);
+          //  if(r_i >= 1)               {Limiter_i[iVar] = 1;  }
+          //  else if(r_i >= 0 && r_i<1) {Limiter_i[iVar] = r_i;}
+          //  else                       {Limiter_i[iVar] = 0;  }
 
-        Primitive_i[iVar] = V_i[iVar] + lim_ij*Project_Grad_i;
-        Primitive_j[iVar] = V_j[iVar] + lim_ij*Project_Grad_j;
+          //  if(r_j >= 1)               {Limiter_j[iVar] = 1;  }
+          //  else if(r_j >= 0 && r_j<1) {Limiter_j[iVar] = r_j;}
+          //  else                       {Limiter_j[iVar] = 0;  }
+
+          //}
+          if (lim_i > Limiter_i[iVar] && Limiter_i[iVar] != 0) lim_i = Limiter_i[iVar];
+          if (lim_j > Limiter_j[iVar] && Limiter_j[iVar] != 0) lim_j = Limiter_j[iVar];
+          }
+        else {
+          Primitive_i[iVar] = V_i[iVar] + Project_Grad_i[iVar];
+          Primitive_j[iVar] = V_j[iVar] + Project_Grad_j[iVar];
+        }
+      }
+      su2double lim_ij = min(lim_i, lim_j);
+
+      for (iVar = 0; iVar < nPrimVarGrad; iVar++) {
+        if (limiter){
+          //if (minmod){
+          //  Primitive_i[iVar] = V_i[iVar] + Limiter_i[iVar]*Project_Grad_i[iVar];
+          //  Primitive_j[iVar] = V_j[iVar] + Limiter_j[iVar]*Project_Grad_j[iVar];
+          //}
+          //else{
+            Primitive_i[iVar] = V_i[iVar] + lim_ij*Project_Grad_i[iVar];
+            Primitive_j[iVar] = V_j[iVar] + lim_ij*Project_Grad_j[iVar];
+          //}
+        }
       }
 
       /*--- Check for non-physical solutions after reconstruction. If found, use the
