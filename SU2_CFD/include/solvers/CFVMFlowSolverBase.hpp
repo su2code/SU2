su2code/SU2

commit aab916ff665613ef3e8f903b1e86213c04aa59cb

Move residual norm calculation helper to CSolver
and use it from other locations in the code as well.

SU2_CFD/include/solvers/CFVMFlowSolverBase.hpp
@@ -775,13 +775,8 @@ class CFVMFlowSolverBase : public CSolver {
     const su2double RK_FuncCoeff[] = {1.0/6.0, 1.0/3.0, 1.0/3.0, 1.0/6.0};
     const su2double RK_TimeCoeff[] = {0.5, 0.5, 1.0, 1.0};
 
-    /*--- Set shared residual variables to 0 and declare
-     *    local ones for current thread to work on. ---*/
-
-    SetResToZero();
-
+    /*--- Local residual variables for current thread ---*/
     su2double resMax[MAXNVAR] = {0.0}, resRMS[MAXNVAR] = {0.0};
-    const su2double* coordMax[MAXNVAR] = {nullptr};
     unsigned long idxMax[MAXNVAR] = {0};
 
     /*--- Update the solution and residuals ---*/
@@ -832,23 +827,13 @@ class CFVMFlowSolverBase : public CSolver {
           }
 
           /*--- Update residual information for current thread. ---*/
-          resRMS[iVar] += Res*Res;
-          if (fabs(Res) > resMax[iVar]) {
-            resMax[iVar] = fabs(Res);
-            idxMax[iVar] = iPoint;
-            coordMax[iVar] = geometry->nodes->GetCoord(iPoint);
-          }
+          ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
         }
       }
       END_SU2_OMP_FOR
       /*--- Reduce residual information over all threads in this rank. ---*/
-      SU2_OMP_CRITICAL
-      for (unsigned short iVar = 0; iVar < nVar; iVar++) {
-        Residual_RMS[iVar] += resRMS[iVar];
-        AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), coordMax[iVar]);
-      }
-      END_SU2_OMP_CRITICAL
-      SU2_OMP_BARRIER
+      ResidualReductions_FromAllThreads(geometry, config, resRMS,resMax,idxMax);
+
     }
 
     /*--- MPI solution ---*/
@@ -857,9 +842,6 @@ class CFVMFlowSolverBase : public CSolver {
     CompleteComms(geometry, config, SOLUTION);
 
     if (!adjoint) {
-      /*--- Compute the root mean square residual ---*/
-      SetResidual_RMS(geometry, config);
-
       /*--- For verification cases, compute the global error metrics. ---*/
       ComputeVerificationError(geometry, config);
     }
@@ -893,12 +875,8 @@ class CFVMFlowSolverBase : public CSolver {
 
     const bool implicit = (config->GetKind_TimeIntScheme_Flow() == EULER_IMPLICIT);
 
-    /*--- Set shared residual variables to 0 and declare local ones for current thread to work on. ---*/
-
-    SetResToZero();
-
+    /*--- Local residual variables for current thread ---*/
     su2double resMax[MAXNVAR] = {0.0}, resRMS[MAXNVAR] = {0.0};
-    const su2double* coordMax[MAXNVAR] = {nullptr};
     unsigned long idxMax[MAXNVAR] = {0};
 
     /*--- Add pseudotime term to Jacobian. ---*/
@@ -948,26 +926,15 @@ class CFVMFlowSolverBase : public CSolver {
         LinSysRes[total_index] = - (LinSysRes[total_index] + local_Res_TruncError[iVar]);
         LinSysSol[total_index] = 0.0;
 
-        su2double Res = fabs(LinSysRes[total_index]);
-        resRMS[iVar] += Res*Res;
-        if (Res > resMax[iVar]) {
-          resMax[iVar] = Res;
-          idxMax[iVar] = iPoint;
-          coordMax[iVar] = geometry->nodes->GetCoord(iPoint);
-        }
+        /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
+        ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
       }
     }
     END_SU2_OMP_FOR
-    SU2_OMP_CRITICAL
-    for (unsigned short iVar = 0; iVar < nVar; iVar++) {
-      Residual_RMS[iVar] += resRMS[iVar];
-      AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), coordMax[iVar]);
-    }
-    END_SU2_OMP_CRITICAL
-    SU2_OMP_BARRIER
 
-    /*--- Compute the root mean square residual ---*/
-    SetResidual_RMS(geometry, config);
+    /*--- "Add" residuals from all threads to global residual variables. ---*/
+    ResidualReductions_FromAllThreads(geometry, config, resRMS,resMax,idxMax);
+
   }
 
   /*!

SU2_CFD/include/solvers/CScalarSolver.inl
@@ -391,32 +391,6 @@ void CScalarSolver<VariableType>::ImplicitEuler_Iteration(CGeometry* geometry, C
   CompleteImplicitIteration(geometry, solver_container, config);
 }
 
-template<class VariableType>
-void CScalarSolver<VariableType>::ResidualReductions_PerThread(unsigned long iPoint, unsigned short iVar, su2double* resRMS, su2double* resMax, unsigned long* idxMax) const {
-  su2double Res = fabs(LinSysRes(iPoint,iVar));
-  resRMS[iVar] += Res * Res;
-  if (Res > resMax[iVar]) {
-    resMax[iVar] = Res;
-    idxMax[iVar] = iPoint;
-  }
-}
-
-template<class VariableType>
-void CScalarSolver<VariableType>::ResidualReductions_FromAllThreads(const CGeometry* geometry, const CConfig* config, const su2double* resRMS, const su2double* resMax, const unsigned long* idxMax) {
-  SetResToZero();
-
-  SU2_OMP_CRITICAL
-  for (unsigned short iVar = 0; iVar < nVar; iVar++) {
-    Residual_RMS[iVar] += resRMS[iVar];
-    AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), geometry->nodes->GetCoord(idxMax[iVar]));
-  }
-  END_SU2_OMP_CRITICAL
-  SU2_OMP_BARRIER
-
-  /*--- Compute the root mean square residual ---*/
-  SetResidual_RMS(geometry, config);
-}
-
 template <class VariableType>
 void CScalarSolver<VariableType>::ExplicitEuler_Iteration(CGeometry* geometry, CSolver** solver_container,
                                                        CConfig* config) {

SU2_CFD/include/solvers/CSolver.hpp
@@ -437,6 +437,39 @@ public:
                                            unsigned short iMesh,
                                            unsigned short RunTime_EqSystem) { }
 
+  /*! "Add" residual at (iPoint,iVar) to residual variables local to the thread.
+   *  \param[in] iPoint - Point index.
+   *  \param[in] iVar - Variable index.
+   *  \param[in,out] resRMS - increases by pow(Residual, 2)
+   *  \param[in,out] resMax - increases to max(resMax, Residual)
+   *  \param[in,out] idxMax - changes when resMax increases
+   */
+  inline void ResidualReductions_PerThread(unsigned long iPoint, unsigned short iVar, su2double* resRMS, su2double* resMax, unsigned long* idxMax) const {
+    su2double Res = fabs(LinSysRes(iPoint,iVar));
+    resRMS[iVar] += Res * Res;
+    if (Res > resMax[iVar]) {
+      resMax[iVar] = Res;
+      idxMax[iVar] = iPoint;
+    }
+  }
+
+  /*! "Add" local residual variables of all threads to compute global residual variables.
+   */
+  inline void ResidualReductions_FromAllThreads(const CGeometry* geometry, const CConfig* config, const su2double* resRMS, const su2double* resMax, const unsigned long* idxMax){
+    SetResToZero();
+
+    SU2_OMP_CRITICAL
+    for (unsigned short iVar = 0; iVar < nVar; iVar++) {
+      Residual_RMS[iVar] += resRMS[iVar];
+      AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), geometry->nodes->GetCoord(idxMax[iVar]));
+    }
+    END_SU2_OMP_CRITICAL
+    SU2_OMP_BARRIER
+
+    /*--- Compute the root mean square residual ---*/
+    SetResidual_RMS(geometry, config);
+  }
+
   /*!
    * \brief Set the RMS and MAX residual to zero.
    */

SU2_CFD/src/solvers/CDiscAdjSolver.cpp
@@ -305,9 +305,7 @@ void CDiscAdjSolver::ExtractAdjoint_Solution(CGeometry *geometry, CConfig *confi
   const su2double relax = (config->GetInnerIter()==0) ? 1.0 : config->GetRelaxation_Factor_Adjoint();
 
   /*--- Thread-local residual variables. ---*/
-
   su2double resMax[MAXNVAR] = {0.0}, resRMS[MAXNVAR] = {0.0};
-  const su2double* coordMax[MAXNVAR] = {nullptr};
   unsigned long idxMax[MAXNVAR] = {0};
 
   /*--- Set the old solution and compute residuals. ---*/
@@ -329,13 +327,8 @@ void CDiscAdjSolver::ExtractAdjoint_Solution(CGeometry *geometry, CConfig *confi
       nodes->AddSolution(iPoint, iVar, relax*residual);
 
       if (iPoint < nPointDomain) {
-        /*--- Update residual information for current thread. ---*/
-        resRMS[iVar] += residual*residual;
-        if (fabs(residual) > resMax[iVar]) {
-          resMax[iVar] = fabs(residual);
-          idxMax[iVar] = iPoint;
-          coordMax[iVar] = geometry->nodes->GetCoord(iPoint);
-        }
+        /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
+        ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
       }
     }
   }
@@ -344,18 +337,8 @@ void CDiscAdjSolver::ExtractAdjoint_Solution(CGeometry *geometry, CConfig *confi
   /*--- Residuals and time_n terms are not needed when evaluating multizone cross terms. ---*/
   if (CrossTerm) return;
 
-  SetResToZero();
-
-  /*--- Reduce residual information over all threads in this rank. ---*/
-  SU2_OMP_CRITICAL
-  for (auto iVar = 0u; iVar < nVar; iVar++) {
-    Residual_RMS[iVar] += resRMS[iVar];
-    AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), coordMax[iVar]);
-  }
-  END_SU2_OMP_CRITICAL
-  SU2_OMP_BARRIER
-
-  SetResidual_RMS(geometry, config);
+  /*--- "Add" residuals from all threads to global residual variables. ---*/
+  ResidualReductions_FromAllThreads(geometry, config, resRMS,resMax,idxMax);
 
   SU2_OMP_MASTER {
     SetIterLinSolver(direct_solver->System.GetIterations());

SU2_CFD/src/solvers/CFEASolver.cpp
@@ -1847,41 +1847,23 @@ void CFEASolver::Postprocessing(CGeometry *geometry, CConfig *config, CNumerics 
 
     const auto ResidualAux = computeLinearResidual(Jacobian, LinSysSol, LinSysRes);
 
-    /*--- Set maximum residual to zero. ---*/
-
-    SetResToZero();
-
     SU2_OMP_PARALLEL {
 
     /*--- Compute the residual. ---*/
-
     su2double resMax[MAXNVAR] = {0.0}, resRMS[MAXNVAR] = {0.0};
-    const su2double* coordMax[MAXNVAR] = {nullptr};
     unsigned long idxMax[MAXNVAR] = {0};
 
     SU2_OMP_FOR_STAT(omp_chunk_size)
     for (auto iPoint = 0ul; iPoint < nPointDomain; iPoint++) {
       for (auto iVar = 0ul; iVar < nVar; iVar++) {
-        su2double Res = fabs(ResidualAux(iPoint, iVar));
-        resRMS[iVar] += Res*Res;
-        if (Res > resMax[iVar]) {
-          resMax[iVar] = Res;
-          idxMax[iVar] = iPoint;
-          coordMax[iVar] = geometry->nodes->GetCoord(iPoint);
-        }
+        /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
+        ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
       }
     }
     END_SU2_OMP_FOR
-    SU2_OMP_CRITICAL
-    for (auto iVar = 0ul; iVar < nVar; iVar++) {
-      Residual_RMS[iVar] += resRMS[iVar];
-      AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), coordMax[iVar]);
-    }
-    END_SU2_OMP_CRITICAL
-    SU2_OMP_BARRIER
 
-    /*--- Compute the root mean square residual. ---*/
-    SetResidual_RMS(geometry, config);
+    /*--- "Add" residuals from all threads to global residual variables. ---*/
+    ResidualReductions_FromAllThreads(geometry, config, resRMS,resMax,idxMax);
 
     }
     END_SU2_OMP_PARALLEL

SU2_CFD/include/solvers/CScalarSolver.hpp
@@ -243,19 +243,6 @@ class CScalarSolver : public CSolver {
     END_SU2_OMP_FOR
   }
 
-  /*! "Add" residual at (iPoint,iVar) to residual variables local to the thread.
-   *  \param[in] iPoint - Point index.
-   *  \param[in] iVar - Variable index.
-   *  \param[in,out] resRMS - increases by pow(Residual, 2)
-   *  \param[in,out] resMax - increases to max(resMax, Residual)
-   *  \param[in,out] idxMax - changes when resMax increases
-   */
-  void ResidualReductions_PerThread(unsigned long iPoint, unsigned short iVar, su2double* resRMS, su2double* resMax, unsigned long* idxMax) const;
-
-  /*! "Add" local residual variables of all threads to compute global residual variables.
-   */
-  void ResidualReductions_FromAllThreads(const CGeometry* geometry, const CConfig* config, const su2double* resRMS, const su2double* resMax, const unsigned long* idxMax);
-
   /*!
    * \brief Prepare an implicit iteration.
    * \param[in] geometry - Geometrical definition of the problem.

commit b9eb7bb106e21d0dd10b3a90e126a20ed5fb6559

Fix error introduced in aab916ff
Sometimes the residual norm is computed from residuals other
than LinSysRes.

SU2_CFD/include/solvers/CFVMFlowSolverBase.hpp
@@ -827,7 +827,7 @@ class CFVMFlowSolverBase : public CSolver {
           }
 
           /*--- Update residual information for current thread. ---*/
-          ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
+          ResidualReductions_PerThread(iPoint,iVar,Res,resRMS,resMax,idxMax);
         }
       }
       END_SU2_OMP_FOR
@@ -927,7 +927,7 @@ class CFVMFlowSolverBase : public CSolver {
         LinSysSol[total_index] = 0.0;
 
         /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
-        ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
+        ResidualReductions_PerThread(iPoint,iVar,LinSysRes[total_index], resRMS,resMax,idxMax);
       }
     }
     END_SU2_OMP_FOR

SU2_CFD/include/solvers/CScalarSolver.inl
@@ -308,7 +308,7 @@ void CScalarSolver<VariableType>::PrepareImplicitIteration(CGeometry* geometry, 
       LinSysSol[total_index] = 0.0;
 
       /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
-      ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
+      ResidualReductions_PerThread(iPoint,iVar,LinSysRes[total_index],resRMS,resMax,idxMax);
     }
   }
   END_SU2_OMP_FOR
@@ -409,7 +409,7 @@ void CScalarSolver<VariableType>::ExplicitEuler_Iteration(CGeometry* geometry, C
 
     for(auto iVar=0u; iVar < nVar; iVar++){
       /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
-      ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
+      ResidualReductions_PerThread(iPoint,iVar,LinSysRes(iPoint, iVar),resRMS,resMax,idxMax);
       /*--- Explicit Euler step: ---*/
       LinSysSol(iPoint, iVar) = -dt/Vol * LinSysRes(iPoint, iVar);
     }

SU2_CFD/include/solvers/CSolver.hpp
@@ -440,15 +440,16 @@ public:
   /*! "Add" residual at (iPoint,iVar) to residual variables local to the thread.
    *  \param[in] iPoint - Point index.
    *  \param[in] iVar - Variable index.
+   *  \param[in] res - Residual at (iPoint,iVar), e.g. LinSysRes(iPoint,iVar)
    *  \param[in,out] resRMS - increases by pow(Residual, 2)
    *  \param[in,out] resMax - increases to max(resMax, Residual)
    *  \param[in,out] idxMax - changes when resMax increases
    */
-  inline void ResidualReductions_PerThread(unsigned long iPoint, unsigned short iVar, su2double* resRMS, su2double* resMax, unsigned long* idxMax) const {
-    su2double Res = fabs(LinSysRes(iPoint,iVar));
-    resRMS[iVar] += Res * Res;
-    if (Res > resMax[iVar]) {
-      resMax[iVar] = Res;
+  inline void ResidualReductions_PerThread(unsigned long iPoint, unsigned short iVar, su2double res, su2double* resRMS, su2double* resMax, unsigned long* idxMax) const {
+    res = fabs(res);
+    resRMS[iVar] += res * res;
+    if (res > resMax[iVar]) {
+      resMax[iVar] = res;
       idxMax[iVar] = iPoint;
     }
   }

SU2_CFD/src/solvers/CDiscAdjSolver.cpp
@@ -328,7 +328,7 @@ void CDiscAdjSolver::ExtractAdjoint_Solution(CGeometry *geometry, CConfig *confi
 
       if (iPoint < nPointDomain) {
         /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
-        ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
+        ResidualReductions_PerThread(iPoint,iVar,residual,resRMS,resMax,idxMax);
       }
     }
   }

SU2_CFD/src/solvers/CFEASolver.cpp
@@ -1857,7 +1857,7 @@ void CFEASolver::Postprocessing(CGeometry *geometry, CConfig *config, CNumerics 
     for (auto iPoint = 0ul; iPoint < nPointDomain; iPoint++) {
       for (auto iVar = 0ul; iVar < nVar; iVar++) {
         /*--- "Add" residual at (iPoint,iVar) to local residual variables. ---*/
-        ResidualReductions_PerThread(iPoint,iVar,resRMS,resMax,idxMax);
+        ResidualReductions_PerThread(iPoint,iVar,ResidualAux(iPoint, iVar),resRMS,resMax,idxMax);
       }
     }
     END_SU2_OMP_FOR