d20b2c5dc91253c3a6a62c9ce7f00c8641698e15
Max Aehle <max.aehle@scicomp.uni-kl.de>
2021-11-17 08:54:42 +0100

    Move ResidualReductions_* to protected part

--- SU2_CFD/include/solvers/CSolver.hpp
+++ SU2_CFD/include/solvers/CSolver.hpp
@@ -438,44 +438,6 @@ public:
                                            unsigned short RunTime_EqSystem) { }
 
   /*!
-   * \brief "Add" residual at (iPoint,iVar) to residual variables local to the thread.
-   *  \param[in] iPoint - Point index.
-   *  \param[in] iVar - Variable index.
-   *  \param[in] res - Residual at (iPoint,iVar), e.g. LinSysRes(iPoint,iVar)
-   *  \param[in,out] resRMS - increases by pow(Residual, 2)
-   *  \param[in,out] resMax - increases to max(resMax, Residual)
-   *  \param[in,out] idxMax - changes when resMax increases
-   */
-  static inline void ResidualReductions_PerThread(unsigned long iPoint, unsigned short iVar, su2double res, su2double* resRMS, su2double* resMax,
-                                                  unsigned long* idxMax) {
-    res = fabs(res);
-    resRMS[iVar] += res * res;
-    if (res > resMax[iVar]) {
-      resMax[iVar] = res;
-      idxMax[iVar] = iPoint;
-    }
-  }
-
-  /*!
-   * \brief "Add" local residual variables of all threads to compute global residual variables.
-   */
-  inline void ResidualReductions_FromAllThreads(const CGeometry* geometry, const CConfig* config, const su2double* resRMS, const su2double* resMax,
-                                                const unsigned long* idxMax){
-    SetResToZero();
-
-    SU2_OMP_CRITICAL
-    for (unsigned short iVar = 0; iVar < nVar; iVar++) {
-      Residual_RMS[iVar] += resRMS[iVar];
-      AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), geometry->nodes->GetCoord(idxMax[iVar]));
-    }
-    END_SU2_OMP_CRITICAL
-    SU2_OMP_BARRIER
-
-    /*--- Compute the root mean square residual ---*/
-    SetResidual_RMS(geometry, config);
-  }
-
-  /*!
    * \brief Set the RMS and MAX residual to zero.
    */
   inline void SetResToZero() {
@@ -4366,4 +4328,43 @@ protected:
   void SetVerificationSolution(unsigned short nDim,
                                unsigned short nVar,
                                CConfig        *config);
+
+  /*!
+   * \brief "Add" residual at (iPoint,iVar) to residual variables local to the thread.
+   *  \param[in] iPoint - Point index.
+   *  \param[in] iVar - Variable index.
+   *  \param[in] res - Residual at (iPoint,iVar), e.g. LinSysRes(iPoint,iVar)
+   *  \param[in,out] resRMS - increases by pow(Residual, 2)
+   *  \param[in,out] resMax - increases to max(resMax, Residual)
+   *  \param[in,out] idxMax - changes when resMax increases
+   */
+  static inline void ResidualReductions_PerThread(unsigned long iPoint, unsigned short iVar, su2double res, su2double* resRMS, su2double* resMax,
+                                                  unsigned long* idxMax) {
+    res = fabs(res);
+    resRMS[iVar] += res * res;
+    if (res > resMax[iVar]) {
+      resMax[iVar] = res;
+      idxMax[iVar] = iPoint;
+    }
+  }
+
+  /*!
+   * \brief "Add" local residual variables of all threads to compute global residual variables.
+   */
+  inline void ResidualReductions_FromAllThreads(const CGeometry* geometry, const CConfig* config, const su2double* resRMS, const su2double* resMax,
+                                                const unsigned long* idxMax){
+    SetResToZero();
+
+    SU2_OMP_CRITICAL
+    for (unsigned short iVar = 0; iVar < nVar; iVar++) {
+      Residual_RMS[iVar] += resRMS[iVar];
+      AddRes_Max(iVar, resMax[iVar], geometry->nodes->GetGlobalIndex(idxMax[iVar]), geometry->nodes->GetCoord(idxMax[iVar]));
+    }
+    END_SU2_OMP_CRITICAL
+    SU2_OMP_BARRIER
+
+    /*--- Compute the root mean square residual ---*/
+    SetResidual_RMS(geometry, config);
+  }
+
 };
