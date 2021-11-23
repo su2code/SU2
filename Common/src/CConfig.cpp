54726c06f589b05e5c92f11b02c287a843d60e30
TobiKattmann <Tobias.Kattmann@de.bosch.com>
2021-11-23 17:13:46 +0100

    Little Muscl_species addition in CConfig

--- Common/src/CConfig.cpp
+++ Common/src/CConfig.cpp
@@ -5060,7 +5060,7 @@ void CConfig::SetPostprocessing(SU2_COMPONENT val_software, unsigned short val_i
    gradients for uwpind reconstruction. Set additional booleans to
    minimize overhead as appropriate. */
 
-  if (MUSCL_Flow || MUSCL_Turb || MUSCL_Heat || MUSCL_AdjFlow) {
+  if (MUSCL_Flow || MUSCL_Turb || MUSCL_Species || MUSCL_Heat || MUSCL_AdjFlow) {
 
     ReconstructionGradientRequired = true;
 
