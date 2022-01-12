ca06c4e22b6aab165a2832ca1fcb58115a9755cf
Nijso <nijso@hotmail.com>
2022-01-12 22:17:48 +0100

    Update Common/src/CConfig.cpp
    Co-authored-by: Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>

--- Common/src/CConfig.cpp
+++ Common/src/CConfig.cpp
@@ -8101,17 +8101,10 @@ string CConfig::GetFilename(string filename, string ext, int timeIter) const {
 }
 
 string CConfig::GetFilename_Iter(string filename_iter, unsigned long curInnerIter, unsigned long curOuterIter) const {
-
-  std::stringstream inner_iter_ss;
-  inner_iter_ss << "_" << std::setw(8) << std::setfill('0') << curInnerIter;
-  std::stringstream outer_iter_ss;
-  outer_iter_ss << "_" << std::setw(8) << std::setfill('0') << curOuterIter;
-
-  if (GetMultizone_Problem())
-    filename_iter.append(outer_iter_ss.str());
-  else
-    filename_iter.append(inner_iter_ss.str());
-  return filename_iter;
+  const auto iter = GetMultizone_Problem() ? curOuterIter : curInnerIter;
+  std::stringstream iter_ss;
+  iter_ss << filename_iter << "_" << std::setw(6) << std::setfill('0') << iter;
+  return iter_ss.str();
 }
 
 string CConfig::GetUnsteady_FileName(string val_filename, int val_iter, string ext) const {
