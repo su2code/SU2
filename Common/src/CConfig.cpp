75746ac3e3a1ab6049a87ca9f0ac9f71c7eab671
TobiKattmann <tobias.kattmann@gmail.com>
2022-02-02 13:13:53 +0100

    Additional warning for mutlizone, if DV_MARKERs not found.
    Co-authored-by: Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>

--- Common/src/CConfig.cpp
+++ Common/src/CConfig.cpp
@@ -5733,11 +5733,16 @@ void CConfig::SetMarkers(SU2_COMPONENT val_software) {
         break;
       }
     }
-    // In case of multiple zones, the markers might appear only in zonal config and not in the Master.
-    // A loop over all zones would need to be included which is not straight forward as this can only be
-    // checked once all zonal configs are read.
-    if(!found && (nZone==1)) {
-      SU2_MPI::Error("DV_MARKER contains marker names that do not exist in the lists of BCs in the config file.", CURRENT_FUNCTION);
+    
+    if(!found) {
+      if (nZone==1)
+        SU2_MPI::Error("DV_MARKER contains marker names that do not exist in the lists of BCs in the config file.", CURRENT_FUNCTION);
+      // In case of multiple zones, the markers might appear only in zonal config and not in the Master.
+      // A loop over all zones would need to be included which is not straight forward as this can only be
+      // checked once all zonal configs are read.
+      else
+        cout << "Warning: DV_MARKER contains marker names that do not exist in the lists of BCs of the master config file.\n" <<
+                "Make sure the marker names exist in the zonal config files" << endl;
     }
   }
 
