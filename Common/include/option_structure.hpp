d8ca557889ca18638606604e5e0831c66a13bc22
WallyMaier <wallytmaier@gmail.com>
2022-03-24 10:21:02 -0700

    small fixes

--- Common/include/option_structure.hpp
+++ Common/include/option_structure.hpp
@@ -969,7 +969,7 @@ enum class SST_OPTIONS {
   UNCERTAINTY, /*!< \brief Menter k-w SST model with uncertainty quantification modifications. */
   RC,          /*!< \brief Menter k-w SST model with rotation/curvature corrections. */
 };
-static const MapType<std::string, SST_OPTION> SST_Options_Map = {
+static const MapType<std::string, SST_OPTIONS> SST_Options_Map = {
   MakePair("NONE", SST_OPTIONS::NONE)
   MakePair("V1994", SST_OPTIONS::V1994)
   MakePair("V2003", SST_OPTIONS::V2003)
@@ -985,14 +985,13 @@ static const MapType<std::string, SST_OPTION> SST_Options_Map = {
  * \brief Structure containing parsed SST options.
  */
 struct SST_ParsedOptions {
-  string sst_string = "Menter's k-w SST"; /*!< \brief Initial string for SST problems. */
+  std::string sst_string = "Menter's k-w SST"; /*!< \brief Initial string for SST problems. */
 
   SST_OPTIONS version;       /*!< \brief Enum SST base model. */
   SST_OPTIONS production;    /*!< \brief Enum for production corrections/modifiers for SST model. */
   bool sust;                 /*!< \brief Bool for SST model with sustaining terms. */
   bool rc;                   /*!< \brief Bool for SST model with rotational corrections. */
   bool m;                    /*!< \brief Bool for modified (m) SST model. */
-
 }
 
 /*!
