1640f2a2cc1c35251adea03339a1aa849dc27dcd
bigfooted <bigfootedrockmidget@hotmail.com>
2022-04-11 09:26:22 +0200

    turbsources modification

--- SU2_CFD/include/numerics/turbulent/turb_sources.hpp
+++ SU2_CFD/include/numerics/turbulent/turb_sources.hpp
@@ -749,7 +749,8 @@ class CSourcePieceWise_TurbSST final : public CNumerics {
 
 
 
-      su2double pk = Eddy_Viscosity_i * pow(StrainMag, 2) - 2.0 / 3.0 * Density_i * ScalarVar_i[0] * diverg;
+      su2double pk = Eddy_Viscosity_i * pow(P_Base, 2) - 2.0 / 3.0 * Density_i * ScalarVar_i[0] * diverg;
+      pk = Eddy_Viscosity_i * pow(StrainMag, 2) - 2.0 / 3.0 * Density_i * ScalarVar_i[0] * diverg;
       pk = max(0.0, min(pk, 20.0 * beta_star * Density_i * ScalarVar_i[1] * ScalarVar_i[0]));
 
       const su2double zeta = max(ScalarVar_i[1], VorticityMag * F2_i / a1);
