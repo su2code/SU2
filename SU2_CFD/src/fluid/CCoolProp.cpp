bfed1ecbfc67765fe43dd121d0b41bce063350cb
Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>
2022-10-21 18:26:48 -0700

    Update SU2_CFD/src/fluid/CCoolProp.cpp

--- SU2_CFD/src/fluid/CCoolProp.cpp
+++ SU2_CFD/src/fluid/CCoolProp.cpp
@@ -29,74 +29,74 @@
 #ifdef USE_COOLPROP
 
 CCoolProp::CCoolProp(string fluidname) : CFluidModel() {
-    fluid_entity = std::unique_ptr<CoolProp::AbstractState>(CoolProp::AbstractState::factory("HEOS",fluidname));
-    Gas_Constant = fluid_entity->gas_constant()/fluid_entity->molar_mass();
-    Pressure_Critical = fluid_entity->p_critical();
-    Temperature_Critical = fluid_entity->T_critical();
+  fluid_entity = std::unique_ptr<CoolProp::AbstractState>(CoolProp::AbstractState::factory("HEOS",fluidname));
+  Gas_Constant = fluid_entity->gas_constant()/fluid_entity->molar_mass();
+  Pressure_Critical = fluid_entity->p_critical();
+  Temperature_Critical = fluid_entity->T_critical();
 }
 
 void CCoolProp::SetTDState_rhoe(su2double rho, su2double e) {
-    Density = rho;
-    StaticEnergy = e;
-    fluid_entity->update(CoolProp::DmassUmass_INPUTS, Density , StaticEnergy);
-    Cp = fluid_entity->cpmass();
-    Cv = fluid_entity->cvmass();
-    Gamma = Cp/Cv;
-    Pressure = fluid_entity->p();
-    Temperature = fluid_entity->T();
-    SoundSpeed2 = pow(fluid_entity->speed_sound(), 2);
-    Entropy = fluid_entity->smass();
-    dPdrho_e = fluid_entity->first_partial_deriv(CoolProp::iP,CoolProp::iDmass,CoolProp::iUmass);
-    dPde_rho = fluid_entity->first_partial_deriv(CoolProp::iP,CoolProp::iUmass,CoolProp::iDmass);
-    dTdrho_e = fluid_entity->first_partial_deriv(CoolProp::iT,CoolProp::iDmass,CoolProp::iUmass);
-    dTde_rho = fluid_entity->first_partial_deriv(CoolProp::iT,CoolProp::iUmass,CoolProp::iDmass);
+  Density = rho;
+  StaticEnergy = e;
+  fluid_entity->update(CoolProp::DmassUmass_INPUTS, Density , StaticEnergy);
+  Cp = fluid_entity->cpmass();
+  Cv = fluid_entity->cvmass();
+  Gamma = Cp/Cv;
+  Pressure = fluid_entity->p();
+  Temperature = fluid_entity->T();
+  SoundSpeed2 = pow(fluid_entity->speed_sound(), 2);
+  Entropy = fluid_entity->smass();
+  dPdrho_e = fluid_entity->first_partial_deriv(CoolProp::iP,CoolProp::iDmass,CoolProp::iUmass);
+  dPde_rho = fluid_entity->first_partial_deriv(CoolProp::iP,CoolProp::iUmass,CoolProp::iDmass);
+  dTdrho_e = fluid_entity->first_partial_deriv(CoolProp::iT,CoolProp::iDmass,CoolProp::iUmass);
+  dTde_rho = fluid_entity->first_partial_deriv(CoolProp::iT,CoolProp::iUmass,CoolProp::iDmass);
 }
 
 void CCoolProp::SetTDState_PT(su2double P, su2double T) {
-    fluid_entity->update(CoolProp::PT_INPUTS,P,T);
-    su2double  rho = fluid_entity->rhomass();
-    su2double e    = fluid_entity->umass();
-    SetTDState_rhoe(rho, e);
+  fluid_entity->update(CoolProp::PT_INPUTS,P,T);
+  su2double  rho = fluid_entity->rhomass();
+  su2double e    = fluid_entity->umass();
+  SetTDState_rhoe(rho, e);
 }
 
 void CCoolProp::SetTDState_Prho(su2double P, su2double rho) {
-    fluid_entity->update(CoolProp::DmassP_INPUTS, rho, P);
-    su2double e = fluid_entity->umass();
-    SetTDState_rhoe(rho, e);
+  fluid_entity->update(CoolProp::DmassP_INPUTS, rho, P);
+  su2double e = fluid_entity->umass();
+  SetTDState_rhoe(rho, e);
 }
 
 void CCoolProp::SetEnergy_Prho(su2double P, su2double rho) {
-    fluid_entity->update(CoolProp::DmassP_INPUTS, rho, P);
-    StaticEnergy  = fluid_entity->umass();
+  fluid_entity->update(CoolProp::DmassP_INPUTS, rho, P);
+  StaticEnergy  = fluid_entity->umass();
 }
 
 void CCoolProp::SetTDState_hs(su2double h, su2double s) {
-    fluid_entity->update(CoolProp::HmassSmass_INPUTS, h, s);
-    su2double  rho = fluid_entity->rhomass();
-    su2double e    = fluid_entity->umass();
-    SetTDState_rhoe(rho, e);
+  fluid_entity->update(CoolProp::HmassSmass_INPUTS, h, s);
+  su2double  rho = fluid_entity->rhomass();
+  su2double e    = fluid_entity->umass();
+  SetTDState_rhoe(rho, e);
 }
 
 void  CCoolProp::SetTDState_Ps(su2double P, su2double s) {
-    fluid_entity->update(CoolProp::PSmass_INPUTS, P , s);
-    su2double  Rho = fluid_entity->rhomass();
-    su2double e    = fluid_entity->umass();
-    SetTDState_rhoe(Rho, e);
+  fluid_entity->update(CoolProp::PSmass_INPUTS, P , s);
+  su2double  Rho = fluid_entity->rhomass();
+  su2double e    = fluid_entity->umass();
+  SetTDState_rhoe(Rho, e);
 }
 
 void CCoolProp::SetTDState_rhoT(su2double rho, su2double T) {
-    fluid_entity->update(CoolProp::DmassT_INPUTS, rho, T);
-    su2double  Rho = fluid_entity->rhomass();
-    su2double e    = fluid_entity->umass();
-    SetTDState_rhoe(Rho, e);
+  fluid_entity->update(CoolProp::DmassT_INPUTS, rho, T);
+  su2double  Rho = fluid_entity->rhomass();
+  su2double e    = fluid_entity->umass();
+  SetTDState_rhoe(Rho, e);
 }
 
 void  CCoolProp::ComputeDerivativeNRBC_Prho(su2double P, su2double rho) {
-    SetTDState_Prho(P, rho);
-    dhdrho_P = fluid_entity->first_partial_deriv(CoolProp::iHmass,CoolProp::iDmass,CoolProp::iP);
-    dhdP_rho = fluid_entity->first_partial_deriv(CoolProp::iHmass,CoolProp::iP,CoolProp::iDmass);
-    dsdP_rho = fluid_entity->first_partial_deriv(CoolProp::iSmass,CoolProp::iP,CoolProp::iDmass);
-    dsdrho_P = fluid_entity->first_partial_deriv(CoolProp::iSmass,CoolProp::iDmass,CoolProp::iP);
+  SetTDState_Prho(P, rho);
+  dhdrho_P = fluid_entity->first_partial_deriv(CoolProp::iHmass,CoolProp::iDmass,CoolProp::iP);
+  dhdP_rho = fluid_entity->first_partial_deriv(CoolProp::iHmass,CoolProp::iP,CoolProp::iDmass);
+  dsdP_rho = fluid_entity->first_partial_deriv(CoolProp::iSmass,CoolProp::iP,CoolProp::iDmass);
+  dsdrho_P = fluid_entity->first_partial_deriv(CoolProp::iSmass,CoolProp::iDmass,CoolProp::iP);
 }
 
 #else
