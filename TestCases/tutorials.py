8b442b43f2a37a864cd44ca59cd7a01703c190fb
Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>
2022-10-28 10:21:43 -0700

    Update TestCases/tutorials.py

--- TestCases/tutorials.py
+++ TestCases/tutorials.py
@@ -174,11 +174,8 @@ def main():
     tutorial_trans_flatplate_T3A            = TestCase('transitional_flatplate_tutorial_T3A')
     tutorial_trans_flatplate_T3A.cfg_dir    = "../Tutorials/compressible_flow/Transitional_Flat_Plate/Langtry_and_Menter/T3A"
     tutorial_trans_flatplate_T3A.cfg_file   = "transitional_LM_model_ConfigFile.cfg"
-    tutorial_trans_flatplate_T3A.test_iter  = 0
-    tutorial_trans_flatplate_T3A.test_vals  = [0.663915, 4.088903, -6.733057, -4.697968] #last 4 columns
-    tutorial_trans_flatplate_T3A.su2_exec   = "mpirun -np 2 SU2_CFD"
-    tutorial_trans_flatplate_T3A.timeout    = 1600
-    tutorial_trans_flatplate_T3A.tol        = 0.00001
+    tutorial_trans_flatplate_T3A.test_iter  = 20
+    tutorial_trans_flatplate_T3A.test_vals  = [0.663915, 4.088903, -6.733057, -4.697968]
     tutorial_trans_flatplate_T3A.no_restart = True
     test_list.append(tutorial_trans_flatplate_T3A)
     
@@ -186,11 +183,8 @@ def main():
     tutorial_trans_flatplate_T3Am            = TestCase('transitional_flatplate_tutorial_T3Am')
     tutorial_trans_flatplate_T3Am.cfg_dir    = "../Tutorials/compressible_flow/Transitional_Flat_Plate/Langtry_and_Menter/T3A-"
     tutorial_trans_flatplate_T3Am.cfg_file   = "transitional_LM_model_ConfigFile.cfg"
-    tutorial_trans_flatplate_T3Am.test_iter  = 0
-    tutorial_trans_flatplate_T3Am.test_vals  = [-1.955813, 4.655101, -6.548565, -3.782123] #last 4 columns
-    tutorial_trans_flatplate_T3Am.su2_exec   = "mpirun -np 2 SU2_CFD"
-    tutorial_trans_flatplate_T3Am.timeout    = 1600
-    tutorial_trans_flatplate_T3Am.tol        = 0.00001
+    tutorial_trans_flatplate_T3Am.test_iter  = 20
+    tutorial_trans_flatplate_T3Am.test_vals  = [-1.955813, 4.655101, -6.548565, -3.782123]
     tutorial_trans_flatplate_T3Am.no_restart = True
     test_list.append(tutorial_trans_flatplate_T3Am)
 
