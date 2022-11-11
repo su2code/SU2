ad44f6f67af27c9e5adb4d83a902f0155f2f0497
PENGYAN777 <peng.yan@mail.polimi.it>
2022-11-11 17:39:45 +0100

    suggestions from code review applied

--- SU2_CFD/src/fluid/CCoolProp.cpp
+++ SU2_CFD/src/fluid/CCoolProp.cpp
@@ -2,7 +2,7 @@
  * \file CCoolProp.cpp
  * \brief Source of the fluid model from CoolProp.
  * \author P. Yan, G. Gori, A. Guardone
- * \version 7.3.1 "Blackbird"
+ * \version 7.4.0 "Blackbird"
  *
  * SU2 Project Website: https://su2code.github.io
  *
