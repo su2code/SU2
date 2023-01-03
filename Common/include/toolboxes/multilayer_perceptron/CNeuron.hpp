cc29f494a2dc68de1220c9a61a21fa29431a55a2
EvertBunschoten <evertbun@xs4all.nl>
2023-01-03 16:45:37 +0100

    added clang formatting to multilayer-perceptron files

--- Common/include/toolboxes/multilayer_perceptron/CNeuron.hpp
+++ Common/include/toolboxes/multilayer_perceptron/CNeuron.hpp
@@ -27,91 +27,88 @@
 #pragma once
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
-#include <cstdlib>
 
 #include "../../CConfig.hpp"
 
-namespace MLPToolbox{
-class CNeuron
-{
-private:
-    unsigned long i_neuron;         /*!< Neuron identification number */
-    su2double output{0},            /*!< Output value of the current neuron */
-              input{0},             /*!< Input value of the current neuron */
-              doutput_dinput{0},    /*!< Gradient of output with respect to input */
-              bias{0};              /*!< Bias value at current neuron */
-    su2vector<su2double> doutput_dinputs;
+namespace MLPToolbox {
+class CNeuron {
+ private:
+  unsigned long i_neuron; /*!< Neuron identification number */
+  su2double output{0},    /*!< Output value of the current neuron */
+      input{0},           /*!< Input value of the current neuron */
+      doutput_dinput{0},  /*!< Gradient of output with respect to input */
+      bias{0};            /*!< Bias value at current neuron */
+  su2vector<su2double> doutput_dinputs;
 
-public:
+ public:
+  /*!
+   * \brief Set neuron identification number
+   * \param[in] input - Identification number
+   */
+  void setNumber(unsigned long input) { i_neuron = input; }
 
-    /*!
-    * \brief Set neuron identification number
-    * \param[in] input - Identification number
-    */
-    void setNumber(unsigned long input) { i_neuron = input; }
+  /*!
+   * \brief Get neuron identification number
+   * \return Identification number
+   */
+  unsigned long getNumber() const { return i_neuron; }
 
-    /*!
-    * \brief Get neuron identification number
-    * \return Identification number
-    */
-    unsigned long getNumber() const { return i_neuron; }
+  /*!
+   * \brief Set neuron output value
+   * \param[in] input - activation function output value
+   */
+  void setOutput(su2double input) { output = input; }
 
-    /*!
-    * \brief Set neuron output value
-    * \param[in] input - activation function output value
-    */
-    void setOutput(su2double input) { output = input; }
+  /*!
+   * \brief Get neuron output value
+   * \return Output value
+   */
+  su2double getOutput() const { return output; }
 
-    /*!
-    * \brief Get neuron output value
-    * \return Output value
-    */
-    su2double getOutput() const { return output; }
+  /*!
+   * \brief Set neuron input value
+   * \param[in] input - activation function input value
+   */
+  void setInput(su2double x) { input = x; }
 
-    /*!
-    * \brief Set neuron input value
-    * \param[in] input - activation function input value
-    */
-    void setInput(su2double x) { input = x; }
+  /*!
+   * \brief Get neuron input value
+   * \return input value
+   */
+  su2double getInput() const { return input; }
 
-    /*!
-    * \brief Get neuron input value
-    * \return input value
-    */
-    su2double getInput() const { return input; }
+  /*!
+   * \brief Set neuron bias
+   * \param[in] input - bias value
+   */
+  void setBias(su2double input) { bias = input; }
 
-    /*!
-    * \brief Set neuron bias
-    * \param[in] input - bias value
-    */
-    void setBias(su2double input) { bias = input; }
+  /*!
+   * \brief Get neuron bias value
+   * \return bias value
+   */
+  su2double getBias() const { return bias; }
 
-    /*!
-    * \brief Get neuron bias value
-    * \return bias value
-    */
-    su2double getBias() const { return bias; }
+  /*!
+   * \brief Size the derivative of the neuron output wrt MLP inputs.
+   * \param[in] nInputs - Number of MLP inputs.
+   */
+  void sizeGradient(std::size_t nInputs) { doutput_dinputs.resize(nInputs); }
 
-    /*!
-    * \brief Size the derivative of the neuron output wrt MLP inputs.
-    * \param[in] nInputs - Number of MLP inputs.
-    */
-    void sizeGradient(std::size_t nInputs) { doutput_dinputs.resize(nInputs); }
-    
-    /*!
-    * \brief Set neuron output gradient with respect to its input value
-    * \param[in] input - Derivative of activation function with respect to input
-    */
-    void setGradient(std::size_t iInput, su2double input) { doutput_dinputs[iInput] = input; }
+  /*!
+   * \brief Set neuron output gradient with respect to its input value
+   * \param[in] input - Derivative of activation function with respect to input
+   */
+  void setGradient(std::size_t iInput, su2double input) { doutput_dinputs[iInput] = input; }
 
-    /*!
-    * \brief Get neuron output gradient with respect to input value
-    * \return output gradient wrt input value
-    */
-    su2double getGradient(std::size_t iInput) const { return doutput_dinputs[iInput]; }
+  /*!
+   * \brief Get neuron output gradient with respect to input value
+   * \return output gradient wrt input value
+   */
+  su2double getGradient(std::size_t iInput) const { return doutput_dinputs[iInput]; }
 };
 
-}
-
+}  // namespace MLPToolbox
