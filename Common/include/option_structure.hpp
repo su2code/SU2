1ec1dfc3b9c0a758ecfdf65d698f391db0278903
jtneedels <jneedels@stanford.edu>
2023-01-06 19:53:42 -0800

    remove line from option_structure
    Signed-off-by: jtneedels <jneedels@stanford.edu>

--- Common/include/option_structure.hpp
+++ Common/include/option_structure.hpp
@@ -1542,7 +1542,6 @@ enum BC_TYPE {
   DAMPER_BOUNDARY = 41,       /*!< \brief Damper. */
   CHT_WALL_INTERFACE = 50,    /*!< \brief Domain interface definition. */
   SMOLUCHOWSKI_MAXWELL = 55,  /*!< \brief Smoluchoski/Maxwell wall boundary condition. */
-  NEMO_SUPERSONIC_INLET = 56, /*!< \brief NEMO supersonic inlet condition */
   SEND_RECEIVE = 99,          /*!< \brief Boundary send-receive definition. */
 };
 
