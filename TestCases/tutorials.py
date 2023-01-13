a5b5b039dd207f0f474f5c4db0d21ba7431c0ec6
Cristopher Morales <98025159+Cristopher-Morales@users.noreply.github.com>
2023-01-13 13:16:25 +0100

    adding tutorial for composition-dependent model to tutorials.py (#1886)
    * addint tutotial fluid_mixture model
    
    * tutorial branch
    
    * updating tutorials.py
    
    * --
    
    * updating residuals
    
    Co-authored-by: Wally Maier <wallytmaier@gmail.com>

--- TestCases/tutorials.py
+++ TestCases/tutorials.py
@@ -104,6 +104,16 @@ def main():
     DAspecies3_primitiveVenturi.command   = TestCase.Command("mpirun -n 2", "SU2_CFD_AD")
     DAspecies3_primitiveVenturi.new_output = True
     test_list.append(DAspecies3_primitiveVenturi)
+    
+    # 2 species (1 eq) kenics static mixer for composition-dependent model
+    kenics_mixer_tutorial           = TestCase('kenics_mixer_tutorial')
+    kenics_mixer_tutorial.cfg_dir   = "../Tutorials/incompressible_flow/Inc_Species_Transport_Composition_Dependent_Model"
+    kenics_mixer_tutorial.cfg_file  = "kenics_mixer_tutorial.cfg"
+    kenics_mixer_tutorial.test_iter = 10
+    kenics_mixer_tutorial.test_vals = [-7.489841, -6.823474, -6.838069, -5.157396, -7.902273, -3.174235, -7.448166, 5.000000, -1.862000, 4.000000, -5.173789, 3.000000, -6.373917, 0.025135, 0.000000, 0.025135, 0.000000, 64.095000, 8.479500, 48.089000, 7.526700]
+    kenics_mixer_tutorial.command   = TestCase.Command("mpirun -n 2", "SU2_CFD")
+    kenics_mixer_tutorial.new_output = True
+    test_list.append(kenics_mixer_tutorial)
 
     ### Compressible Flow
 
