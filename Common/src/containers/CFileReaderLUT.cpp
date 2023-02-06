69a432fe30e4d5ffd377e460381a73471e384e7b
EvertBunschoten <evertbun@xs4all.nl>
2023-02-06 14:08:13 +0100

    Fixed bug with table level checks for 3D tables

--- Common/src/containers/CFileReaderLUT.cpp
+++ Common/src/containers/CFileReaderLUT.cpp
@@ -128,16 +128,18 @@ void CFileReaderLUT::ReadRawLUT(const string& file_name) {
         GetNextNonEmptyLine(file_stream, line);
         table_levels[i_level] = stod(line);
       }
+
+      /*--- Checking whether table levels are ordered and appearance of duplicates ---*/
+      for (auto i_level = 0u; i_level < n_levels - 1; i_level++) {
+        if (table_levels[i_level] > table_levels[i_level + 1])
+          SU2_MPI::Error("Table levels are not provided in ascending order.", CURRENT_FUNCTION);
+      }
+      auto duplicate = adjacent_find(table_levels.begin(), table_levels.end()) != table_levels.end();
+      if (duplicate)
+        SU2_MPI::Error("Duplicate table levels are present in LUT file", CURRENT_FUNCTION);
     }
 
-    /*--- Checking whether table levels are ordered and appearance of duplicates ---*/
-    for (auto i_level = 0u; i_level < n_levels - 1; i_level++) {
-      if (table_levels[i_level] > table_levels[i_level + 1])
-        SU2_MPI::Error("Table levels are not provided in ascending order.", CURRENT_FUNCTION);
-    }
-    auto duplicate = adjacent_find(table_levels.begin(), table_levels.end()) != table_levels.end();
-    if (duplicate)
-      SU2_MPI::Error("Duplicate table levels are present in LUT file", CURRENT_FUNCTION);
+    
 
     /*--- number of variables in LUT ---*/
     if (line.compare("[Number of variables]") == 0) {
