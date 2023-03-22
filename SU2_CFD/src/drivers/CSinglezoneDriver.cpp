f3ffef46831a3324acf12e55bc411b1cddcb8143
bigfooted <nijso@hotmail.com>
2023-03-22 15:55:38 +0100

    fix solver issue

--- SU2_CFD/src/drivers/CSinglezoneDriver.cpp
+++ SU2_CFD/src/drivers/CSinglezoneDriver.cpp
@@ -135,7 +135,7 @@ void CSinglezoneDriver::Preprocess(unsigned long TimeIter) {
                                                                             solver_container[ZONE_0][INST_0],
                                                                             config_container[ZONE_0], TimeIter);
   }
-  else if (config_container[ZONE_0]->GetKind_Species_Model() != SPECIES_MODEL::NONE) {
+  if (config_container[ZONE_0]->GetKind_Species_Model() != SPECIES_MODEL::NONE) {
     solver_container[ZONE_0][INST_0][MESH_0][SPECIES_SOL]->SetInitialCondition(geometry_container[ZONE_0][INST_0],
                                                                                 solver_container[ZONE_0][INST_0],
                                                                                 config_container[ZONE_0], TimeIter);
