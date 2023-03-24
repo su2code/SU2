9ece118eb75231d3ba515e3dfec175706205a96a
Nijso <bigfootedrockmidget@hotmail.com>
2023-03-24 09:54:36 +0100

    Update SU2_CFD/src/fluid/CFluidFlamelet.cpp
    Co-authored-by: Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>

--- SU2_CFD/src/fluid/CFluidFlamelet.cpp
+++ SU2_CFD/src/fluid/CFluidFlamelet.cpp
@@ -162,7 +162,7 @@ void CFluidFlamelet::SetTDState_T(su2double val_temperature, const su2double* va
 
 /* --- Total enthalpy is the transported variable, but we usually have temperature as a boundary condition,
        so we do a reverse lookup */
-unsigned long CFluidFlamelet::GetEnthFromTemp(su2double* val_enth, const su2double val_prog, const su2double val_temp,
+unsigned long CFluidFlamelet::GetEnthFromTemp(su2double& val_enth, const su2double val_prog, const su2double val_temp,
                                               const su2double initial_value) {
   string name_prog = table_scalar_names[I_PROGVAR];
   string name_enth = table_scalar_names[I_ENTH];
