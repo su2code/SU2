f4feba9c2a7ae3c2fdb68f20fcbe998fe02f9388
Johannes Blühdorn <johannes.bluehdorn@scicomp.uni-kl.de>
2023-07-25 12:43:16 +0200

    Update bounds checking parameters.

--- Common/include/basic_types/ad_structure.hpp
+++ Common/include/basic_types/ad_structure.hpp
@@ -384,14 +384,14 @@ FORCEINLINE void SetDerivative(int index, const double val) {
   if (index == 0)  // Allow multiple threads to "set the derivative" of passive variables without causing data races.
     return;
 
-  AD::getTape().setGradient(index, val, codi::AdjointsBoundsChecking::False);
+  AD::getTape().setGradient(index, val, codi::AdjointsManagement::Manual);
 }
 
 // WARNING: For performance reasons, this method does not perform bounds checking.
 // If called after tape evaluations, the adjoints should exist.
 // Otherwise, please ensure sufficient adjoint vector size by a call to AD::ResizeAdjoints().
 FORCEINLINE double GetDerivative(int index) {
-  return AD::getTape().getGradient(index, codi::AdjointsBoundsChecking::False);
+  return AD::getTape().getGradient(index, codi::AdjointsManagement::Manual);
 }
 
 FORCEINLINE bool IsIdentifierActive(su2double const& value) {
