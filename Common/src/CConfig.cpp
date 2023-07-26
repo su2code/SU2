78f995c1d06dcece5cde5d2c0e5fc6c45fee60c6
EvertBunschoten <evertbun@xs4all.nl>
2023-07-26 10:39:15 +0200

    buf fix for spark initialization method

--- Common/src/CConfig.cpp
+++ Common/src/CConfig.cpp
@@ -1375,8 +1375,9 @@ void CConfig::SetConfig_Options() {
   /*!\brief SPARK_INIT \n DESCRIPTION: spark initialization using the flamelet model \ingroup Config*/
   for (auto iSpark=0u; iSpark<6; ++iSpark) spark_init[iSpark]=0;
   addDoubleArrayOption("SPARK_INIT", 6, spark_init);
+  unsigned short dummy;
   /*!\brief SPARK_REACTION_RATES \n DESCRIPTION: Net source term values applied to species within spark area during spark ignition. \ingroup Config*/
-  addDoubleListOption("SPARK_REACTION_RATES", nSpecies_Init, spark_reaction_rates);
+  addDoubleListOption("SPARK_REACTION_RATES", dummy, spark_reaction_rates);
 
   /*--- Options related to mass diffusivity and thereby the species solver. ---*/
 
