c48810a84d612b1a1ddf9848a6d3097d0a23e460
TripleRider <TripleRider@users.noreply.github.com>
2023-11-07 22:23:01 -0800

    Fix cpu arch returned value
    Modify TestCase.py related to #2159.

--- TestCases/TestCase.py
+++ TestCases/TestCase.py
@@ -111,8 +111,8 @@ class TestCase:
         self.ntest_vals = 4
         self.test_vals = []
         self.test_vals_aarch64 = []
-        self.cpu_arch = platform.processor()
-        self.enabled_on_cpu_arch = ["x86_64", "aarch64"]
+        self.cpu_arch = platform.machine().casefold()
+        self.enabled_on_cpu_arch = ["x86_64","amd64","aarch64","arm64"]
         self.enabled_with_tsan = True
         self.command = self.Command()
         self.timeout = 0
@@ -957,7 +957,7 @@ class TestCase:
 
     def adjust_test_data(self):
 
-        if self.cpu_arch == 'aarch64':
+        if self.cpu_arch == 'aarch64' or self.cpu_arch == 'arm64':
             if len(self.test_vals_aarch64) != 0:
                 self.test_vals = self.test_vals_aarch64
 
