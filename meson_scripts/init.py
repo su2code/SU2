be833010b0532aa9eadd8e6119d0e90917cfeb22
Johannes Blühdorn <johannes.bluehdorn@scicomp.uni-kl.de>
2024-02-01 14:57:16 +0100

    CoDiPack update.

--- meson_scripts/init.py
+++ meson_scripts/init.py
@@ -55,7 +55,7 @@ def init_submodules(
 
     # This information of the modules is used if projects was not cloned using git
     # The sha tag must be maintained manually to point to the correct commit
-    sha_version_codi = "0ad036f2c8254fa7b77180a443d99248c047c877"
+    sha_version_codi = "9ca6c38280610b3ea5337ca3e5b5085ee1c66b59"
     github_repo_codi = "https://github.com/scicompkl/CoDiPack"
     sha_version_medi = "aafc2d1966ba1233640af737e71c77c1a86183fd"
     github_repo_medi = "https://github.com/SciCompKL/MeDiPack"
