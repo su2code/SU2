4997a989bf5f841b664d1037f85b84901dffd4e6
bigfooted <bigfootedrockmidget@hotmail.com>
2024-02-04 14:46:20 +0100

    symmetry should be called before strong boundaries

--- SU2_CFD/src/integration/CIntegration.cpp
+++ SU2_CFD/src/integration/CIntegration.cpp
@@ -135,9 +135,9 @@ void CIntegration::Space_Integration(CGeometry *geometry,
       case FAR_FIELD:
         solver_container[MainSolver]->BC_Far_Field(geometry, solver_container, conv_bound_numerics, visc_bound_numerics, config, iMarker);
         break;
-      //case SYMMETRY_PLANE:
-      //  solver_container[MainSolver]->BC_Sym_Plane(geometry, solver_container, conv_bound_numerics, visc_bound_numerics, config, iMarker);
-      //  break;
+      case SYMMETRY_PLANE:
+        solver_container[MainSolver]->BC_Sym_Plane(geometry, solver_container, conv_bound_numerics, visc_bound_numerics, config, iMarker);
+        break;
     }
   }
 
@@ -186,15 +186,6 @@ void CIntegration::Space_Integration(CGeometry *geometry,
   if (config->GetnMarker_Periodic() > 0) {
     solver_container[MainSolver]->BC_Periodic(geometry, solver_container, conv_bound_numerics, config);
   }
-
-
-  for (iMarker = 0; iMarker < config->GetnMarker_All(); iMarker++) {
-    KindBC = config->GetMarker_All_KindBC(iMarker);
-      if (KindBC == SYMMETRY_PLANE)
-        solver_container[MainSolver]->BC_Sym_Plane(geometry, solver_container, conv_bound_numerics, visc_bound_numerics, config, iMarker);
-  }
-
-
 
   //AD::ResumePreaccumulation(pausePreacc);
 
