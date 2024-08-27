a0e09d76112012b1a35c9a40e7abfce2f5a13504
Areen Raj <areenraj@gmail.com>
2024-08-27 14:35:40 +0530

    Making it PR Ready

--- build_SU2.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-rm -r build
-
-export PKG_CONFIG_PATH="/home/areen/Programs/OpenBLAS_install/lib:$PKG_CONFIG_PATH"
-export PKG_CONFIG_PATH="/usr/local/cuda/lib64:$PKG_CONFIG_PATH"
-export CUDA_PATH="/usr/local/cuda:$CUDA_PATH"
-./meson.py build --buildtype=debug -Denable-pywrapper=true -Dwith-mpi=disabled -Denable-openblas=true -Denable-cuda=true --prefix=/home/areen/Programs/SU2_Install_GPU
-./ninja -C build install
--- build_SU2_CPU.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-rm -r build
-
-export PKG_CONFIG_PATH="/home/areen/Programs/OpenBLAS_install/lib:$PKG_CONFIG_PATH"
-./meson.py build --buildtype=debug -Denable-pywrapper=true -Dwith-mpi=disabled -Denable-openblas=true  --prefix=/home/areen/Programs/SU2_Install_CPU
-./ninja -C build install
