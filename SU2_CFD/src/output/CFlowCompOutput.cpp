bc56bddd774404541ce5ef077bf651ba4c3c0dae
Francesco Neri <francesconeri95@gmail.com>
2024-09-04 16:37:12 +0200

    corrected turbo output

--- SU2_CFD/src/output/CFlowCompOutput.cpp
+++ SU2_CFD/src/output/CFlowCompOutput.cpp
@@ -675,7 +675,7 @@ void CFlowCompOutput::WriteTurboSpanwisePerformance(std::shared_ptr<CTurboOutput
   SpanWiseValuesOut = geometry->GetSpanWiseValue(OUTFLOW);
 
   /*--- Writing Span wise inflow thermodynamic quantities. ---*/
-  spanwise_performance_filename = "TURBOMACHINERY/inflow_spanwise_thermodynamic_values.dat";
+  spanwise_performance_filename = "TURBOMACHINERY/inflow_spanwise_thermodynamic_values";
   if (nZone > 1) {
     spanwise_performance_filename.append("_" + std::to_string(val_iZone) + ".dat");
   } else {
@@ -713,12 +713,13 @@ void CFlowCompOutput::WriteTurboSpanwisePerformance(std::shared_ptr<CTurboOutput
     file.width(30); file << BladePerf->GetInletState().GetTotalEnthalpy()*config[ZONE_0]->GetEnergy_Ref();
     file.width(30); file << BladePerf->GetInletState().GetDensity()*config[ZONE_0]->GetDensity_Ref();
     file.width(30); file << BladePerf->GetInletState().GetEntropy()*config[ZONE_0]->GetEnergy_Ref()/config[ZONE_0]->GetTemperature_Ref();
+    file << endl;
   }
 
   file.close();
 
   /*--- Writing Span wise outflow thermodynamic quantities. ---*/
-  spanwise_performance_filename = "TURBOMACHINERY/outflow_spanwise_thermodynamic_values.dat";
+  spanwise_performance_filename = "TURBOMACHINERY/outflow_spanwise_thermodynamic_values";
   if (nZone > 1) {
     spanwise_performance_filename.append("_" + std::to_string(val_iZone) + ".dat");
   } else {
@@ -757,6 +758,7 @@ void CFlowCompOutput::WriteTurboSpanwisePerformance(std::shared_ptr<CTurboOutput
     file.width(30); file << BladePerf->GetOutletState().GetTotalEnthalpy()*config[ZONE_0]->GetEnergy_Ref();
     file.width(30); file << BladePerf->GetOutletState().GetDensity()*config[ZONE_0]->GetDensity_Ref();
     file.width(30); file << BladePerf->GetOutletState().GetEntropy()*config[ZONE_0]->GetEnergy_Ref()/config[ZONE_0]->GetTemperature_Ref();
+    file << endl;
   }
 
   file.close();
