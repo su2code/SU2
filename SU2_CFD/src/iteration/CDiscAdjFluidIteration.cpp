a284b6b85aa28409d3ed23bf5fb83ca72c0620f8
Josh Kelly <J.Kelly7@liv.ac.uk>
2025-01-09 14:50:04 +0000

    Resolved tag tape debugging errors

--- SU2_CFD/src/iteration/CDiscAdjFluidIteration.cpp
+++ SU2_CFD/src/iteration/CDiscAdjFluidIteration.cpp
@@ -472,11 +472,7 @@ void CDiscAdjFluidIteration::SetDependencies(CSolver***** solver, CGeometry**** 
   solvers0[FLOW_SOL]->CompleteComms(geometry0, config[iZone], MPI_QUANTITIES::SOLUTION);
 
   if (config[iZone]->GetBoolTurbomachinery()) {
-    solvers0[FLOW_SOL]->PreprocessAverage(solvers0, geometry0, config[iZone], INFLOW);
-    solvers0[FLOW_SOL]->PreprocessAverage(solvers0, geometry0, config[iZone], OUTFLOW);
-    solvers0[FLOW_SOL]->TurboAverageProcess(solvers0, geometry0, config[iZone], INFLOW);
-    solvers0[FLOW_SOL]->TurboAverageProcess(solvers0, geometry0, config[iZone], OUTFLOW);
-    solvers0[FLOW_SOL]->GatherInOutAverageValues(config[iZone], geometry0);
+    solvers0[FLOW_SOL]->InitTurboContainers(geometry0, config[iZone]);
   }
   if (turbulent && !config[iZone]->GetFrozen_Visc_Disc()) {
     solvers0[TURB_SOL]->Postprocessing(geometry0, solvers0,
