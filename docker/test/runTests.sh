83cc22eeea58533d52a563322c0292fa3681708d
bigfooted <bigfootedrockmidget@hotmail.com>
2025-02-01 10:12:51 +0100

    copy tutorials folder using docker

--- docker/test/runTests.sh
+++ docker/test/runTests.sh
@@ -101,8 +101,10 @@ else
     exit 1
   fi
 fi
+echo "Copying Tutorials folder to tests folder."
 cp -R ./Tutorials/ tests/.
-cp -r src/SU2/TestCases/Tutorials tests/.
+echo "Copying TestCases/Tutorials folder to tests/Tutorials folder"
+cp -R src/SU2/TestCases/Tutorials/* tests/Tutorials/.
 
 # Set the environment variables
 export SU2_RUN=$PWD/install/bin
