5cef5ee83047344c25f5f8de300b921435db2cd0
Cristopher-Morales <c.m.ubal@gmail.com>
2025-02-20 09:34:49 +0100

    axisymmetric cleaning

--- SU2_CFD/src/numerics/flow/flow_sources.cpp
+++ SU2_CFD/src/numerics/flow/flow_sources.cpp
@@ -140,10 +140,8 @@ void CSourceAxisymmetric_Flow::ResidualDiffusion(){
   su2double laminar_viscosity_i    = V_i[nDim+5];
   su2double eddy_viscosity_i       = V_i[nDim+6];
   su2double thermal_conductivity_i = V_i[nDim+7];
-  su2double heat_capacity_cp_i     = V_i[nDim+8];
 
   su2double total_viscosity_i = laminar_viscosity_i + eddy_viscosity_i;
-  su2double total_conductivity_i = thermal_conductivity_i + heat_capacity_cp_i*eddy_viscosity_i/Prandtl_Turb;
 
   su2double u = U_i[1]/U_i[0];
   su2double v = U_i[2]/U_i[0];
@@ -156,7 +154,7 @@ void CSourceAxisymmetric_Flow::ResidualDiffusion(){
   residual[3] -= Volume*(yinv*(total_viscosity_i*(u*(PrimVar_Grad_i[2][0]+PrimVar_Grad_i[1][1])
                                                  +v*TWO3*(2*PrimVar_Grad_i[2][1]-PrimVar_Grad_i[1][0]
                                                  -v*yinv+U_i[0]*turb_ke_i))
-                                                 +total_conductivity_i*PrimVar_Grad_i[0][1])
+                                                 +thermal_conductivity_i*PrimVar_Grad_i[0][1])
                                                  -TWO3*(AuxVar_Grad_i[1][1]+AuxVar_Grad_i[2][0]));
 }
 
