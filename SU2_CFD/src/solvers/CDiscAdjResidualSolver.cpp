f232f79ad3c5f3a2546bf0770e2dbf6c40b3c78d
Harsh Patel <51417692+patelha57@users.noreply.github.com>
2025-02-26 20:17:48 -0800

    Update version in CDiscAdjResidualSolver.cpp
    Co-authored-by: Nijso <bigfootedrockmidget@hotmail.com>

--- SU2_CFD/src/solvers/CDiscAdjResidualSolver.cpp
+++ SU2_CFD/src/solvers/CDiscAdjResidualSolver.cpp
@@ -2,14 +2,14 @@
  * \file CDiscAdjResidualSolver.cpp
  * \brief Main subroutines for solving the residual-based discrete adjoint problem.
  * \author H. Patel, A. Gastaldi
- * \version 7.5.1 "Blackbird"
+ * \version 8.1.0 "Harrier"
  *
  * SU2 Project Website: https://su2code.github.io
  *
  * The SU2 Project is maintained by the SU2 Foundation
  * (http://su2foundation.org)
  *
- * Copyright 2012-2023, SU2 Contributors (cf. AUTHORS.md)
+ * Copyright 2012-2024, SU2 Contributors (cf. AUTHORS.md)
  *
  * SU2 is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
