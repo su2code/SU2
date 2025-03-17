86e59b4e55eb92088753960da3d2c1e23332e0d5
Areen Raj <areenraj@gmail.com>
2025-03-17 17:20:27 +0530

    Version Number Change

--- Common/src/linear_algebra/GPU_lin_alg.cu
+++ Common/src/linear_algebra/GPU_lin_alg.cu
@@ -2,7 +2,7 @@
  * \file GPU_lin_alg.cu
  * \brief Implementation of Matrix Vector Product CUDA Kernel
  * \author A. Raj
- * \version 8.0.1 "Harrier"
+ * \version 8.1.0 "Harrier"
  *
  * SU2 Project Website: https://su2code.github.io
  *
