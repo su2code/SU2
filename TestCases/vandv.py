54700d38c9803e3059960b6724c2d66816ddef44
Rutvik Khedkar <140829696+Bot-Enigma-0@users.noreply.github.com>
2025-03-24 12:57:58 +0000

    removing extra spaces, highlighted by codefactor

--- TestCases/vandv.py
+++ TestCases/vandv.py
@@ -86,19 +86,19 @@ def main():
     test_list.append(swbli_sst)
 
     # DSMA661 - SA   
-    dsma661_sa             = TestCase('dsma661_sa')
-    dsma661_sa.cfg_dir     = "vandv/rans/dsma661"
-    dsma661_sa.cfg_file    = "dsma661_sa_config.cfg"
-    dsma661_sa.test_iter   = 5
-    dsma661_sa.test_vals   = [-11.011200, -8.142168, -9.022097, -5.967124, -10.593372, 0.155689, 0.024173]
+    dsma661_sa            = TestCase('dsma661_sa')
+    dsma661_sa.cfg_dir    = "vandv/rans/dsma661"
+    dsma661_sa.cfg_file   = "dsma661_sa_config.cfg"
+    dsma661_sa.test_iter  = 5
+    dsma661_sa.test_vals  = [-11.011200, -8.142168, -9.022097, -5.967124, -10.593372, 0.155689, 0.024173]
     test_list.append(dsma661_sa)
 
     # DSMA661 - SST-V2003m
-    dsma661_sst            = TestCase('dsma661_sst')
-    dsma661_sst.cfg_dir    = "vandv/rans/dsma661"
-    dsma661_sst.cfg_file   = "dsma661_sst_config.cfg"
-    dsma661_sst.test_iter  = 5
-    dsma661_sst.test_vals  = [-11.010698, -8.227583, -9.039694, -5.941268, -11.102554, -7.855195, 0.156247, 0.023882]
+    dsma661_sst           = TestCase('dsma661_sst')
+    dsma661_sst.cfg_dir   = "vandv/rans/dsma661"
+    dsma661_sst.cfg_file  = "dsma661_sst_config.cfg"
+    dsma661_sst.test_iter = 5
+    dsma661_sst.test_vals = [-11.010698, -8.227583, -9.039694, -5.941268, -11.102554, -7.855195, 0.156247, 0.023882]
     test_list.append(dsma661_sst)
 
     ##########################
