83e7e7f05a3fb9359b43e7405285d7194e4e1bc6
rois1995 <rauxa1995@hotmail.it>
2025-03-26 15:15:55 +0100

    - revert changes

--- Common/src/geometry/CPhysicalGeometry.cpp
+++ Common/src/geometry/CPhysicalGeometry.cpp
@@ -10559,10 +10559,9 @@ void CPhysicalGeometry::SetWallDistance(CADTElemClass* WallADT, const CConfig* c
 
         WallADT->DetermineNearestElement(nodes->GetCoord(iPoint), dist, markerID, elemID, rankID);
 
-        // if (nodes->GetWall_Distance(iPoint) == 0) dist = 0;
+        if (nodes->GetWall_Distance(iPoint) == 0) dist = 0;
 
-        // if (dist < nodes->GetWall_Distance(iPoint) || dist == 0) {
-          if (dist < nodes->GetWall_Distance(iPoint)) {
+        if (dist < nodes->GetWall_Distance(iPoint) || dist == 0) {
           nodes->SetWall_Distance(iPoint, dist, rankID, iZone, markerID, elemID);
         }
       }
