82360ca09d12f82b4b255387ff3a241820106d99
Nijso <bigfootedrockmidget@hotmail.com>
2025-07-10 20:23:51 +0200

    Update SU2_CFD/include/drivers/CDriverBase.hpp
    Co-authored-by: Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>

--- SU2_CFD/include/drivers/CDriverBase.hpp
+++ SU2_CFD/include/drivers/CDriverBase.hpp
@@ -776,67 +776,6 @@ class CDriverBase {
    * \param[in] iPoint - Point index.
    * \param[out] solutionvector - Vector values of the solution.
    */
-inline vector<passivedouble> GetSolutionVector(unsigned short iSolver, unsigned long iPoint) {
-  auto* solver = solver_container[selected_zone][INST_0][MESH_0][iSolver];
-  auto* nodes = solver->GetNodes();
-  auto nVar = GetNumberSolverVars(iSolver);
-  vector<passivedouble> solutionvector(nVar, 0.0);
-  for (auto iVar = 0u; iVar < nVar; ++iVar) {
-    solutionvector[iVar] = SU2_TYPE::GetValue(nodes->GetSolution(iPoint,iVar));
-  }
-  return solutionvector;
-}
-
-/* The vector with actual solution values */
-inline void SetSolutionVector(unsigned short iSolver, unsigned long iPoint, vector<passivedouble> solutionVector) {
-  auto* solver = solver_container[selected_zone][INST_0][MESH_0][iSolver];
-  auto* nodes = solver->GetNodes();
-
-  /*--- check the size of the solver variables ---*/
-  unsigned short nVar = GetNumberSolverVars(iSolver);
-  if (nVar != solutionVector.size() )
-    SU2_MPI::Error("Solution Vector size is not equal to Solver size.", CURRENT_FUNCTION);
-  for (unsigned int iVar = 0u; iVar < nVar; ++iVar) {
-    nodes->SetSolution(iPoint,iVar, solutionVector[iVar]);
-    nodes->SetSolution_Old(iPoint,iVar, solutionVector[iVar]);
-  }
-}
-
-
-  /*!
-   * \brief Get the primitive variables vector in a point for a specific solver
-   * \param[in] iSolver - Solver index.
-   * \param[in] iPoint - Point index.
-   * \param[out] solutionvector - Vector values of the primitive variables.
-   */
-inline vector<passivedouble> GetPrimitiveVector(unsigned short iSolver, unsigned long iPoint) {
-  auto* solver = solver_container[selected_zone][INST_0][MESH_0][iSolver];
-  auto* nodes = solver->GetNodes();
-  auto nPrimvar = GetNumberPrimitiveVars(iSolver);
-  vector<passivedouble> solutionvector(nPrimvar, 0.0);
-  for (auto iVar = 0u; iVar < nPrimvar; ++iVar) {
-    solutionvector[iVar] = SU2_TYPE::GetValue(nodes->GetPrimitive(iPoint,iVar));
-  }
-  return solutionvector;
-}
-
-
-  /*!
-   * \brief Get the primitive variables vector in a point for a specific solver
-   * \param[in] iSolver - Solver index.
-   * \param[in] iPoint - Point index.
-   * \param[out] solutionvector - Vector values of the primitive variables.
-   */
-inline void SetPrimitiveVector(unsigned short iSolver, unsigned long iPoint, vector<passivedouble> primitiveVector) {
-  auto* solver = solver_container[selected_zone][INST_0][MESH_0][iSolver];
-  auto* nodes = solver->GetNodes();
-  auto nPrimvar = GetNumberPrimitiveVars(iSolver);
-  vector<passivedouble> solutionvector(nPrimvar, 0.0);
-
-  for (auto iVar = 0u; iVar < nPrimvar; ++iVar) {
-    nodes->SetPrimitive(iPoint,iVar, primitiveVector[iVar]);
-  }
-}
 
 /// \}
 
