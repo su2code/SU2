a1131499ea132041b9c727617d743f37d222cb95
bigfooted <bigfootedrockmidget@hotmail.com>
2025-07-12 12:59:46 +0200

    update buoyancy

--- TestCases/py_wrapper/custom_source_buoyancy/run.py
+++ TestCases/py_wrapper/custom_source_buoyancy/run.py
@@ -26,17 +26,17 @@
 
 import sys
 import pysu2
-from mpi4py import MPI
+import numpy as np
 
+# with mpi:
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+# without mpi:
+#  comm = 0
 
 def main():
-  """
-  custom source to add buoyancy term.
-  """
-  # parallel
-  comm = MPI.COMM_WORLD
-  # serial
-  #comm = 0
+
 
   # Initialize the primal driver of SU2, this includes solver preprocessing.
   try:
@@ -45,8 +45,9 @@ def main():
     print('A TypeError occured in pysu2.CSinglezoneDriver : ', exception)
     raise
 
-  print("\n------------------------------ Begin Solver -----------------------------")
-  sys.stdout.flush()
+  if rank == 0:
+    print("\n------------------------------ Begin Solver -----------------------------")
+    sys.stdout.flush()
 
   # we need to add a source term to the energy equation. For this, we need to get the solver and the variable first.
   # we then loop over all points and for these points, we add the source term
@@ -54,73 +55,54 @@ def main():
 
   # index to the flow solver
   iSOLVER = driver.GetSolverIndices()['INC.FLOW']
-  #print("index of flow solver = ",iSOLVER)
 
   # all the indices and the map to the names of the primitives
   primindex = driver.GetPrimitiveIndices()
-  #print("indices of primitives=",primindex)
-  #print("number of primitives:",len(primindex))
-
-  #print("number of elements:",driver.GetNumberElements())
 
   nVars = driver.GetNumberSolverVars(iSOLVER)
-  #print("number of solver variables:",nVars)
   varindex = primindex.copy()
-  for prim in varindex.copy():
-    if varindex[prim] >=nVars:
-      del varindex[prim]
-  varindex = dict(sorted(varindex.items(), key=lambda item: item[1]))
+  #for prim in varindex.copy():
+  #  if varindex[prim] >=nVars:
+  #    del varindex[prim]
+  #varindex = dict(sorted(varindex.items(), key=lambda item: item[1]))
 
-
-
-  print("solver variable names:",varindex)
   iDENSITY = primindex.get("DENSITY")
-  print("index of density = ",iDENSITY)
+  #index_Vel = varindex.get("VELOCITY_X")
 
-  index_Vel = varindex.get("VELOCITY_X")
-  print("index of velocity = ",index_Vel)
-  custom_source_vector = [0.0 for i in range(nVars)]
-  print("custom source vector = ", custom_source_vector)
-
-  #print("max. number of inner iterations: ",driver.GetNumberInnerIter());
-  #print("max nr of outer iterations: ",driver.GetNumberOuterIter());
-
-  # is in domain: isdomain = driver.GetNodeDomain(iPoint)
-  #for i_vertex in range(n_vertex)
-  #AllSolutions = driver.GetAllSolutions(iSOLVER)
   Body_Force_Vector = [0.0, -9.81, 0.0]
   DensityInc_0 = driver.GetDensity_FreeStreamND()
-  #print("rho freestream = ",DensityInc_0)
   Force_Ref = driver.GetForce_Ref()
-  #print("reference force = ",Force_Ref)
 
-  Iter = driver.GetNumberInnerIter()
-  print("1. inner iterations = ",Iter)
+  # super important to actually push the commands.
+  sys.stdout.flush()
 
-  for inner_iter in range(Iter):
+  # run N iterations
+  for inner_iter in range(11):
+    if (rank==0):
+      print("python iteration ", inner_iter)
+
     # set the source term, per point
-    print(driver.GetNumberNodes() - driver.GetNumberHaloNodes())
     for i_node in range(driver.GetNumberNodes() - driver.GetNumberHaloNodes()):
-      #SolutionVector =  driver.GetSolutionVector(iSOLVER,i_node)
-      PrimitiveVector =  driver.GetPrimitiveVector(iSOLVER,i_node)
-      DensityInc_i = PrimitiveVector[iDENSITY]
+      DensityInc_i = driver.Primitives()(i_node,iDENSITY)
 
       for iDim in range(nDim):
-        custom_source_vector[iDim+1] = -(DensityInc_i - DensityInc_0) * Body_Force_Vector[iDim] / Force_Ref
+        custom_source_vector = (DensityInc_i - DensityInc_0) * Body_Force_Vector[iDim] / Force_Ref
+        driver.UserDefinedSource(iSOLVER).Set(i_node,iDim+1,custom_source_vector)
 
-      #driver.SetPointCustomSource(iSOLVER, i_node, custom_source_vector)
-
-    print("   ***   inner iteration:",inner_iter)
     driver.Preprocess(inner_iter)
-
-    # Run one time iteration.
     driver.Run()
+
     driver.Postprocess()
     driver.Update()
 
     # Monitor the solver and output solution to file if required.
-    #driver.Monitor(inner_iter)
+    #stopcalc = driver.Monitor(inner_iter)
     driver.Output(inner_iter)
+
+    #if (stopcalc):
+    #    if (rank==0):
+    #        "Max iterations or convergence criteria reached, stopping."
+    #    break;
 
   # Finalize the solver and exit cleanly.
   driver.Finalize()
