12dd8627485641381db3037ca703a51a64de9f5a
Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>
2025-10-25 21:43:50 -0700

    Update SU2_CFD/src/output/CFlowOutput.cpp

--- SU2_CFD/src/output/CFlowOutput.cpp
+++ SU2_CFD/src/output/CFlowOutput.cpp
@@ -1721,7 +1721,7 @@ void CFlowOutput::AddAerodynamicCoefficients(const CConfig* config) {
   AddHistoryOutput("AOA", "AoA", ScreenOutputFormat::FIXED, "AOA", "Angle of attack");
   
   // CUSTOM_OBJFUNC is added here so historyMap.py knows how to get its value, the actual output is COMBO.
-  AddHistoryOutput("CUSTOM_OBJFUNC", "ComboObj", ScreenOutputFormat::SCIENTIFIC, "COMBO", "Custom obj. function value.", HistoryFieldType::COEFFICIENT);
+  if (false) AddHistoryOutput("CUSTOM_OBJFUNC", "ComboObj", ScreenOutputFormat::SCIENTIFIC, "COMBO", "Custom obj. function value.", HistoryFieldType::COEFFICIENT);
   AddHistoryOutput("COMBO", "ComboObj", ScreenOutputFormat::SCIENTIFIC, "COMBO", "Combined obj. function value.", HistoryFieldType::COEFFICIENT);
 }
 
