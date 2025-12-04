684a5a08b56150b62989b8b52466799da4849fbc
Cristopher Morales <98025159+Cristopher-Morales@users.noreply.github.com>
2025-12-04 13:26:30 +0100

    Update SU2_CFD/include/fluid/CConstantDensity.hpp
    Co-authored-by: Nijso <bigfootedrockmidget@hotmail.com>

--- SU2_CFD/include/fluid/CConstantDensity.hpp
+++ SU2_CFD/include/fluid/CConstantDensity.hpp
@@ -62,6 +62,8 @@ class CConstantDensity final : public CFluidModel {
   /*!
    * \brief Set the Dimensionless State using Enthalpy.
    * \param[in] val_enthalpy - Enthalpy value at the point.
+   * \param[in] val_scalars - not used here. 
+   
    */
   void SetTDState_h(su2double val_enthalpy, const su2double* val_scalars = nullptr) override {
     Enthalpy = val_enthalpy;
