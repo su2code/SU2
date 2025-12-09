d6606df9185994dc945926e999d7cacd1bc5d24f
Cristopher-Morales <c.m.ubal@gmail.com>
2025-12-09 10:16:43 +0100

    fix compile issue

--- SU2_CFD/include/variables/CIncEulerVariable.hpp
+++ SU2_CFD/include/variables/CIncEulerVariable.hpp
@@ -73,6 +73,7 @@ public:
              Streamwise_Periodic_RecoveredTemperature; /*!< \brief Recovered/Physical temperature [K] for streamwise periodic flow. */
   su2double TemperatureLimits[2];                      /*!< \brief Temperature limits [K]. */
   su2double TemperatureInc = 0.0;                      /*!< \brief Temperature [K] imposed when energy equation is switch off. */
+  bool Energy;                                         /*!< \brief Flag for Energy equation in incompressible flows. */
  public:
   /*!
    * \brief Constructor of the class.
