3e8292c2de06dbd59cc1cf307afc3d2fc43ad495
Shubham shukla <shubhushukla586@gmail.com>
2025-12-27 23:54:07 +0530

    Update SU2_CFD/src/solvers/CTurbSSTSolver.cpp
    Co-authored-by: Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>

--- SU2_CFD/src/solvers/CTurbSSTSolver.cpp
+++ SU2_CFD/src/solvers/CTurbSSTSolver.cpp
@@ -272,7 +272,7 @@ void CTurbSSTSolver::Postprocessing(CGeometry *geometry, CSolver **solver_contai
         }
         shearStress = sqrt(shearStress);
 
-        const su2double FrictionVelocity = max(sqrt(shearStress/flowNodes->GetDensity(iPoint)), 1e-10);
+        const su2double FrictionVelocity = max(sqrt(shearStress/flowNodes->GetDensity(iPoint)), EPS);
         const su2double wall_dist = geometry->vertex[iMarker][iVertex]->GetNearestNeighborDistance();
 
         const su2double Derivative = flowNodes->GetLaminarViscosity(jPoint) * pow(nodes->GetSolution(jPoint, 0), 0.673) / max(wall_dist, EPS);
