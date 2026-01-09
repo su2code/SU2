51611e113b07ff84fbeb44dcab20827c3f1f4f40
bigfooted <bigfootedrockmidget@hotmail.com>
2026-01-09 16:27:22 +0100

    update final regression tests

--- TestCases/parallel_regression.py
+++ TestCases/parallel_regression.py
@@ -1521,7 +1521,7 @@ def main():
     pywrapper_buoyancy.cfg_file = "lam_buoyancy_cavity.cfg"
     pywrapper_buoyancy.test_iter = 0
     pywrapper_buoyancy.test_vals = [-13.314107, -13.764877, -13.136439, -5.379183]
-    pywrapper_buoyancy.test_vals_aarch64 = [-17.746018, -17.460693, -17.430708, -12.260624]
+    pywrapper_buoyancy.test_vals_aarch64 = [-13.227985, -13.678478, -13.050758, -7.777151]
     pywrapper_buoyancy.command = TestCase.Command("mpirun -np 2", "python", "run.py")
     test_list.append(pywrapper_buoyancy)
 
