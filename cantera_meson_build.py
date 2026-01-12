57e1b429d06a52e2cc93cbd9c1c9cb45cb982321
Cristopher-Morales <c.m.ubal@gmail.com>
2026-01-12 21:37:04 +0100

    improve explanation

--- cantera_meson_build.py
+++ cantera_meson_build.py
@@ -4,14 +4,14 @@
 #  \brief create necessary meson.build file for linking cantera to
 #         SU2.
 #  \author C.Morales Ubal
-#  \version 8.2.0 "Harrier"
+#  \version 8.4.0 "Harrier"
 #
 # SU2 Project Website: https://su2code.github.io
 #
 # The SU2 Project is maintained by the SU2 Foundation
 # (http://su2foundation.org)
 #
-# Copyright 2012-2025, SU2 Contributors (cf. AUTHORS.md)
+# Copyright 2012-2026, SU2 Contributors (cf. AUTHORS.md)
 #
 # SU2 is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -31,10 +31,10 @@ absolute_path = sys.path[0]
 relative_path = "subprojects/cantera"
 meson_path = os.path.join(absolute_path, relative_path)
 
-# Where to create meson.build
+# Path to the meson.build under SU2/subprojects/cantera/
 meson_build_path = os.path.join(meson_path, "meson.build")
 
-# Detect filename if already exists
+# Create meson.build in the cantera subproject if it does not already exist
 if not os.path.exists(meson_build_path):
    print(f"Writing meson.build in {meson_path}")
    meson_build_content = ["project('cantera', 'c', 'cpp', default_options: ['cpp_std=c++17'])\n",
