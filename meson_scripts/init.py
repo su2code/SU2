c84d744743d6881e619d96863706617b458de539
EvertBunschoten <evertbun@xs4all.nl>
2026-03-06 11:18:28 +0100

    SHA tag update for MLPCpp

--- meson_scripts/init.py
+++ meson_scripts/init.py
@@ -74,7 +74,7 @@ def init_submodules(
     github_repo_mel = "https://github.com/pcarruscag/MEL"
     sha_version_fado = "ce7ee018e4e699af5028d69baa1939fea290e18a"
     github_repo_fado = "https://github.com/pcarruscag/FADO"
-    sha_version_mlpcpp = "ff57e0cf9e60127196d3f1be71e711d47ff646ef"
+    sha_version_mlpcpp = "dc748c5a324db4d1616227f4393d9f5be09da4bb"
     github_repo_mlpcpp = "https://github.com/EvertBunschoten/MLPCpp"
     sha_version_eigen = "d71c30c47858effcbd39967097a2d99ee48db464"
     github_repo_eigen = "https://gitlab.com/libeigen/eigen.git"
