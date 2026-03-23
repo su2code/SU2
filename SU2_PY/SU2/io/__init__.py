fea7495adc545d82513ee01286aad51136abad84
riddhi1228 <24ume036@lnmiit.ac.in>
2026-03-23 02:19:24 +0000

    fix: add comment to ImportError except clause in __init__.py

--- SU2_PY/SU2/io/__init__.py
+++ SU2_PY/SU2/io/__init__.py
@@ -9,7 +9,8 @@ from .filelock import filelock
 from .config import Config
 from .state import State_Factory as State
 from .historyMap import history_header_map as historyOutFields
+
 try:
     from .fwh import FWHData
-except ImportError:
+except ImportError:  # numpy not available in all environments
     pass
