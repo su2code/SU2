7015adcaafca35a94ccf750b694ba14b6e165f23
EvertBunschoten <evertbun@xs4all.nl>
2026-03-25 09:52:26 +0100

    const correction

--- SU2_CFD/src/solvers/CSpeciesFlameletSolver.cpp
+++ SU2_CFD/src/solvers/CSpeciesFlameletSolver.cpp
@@ -851,7 +851,7 @@ su2double CSpeciesFlameletSolver::ThickenedFlameCorrection(const CGeometry* geom
 }
 
 su2double CSpeciesFlameletSolver::GetOverallFlameThickness(CGeometry* geometry, CSolver** solver_container) const {
-  CFlowVariable const * flowNodes = su2staticcast_p<CFlowVariable*>(solver_container[FLOW_SOL]->GetNodes());
+  const CFlowVariable* flowNodes = su2staticcast_p<CFlowVariable*>(solver_container[FLOW_SOL]->GetNodes());
   su2double pvmax_local{-1e3}, pvmin_local{1e3}, pvmax_global{0.0},pvmin_global{0.0}, gradpv_local{0.0}, gradpv_global{0.0}, Tmax_local{-1e6},Tmax_global{0.0};
   
   SU2_OMP_FOR_STAT(omp_chunk_size)
