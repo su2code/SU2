e9c968edbca741db10c81a4ea9cdfc52429dbe84
bigfooted <bigfootedrockmidget@hotmail.com>
2026-04-02 23:58:02 +0200

    update some regressions

--- TestCases/serial_regression.py
+++ TestCases/serial_regression.py
@@ -1092,7 +1092,7 @@ def main():
     airfoilRBF.cfg_file  = "config.cfg"
     airfoilRBF.test_iter = 1
 
-    airfoilRBF.test_vals = [1.000000, 0.185189, -3.373715
+    airfoilRBF.test_vals = [1.000000, 0.185189, -3.373715]
     airfoilRBF.tol       = 0.0001
     airfoilRBF.multizone = True
     test_list.append(airfoilRBF)
