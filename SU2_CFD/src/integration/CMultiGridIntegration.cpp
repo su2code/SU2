732d0168300e86e8cc8682432a0598ece9ee7b11
Nijso <bigfootedrockmidget@hotmail.com>
2026-04-10 19:10:03 +0200

    Update SU2_CFD/src/integration/CMultiGridIntegration.cpp
    Co-authored-by: Pedro Gomes <38071223+pcarruscag@users.noreply.github.com>

--- SU2_CFD/src/integration/CMultiGridIntegration.cpp
+++ SU2_CFD/src/integration/CMultiGridIntegration.cpp
@@ -568,9 +568,7 @@ void CMultiGridIntegration::PreSmoothing(unsigned short RunTime_EqSystem,
   const bool early_exit = mgOpts.MG_Smooth_EarlyExit && (nPreSmooth > 1);
 
   /*--- Reset the shared early-exit flag (master only). ---*/
-  BEGIN_SU2_OMP_SAFE_GLOBAL_ACCESS
-  { mg_early_exit_flag = false; }
-  END_SU2_OMP_SAFE_GLOBAL_ACCESS
+  SU2_OMP_SAFE_GLOBAL_ACCESS(mg_early_exit_flag = false;)
   for (unsigned short iPreSmooth = 0; iPreSmooth < nPreSmooth; iPreSmooth++) {
 
     /*--- Time and space integration ---*/
