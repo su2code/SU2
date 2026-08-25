462a6e221f0a3f39fb2665a17c07b611d21d9435
Ole Burghardt <ole.burghardt@scicomp.uni-kl.de>
2026-08-25 17:04:17 +0200

    Repeat check whether LoadHistoryData covers the combo objective function calculation entirely.

--- SU2_CFD/src/output/CFlowOutput.cpp
+++ SU2_CFD/src/output/CFlowOutput.cpp
@@ -1405,14 +1405,6 @@ void CFlowOutput::SetVolumeOutputFieldsScalarSolution(const CConfig* config){
   }
 }
 
-void CFlowOutput::LoadCustomAndComboObjectiveFunctions(CConfig *config, CGeometry *geometry, CSolver **solver) {
-
-  /*--- Set the values that will be loaded by GetTotal_ComboObj in the multizone discrete adjoint solver. --- */
-  SetAnalyzeSurface(solver, geometry, config, false);
-  SetCustomOutputs(solver, geometry, config);
-  SetCustomAndComboObjectives(FLOW_SOL, config, solver);
-}
-
 void CFlowOutput::SetVolumeOutputFieldsScalarResidual(const CConfig* config) {
   /*--- Only place outputs of the "RESIDUAL" group here. ---*/
 
