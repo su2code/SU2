5f1ac9621721de015621341621cf497ea69d91c7
ManasBagul23 <bagulmanas23@gmail.com>
2026-09-17 12:39:52 +0530

    Only skip Barth-Jespersen limiting for a zero projection
    A projection smaller than machine epsilon is still a projection that
    may need limiting, only proj == 0 has nothing to limit.

--- SU2_CFD/include/limiters/CLimiterDetails.hpp
+++ SU2_CFD/include/limiters/CLimiterDetails.hpp
@@ -144,13 +144,11 @@ struct LimiterHelpers
 template<>
 struct CLimiterDetails<LIMITER::BARTH_JESPERSEN>
 {
-  su2double eps;
-
   /*!
-   * \brief Set a small epsilon to avoid divisions by 0.
+   * \brief Nothing to precompute for this kind of limiter.
    */
   template<class... Ts>
-  inline void preprocess(Ts&...) {eps = LimiterHelpers<>::epsilon();}
+  inline void preprocess(Ts&...) {}
 
   /*!
    * \brief No geometric modification for this kind of limiter.
@@ -164,7 +162,7 @@ struct CLimiterDetails<LIMITER::BARTH_JESPERSEN>
    */
   inline su2double limiterFunction(size_t, su2double proj, su2double delta) const
   {
-    if (fabs(proj) <= eps) return 1.0;
+    if (proj == 0.0) return 1.0;
     return min(delta / proj, 1.0);
   }
 };
